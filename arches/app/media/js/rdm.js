18e4b76044c9e74be83f84273762d4b5d9d9441e
Rob Gaston <rgaston@fargeo.com>
2014-10-10 11:13:01 -0700

    starting to integrate latest UI updates into RDM

--- arches/app/media/js/rdm.js
+++ arches/app/media/js/rdm.js
@@ -7,6 +7,46 @@ require([
     'views/rdm/concept-report',
     'jquery-validate',
 ], function($, Backbone, arches, ConceptModel, ConceptTree, ConceptReport) {
+    jQuery(document).ready(function() {
+        var header_offset = $("#appheader").offset(),
+            sidebar = $("#sidebar"),
+            crud = $("#crud"),
+            sidebar_bottom = sidebar.offset().top + sidebar.height(),
+            menu_width = $("#sidebar").outerWidth();
+
+
+        $(window).scroll(function() {
+            var sidebar_height = sidebar.height(),
+                crud_height = crud.height(),
+                footer_top = $(".footer").offset().top - $(window).scrollTop();
+
+            if (window.scrollY >= header_offset.top) {
+                $(".sticky").addClass("fixed");
+                
+                if (crud_height > sidebar_height) {
+                    $("#sidebar").addClass("fixed-menu");
+                    $("#sidebar").css("width", menu_width);
+                    
+                    if (footer_top < sidebar_bottom) {
+                        $("#sidebar").css("margin-top", footer_top - sidebar_bottom);
+                    } else {
+                        $("#sidebar").css("margin-top", "10px");
+                    }
+                }
+
+            } else {
+                $(".sticky").removeClass("fixed");
+                $("#sidebar").removeClass("fixed-menu");
+                $("#sidebar").css("margin-top", "0px");
+                $("#crud").removeClass("fixed-spacer");
+                $("#crud").css("margin-top", "0px");
+            }
+
+        });
+    });
+
+
+
     $(document).ready(function() {
         var concept = new ConceptModel({
                 id: $('#selected-conceptid').val()
@@ -24,18 +64,18 @@ require([
             window.history.pushState({}, "conceptid", concept.get('id'));
         });
 
-        conceptTree.on('conceptMoved', function (){
+        conceptTree.on('conceptMoved', function() {
             conceptReport.render();
         });
 
         conceptReport.on({
-            'valueSaved': function () {
+            'valueSaved': function() {
                 conceptTree.render();
             },
-            'conceptDeleted': function () {
+            'conceptDeleted': function() {
                 conceptTree.render();
             },
-            'conceptAdded': function () {
+            'conceptAdded': function() {
                 conceptTree.render();
             }
         });
