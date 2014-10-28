5f60585a2cf31ff99ccf08aac0096fc6d444e31c
Rob Gaston <rgaston@fargeo.com>
2014-10-28 10:15:22 -0700

    refactoring context_processor

--- arches/app/utils/context_processors.py
+++ arches/app/utils/context_processors.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.template import RequestContext
 
 def livereload(request):
-    "A context processor that provides the livereload port from settings."
-    return {
-        'livereload_port': settings.LIVERELOAD_PORT
-    }
+	return {
+		'livereload_port': settings.LIVERELOAD_PORT
+	}
