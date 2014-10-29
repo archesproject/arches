f1637128bb03b24105e9b943dcff3d200572e254
Rob Gaston <rgaston@fargeo.com>
2014-10-29 14:01:56 -0700

    detecting when dropzone is not already attached to add image form for initial setup

--- arches/app/media/js/views/rdm/modals/add-image-form.js
+++ arches/app/media/js/views/rdm/modals/add-image-form.js
@@ -1,12 +1,16 @@
-define(['jquery', 'backbone', 'bootstrap', 'dropzone'], function ($, Backbone) {
+define(['jquery', 'backbone', 'dropzone', 'bootstrap'], function ($, Backbone, dropzone) {
     return Backbone.View.extend({
         initialize: function(options) {
-            this.$el.find('.dropzone').dropzone({
-                //url: "/file/post",
-                addRemoveLinks : true,
-                maxFilesize: 0.5,
-                dictResponseError: 'Error uploading file!'
-            });
+            var dropzoneEl = this.$el.find('.dropzone');
+            // detect if dropzone is attached, and if not init
+            if (!dropzoneEl.hasClass('dz-clickable')) {
+                dropzoneEl.dropzone({
+                    addRemoveLinks : true,
+                    maxFilesize: 0.5,
+                    dictResponseError: 'Error uploading file!'
+                });
+            }
+            
             this.$el.find('.modal').modal('show');
         }
     });
