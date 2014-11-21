19a38170107b390c591a944d4f342e932b94e978
Alexei Peters <apeters@fargeo.com>
2014-11-21 12:06:46 -0800

    improving the way forms manage their data

--- arches/app/media/js/views/forms/base.js
+++ arches/app/media/js/views/forms/base.js
@@ -10,11 +10,24 @@ define(['jquery', 'backbone'], function ($, Backbone) {
         initialize: function() {
             this.form = this.$el;
             this.formdata = JSON.parse(this.form.find('#formdata').val());
+            this._rawdata = JSON.parse(this.form.find('#formdata').val());
+        },
+
+        getData: function(){
+            return this.formdata;
+        },
+
+        validate: function(){
+            return true;
         },
 
         submit: function(){
-            this.form.submit();
+            if (this.validate()){
+                //this.formdata = this.getData();
+                this.form.find('#formdata').val(this.getData());
+                this.form.submit(); 
+            }
         }
-        
+
     });
 });
