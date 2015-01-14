e166bed463216632e108df5f4b49992c40a3125e
Rob Gaston <rgaston@fargeo.com>
2015-01-14 11:28:19 -0800

    cleaning up indentation

--- arches/app/media/js/map/resource-layers.js
+++ arches/app/media/js/map/resource-layers.js
@@ -1,23 +1,23 @@
 define([
-    'openlayers',
-    'underscore',
-    'knockout',
-    'arches',
-    'layer-info',
-    'map/resource-layer-model'
+        'openlayers',
+        'underscore',
+        'knockout',
+        'arches',
+        'layer-info',
+        'map/resource-layer-model'
 ], function(ol, _, ko, arches, layerInfo, ResourceLayerModel) {
-    var resourceFeatures = ko.observableArray();
-    var layers = [];
+        var resourceFeatures = ko.observableArray();
+        var layers = [];
 
-    _.each(layerInfo, function (item, entitytypeid) {
-      item.entitytypeid = entitytypeid;
-      layers.push(new ResourceLayerModel(item, function(features) {
-        resourceFeatures(resourceFeatures().concat(features));
-      }));
-    });
+        _.each(layerInfo, function (item, entitytypeid) {
+            item.entitytypeid = entitytypeid;
+            layers.push(new ResourceLayerModel(item, function(features) {
+                resourceFeatures(resourceFeatures().concat(features));
+            }));
+        });
 
-    return {
-      layers: layers,
-      features: resourceFeatures
-    };
+        return {
+            layers: layers,
+            features: resourceFeatures
+        };
 });
