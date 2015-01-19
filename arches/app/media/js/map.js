eac8853380d0670bb33e0e5032946806f0afae51
Rob Gaston <rgaston@fargeo.com>
2015-01-19 11:31:41 -0800

    a little style cleanup for map and search

--- arches/app/media/js/map.js
+++ arches/app/media/js/map.js
@@ -338,13 +338,6 @@ require([
                 maximumSelectionSize: 1
             });
 
-            //Override default style of Select2 geocode mask
-            $(".select2-search-field").css("font-size", "15px");
-            $(".select2-search-field").css("font-weight", "200");
-            $(".select2-search-choice").css("margin-top", "8px");
-            $(".select2-container-multi .select2-choices").css("border-radius", "3px");
-
-
             $('.geocodewidget').on("select2-selecting", function(e) {
                 var geom = geoJSON.readGeometry(e.object.geometry)
                 geom.transform(ol.proj.get('EPSG:4326'), ol.proj.get('EPSG:3857'));
