128be8d3a29abff9438ce5742a8d2e1a69946863
Rob Gaston <rgaston@fargeo.com>
2015-03-16 12:36:02 -0700

    loading django settings in docs conf.py...

--- arches/docs/conf.py
+++ arches/docs/conf.py
@@ -20,6 +20,7 @@ import shlex
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "arches.settings")
 
 # -- General configuration ------------------------------------------------
 
