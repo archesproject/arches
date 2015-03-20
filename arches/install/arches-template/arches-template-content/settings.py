2d03a8f8801aa8656c46f98c38fac88b4d05fa5d
chiatt <chiatt@fargeo.com>
2015-03-20 16:12:44 -0700

    Added conditional settings based on whether the application's base is arches or an arches application

--- arches/install/arches-template/arches-template-content/settings.py
+++ arches/install/arches-template/arches-template-content/settings.py
@@ -10,8 +10,13 @@ ROOT_URLCONF = '%s.urls' % (PACKAGE_NAME)
 INSTALLED_APPS = INSTALLED_APPS + (PACKAGE_NAME,)
 STATICFILES_DIRS = (os.path.join(PACKAGE_ROOT, 'media'),) + STATICFILES_DIRS
 TEMPLATE_DIRS = (os.path.join(PACKAGE_ROOT, 'templates'),os.path.join(PACKAGE_ROOT, 'templatetags')) + TEMPLATE_DIRS
+
+{% if source_app == 'arches' %}
 RESOURCE_MODEL = {'default': '{{ app_name }}.models.resource.Resource'}
-    
+{% else %}
+#RESOURCE_MODEL = {'default': '{{ app_name }}.models.resource.Resource'}
+{% endif %}
+
 # DEFAULT_MAP_X = -13179347.3099
 # DEFAULT_MAP_Y = 4031285.8349
 # DEFAULT_MAP_ZOOM = 10
@@ -19,9 +24,94 @@ RESOURCE_MODEL = {'default': '{{ app_name }}.models.resource.Resource'}
 # MAP_MAX_ZOOM = 19
 # MAP_EXTENT = '-13228037.69691764,3981296.0184014924,-13123624.71628009,4080358.407059081'
 
+{% if source_app == 'arches' %}
 def RESOURCE_TYPE_CONFIGS():
-    return {}
+    return {
+        # 'HERITAGE_RESOURCE.E18': {
+        #     'resourcetypeid': 'HERITAGE_RESOURCE.E18',
+        #     'name': _('Historic Resource'),
+        #     'icon_class': 'fa fa-university',
+        #     'default_page': 'summary',
+        #     'default_description': 'No description available',
+        #     'description_node': _('DESCRIPTION.E62'),
+        #     'categories': [_('Resource')],
+        #     'has_layer': True,
+        #     'on_map': False,
+        #     'marker_color': '#fa6003',
+        #     'stroke_color': '#fb8c49',
+        #     'fill_color': '#ffc29e',
+        #     'primary_name_lookup': {
+        #         'entity_type': 'NAME.E41',
+        #         'lookup_value': 'Primary'
+        #     },
+        #     'sort_order': 1
+        # },
+        # 'HERITAGE_RESOURCE_GROUP.E27': {
+        #     'resourcetypeid': 'HERITAGE_RESOURCE_GROUP.E27',
+        #     'name': _('Historic District'),
+        #     'icon_class': 'fa fa-th',
+        #     'default_page': 'summary',
+        #     'default_description': 'No description available',
+        #     'description_node': _('REASONS.E62'),
+        #     'categories': [_('Resource')],
+        #     'has_layer': True,
+        #     'on_map': False,
+        #     'marker_color': '#FFC53D',
+        #     'stroke_color': '#d9b562',
+        #     'fill_color': '#eedbad',
+        #     'primary_name_lookup': {
+        #         'entity_type': 'NAME.E41',
+        #         'lookup_value': 'Primary'
+        #     },
+        #     'sort_order': 2
+        # }
+    }
 
+{% else %}
+# def RESOURCE_TYPE_CONFIGS():
+#     return { 
+#         'HERITAGE_RESOURCE.E18': {
+#             'resourcetypeid': 'HERITAGE_RESOURCE.E18',
+#             'name': _('Historic Resource'),
+#             'icon_class': 'fa fa-university',
+#             'default_page': 'summary',
+#             'default_description': 'No description available',
+#             'description_node': _('DESCRIPTION.E62'),
+#             'categories': [_('Resource')],
+#             'has_layer': True,
+#             'on_map': False,
+#             'marker_color': '#fa6003',
+#             'stroke_color': '#fb8c49',
+#             'fill_color': '#ffc29e',
+#             'primary_name_lookup': {
+#                 'entity_type': 'NAME.E41',
+#                 'lookup_value': 'Primary'
+#             },
+#             'sort_order': 1
+#         },
+#         'HERITAGE_RESOURCE_GROUP.E27': {
+#             'resourcetypeid': 'HERITAGE_RESOURCE_GROUP.E27',
+#             'name': _('Historic District'),
+#             'icon_class': 'fa fa-th',
+#             'default_page': 'summary',
+#             'default_description': 'No description available',
+#             'description_node': _('REASONS.E62'),
+#             'categories': [_('Resource')],
+#             'has_layer': True,
+#             'on_map': False,
+#             'marker_color': '#FFC53D',
+#             'stroke_color': '#d9b562',
+#             'fill_color': '#eedbad',
+#             'primary_name_lookup': {
+#                 'entity_type': 'NAME.E41',
+#                 'lookup_value': 'Primary'
+#             },
+#             'sort_order': 2
+#         }
+#     }
+{% endif %}
+
+#GEOCODING_PROVIDER = ''
 
 RESOURCE_GRAPH_LOCATIONS = (
     # Put strings here, like "/home/data/resource_graphs" or "C:/data/resource_graphs".
@@ -36,7 +126,7 @@ CONCEPT_SCHEME_LOCATIONS = (
     # Don't forget to use absolute paths, not relative paths.
     
     #'absolute/path/to/authority_files',
-    os.path.normpath(os.path.join(PACKAGE_ROOT, 'source_data', 'concepts', 'authority_files')),
+    # os.path.normpath(os.path.join(PACKAGE_ROOT, 'source_data', 'concepts', 'authority_files')),
 )
 
 BUSISNESS_DATA_FILES = (
@@ -72,7 +162,7 @@ LOGGING = {
     },
 }
 
-EXPORT_CONFIG = os.path.normpath(os.path.join(PACKAGE_ROOT, 'source_data', 'business_data', 'resource_export_mappings.json'))
+#EXPORT_CONFIG = os.path.normpath(os.path.join(PACKAGE_ROOT, 'source_data', 'business_data', 'resource_export_mappings.json'))
 
 try:
     from settings_local import *
