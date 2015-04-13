2d9674f73884f2a0fa725051bcb1e0ffc739aa7f
Rob Gaston <rgaston@fargeo.com>
2015-04-13 13:17:15 -0700

    removing staticfiles url change

--- arches/urls.py
+++ arches/urls.py
@@ -64,6 +64,3 @@ urlpatterns = patterns('',
 
 urlpatterns += staticfiles_urlpatterns()
 
-from django.conf import settings
-from django.conf.urls.static import static
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
