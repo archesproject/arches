ed505dc5038d8c0580f1ae828ba5464541ea7f86
Alexei Peters <apeters@fargeo.com>
2016-01-14 14:05:25 -0800

    update django override admin.py to 1.8.8, update settings.py file to 1.8.8 compatibility, add migrations folder to models

--- arches/settings.py
+++ arches/settings.py
@@ -253,10 +253,12 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'arches.app.utils.set_anonymous_user.SetAnonymousUser',
 )
+
 
 ROOT_URLCONF = 'arches.urls'
 
