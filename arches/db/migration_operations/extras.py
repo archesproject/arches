a68b55ead3e511611b9ca04c0def95721926770f
Alexei Peters <apeters@fargeo.com>
2016-02-04 15:28:40 -0800

    add migration operation for creating an extension in postgres

---
+++ arches/db/migration_operations/extras.py
@@ -0,0 +1,20 @@
+from django.db.migrations.operations.base import Operation
+
+class CreateExtension(Operation):
+
+    def __init__(self, name):
+        self.name = name
+
+    def state_forwards(self, app_label, state):
+        pass
+
+    def database_forwards(self, app_label, schema_editor, from_state, to_state):
+        schema_editor.execute("CREATE EXTENSION IF NOT EXISTS %s" % self.name)
+
+    def database_backwards(self, app_label, schema_editor, from_state, to_state):
+        schema_editor.execute("DROP EXTENSION IF EXISTS %s" % self.name)
+
+    def describe(self):
+        return "Creates extension %s" % self.name
+
+        
