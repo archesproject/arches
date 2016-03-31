2bce7828d4e48dcb9683b23520e8c449e95c184f
Ryan Anderson <randerson@fargeo.com>
2016-03-31 11:08:59 -0700

    Add files for aws codedeploy and test.

---
+++ arches/install/install_arches.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+rm -r -y ~/arches
+rm -r -y ~/ENV
+
+sudo apt-get install -y git
+sudo apt-get install -y python-pip
+sudo -H pip install virtualenv==13.1.2
+
+cd ~
+git clone -b travis_ci_tests https://github.com/archesproject/arches.git
+
+virtualenv ~/ENV
+source ~/ENV/bin/activate
+
+cd arches
+python setup.py install
+
+python manage.py packages -o setup
+
+python manage.py runserver
