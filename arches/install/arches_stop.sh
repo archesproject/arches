706feae7a7f19468f44ea56767595324fe41d97b
Ryan Anderson <randerson@fargeo.com>
2016-04-04 10:23:49 -0700

    Edits to start and stop scripts.

--- arches/install/arches_stop.sh
+++ arches/install/arches_stop.sh
@@ -2,8 +2,9 @@
 
 
 
+sudo -u postgres dropdb training
+
 sudo -u postgres psql -d postgres -c "UPDATE pg_database SET datistemplate='false' WHERE datname='template_postgis_20'"
 sudo -u postgres dropdb template_postgis_20
 # sudo -u postgres psql -d postgres -c "UPDATE pg_database SET datistemplate='false' WHERE datname='template0'"
 # sudo -u postgres dropdb template0
-sudo -u postgres dropdb training
