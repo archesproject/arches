325cad711da89fa3c73c4db9ba58c7c01d05edcc
Rob Gaston <rgaston@fargeo.com>
2016-04-04 14:05:19 -0700

    adding more graph manager tests

--- tests/models/resource_graph_tests.py
+++ tests/models/resource_graph_tests.py
@@ -21,7 +21,6 @@ from tests import test_settings
 from tests.base_test import ArchesTestCase
 from django.test import Client
 from django.core.urlresolvers import reverse
-from arches.app.models import models
 from arches.management.commands.package_utils import resource_graphs
 from arches.app.models.models import Node
 
@@ -29,6 +28,8 @@ def setUpModule():
     resource_graphs.load_graphs(os.path.join(test_settings.RESOURCE_GRAPH_LOCATIONS))
 
 NODE_COUNT = 112
+PLACE_NODE_COUNT = 17
+client = Client()
 
 class ResourceGraphTests(ArchesTestCase):
 
@@ -48,10 +49,49 @@ class ResourceGraphTests(ArchesTestCase):
         Test the graph manager view
 
         """
-        c = Client()
-        response = c.get(reverse('graph', kwargs={'nodeid':'d8f4db21-343e-4af3-8857-f7322dc9eb4b'}))
+        url = reverse('graph', kwargs={'nodeid':'d8f4db21-343e-4af3-8857-f7322dc9eb4b'})
+        response = client.get(url)
         graph = json.loads(response.context['graph'])
         node_count = len(graph['nodes'])
         self.assertEqual(node_count, NODE_COUNT)
         edge_count = len(graph['edges'])
         self.assertEqual(edge_count, NODE_COUNT-1)
+
+    def test_node_update(self):
+        """
+        Test updating a node (HERITAGE_RESOURCE_PLACE) via node view
+
+        """
+        url = reverse('node', kwargs={'nodeid':'9b35fd39-6668-4b44-80fb-d50d0e5211a2'})
+        post_data = {
+            "description": "",
+            "istopnode":False,
+            "ontologyclass": "",
+            "nodeid": "9b35fd39-6668-4b44-80fb-d50d0e5211a2",
+            "branchmetadata_id":None,
+            "datatype": "",
+            "nodegroup_id": "9b35fd39-6668-4b44-80fb-d50d0e5211a2",
+            "name": "new node name"
+        }
+        content_type = 'application/x-www-form-urlencoded'
+        response = client.post(url, json.dumps(post_data), content_type)
+        response_json = json.loads(response.content)
+        self.assertEqual(len(response_json['group_nodes']), PLACE_NODE_COUNT-1)
+        self.assertEqual(response_json['node']['name'], 'new node name')
+        node = Node.objects.get(nodeid="9b35fd39-6668-4b44-80fb-d50d0e5211a2")
+        self.assertEqual(node.name, 'new node name')
+        self.assertTrue(node.is_collector())
+
+    def test_node_delete(self):
+        """
+        Test delete a node (HERITAGE_RESOURCE_PLACE) via node view
+
+        """
+        url = reverse('node', kwargs={'nodeid':'9b35fd39-6668-4b44-80fb-d50d0e5211a2'})
+        response = client.delete(url)
+        self.assertEqual(response.status_code, 200)
+        root = Node.objects.get(nodeid='d8f4db21-343e-4af3-8857-f7322dc9eb4b')
+        node_count = len(root.get_downstream_nodes())
+        self.assertEqual(node_count, NODE_COUNT-1-PLACE_NODE_COUNT)
+        edge_count = len(root.get_downstream_edges())
+        self.assertEqual(edge_count, NODE_COUNT-1-PLACE_NODE_COUNT)
