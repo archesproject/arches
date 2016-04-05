54241eb16b088dccf3c17271ec6f30da89419a8c
Ryan Anderson <randerson@fargeo.com>
2016-04-05 12:34:58 -0700

    Test codedeploy

--- arches/install/arches_start.sh
+++ arches/install/arches_start.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-python manage.py runserver 0.0.0.0:80 &
-
 sudo service elasticsearch start
 sudo service apache2 start
--- arches/install/arches_stop.sh
+++ arches/install/arches_stop.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+sudo service elasticsearch stop
+
 sudo -u postgres dropdb training
 
 sudo -u postgres psql -d postgres -c "UPDATE pg_database SET datistemplate='false' WHERE datname='template_postgis_20'"
