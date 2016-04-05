5e90accaa48aa58bee30b16586f69cb324fd7028
Ryan Anderson <randerson@fargeo.com>
2016-04-05 13:31:54 -0700

    Test codedeploy

--- arches/install/install_arches.sh
+++ arches/install/install_arches.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo rm -r -y ~/arches
-sudo rm -r -y ~/ENV
+sudo rm -r -y /home/ubuntu/arches
+sudo rm -r -y /home/ubuntu/ENV
 
 sudo apt-get install -y git
 sudo apt-get install -y python-pip
@@ -9,10 +9,10 @@ sudo -H pip install virtualenv==13.1.2
 
 git clone -b travis_ci_tests https://github.com/archesproject/arches.git /home/ubuntu/arches
 
-virtualenv ~/ENV
-source ~/ENV/bin/activate
+virtualenv /home/ubuntu/ENV
+source /home/ubuntu/ENV/bin/activate
 
-cd arches
+cd /home/ubuntu/arches
 python setup.py install
 
 python manage.py packages -o setup
