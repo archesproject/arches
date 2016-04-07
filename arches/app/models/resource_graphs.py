ca07d20c65dbc0a3eeb5ba062452fc387b39e6a8
Alexei Peters <apeters@fargeo.com>
2016-04-07 09:52:36 -0700

    refactor ResourceGraph Class, use objects for nodes and edges instead of arrays (this is an exploration really),

--- arches/app/models/resource_graphs.py
+++ arches/app/models/resource_graphs.py
@@ -17,6 +17,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 '''
 
 import uuid
+from copy import deepcopy 
 from arches.app.models import models
 from arches.app.utils.betterJSONSerializer import JSONSerializer, JSONDeserializer
 
@@ -28,67 +29,87 @@ class ResourceGraph(object):
 
     def __init__(self, *args, **kwargs):
         self.root = None
-        self.nodes = []
-        self.edges = []
+        self.nodes = {}
+        self.edges = {}
         if args:
             if isinstance(args[0], basestring):
-                try:
-                    uuid.UUID(args[0])
-                    root = models.Node.objects.get(pk=args[0])
-                    self.get_nodes_and_edges(root)
-                except(ValueError):
-                    pass
-                    #self.load(JSONDeserializer().deserialize(args[0]))
+                uuid.UUID(args[0])
+                root = models.Node.objects.get(pk=args[0])
+                self.get_nodes_and_edges(root)
             elif isinstance(args[0], models.Node):
                 self.get_nodes_and_edges(args[0])
             elif args[0]["nodes"] and args[0]["edges"]:
-                self.nodes = args[0]["nodes"]
-                self.edges = args[0]["edges"]
+                for node in args[0]["nodes"]:
+                    newNode = self.addNode(node)
+                    if node['istopnode']:
+                        self.root = newNode
+                    
+                for edge in args[0]["edges"]:
+                    self.addEdge(edge)
 
-    def _save(self):
+    def addNode(self, node):
+        """
+        Adds a node to this graph
+
+        Arguments:
+        node -- a dictionary representing a Node instance or an actual models.Node instance
+
+        """
+
+        if not isinstance(node, models.Node):
+            nodeobj = node.copy()
+            node = models.Node()
+            node.nodeid = nodeobj.get('nodeid', None)
+            node.name = nodeobj.get('name', '')
+            node.description = nodeobj.get('description','')
+            node.istopnode = nodeobj.get('istopnode','')
+            node.ontologyclass = nodeobj.get('ontologyclass','')
+            node.datatype = nodeobj.get('datatype','')
+            node.nodegroup_id = nodeobj.get('nodegroupid','')
+            node.branchmetadata_id = nodeobj.get('branchmetadataid','')
+
+        if node.pk == None:
+            node.pk = uuid.uuid4()    
+        self.nodes[node.pk] = node
+        return node
+
+    def addEdge(self, edge):
+        """
+        Adds an edge to this graph
+
+        will throw an error if the domain or range nodes referenced in this edge haven't 
+        already been added to this graph
+
+        Arguments:
+        edge -- a dictionary representing a Edge instance or an actual models.Edge instance
+        
+        """
+
+        if not isinstance(edge, models.Edge):
+            egdeobj = edge.copy()
+            edge = models.Edge()
+            edge.edgeid = egdeobj.get('edgeid', None)
+            edge.rangenode = self.nodes[egdeobj.get('rangenodeid')]
+            edge.domainnode = self.nodes[egdeobj.get('domainnodeid')]
+            edge.ontologyproperty = egdeobj.get('ontologyproperty', '')
+            edge.branchmetadataid = egdeobj.get('branchmetadataid', '')
+
+        if edge.pk == None:
+            edge.pk = uuid.uuid4()  
+        self.edges[edge.pk] = edge
+        return edge
+
+    def save(self):
         """
         Saves an entity back to the db, returns a DB model instance, not an instance of self
 
         """
 
-        for node in self.nodes:
-            newNode = models.Node()
+        for node_id, node in self.nodes.iteritems():
+            node.save()
 
-            try:
-                uuid.UUID(str(node['nodeid']))
-                newNode.nodeid = node['nodeid']
-            except(ValueError):
-                node['nodeid'] = str(uuid.uuid4())
-
-            newNode.nodeid = node.get('nodeid')
-            newNode.name = node.get('name', '')
-            newNode.description = node.get('description','')
-            newNode.istopnode = node.get('istopnode','')
-            newNode.crmclass = node.get('crmclass','')
-            newNode.datatype = node.get('datatype','')
-            newNode.status = node.get('status','')
-
-            node['nodeid'] = newNode.nodeid
-            newNode.save()
-
-
-        for edge in self.edges:
-            newEdge = models.Edge()
-            # self.get_node_id_from_text()
-
-            try:
-                uuid.UUID(edge['edgeid'])
-            except(ValueError):
-                edge['edgeid'] = str(uuid.uuid4())
-
-            newEdge.edgeid = edge.get('edgeid')
-            newEdge.rangenode_id = edge.get('rangenodeid')
-            newEdge.domainnode_id = edge.get('domainnodeid')
-            newEdge.ontologyproperty = edge.get('ontologyproperty', '')
-            newEdge.branchmetadataid = edge.get('branchmetadataid', '')
-
-            edge['edgeid'] = newEdge.edgeid
-            newEdge.save()
+        for edge_id, edge in self.edges.iteritems():
+            edge.save()
 
     def get_nodes_and_edges(self, node):
         """
@@ -97,25 +118,29 @@ class ResourceGraph(object):
         """
         
         self.root = node
-        self.nodes.append(node)
+        self.addNode(node)
 
         child_nodes, child_edges = node.get_child_nodes_and_edges()
 
-        self.nodes.extend(child_nodes)
-        self.edges.extend(child_edges)
+        for node in child_nodes:
+            self.addNode(node)
+        for edge in child_edges:
+            self.addEdge(edge)
 
     def append_branch(self, property, nodeid=None, branch_root=None, branchmetadataid=None):  
         """
         Appends a branch onto this graph
 
-        property: the property to use when appending the branch
+        Arguments:
+        property -- the property to use when appending the branch
 
-        nodeid: if given will append the branch to this node, if not supplied will 
+        Keyword Arguments:
+        nodeid -- if given will append the branch to this node, if not supplied will 
         append the branch to the root of this graph 
 
-        branch_root: the root node of the branch you want to append
+        branch_root -- the root node of the branch you want to append
 
-        branchmetadataid: get the branch to append based on the branchmetadataid, 
+        branchmetadataid -- get the branch to append based on the branchmetadataid, 
         if given, branch_root takes precedence
 
         """
@@ -125,47 +150,42 @@ class ResourceGraph(object):
         branch_root.istopnode = False
         branch_graph = ResourceGraph(branch_root)
         new_branch = branch_graph.copy()
+        new_branch.save()
         newEdge = models.Edge(
-            domainnode_id = (nodeid if nodeid else self.root.pk),
+            domainnode = (self.nodes[nodeid] if nodeid else self.root),
             rangenode = new_branch.root,
             ontologyproperty = property
         )
         newEdge.save()
-        branch_graph.edges.append(newEdge)
+        branch_graph.addEdge(newEdge)
         return branch_graph
 
     def copy(self):
-        mapping = {}
-        ret = ResourceGraph()
-        self.root.pk = None
-        self.root.save()
-        mapping[self.root.pk] = self.root
-        for edge in self.edges:
-            if(edge.rangenode_id not in mapping):
-                #mapping[edge.rangenode_id] = None
-                edge.rangenode.pk = None
-                mapping[edge.rangenode_id] = edge.rangenode.save()
-            else:
-                edge.rangenode = mapping[edge.rangenode_id]
-            if(edge.domainnode_id not in mapping):
-                edge.domainnode.pk = None
-                mapping[edge.domainnode_id] = edge.domainnode.save()
-            else:
-                edge.domainnode = mapping[edge.domainnode_id]
-            edge.pk = None
-            edge.save()
-            ret.edges.append(edge)
-        for nodeid, node in mapping.iteritems():
-            if str(self.root.pk) == str(nodeid):
-                ret.root = node
-            ret.nodes.append(node)
+        """
+        returns an unsaved copy of self
 
+        """
+
+        copy_of_self = deepcopy(self)
+        for edge_id, edge in copy_of_self.edges.iteritems():
+            edge.domainnode.pk = uuid.uuid4()
+            edge.rangenode.pk = uuid.uuid4()
+
+        copy_of_self.nodes = {node.pk:node for node_id, node in copy_of_self.nodes.iteritems()}
+
+        return copy_of_self
+
+    def serialize(self):
+        ret = {}
+        ret['root'] = self.root
+        ret['nodes'] = [node for key, node in self.nodes.iteritems()]
+        ret['edges'] = [edge for key, edge in self.edges.iteritems()]
         return ret
 
-    def get_node_id_from_text(self):
-        for edge in self.edges:
-            for node in self.nodes:
-                if edge['domainnodeid'] == node['name']:
-                    edge['domainnodeid'] = node['nodeid']
-                if edge['rangenodeid'] == node['name']:
-                    edge['rangenodeid'] = node['nodeid']
+    # def get_node_id_from_text(self):
+    #     for edge in self.edges:
+    #         for node in self.nodes:
+    #             if edge['domainnodeid'] == node['name']:
+    #                 edge['domainnodeid'] = node['nodeid']
+    #             if edge['rangenodeid'] == node['name']:
+    #                 edge['rangenodeid'] = node['nodeid']
