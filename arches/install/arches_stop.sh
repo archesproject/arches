ddf08413c53ff56e3bd431cb9d2b3b3fececf9d9
Ryan Anderson <randerson@fargeo.com>
2016-04-13 13:01:34 -0700

    Change permissions on arches_stop and arches_start and add command to clean out deployment copy folder.

--- arches/install/arches_stop.sh
+++ arches/install/arches_stop.sh
@@ -7,3 +7,5 @@ sudo -u postgres dropdb training
 sudo -u postgres psql -d postgres -c "UPDATE pg_database SET datistemplate='false' WHERE datname='template_postgis_20'"
 sudo -u postgres dropdb template_postgis_20
 
+sudo rm -r /var/www/html/arches
+sudo rm /var/www/html/appspec.yml
