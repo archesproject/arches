e69dbbc5fe932c657731a2f91caf3f8310ce779f
Alexei Peters <apeters@fargeo.com>
2016-04-12 17:00:56 -0700

    implement filter function to search on name, data type, and ontology class

--- arches/app/media/js/views/graph-manager/node-list.js
+++ arches/app/media/js/views/graph-manager/node-list.js
@@ -4,6 +4,17 @@ define([
     var NodeList = ListView.extend({
 
         single_select: true,
+        filter_function: function(newValue){
+            var filter = this.filter().toLowerCase();
+            this.items().forEach(function(item){
+                item.filtered(true);
+                if(item.name().toLowerCase().indexOf(filter) !== -1 ||
+                   item.datatype().toLowerCase().indexOf(filter) !== -1 ||
+                   item.ontologyclass.toLowerCase().indexOf(filter) !== -1){
+                    item.filtered(false);
+                }
+            }, this);
+        },
 
         initialize: function(options) {
             ListView.prototype.initialize.apply(this, arguments);
