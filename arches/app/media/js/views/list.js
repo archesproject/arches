304fbd5edbc7ab542720491a0026bdeabaaf7384
Alexei Peters <apeters@fargeo.com>
2016-04-12 17:00:17 -0700

    make filter function overridable, re #351

--- arches/app/media/js/views/list.js
+++ arches/app/media/js/views/list.js
@@ -7,20 +7,20 @@ define([
 
         items: ko.observableArray(),
         single_select: true,
+        filter_function: function(newValue){
+            var filter = this.filter().toLowerCase();
+            this.items().forEach(function(item){
+                var name = typeof item.name === 'string' ? item.name : item.name();
+                item.filtered(true);
+                if(name.toLowerCase().indexOf(filter) !== -1){
+                    item.filtered(false);
+                }
+            }, this);
+        },
 
         initialize: function(options) {
             this.filter = ko.observable('');
-            this.filter.subscribe(function(newValue){
-                var filter = this.filter().toLowerCase();
-                this.items().forEach(function(item){
-                    var name = typeof item.name === 'string' ? item.name : item.name();
-                    item.filtered(true);
-                    if(name.toLowerCase().indexOf(filter) !== -1){
-                        item.filtered(false);
-                    }
-                }, this);
-            }, this, 'change')
-
+            this.filter.subscribe(this.filter_function, this, 'change');
         },
 
         selectItem: function(item, evt){
