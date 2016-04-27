9766b6a1df91e160c7773c8976b09fd511604b05
Rob Gaston <rgaston@fargeo.com>
2016-04-27 13:05:31 -0700

    updating login page

--- arches/app/media/js/login.js
+++ arches/app/media/js/login.js
@@ -2,7 +2,43 @@ require([
     'jquery',
     'bootstrap'
 ], function($) {
-    setTimeout(function() {
-        $("#login-failed-alert").alert('close');
-    }, 1000);
+    $(document).ready(function() {
+    	var $imgHolder 	= $('#demo-bg-list');
+    	var $bgBtn 		= $imgHolder.find('.demo-chg-bg');
+    	var $target 	= $('#bg-overlay');
+
+    	$bgBtn.on('click', function(e){
+    		e.preventDefault();
+    		e.stopPropagation();
+
+
+    		var $el = $(this);
+    		if ($el.hasClass('active') || $imgHolder.hasClass('disabled'))return;
+    		if ($el.hasClass('bg-trans')) {
+    			$target.css('background-image','none');
+    			$imgHolder.removeClass('disabled');
+    			$bgBtn.removeClass('active');
+    			$el.addClass('active');
+
+    			return;
+    		}
+
+    		$imgHolder.addClass('disabled');
+    		var url = $el.attr('src').replace('/thumbs','');
+
+    		$('<img/>').attr('src' , url).load(function(){
+    			$target.css('background-image', 'url("' + url + '")');
+    			$imgHolder.removeClass('disabled');
+    			$bgBtn.removeClass('active');
+    			$el.addClass('active');
+
+    			$(this).remove();
+    		})
+
+    	});
+
+        setTimeout(function() {
+            $("#login-failed-alert").alert('close');
+        }, 1000);
+    });
 });
