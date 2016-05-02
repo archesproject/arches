a21f9e24ee8e5b351398ea30dccedac6176788dc
Rob Gaston <rgaston@fargeo.com>
2016-05-02 14:59:51 -0700

    stubbing out graph settings form with icons

--- arches/app/media/js/graph-settings.js
+++ arches/app/media/js/graph-settings.js
@@ -1,7 +1,20 @@
 require([
+    'underscore',
+    'knockout',
     'views/page-view'
-], function(PageView) {
-    var viewModel = {};
+], function(_, ko, PageView) {
+    var icons = JSON.parse($('#icon-data').val());
+    var iconFilter = ko.observable('');
+    var selectedIconClass = ko.observable('');
+    var viewModel = {
+        iconFilter: iconFilter,
+        icons: ko.computed(function () {
+            return _.filter(icons, function (icon) {
+                return icon.name.indexOf(iconFilter()) >= 0;
+            });
+        }),
+        selectedIconClass: selectedIconClass
+    };
 
     new PageView({
         viewModel: viewModel
