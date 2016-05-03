16445f43c77a4832d9a438d0ff358224227b9fd9
Rob Gaston <rgaston@fargeo.com>
2016-05-03 12:33:16 -0700

    putting logic to extend jquery ajax to support csrf token in it's own globally required module

---
+++ arches/app/media/js/utils/set-csrf-token.js
@@ -0,0 +1,13 @@
+define(['jquery', 'js-cookie'], function ($, Cookies) {
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", Cookies.get('csrftoken'));
+            }
+        }
+    });
+});
