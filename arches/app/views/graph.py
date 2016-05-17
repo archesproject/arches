16d6b766c1d5e8a68dd6652c306033fc85ac20cc
Rob Gaston <rgaston@fargeo.com>
2016-05-17 14:36:05 -0700

    fixing error in graph view

--- arches/app/views/graph.py
+++ arches/app/views/graph.py
@@ -16,7 +16,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 '''
 
-from django.conf import settings
+from django.conf import settings as app_settings
 from django.db import transaction
 from django.shortcuts import render
 from django.db.models import Q
