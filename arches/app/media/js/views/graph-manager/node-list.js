7de51a5da7e869dbec6ad5302a56890b79429038
Alexei Peters <apeters@fargeo.com>
2016-05-23 15:54:49 -0700

    fix incorrect ko property reference

--- arches/app/media/js/views/graph-manager/node-list.js
+++ arches/app/media/js/views/graph-manager/node-list.js
@@ -15,7 +15,7 @@ define([
                 item.filtered(true);
                 if(item.name().toLowerCase().indexOf(filter) !== -1 ||
                     item.datatype().toLowerCase().indexOf(filter) !== -1 ||
-                    item.ontologyclass_id.toLowerCase().indexOf(filter) !== -1){
+                    item.ontologyclass_id().toLowerCase().indexOf(filter) !== -1){
                     item.filtered(false);
                 }
             }, this);
