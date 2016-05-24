b9b63a36a93cb4c8605444b168bf11fdcd830ebc
Rob Gaston <rgaston@fargeo.com>
2016-05-24 10:10:32 -0700

    hover styles in graph list

---
+++ arches/app/media/js/bindings/hover.js
@@ -0,0 +1,16 @@
+define([
+    'knockout',
+], function (ko) {
+    ko.bindingHandlers.hover = {
+        init: function (element, valueAccessor) {
+            var value = valueAccessor();
+            ko.applyBindingsToNode(element, {
+                event: {
+                    mouseenter: function () { value(true) },
+                    mouseleave: function () { value(false) }
+                }
+            });
+        }
+    };
+    return ko.bindingHandlers.hover;
+});
