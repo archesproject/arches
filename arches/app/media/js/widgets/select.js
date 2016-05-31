70aee7cb6f60189f15b599a142d09168c42407d9
Rob Gaston <rgaston@fargeo.com>
2016-05-31 16:39:39 -0700

    fixing select component

--- arches/app/media/js/widgets/select.js
+++ arches/app/media/js/widgets/select.js
@@ -11,7 +11,7 @@ define(['knockout', 'plugins/knockout-select2'], function (ko) {
                 <div class="form-group">\
                     <label class="col-xs-12 control-label widget-input-label" for="" data-bind="text:label"></label>\
                     <div class="col-xs-12 col-md-9">\
-                        <input class="select2 arches-select2-crud-form" data-bind="select2: {value: selectedValue, select2Config: options, placeholder: placeholder}"></input>\
+                        <input class="select2 arches-select2-crud-form" data-bind="select2: {value: selectedValue, select2Config: {data: options}, placeholder: placeholder}"></input>\
                     </div>\
                 </div>\
             </div>'
