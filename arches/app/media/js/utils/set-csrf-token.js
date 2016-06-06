0a991e7ab27cf7c21c378238a27093d466e2585c
Rob Gaston <rgaston@fargeo.com>
2016-06-06 10:17:39 -0700

    re: #464

--- arches/app/media/js/utils/set-csrf-token.js
+++ arches/app/media/js/utils/set-csrf-token.js
@@ -1,4 +1,12 @@
 define(['jquery', 'js-cookie'], function ($, Cookies) {
+    /**
+    * csrfSafeMethod - checks if the request method is CSRF safe (using regex)
+    * this function is called before every request made using jQuery, and
+    * the CSRF token is set accordingly
+    *
+    * @param  {string} the request method name
+    * @return {boolean} true if the method is CSRF safe
+    */
     function csrfSafeMethod(method) {
         // these HTTP methods do not require CSRF protection
         return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
