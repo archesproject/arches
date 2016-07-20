635d1a6dcc6987c720b9ea2e534a4d4bbdf96561
Rob Gaston <rgaston@fargeo.com>
2016-07-20 11:57:04 -0700

    stubbing out card configuration views

---
+++ arches/app/media/js/views/graph/card-configuration/card-components-tree.js
@@ -0,0 +1,21 @@
+define([
+    'backbone'
+], function(Backbone) {
+    var CardComponentsTree = Backbone.View.extend({
+        /**
+        * A backbone view representing a card components tree
+        * @augments Backbone.View
+        * @constructor
+        * @name CardComponentsTree
+        */
+
+        /**
+        * Initializes the view with optional parameters
+        * @memberof CardComponentsTree.prototype
+        */
+        initialize: function(options) {
+
+        }
+    });
+    return CardComponentsTree;
+});
---
+++ arches/app/media/js/views/graph/card-configuration/card-form-preview.js
@@ -0,0 +1,21 @@
+define([
+    'backbone'
+], function(Backbone) {
+    var CardFormPreview = Backbone.View.extend({
+        /**``
+        * A backbone view representing a card form preview
+        * @augments Backbone.View
+        * @constructor
+        * @name CardFormPreview
+        */
+
+        /**
+        * Initializes the view with optional parameters
+        * @memberof CardFormPreview.prototype
+        */
+        initialize: function(options) {
+
+        }
+    });
+    return CardFormPreview;
+});
