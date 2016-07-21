3342f7f86c97d6db391ba3a9e0fd390c6caffc80
Rob Gaston <rgaston@fargeo.com>
2016-07-21 11:50:55 -0700

    adding basic client card model re: #620

--- arches/app/media/js/views/graph/card-configuration.js
+++ arches/app/media/js/views/graph/card-configuration.js
@@ -1,17 +1,39 @@
 require([
     'knockout',
+    'models/card',
     'views/graph/graph-page-view',
     'views/graph/card-configuration/card-component-form',
     'views/graph/card-configuration/card-components-tree',
     'views/graph/card-configuration/card-form-preview',
     'card-configuration-data'
-], function(ko, PageView, CardComponentForm, CardComponentsTree, CardFormPreview, data) {
+], function(ko, CardModel, PageView, CardComponentForm, CardComponentsTree, CardFormPreview, data) {
     /**
     * set up the page view model with the related sub views
     */
+    data.card.cards = data.subCards;
+    var setupCard = function (card) {
+        card.nodegroup = data.nodeGroups.find(function (nodegroup) {
+            return nodegroup.nodegroupid === card.nodegroup_id;
+        });
+        card.nodes = data.nodes.filter(function (node) {
+            return node.nodegroup_id === card.nodegroup_id;
+        });
+        card.cards.forEach(function(card) {
+            setupCard(card);
+        });
+    };
+    setupCard(data.card);
+
+    var cardModel = new CardModel({
+        data: data.card,
+        datatypes: data.datatypes
+    });
+    
     var viewModel = {
         cardComponentForm: new CardComponentForm(),
-        cardComponentsTree: new CardComponentsTree(),
+        cardComponentsTree: new CardComponentsTree({
+            card: cardModel
+        }),
         cardFormPreview: new CardFormPreview()
     };
 
