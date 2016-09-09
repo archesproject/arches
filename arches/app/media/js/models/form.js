fbb450990a0d05ccc28d4f1764c687c4d0df5004
Cyrus <chiatt@fargeo.com>
2016-09-09 13:03:05 -0700

    added form-settings files

---
+++ arches/app/media/js/models/form.js
@@ -0,0 +1,43 @@
+define(['arches',
+    'models/abstract',
+    'models/node',
+    'models/card-widget',
+    'knockout',
+    'knockout-mapping',
+    'underscore'
+], function (arches, AbstractModel, NodeModel, CardWidgetModel, ko, koMapping, _) {
+    var FormModel = AbstractModel.extend({
+        /**
+        * A backbone model to manage form data
+        * @augments AbstractModel
+        * @constructor
+        * @name FormModel
+        */
+
+        url: arches.urls.form,
+
+        constructor: function(attributes, options){
+            options || (options = {});
+            options.parse = true;
+            AbstractModel.prototype.constructor.call(this, attributes, options);
+        },
+
+        /**
+         * parse - parses the passed in attributes into a {@link FormModel}
+         * @memberof FormModel.prototype
+         * @param  {object} attributes - the properties to seed a {@link FormModel} with
+         */
+        parse: function(attributes){
+          console.log(attributes)
+        },
+
+        toJSON: function(){
+            var ret = {};
+            for(var key in this.attributes){
+                console.log(key)
+            }
+            return ret;
+        },
+
+    return FormModel;
+});
---
+++ arches/app/media/js/views/graph/form-settings.js
@@ -0,0 +1,95 @@
+require([
+    'jquery',
+    'underscore',
+    'knockout',
+    'knockout-mapping',
+    'views/graph/graph-page-view',
+    'form-settings-data'
+], function($, _, ko, koMapping, PageView, data) {
+    /**
+    * prep data for models
+    */
+    var resourceJSON = JSON.stringify(data.resources);
+    data.resources.forEach(function(resource) {
+        resource.isRelatable = ko.observable(resource.is_relatable);
+    });
+    var srcJSON = JSON.stringify(data.graph);
+
+    /**
+    * setting up page view model
+    */
+    var graph = koMapping.fromJS(data.graph);
+    var iconFilter = ko.observable('');
+    var ontologyClass = ko.observable(data.node.ontologyclass);
+    var jsonData = ko.computed(function() {
+        var relatableResourceIds = _.filter(data.resources, function(resource){
+            return resource.isRelatable();
+        }).map(function(resource){
+            return resource.id
+        });
+        if (graph.ontology_id() === undefined) {
+            graph.ontology_id(null);
+        }
+        return JSON.stringify({
+            graph: koMapping.toJS(graph),
+            relatable_resource_ids: relatableResourceIds,
+            ontology_class: ontologyClass()
+        });
+    });
+    var jsonCache = ko.observable(jsonData());
+    var dirty = ko.computed(function () {
+        return jsonData() !== jsonCache();
+    });
+    var viewModel = {
+        dirty: dirty,
+        iconFilter: iconFilter,
+        icons: ko.computed(function () {
+            return _.filter(data.icons, function (icon) {
+                return icon.name.indexOf(iconFilter()) >= 0;
+            });
+        }),
+        graph: graph,
+        resources: data.resources,
+        ontologies: data.ontologies,
+        ontologyClass: ontologyClass,
+        ontologyClasses: ko.computed(function () {
+            return _.filter(data.ontologyClasses, function (ontologyClass) {
+                return ontologyClass.ontology_id === graph.ontology_id();
+            });
+        }),
+        save: function () {
+            pageView.viewModel.loading(true);
+            $.ajax({
+                type: "POST",
+                url: '',
+                data: jsonData(),
+                success: function(response) {
+                    jsonCache(jsonData());
+                    pageView.viewModel.loading(false);
+                },
+                failure: function(response) {
+                    pageView.viewModel.loading(false);
+                }
+            });
+        },
+        reset: function () {
+            _.each(JSON.parse(srcJSON), function(value, key) {
+                graph[key](value);
+            });
+            JSON.parse(resourceJSON).forEach(function(jsonResource) {
+                var resource = _.find(data.resources, function (resource) {
+                    return resource.id === jsonResource.id;
+                });
+                resource.isRelatable(jsonResource.is_relatable);
+            });
+            jsonCache(jsonData());
+        }
+    };
+
+    /**
+    * a GraphPageView representing the graph settings page
+    */
+    var pageView = new PageView({
+        viewModel: viewModel
+    });
+});
