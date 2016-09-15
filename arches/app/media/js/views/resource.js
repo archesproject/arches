77d80cc48586810d9e1bc9411a149bf5e6070816
Alexei Peters <apeters@fargeo.com>
2016-09-15 14:32:03 -0700

    improve page navigation, re #765

--- arches/app/media/js/views/resource.js
+++ arches/app/media/js/views/resource.js
@@ -2,13 +2,33 @@ require([
     'jquery',
     'underscore',
     'knockout',
+    'arches',
     'views/base-manager',
-], function($, _, ko, BaseManagerView) {
+    'bindings/chosen'
+], function($, _, ko, arches, BaseManagerView) {
 
     /**
     * a BaseManagerView representing the resource listing and recent edits pages
     */
-    var ResourceView = BaseManagerView.extend({});
-    new ResourceView();
+    var ResourceView = BaseManagerView.extend({
+        initialize: function(options){
+            var self = this;
+            this.viewModel.showResources = ko.observable(true);
+
+            _.defaults(this.viewModel, {
+                showFind: ko.observable(false),
+                graphId: ko.observable(null),
+            });
+
+            this.viewModel.graphId.subscribe(function (graphid) {
+                if(graphid && graphid !== ""){
+                    self.viewModel.navigate(arches.urls.graph + graphid + '/add_resource');
+                }
+            });
+
+            BaseManagerView.prototype.initialize.call(this, options)
+        }
+    });
+    return new ResourceView();
 
 });
