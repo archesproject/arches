dbfa66f1b7d9e791f10142f92554d82a33b84055
Ryan Anderson <randerson@fargeo.com>
2016-10-21 15:36:44 -0700

    1010 cards x nodes widgets data export (#1037)
    * Add cards_x_nodes_x_widgets data to graph export and graph import.
    
    * Refactor, change command from 'export_resource_graphs' to 'export_graphs'. Add ability to export just branches or just resource models'

--- arches/app/utils/data_management/resource_graphs/exporter.py
+++ arches/app/utils/data_management/resource_graphs/exporter.py
@@ -52,23 +52,26 @@ def get_card_x_node_x_widget_data_for_export(resource_graph):
     return cards_x_nodes_x_widgets
 
 
-def get_graphs_for_export(resource_list=None):
+def get_graphs_for_export(graphids=None):
     graphs = {}
     graphs['graph'] = []
-    if resource_list == None:
-        resource_graph_query = Graph.objects.all().exclude(name='Arches configuration')
-        for resource_graph in resource_graph_query:
-            resource_graph['cards_x_nodes_x_widgets'] = get_card_x_node_x_widget_data_for_export(resource_graph)
-            graphs['graph'].append(resource_graph)
+    if graphids == None:
+        resource_graph_query = JSONSerializer().serializeToPython(Graph.objects.all().exclude(name='Arches configuration'))
+    elif graphids == 'resources':
+        resource_graph_query = JSONSerializer().serializeToPython(Graph.objects.filter(isresource=True).exclude(name='Arches configuration'))
+    elif graphids == 'branches':
+        resource_graph_query = JSONSerializer().serializeToPython(Graph.objects.filter(isresource=False).exclude(name='Arches configuration'))
     else:
-        resource_graph_query = JSONSerializer().serializeToPython(Graph.objects.filter(graphid__in=resource_list))
-        for resource_graph in resource_graph_query:
-            resource_graph['cards_x_nodes_x_widgets'] = get_card_x_node_x_widget_data_for_export(resource_graph)
-            graphs['graph'].append(resource_graph)
+        resource_graph_query = JSONSerializer().serializeToPython(Graph.objects.filter(graphid__in=graphids))
+
+    for resource_graph in resource_graph_query:
+        resource_graph['cards_x_nodes_x_widgets'] = get_card_x_node_x_widget_data_for_export(resource_graph)
+        graphs['graph'].append(resource_graph)
+
     return graphs
 
-def write_graph(export_dir, resource_list):
-    resource_graphs = get_graphs_for_export(resource_list)
+def write_graph(export_dir, graphids):
+    resource_graphs = get_graphs_for_export(graphids)
     graph = {}
     graph = resource_graphs
 
