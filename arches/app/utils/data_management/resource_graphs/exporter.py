0af5f2b2cdf359bc20ef4affd4bb78e26ba01a43
Ryan Anderson <randerson@fargeo.com>
2016-10-24 13:19:58 -0700

    Update HIP branches and tests.

--- arches/app/utils/data_management/resource_graphs/exporter.py
+++ arches/app/utils/data_management/resource_graphs/exporter.py
@@ -56,6 +56,7 @@ def get_forms_for_export(resource_graph):
     return forms
 
 def get_form_x_card_data_for_export(resource_graph):
+    forms_x_cards = []
     for form in resource_graph['forms']:
         forms_x_cards = FormXCard.objects.filter(form_id=form.formid)
     return forms_x_cards
