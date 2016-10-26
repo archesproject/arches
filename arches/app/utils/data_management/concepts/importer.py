fc02420b12a42a3e09c58a7741a53ba261f5b912
Ryan Anderson <randerson@fargeo.com>
2016-10-26 13:36:11 -0700

    322 export reference data (#1058)
    * First cut at exporting reference data to arches json format.
    
    * Change format of reference data in arches json. Fix tests.
    
    * Add threat type authority document to repo. Adjust tests.
    
    * Concept export and refactor export_json command.
    
    * Remove export_graphs command. Use python manage.py packages -o export_json -g 'graphids or "all" or "branches" or "resources"' instead.
    
    * Refactor import cli.

--- arches/app/utils/data_management/concepts/importer.py
+++ arches/app/utils/data_management/concepts/importer.py
@@ -19,38 +19,49 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 from arches.app.models.concept import Concept, ConceptValue
 from arches.app.utils.betterJSONSerializer import JSONSerializer, JSONDeserializer
 from arches.app.models import models
+from django.db import transaction
 
-def import_concepts(reference_data):
-    concepts = reference_data[0]['concepts']
-    values = reference_data[1]['values']
-    relations = reference_data[2]['relations']
+# def import_concepts(reference_data):
+#     concepts = reference_data[0]['concepts']
+#     values = reference_data[1]['values']
+#     relations = reference_data[2]['relations']
+#
+#     concept_objs = {}
+#     for concept in concepts:
+#         concept_obj = Concept()
+#         concept_obj.id = concept['conceptid']
+#         concept_obj.nodetype = concept['nodetype']
+#         concept_obj.legacyoid = concept['legacyoid']
+#         concept_obj.save()
+#
+#         concept_objs[concept_obj.id] = concept_obj
+#
+#     existing_valuetypes = [o.valuetype for o in models.DValueType.objects.all()]
+#     for value in values:
+#         if value['valuetype'] not in existing_valuetypes:
+#             models.DValueType.objects.create(valuetype = value['valuetype'], category = 'undefined', namespace = 'arches')
+#             existing_valuetypes.append(value['valuetype'])
+#
+#         conceptvalue_obj = ConceptValue()
+#         conceptvalue_obj.id = value['valueid']
+#         conceptvalue_obj.conceptid = value['conceptid']
+#         conceptvalue_obj.type = value['valuetype']
+#         conceptvalue_obj.value = value['value']
+#         conceptvalue_obj.language = value['languageid']
+#         conceptvalue_obj.save()
+#
+#     for relation in relations:
+#         if relation['conceptidfrom'] in concept_objs and relation['conceptidto'] in concept_objs:
+#             conceptfrom = concept_objs[relation['conceptidfrom']]
+#             conceptto = concept_objs[relation['conceptidto']]
+#             conceptfrom.add_relation(conceptto, relation['relationtype'])
 
-    concept_objs = {}
-    for concept in concepts:
-        concept_obj = Concept()
-        concept_obj.id = concept['conceptid']
-        concept_obj.nodetype = concept['nodetype']
-        concept_obj.legacyoid = concept['legacyoid']
-        concept_obj.save()
-
-        concept_objs[concept_obj.id] = concept_obj
-
-    existing_valuetypes = [o.valuetype for o in models.DValueType.objects.all()]
-    for value in values:
-        if value['valuetype'] not in existing_valuetypes:
-            models.DValueType.objects.create(valuetype = value['valuetype'], category = 'undefined', namespace = 'arches')
-            existing_valuetypes.append(value['valuetype'])
-
-        conceptvalue_obj = ConceptValue()
-        conceptvalue_obj.id = value['valueid']
-        conceptvalue_obj.conceptid = value['conceptid']
-        conceptvalue_obj.type = value['valuetype']
-        conceptvalue_obj.value = value['value']
-        conceptvalue_obj.language = value['languageid']
-        conceptvalue_obj.save()
-
-    for relation in relations:
-        if relation['conceptidfrom'] in concept_objs and relation['conceptidto'] in concept_objs:
-            conceptfrom = concept_objs[relation['conceptidfrom']]
-            conceptto = concept_objs[relation['conceptidto']]
-            conceptfrom.add_relation(conceptto, relation['relationtype'])
+def import_reference_data(reference_data):
+    # with transaction.atomic():
+    if reference_data != '':
+        print '\nLOADING AUTHORITY FILES FROM JSON'
+        print '-----------------------'
+    for data in reference_data:
+        print data['legacyoid']
+        concept = Concept(data)
+        concept.save()
