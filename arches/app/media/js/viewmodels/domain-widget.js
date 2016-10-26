e6600986ce594c055faa71d6208f7e16befe25ed
Rob Gaston <rgaston@fargeo.com>
2016-10-26 11:04:02 -0700

    more widget refactoring

--- arches/app/media/js/viewmodels/domain-widget.js
+++ arches/app/media/js/viewmodels/domain-widget.js
@@ -2,15 +2,15 @@ define([
     'knockout',
     'underscore',
     'viewmodels/widget'
-], function (ko, _, WidgetViewModel) {
+], function(ko, _, WidgetViewModel) {
     /**
-    * A viewmodel used for domain widgets
-    *
-    * @constructor
-    * @name DomainWidgetViewModel
-    *
-    * @param  {string} params - a configuration object
-    */
+     * A viewmodel used for domain widgets
+     *
+     * @constructor
+     * @name DomainWidgetViewModel
+     *
+     * @param  {string} params - a configuration object
+     */
     var DomainWidgetViewModel = function(params) {
         var self = this;
 
@@ -18,7 +18,7 @@ define([
 
         if (this.node.config.options) {
             this.options = this.node.config.options;
-            this.options().forEach(function (option) {
+            this.options().forEach(function(option) {
                 if (!ko.isObservable(option.text)) {
                     option.text = ko.observable(option.text);
                 }
@@ -26,7 +26,7 @@ define([
             this.node.configKeys.valueHasMutated();
         }
 
-        this.flatOptions = ko.computed(function () {
+        this.flatOptions = ko.computed(function() {
             var options = self.options();
             var flatOptions = [];
             var value = self.value();
@@ -37,25 +37,25 @@ define([
                 }
                 value = valueArray
             }
-            var setupOption = function (option) {
+            var setupOption = function(option) {
                 option.selected = ko.computed({
-                    read: function () {
+                    read: function() {
                         var selected = false;
                         var value = self.value();
-                        if (Array.isArray(value) || typeof value === 'string') {
+                        if (value && value.indexOf) {
                             selected = value.indexOf(option.id) >= 0;
                         }
                         return selected;
                     },
-                    write: function (newValue) {
+                    write: function(selected) {
                         if (self.multiple) {
-                            if (newValue) {
-                                value.push(option.id);
-                                self.value(value);
-                            } else {
-                                self.value(_.without(value, option.id));
-                            }
-                        } else if (newValue) {
+                            var value = self.value();
+                            self.value(
+                                selected ?
+                                _.union(value, [option.id]) :
+                                _.without(value, option.id)
+                            );
+                        } else if (selected) {
                             self.value(option.id);
                         }
                     }
@@ -70,7 +70,7 @@ define([
 
         this.multiple = false;
 
-        this.displayValue = ko.computed(function () {
+        this.displayValue = ko.computed(function() {
             var value = self.value();
             var options = ko.unwrap(self.flatOptions);
             var displayValue = null;
@@ -80,7 +80,7 @@ define([
                 }
                 displayValue = _.map(value, function(id) {
                     var text = null;
-                    var option = _.find(options, function (option) {
+                    var option = _.find(options, function(option) {
                         return option.id === id;
                     });
                     if (option) {
@@ -94,10 +94,10 @@ define([
         });
     };
 
-    var gatherChildren = function (current, list, callback) {
+    var gatherChildren = function(current, list, callback) {
         list.push(current);
         if (current.children) {
-            current.children.forEach(function (child) {
+            current.children.forEach(function(child) {
                 gatherChildren(child, list, callback);
             });
         }
