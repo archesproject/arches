4555cf121f3b8b0f7ae1b68cec67b2305b60f518
Rob Gaston <rgaston@fargeo.com>
2016-11-07 14:48:28 -0800

    allowing user to specify nodes for image report re: #804

--- arches/app/media/js/reports/image.js
+++ arches/app/media/js/reports/image.js
@@ -1,8 +1,10 @@
 define([
+    'underscore',
     'knockout',
     'viewmodels/report',
-    'plugins/knockstrap'
-], function(ko, ReportViewModel) {
+    'plugins/knockstrap',
+    'bindings/chosen'
+], function(_, ko, ReportViewModel) {
     return ko.components.register('image-report', {
         viewModel: function(params) {
             var self = this;
@@ -13,15 +15,20 @@ define([
                 src: '/media/img/photo_missing.png',
                 alt: ''
             }]);
-            console.log(this.config());
 
             if (self.report.get('tiles')) {
                 var imgs = [];
+                var nodes = self.nodes();
                 self.report.get('tiles').forEach(function(tile) {
                     _.each(tile.data, function(val, key) {
                         if (Array.isArray(val)) {
                             val.forEach(function(item) {
-                                if (item.status && item.type && item.status === 'uploaded' && item.type.indexOf('image') > -1) {
+                                if (item.status &&
+                                    item.type &&
+                                    item.status === 'uploaded' &&
+                                    item.type.indexOf('image') > -1 &&
+                                    _.contains(nodes, key)
+                                ) {
                                     imgs.push({
                                         src: item.url,
                                         alt: item.name
@@ -41,10 +48,16 @@ define([
                     widgets = widgets.concat(card.get('widgets')());
                     card.get('cards')().forEach(function(card) {
                         widgets = widgets.concat(card.get('widgets')());
-                    })
-                })
+                    });
+                });
             });
-            console.log(widgets);
+            this.nodeOptions = ko.observableArray(
+                widgets.map(function(widget) {
+                    return widget.node
+                }).filter(function (node) {
+                    return ko.unwrap(node.datatype) === 'file-list';
+                })
+            );
         },
         template: {
             require: 'text!report-templates/image'
