0c0a02a2789ca6e8d235ebdfaf3ef002fb323aa2
Cyrus Hiatt <chiatt@fargeo.com>
2016-11-08 14:59:39 -0800

    added more checks to the map widget test, re #1074

--- tests/ui/pages/page_locators.py
+++ tests/ui/pages/page_locators.py
@@ -34,4 +34,10 @@ class MapWidgetPageLocators(BasePageLocators):
     def __init__(self):
         super(BasePageLocators, self).__init__()
 
-    MAP_TOOLS_BUTTON = (By.ID, 'map-tools')
+    MAP_TOOLS_BUTTON = (By.ID, "map-tools")
+    MAP_TOOLS_BASEMAPS = (By.XPATH, "//*[@id='map-widget-toolbar']/ul/li[1]")
+    SATELLITE_BASE_MAP = (By.XPATH, "//*[@id='map-widget-basemaps']/div[3]")
+    MAP_TOOLS_OVERLAYS = (By.ID, "btn-overlays")
+    OVERLAY_LIBRARY_BUTTON = (By.XPATH, "//*[@id='overlays-panel']/div[1]/h4/i")
+    OVERLAY_TO_ADD = (By.XPATH, "//*[@id='overlay-grid']/div[1]")
+    ADDED_OVERLAYS = (By.CSS_SELECTOR, "#overlays-panel .map-widget-overlay-item .map-overlay-name")
