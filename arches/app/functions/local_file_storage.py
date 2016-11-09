8b7294eb8e5a1178e38fa98504d082f0cb42e04d
Alexei Peters <apeters@fargeo.com>
2016-11-08 20:38:15 -0800

    use underscores for py files, re #1103

--- arches/app/functions/local_file_storage.py
+++ arches/app/functions/local_file_storage.py
@@ -1,5 +1,9 @@
 from arches.app.functions.base import BaseFunction
 
 class LocalFileStorageFunction(BaseFunction):
+    def run(self):
+        self.save(None, None)
+
     def save(self, resource, config):
+        print 'save file'
         pass
