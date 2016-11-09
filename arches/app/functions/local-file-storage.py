a559d8587ec0bc67ce5f433740ba4907be889d4c
Alexei Peters <apeters@fargeo.com>
2016-11-08 16:17:47 -0800

    stub out functions for file upload, and required node, re #1103

---
+++ arches/app/functions/local-file-storage.py
@@ -0,0 +1,5 @@
+from arches.app.functions.base import BaseFunction
+
+class LocalFileStorageFunction(BaseFunction):
+    def save(self, resource, config):
+        pass
---
+++ arches/app/functions/required-nodes.py
@@ -0,0 +1,16 @@
+import uuid
+from arches.app.functions.base import BaseFunction
+from arches.app.models import models
+from arches.app.models.tile import Tile
+
+class AllNodesRequiredFunction(BaseFunction):
+    def get(self, resource, config):
+        return self.get_primary_name_from_nodes(resource, config)
+
+    def get_primary_name_from_nodes(self, resource, config):
+        for tile in models.Tile.objects.filter(nodegroup_id=uuid.UUID(config['nodegroup_id']), sortorder=0):
+            for node in models.Node.objects.filter(nodegroup_id=uuid.UUID(config['nodegroup_id'])):
+                if str(node.nodeid) in tile.data:
+                    config['string_template'] = config['string_template'].replace('<%s>' % node.name, tile.data[str(node.nodeid)])
+
+        return config['string_template']
