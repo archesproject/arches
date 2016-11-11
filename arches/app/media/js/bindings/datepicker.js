89868b5e604a47c500e7836d73c5206cb86d5904
Rob Gaston <rgaston@fargeo.com>
2016-11-10 16:06:29 -0800

    fixes for datepicker widget re: #1067

--- arches/app/media/js/bindings/datepicker.js
+++ arches/app/media/js/bindings/datepicker.js
@@ -13,7 +13,6 @@ define([
     */
     ko.bindingHandlers.datepicker = {
         init: function (element, valueAccessor, allBindingsAccessor) {
-            //initialize datepicker with some optional options
             var options = allBindingsAccessor().datepickerOptions || {};
             var minDate;
             var maxDate;
@@ -56,7 +55,6 @@ define([
 
             $(element).datetimepicker(options);
 
-            //when a user changes the date, update the view model
             ko.utils.registerEventHandler(element, "dp.change", function (event) {
                 var value = valueAccessor();
                 if (ko.isObservable(value)) {
@@ -76,15 +74,12 @@ define([
             });
         },
         update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
-
             var picker = $(element).data("DateTimePicker");
-            //when the view model is updated, update the widget
             if (picker) {
                 var koDate = ko.utils.unwrapObservable(valueAccessor());
+                console.log('update', koDate);
                 if (koDate) {
-                  //in case return from server datetime i am get in this form for example /Date(93989393)/ then fomat this
-                  koDate = (typeof (koDate) !== 'object') ? new Date(parseFloat(koDate.replace(/[^0-9]/g, ''))) : koDate;
-                  koDate = moment(koDate).format(picker.format());
+                  koDate = moment(koDate);
                   picker.date(koDate);
                 }
             }
