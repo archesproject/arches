e66848d8b6ac973c052fe39c2c30226818306461
Alexei Peters <apeters@fargeo.com>
2016-12-12 23:26:44 -0800

    get Tile proxy model provisionally working with save and delete operations, re #1323

--- arches/app/functions/local_file_storage.py
+++ arches/app/functions/local_file_storage.py
@@ -1,37 +1,38 @@
-from arches.app.models import models
+from arches.app.models.tile import Tile
 from arches.app.functions.base import BaseFunction
 
 class LocalFileStorageFunction(BaseFunction):
 
     def save(self, tile, request):
         # a function meant to handle this datatype during tile save
-        try:
-            tile_model = models.Tile.objects.get(pk=tile['tileid'])
-        except models.Tile.DoesNotExist:
-            tile_model = None
+        # try:
+        #     tile_model = Tile.objects.get(pk=tile['tileid'])
+        # except Tile.DoesNotExist:
+        #     tile_model = None
 
-        nodegroup = models.NodeGroup.objects.get(pk=tile['nodegroup_id'])
-        for node in nodegroup.node_set.all():
+        #nodegroup = models.NodeGroup.objects.get(pk=tile['nodegroup_id'])
+        for node in tile.nodegroup.node_set.all():
             if node.datatype == 'file-list':
-                if tile_model is not None:
-                    model_files = tile_model.data[str(node.pk)]
+                if tile is not None:
+                    model_files = tile.data[str(node.pk)]
                     for model_file in model_files:
                         incoming_file = None
-                        for file_json in tile['data'][str(node.pk)]:
+                        for file_json in tile.data[str(node.pk)]:
                             if file_json["file_id"] == model_file["file_id"]:
                                 incoming_file = file_json
                         if incoming_file == None:
                             deleted_file = models.File.objects.get(pk=model_file["file_id"])
                             deleted_file.delete()
-                files = request.FILES.getlist('file-list_' + str(node.pk), [])
-                for file_data in files:
-                    file_model = models.File()
-                    file_model.path = file_data
-                    file_model.save()
-                    for file_json in tile['data'][str(node.pk)]:
-                        if file_json["name"] == file_data.name and file_json["url"] is None:
-                            file_json["file_id"] = str(file_model.pk)
-                            file_json["url"] = str(file_model.path.url)
-                            file_json["status"] = 'uploaded'
+                    
+                    files = request.FILES.getlist('file-list_' + str(node.pk), [])
+                    for file_data in files:
+                        file_model = models.File()
+                        file_model.path = file_data
+                        file_model.save()
+                        for file_json in tile.data[str(node.pk)]:
+                            if file_json["name"] == file_data.name and file_json["url"] is None:
+                                file_json["file_id"] = str(file_model.pk)
+                                file_json["url"] = str(file_model.path.url)
+                                file_json["status"] = 'uploaded'
 
         return tile
