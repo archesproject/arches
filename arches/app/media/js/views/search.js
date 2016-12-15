f7f1a6a5b5bf1c5e5d01635fd22010fe185e9a0d
Alexei Peters <apeters@fargeo.com>
2016-12-14 22:10:24 -0800

    partially working version of term filter, just looks up terms in the index, re #1323

--- arches/app/media/js/views/search.js
+++ arches/app/media/js/views/search.js
@@ -1,5 +1,18 @@
 require([
+    'jquery',
+    'views/search/term-filter', 
     'views/base-manager'
-], function(PageView) {
-    new PageView();
+], function($, TermFilter, BaseManagerView) {
+
+    var viewModel = {};
+
+    var termFilter = new TermFilter({
+        el: $.find('input.resource_search_widget')[0]
+    });
+    
+    var SearchView = new BaseManagerView({
+        viewModel: viewModel
+    });
+
+    return SearchView;
 });
