7b04291538197bbe0456180246a0da4d90bd8d4f
Rob Gaston <rgaston@fargeo.com>
2016-12-21 12:22:15 -0800

    cleanup

--- arches/app/media/js/views/search.js
+++ arches/app/media/js/views/search.js
@@ -40,20 +40,20 @@ require([
                 //el: $('#search-results-container')[0],
                 viewModel: this.viewModel
             });
-            this.viewModel.searchResults.on('mouseover', function(resourceid){
+            this.viewModel.searchResults.on('mouseover', function(resourceid) {
                 this.viewModel.mapFilter.selectFeatureById(resourceid);
             }, this);
-            this.viewModel.searchResults.on('mouseout', function(){
+            this.viewModel.searchResults.on('mouseout', function() {
                 this.viewModel.mapFilter.unselectAllFeatures();
             }, this);
-            this.viewModel.searchResults.on('find_on_map', function(resourceid, data){
+            this.viewModel.searchResults.on('find_on_map', function(resourceid, data) {
                 var extent,
                     expand = !this.viewModel.mapFilter.expanded();
                 if (expand) {
                     this.viewModel.mapFilter.expanded(true);
                 }
 
-                _.each(data.geometries, function (geometryData) {
+                _.each(data.geometries, function(geometryData) {
                     var geomExtent = wkt.readGeometry(geometryData.label).getExtent();
                     geomExtent = ol.extent.applyTransform(geomExtent, ol.proj.getTransform('EPSG:4326', 'EPSG:3857'));
                     extent = extent ? ol.extent.extend(extent, geomExtent) : geomExtent;
@@ -70,7 +70,7 @@ require([
 
             self.isNewQuery = true;
             this.searchQuery = {
-                queryString: function(){
+                queryString: function() {
                     var params = {
                         page: self.viewModel.searchResults.page(),
                         termFilter: ko.toJSON(self.viewModel.termFilter.filter.terms()),
@@ -84,32 +84,34 @@ require([
                         // timeExpanded: self.viewModel.timeFilter.expanded()
                     };
                     if (
-                        self.viewModel.termFilter.filter.terms().length === 0// &&
+                        self.viewModel.termFilter.filter.terms().length === 0 // &&
                         // self.viewModel.timeFilter.filter.year_min_max().length === 0 &&
                         // self.viewModel.timeFilter.filter.filters().length === 0 &&
                         // self.viewModel.mapFilter.filter.geometry.coordinates().length === 0
-                        ) {
+                    ) {
                         params.no_filters = true;
                     }
 
                     params.include_ids = self.isNewQuery;
                     return $.param(params).split('+').join('%20');
                 },
-                changed: ko.pureComputed(function(){
+                changed: ko.pureComputed(function() {
                     var ret = ko.toJSON(this.viewModel.termFilter.changed()) +
                         ko.toJSON(this.viewModel.timeFilter.changed()) +
                         ko.toJSON(this.viewModel.mapFilter.changed());
                     return ret;
-                }, this).extend({ rateLimit: 200 })
+                }, this).extend({
+                    rateLimit: 200
+                })
             };
 
             this.getSearchQuery();
 
-            this.viewModel.searchResults.page.subscribe(function(){
+            this.viewModel.searchResults.page.subscribe(function() {
                 self.doQuery();
             });
 
-            this.searchQuery.changed.subscribe(function(){
+            this.searchQuery.changed.subscribe(function() {
                 self.isNewQuery = true;
                 self.viewModel.searchResults.page(1);
                 self.doQuery();
@@ -119,7 +121,7 @@ require([
             BaseManagerView.prototype.initialize.call(this, options);
         },
 
-        doQuery: function () {
+        doQuery: function() {
             var self = this;
             var queryString = this.searchQuery.queryString();
             if (this.updateRequest) {
@@ -127,29 +129,29 @@ require([
             }
 
             this.viewModel.loading(true);
-            window.history.pushState({}, '', '?'+queryString);
+            window.history.pushState({}, '', '?' + queryString);
 
             this.updateRequest = $.ajax({
                 type: "GET",
                 url: arches.urls.search_results,
                 data: queryString,
                 context: this,
-                success: function(response){
+                success: function(response) {
                     var data = this.viewModel.searchResults.updateResults(response);
                     // this.viewModel.mapFilter.highlightFeatures(data, $('.search-result-all-ids').data('results'));
                     // this.viewModel.mapFilter.applyBuffer();
                     this.isNewQuery = false;
                 },
-                error: function(response, status, error){
+                error: function(response, status, error) {
                     this.viewModel.alert(new AlertViewModel('ep-alert-red', arches.graphImportFailed.title, response));
                 },
-                complete: function (request, status) {
+                complete: function(request, status) {
                     this.viewModel.loading(false);
                 }
             });
         },
 
-        showSavedSearches: function(){
+        showSavedSearches: function() {
             this.clear();
             $('#saved-searches').slideDown('slow');
             $('#search-results').slideUp('slow');
@@ -157,35 +159,37 @@ require([
             this.viewModel.timeFilter.expanded(false);
         },
 
-        hideSavedSearches: function(){
+        hideSavedSearches: function() {
             $('#saved-searches').slideUp('slow');
             $('#search-results').slideDown('slow');
         },
 
-        toggleMapFilter: function(){
-            if($('#saved-searches').is(":visible")){
+        toggleMapFilter: function() {
+            if ($('#saved-searches').is(":visible")) {
                 this.doQuery();
                 this.hideSavedSearches();
             }
             this.viewModel.mapFilter.expanded(!this.viewModel.mapFilter.expanded());
-            window.history.pushState({}, '', '?'+this.searchQuery.queryString());
+            window.history.pushState({}, '', '?' + this.searchQuery.queryString());
         },
 
-        toggleTimeFilter: function(){
-            if($('#saved-searches').is(":visible")){
+        toggleTimeFilter: function() {
+            if ($('#saved-searches').is(":visible")) {
                 this.doQuery();
                 this.hideSavedSearches();
             }
             this.viewModel.timeFilter.expanded(!this.viewModel.timeFilter.expanded());
-            window.history.pushState({}, '', '?'+this.searchQuery.queryString());
+            window.history.pushState({}, '', '?' + this.searchQuery.queryString());
         },
 
-        getSearchQuery: function(){
+        getSearchQuery: function() {
             var doQuery = false;
-            var query = _.chain(decodeURIComponent(location.search).slice(1).split('&') )
+            var query = _.chain(decodeURIComponent(location.search).slice(1).split('&'))
                 // Split each array item into [key, value]
                 // ignore empty string if search is empty
-                .map(function(item) { if (item) return item.split('='); })
+                .map(function(item) {
+                    if (item) return item.split('=');
+                })
                 // Remove undefined in the case the search is empty
                 .compact()
                 // Turn [key, value] arrays into object parameters
@@ -193,14 +197,14 @@ require([
                 // Return the value of the chain operation
                 .value();
 
-            if('page' in query){
+            if ('page' in query) {
                 query.page = JSON.parse(query.page);
                 doQuery = true;
             }
             this.viewModel.searchResults.restoreState(query.page);
 
 
-            if('termFilter' in query){
+            if ('termFilter' in query) {
                 query.termFilter = JSON.parse(query.termFilter);
                 doQuery = true;
             }
@@ -229,14 +233,14 @@ require([
             // this.viewModel.mapFilter.restoreState(query.spatialFilter, query.mapExpanded);
 
 
-            if(doQuery){
+            if (doQuery) {
                 this.doQuery();
                 this.hideSavedSearches();
             }
 
         },
 
-        clear: function(){
+        clear: function() {
             this.viewModel.mapFilter.clear();
             this.viewModel.timeFilter.clear();
             this.viewModel.termFilter.clear();
--- arches/app/media/js/views/search/term-filter.js
+++ arches/app/media/js/views/search/term-filter.js
@@ -4,7 +4,7 @@ define(['jquery',
     'select2',
     'knockout',
     'views/search/base-filter'
-], function ($, Backbone, arches, Select2, ko, BaseFilter) {
+], function($, Backbone, arches, Select2, ko, BaseFilter) {
     return BaseFilter.extend({
 
         initialize: function(options) {
@@ -13,7 +13,7 @@ define(['jquery',
 
             this.filter.terms = ko.observableArray();
 
-        	this.render();
+            this.render();
 
             var resize = function() {
                 $('.resource_search_widget_dropdown .select2-results').css('maxHeight', $(window).height() - self.$el.offset().top - 100 + 'px');
@@ -22,7 +22,7 @@ define(['jquery',
             $(window).resize(resize);
         },
 
-        render: function(){
+        render: function() {
             var self = this;
             this.searchbox = this.$el.select2({
                 dropdownCssClass: 'resource_search_widget_dropdown',
@@ -31,13 +31,13 @@ define(['jquery',
                 ajax: {
                     url: this.getUrl(),
                     dataType: 'json',
-                    data: function (term, page) {
+                    data: function(term, page) {
                         return {
                             q: term, // search term
                             page_limit: 30
                         };
                     },
-                    results: function (data, page) {
+                    results: function(data, page) {
                         var value = $('div.resource_search_widget').find('.select2-input').val();
 
                         // this result is being hidden by a style in arches.css
@@ -53,7 +53,7 @@ define(['jquery',
                             text: value,
                             value: value
                         }];
-                        $.each(data.hits.hits, function(){
+                        $.each(data.hits.hits, function() {
                             results.push({
                                 inverted: false,
                                 type: this._source.options.conceptid ? 'concept' : 'term',
@@ -64,41 +64,45 @@ define(['jquery',
                                 value: this._source.options.conceptid ? this._source.options.conceptid : this._source.term
                             });
                         }, this);
-                        return {results: results};
+                        return {
+                            results: results
+                        };
                     }
                 },
-                formatResult:function(result, container, query, escapeMarkup){
-                    var markup=[];
+                formatResult: function(result, container, query, escapeMarkup) {
+                    var markup = [];
                     window.Select2.util.markMatch(result.text, query.term, markup, escapeMarkup);
                     var context = result.context_label != '' ? '<i class="concept_result_schemaname">(' + result.context_label + ')</i>' : '';
-                    var formatedresult = '<span class="concept_result">' + markup.join("")  + '</span>' + context;
+                    var formatedresult = '<span class="concept_result">' + markup.join("") + '</span>' + context;
                     return formatedresult;
                 },
-                formatSelection: function(result){
+                formatSelection: function(result) {
                     var context = result.context_label != '' ? '<i class="concept_result_schemaname">(' + result.context_label + ')</i>' : '';
                     var markup = '<span data-filter="external-filter"><i class="fa fa-minus" style="margin-right: 7px;display:none;"></i>' + result.text + '</span>' + context;
-                    if(result.inverted){
+                    if (result.inverted) {
                         markup = '<span data-filter="external-filter"><i class="fa fa-minus inverted" style="margin-right: 7px;"></i>' + result.text + '</span>' + context;
                     }
                     return markup;
                 },
-                escapeMarkup: function(m) { return m; }
+                escapeMarkup: function(m) {
+                    return m;
+                }
             }).on('select2-selecting', function(e, el) {
                 self.trigger('select2-selecting', e, el);
-            }).on('change', function(e, el){
+            }).on('change', function(e, el) {
                 self.trigger('change', e, el);
 
-                if(e.added){
-                    if(e.added.type !== 'filter-flag'){
+                if (e.added) {
+                    if (e.added.type !== 'filter-flag') {
                         self.query.filter.terms.push(e.added);
                     }
 
                 }
-                if(e.removed){
-                    if(e.removed.type === 'filter-flag'){
+                if (e.removed) {
+                    if (e.removed.type === 'filter-flag') {
                         self.trigger('filter-removed', e.removed);
-                    }else{
-                        self.query.filter.terms.remove(function(item){
+                    } else {
+                        self.query.filter.terms.remove(function(item) {
                             return item.id === e.removed.id && item.context_label === e.removed.context_label;
                         });
                     }
@@ -120,21 +124,21 @@ define(['jquery',
                 // instead they listen to choice-selected events and use that
                 if (data.type == 'string' || data.type == 'concept' || data.type == 'term') {
                     self.query.filter.terms.removeAll();
-                    $.each(self.searchbox.select2('data'), function(index, term){
+                    $.each(self.searchbox.select2('data'), function(index, term) {
                         self.query.filter.terms.push(term);
                     });
                 }
-                if (data.type == 'filter-flag'){
+                if (data.type == 'filter-flag') {
                     self.trigger('filter-inverted', data);
                 }
             });
         },
 
-        getUrl: function(){
+        getUrl: function() {
             return arches.urls.search_terms;
         },
 
-        addTag: function(term, inverted){
+        addTag: function(term, inverted) {
             var terms = this.searchbox.select2('data');
             terms.unshift({
                 inverted: inverted,
@@ -149,10 +153,10 @@ define(['jquery',
             this.updateTerms(terms);
         },
 
-        removeTag: function(term){
+        removeTag: function(term) {
             var terms = this.searchbox.select2('data');
-            for (var i=terms.length-1; i>=0; i--) {
-                if(terms[i].id == term && terms[i].text == term && terms[i].value == term){
+            for (var i = terms.length - 1; i >= 0; i--) {
+                if (terms[i].id == term && terms[i].text == term && terms[i].value == term) {
                     terms.splice(i, 1);
                     break;
                 }
@@ -161,7 +165,7 @@ define(['jquery',
             this.updateTerms(terms);
         },
 
-        updateTerms: function(terms){
+        updateTerms: function(terms) {
             this.searchbox.select2('data', terms);
 
             $('.resource_search_widget').find('.select2-search-choice').each(function(i, el) {
@@ -174,11 +178,11 @@ define(['jquery',
             });
         },
 
-        restoreState: function(filter){
+        restoreState: function(filter) {
             var self = this;
-            if(typeof filter !== 'undefined' && filter.length > 0){
+            if (typeof filter !== 'undefined' && filter.length > 0) {
                 var results = [];
-                $.each(filter, function(){
+                $.each(filter, function() {
                     self.query.filter.terms.push(this);
 
                     results.push({
@@ -202,7 +206,7 @@ define(['jquery',
             }
         },
 
-        clear: function(){
+        clear: function() {
             this.query.filter.terms.removeAll();
             this.searchbox.select2('data', []);
         }
