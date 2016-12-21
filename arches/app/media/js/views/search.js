29e6f18d81139652a77c2077067798246376de8e
Rob Gaston <rgaston@fargeo.com>
2016-12-21 14:55:37 -0800

    removing unused code

--- arches/app/media/js/views/search.js
+++ arches/app/media/js/views/search.js
@@ -8,33 +8,15 @@ require([
     'views/search/search-results',
     'views/base-manager'
 ], function($, ko, arches, AlertViewModel, BaseFilter, TermFilter, SearchResults, BaseManagerView) {
-
     var SearchView = BaseManagerView.extend({
-        // updateRequest: '',
-
-        // events: {
-        //     'click #view-saved-searches': 'showSavedSearches',
-        //     'click #clear-search': 'clear',
-        //     'click #map-filter-button': 'toggleMapFilter',
-        //     'click #time-filter-button': 'toggleTimeFilter',
-        //     'click a.dataexport': 'exportSearch'
-        // },
-
         initialize: function(options) {
-            var mapFilterText, timeFilterText;
             var self = this;
 
             this.viewModel.termFilter = new TermFilter({
                 el: $.find('input.resource_search_widget')[0]
             });
-
-            this.viewModel.timeFilter = new BaseFilter({
-                //el: $.find('input.resource_search_widget')[0]
-            });
-
-            this.viewModel.mapFilter = new BaseFilter({
-                //el: $.find('input.resource_search_widget')[0]
-            });
+            this.viewModel.timeFilter = new BaseFilter();
+            this.viewModel.mapFilter = new BaseFilter();
 
             this.filters = [
                 this.viewModel.termFilter,
@@ -43,36 +25,8 @@ require([
             ];
 
             this.viewModel.searchResults = new SearchResults({
-                //el: $('#search-results-container')[0],
                 viewModel: this.viewModel
             });
-            this.viewModel.searchResults.on('mouseover', function(resourceid) {
-                this.viewModel.mapFilter.selectFeatureById(resourceid);
-            }, this);
-            this.viewModel.searchResults.on('mouseout', function() {
-                this.viewModel.mapFilter.unselectAllFeatures();
-            }, this);
-            this.viewModel.searchResults.on('find_on_map', function(resourceid, data) {
-                var extent,
-                    expand = !this.viewModel.mapFilter.expanded();
-                if (expand) {
-                    this.viewModel.mapFilter.expanded(true);
-                }
-
-                _.each(data.geometries, function(geometryData) {
-                    var geomExtent = wkt.readGeometry(geometryData.label).getExtent();
-                    geomExtent = ol.extent.applyTransform(geomExtent, ol.proj.getTransform('EPSG:4326', 'EPSG:3857'));
-                    extent = extent ? ol.extent.extend(extent, geomExtent) : geomExtent;
-                });
-                if (extent) {
-                    _.delay(function() {
-                        self.viewModel.mapFilter.zoomToExtent(extent);
-                    }, expand ? 700 : 0);
-                }
-            }, this);
-
-            // mapFilterText = this.viewModel.mapFilter.$el.data().filtertext;
-            // timeFilterText = this.viewModel.timeFilter.$el.data().filtertext;
 
             self.isNewQuery = true;
 
@@ -123,8 +77,6 @@ require([
                 context: this,
                 success: function(response) {
                     var data = this.viewModel.searchResults.updateResults(response);
-                    // this.viewModel.mapFilter.highlightFeatures(data, $('.search-result-all-ids').data('results'));
-                    // this.viewModel.mapFilter.applyBuffer();
                     this.isNewQuery = false;
                 },
                 error: function(response, status, error) {
@@ -134,37 +86,6 @@ require([
                     this.viewModel.loading(false);
                 }
             });
-        },
-
-        showSavedSearches: function() {
-            this.clear();
-            $('#saved-searches').slideDown('slow');
-            $('#search-results').slideUp('slow');
-            this.viewModel.mapFilter.expanded(false);
-            this.viewModel.timeFilter.expanded(false);
-        },
-
-        hideSavedSearches: function() {
-            $('#saved-searches').slideUp('slow');
-            $('#search-results').slideDown('slow');
-        },
-
-        toggleMapFilter: function() {
-            if ($('#saved-searches').is(":visible")) {
-                this.doQuery();
-                this.hideSavedSearches();
-            }
-            this.viewModel.mapFilter.expanded(!this.viewModel.mapFilter.expanded());
-            window.history.pushState({}, '', '?' + this.queryString());
-        },
-
-        toggleTimeFilter: function() {
-            if ($('#saved-searches').is(":visible")) {
-                this.doQuery();
-                this.hideSavedSearches();
-            }
-            this.viewModel.timeFilter.expanded(!this.viewModel.timeFilter.expanded());
-            window.history.pushState({}, '', '?' + this.queryString());
         },
 
         getSearchQuery: function() {
@@ -196,7 +117,6 @@ require([
 
             if (doQuery) {
                 this.doQuery();
-                this.hideSavedSearches();
             }
 
         },
