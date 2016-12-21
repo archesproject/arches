1a8ee3fcf026e5a2fd8711977dbebc24de51d567
Rob Gaston <rgaston@fargeo.com>
2016-12-21 14:59:29 -0800

    cleaning up base-filter

--- arches/app/media/js/views/search/base-filter.js
+++ arches/app/media/js/views/search/base-filter.js
@@ -1,32 +1,29 @@
 define(['jquery', 'backbone', 'knockout'], function($, Backbone, ko) {
     return Backbone.View.extend({
-        filter: {
-            // the various filters managed by this search filter widget
-        },
+        // the various filters managed by this search filter widget
+        filter: {},
 
         initialize: function(options) {
             $.extend(this, options);
         },
 
-        hasFilters: function() {
-            // returns a boolean true if this filter is currently active, else false
-        },
-
+        // append your filters onto this object which ultimately is used in the URL
+        // return true if filters are added
         appendFilters: function(queryStringObject) {
-            // append your filters onto this object which ultimately is used in the URL
-            // return true if filters are added
             return false;
         },
 
+        // a function to drive the state of the ui based on
+        // the filter object passed into the function
+        // the filter objects should take the same form as this.filter
         restoreState: function(filter) {
-            // a function to drive the state of the ui based on
-            // the filter object passed into the function
-            // the filter objects should take the same form as this.query.filter
+            return;
         },
 
+        // a function that clears the filter and restores
+        // the ui to a clean and unfiltered state
         clear: function() {
-            // a function that clears the filter and restores
-            // the ui to a clean and unfiltered state
+            return;
         }
     });
 });
