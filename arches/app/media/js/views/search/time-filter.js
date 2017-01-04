c4cb678e73feda2eab8d8b8326c8597b73614966
Rob Gaston <rgaston@fargeo.com>
2017-01-04 12:55:07 -0800

    more work on time filter re: #1326

--- arches/app/media/js/views/search/time-filter.js
+++ arches/app/media/js/views/search/time-filter.js
@@ -1,10 +1,35 @@
 define([
+    'knockout',
     'views/search/base-filter',
+    'bindings/datepicker',
     'bindings/chosen'
 ],
-function(BaseFilter) {
+function(ko, BaseFilter) {
     return BaseFilter.extend({
         initialize: function(options) {
+            // var updatingDateRange = false;
+            this.fromDate = ko.observable(null);
+            this.toDate = ko.observable(null);
+            this.dateRangeType = ko.observable('custom');
+            // this.dateRangeType = ko.pureComputed({
+            //     read: function() {
+            //         return 'today';
+            //     },
+            //     write: function(value) {
+            //         // set date range value based on selection
+            //     },
+            //     owner: this
+            // });
+            // this.dateRangeType.subscribe(function(value) {
+            //     if (value !== 'custom'){
+            //         updatingDateRange = true;
+            //         // TODO: update from & to dates
+            //
+            //     }
+            //     updatingDateRange = false;
+            // });
+
+
             BaseFilter.prototype.initialize.call(this, options);
         },
 
