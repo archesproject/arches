5b6ca88912b0129669bfaec085258bc82b3afb5a
Cyrus Hiatt <chiatt@fargeo.com>
2017-01-05 10:19:53 -0800

    added a feature collection to search filter params, re #1330

--- arches/app/media/js/views/search/map-filter.js
+++ arches/app/media/js/views/search/map-filter.js
@@ -1,11 +1,41 @@
 define([
+    'knockout',
     'views/search/base-filter',
     'widgets/map'
 ],
-function(BaseFilter) {
+function(ko, BaseFilter) {
     return BaseFilter.extend({
         initialize: function(options) {
             BaseFilter.prototype.initialize.call(this, options);
+            this.filter.feature_collection = ko.observable({
+                  "type": "FeatureCollection",
+                  "features": []
+                });
+        },
+
+        restoreState: function(query) {
+            var doQuery = false;
+            if ('mapFilter' in query) {
+                query.mapFilter = JSON.parse(query.mapFilter);
+                if (query.mapFilter.length > 0) {
+                    this.filter.feature_collection(query.mapFilter);
+                }
+                doQuery = true;
+            }
+            return doQuery;
+        },
+
+        clear: function() {
+            this.filter.feature_collection({
+                  "type": "FeatureCollection",
+                  "features": []
+                });
+        },
+
+        appendFilters: function(filterParams) {
+            filterParams.mapFilter = ko.toJSON(this.filter.feature_collection());
+            return this.filter.feature_collection().features.length === 0;
         }
+
     });
 });
