b407e31dbf1b1138a56b6467cbc40397002235ca
Ryan Anderson <randerson@fargeo.com>
2017-02-03 11:32:12 -0800

    Change TopConcept label in graphs and associated code to rdmCollection.

--- arches/app/media/js/views/graph/datatypes/concept.js
+++ arches/app/media/js/views/graph/datatypes/concept.js
@@ -2,7 +2,7 @@ define(['arches', 'knockout'], function (arches, ko) {
     var name = 'concept-datatype-config';
     ko.components.register(name, {
         viewModel: function(params) {
-            this.topConcept = params.config.topConcept;
+            this.topConcept = params.config.rdmCollection;
             this.conceptCollections = arches.conceptCollections;
             this.conceptCollections.unshift({
               'label': null,
