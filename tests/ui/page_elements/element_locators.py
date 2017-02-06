3e7798dbc5633cf3e894bce9f16bb0482fdb48ae
Jeffrey Munowitch <jmunowitch@gmail.com>
2017-02-06 10:29:36 -0800

    1565 selenium test broken (#1587)
    * Fixing Locators
    
    * Fix Map UI Tests
    
    * Enable Saucelabs
    
    * saucelabs break points
    
    * remove saucelabs debug
    
    * disable saucelabs
    
    * remove remote_browsers

--- tests/ui/page_elements/element_locators.py
+++ tests/ui/page_elements/element_locators.py
@@ -12,7 +12,7 @@ class MapWidgetLocators(BaseWidgetLocators):
     MAP_TOOLS_BASEMAPS = (By.XPATH, "//*[@id='map-widget-toolbar']/ul/li[1]")
     SATELLITE_BASE_MAP = (By.XPATH, "//*[@id='map-widget-basemaps']/div[3]")
     MAP_TOOLS_OVERLAYS = (By.ID, "btn-overlays")
-    OVERLAY_LIBRARY_BUTTON = (By.XPATH, "//*[@id='overlays-panel']/div[1]/h4/i")
+    OVERLAY_LIBRARY_BUTTON = (By.XPATH, "//*[@id='overlays-panel']/div[1]/h4")
     OVERLAY_TO_ADD = (By.XPATH, "//*[@id='overlay-grid']/div[1]")
     ADDED_OVERLAYS = (By.CSS_SELECTOR, "#overlays-panel .map-widget-overlay-item .map-overlay-name")
     MAP_CANVAS = (By.CSS_SELECTOR, ".mapboxgl-canvas")
@@ -22,6 +22,6 @@ class MapWidgetLocators(BaseWidgetLocators):
 class StringWidgetLocators(BaseWidgetLocators):
     def __init__(self):
         super(StringWidgetLocators, self).__init__()
-                      
+
     #INPUT_BOX = (By.XPATH, "//*[@id='content-container']/div[2]/div[4]/div/div[2]/div/div/div[2]/div/div/div/div/div/div[2]/div/form/div/div/div/div/input")
     INPUT_BOX = (By.XPATH, "./div/div/div/input")
