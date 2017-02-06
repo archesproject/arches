4111dc168ebca5d1fc9996f18d3e458a53021673
Cyrus Hiatt <chiatt@fargeo.com>
2017-02-06 08:03:52 -0800

    catching empty tile request error

--- arches/app/views/tileserver.py
+++ arches/app/views/tileserver.py
@@ -80,18 +80,24 @@ def handle_request(request):
 
     config = TileStache.Config.buildConfiguration(config_dict)
     path_info = request.path.replace('/tileserver/', '')
-    query_string = None
-    script_name = None
-
-    status_code, headers, content = TileStache.requestHandler2(
-        config, path_info, query_string, script_name)
-
+    query_string = ""
+    script_name = ""
     response = HttpResponse()
-    response.content = content
-    response.status_code = status_code
-    for header, value in headers.items():
-        response[header] = value
-    response['Content-length'] = str(len(content))
+
+    try:
+        status_code, headers, content = TileStache.requestHandler2(
+            config, path_info, query_string, script_name)
+
+        response.content = content
+        response.status_code = status_code
+        for header, value in headers.items():
+            response[header] = value
+
+        response['Content-length'] = str(len(content))
+
+    except Exception as e:
+        print 'No tile data', e, response
+
     return response
 
 
