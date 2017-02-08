c853b90e343b8a47ba78469d0d688542f0dd2b4b
Cyrus Hiatt <chiatt@fargeo.com>
2017-02-07 18:36:57 -0800

    moved import and export data transformations into datatype methods and created a base concept type from which concept types inherit, re #1541

--- arches/app/datatypes/base.py
+++ arches/app/datatypes/base.py
@@ -3,5 +3,22 @@ class BaseDataType(object):
     def __init__(self, model=None):
         self.datatype_model = model
 
-    def append_to_document(self, document, nodetype):
-        print 'passing'
+    def append_to_document(self, document, nodevalue):
+        """
+        Assigns a given node value to the corresponding key in a document in
+        in preparation to index the document
+        """
+        pass
+
+    def transform_import_values(self, value):
+        """
+        Transforms values from probably string/wkt representation to specified datatype in arches
+        """
+        return value
+
+
+    def transform_export_values(self, value):
+        """
+        Transforms values from probably string/wkt representation to specified datatype in arches
+        """
+        return value
