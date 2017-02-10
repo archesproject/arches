12a1638dc88d32206fdfee446b563c5cb57e548d
Ryan Anderson <randerson@fargeo.com>
2017-02-10 12:38:51 -0800

    Change dev instructions to import smaller dataset.

--- arches/install/install_arches.sh
+++ arches/install/install_arches.sh
@@ -28,7 +28,7 @@ python manage.py packages -o setup_db
 python manage.py packages -o import_reference_data -s arches/db/schemes/arches_concept_scheme.rdf -ow overwrite -st keep
 python manage.py packages -o import_reference_data -s arches/db/schemes/dropdowns.rdf -ow overwrite -st keep
 python manage.py packages -o import_graphs
-python manage.py packages -o import_business_data -s tests/fixtures/data/csv/heritage_resource_model_1000.csv -c tests/fixtures/data/csv/heritage_resource_model.mapping
+python manage.py packages -o import_business_data -s tests/fixtures/data/csv/heritage_resource_model.csv
 python manage.py packages -o add_tileserver_layer -m arches/tileserver/hillshade.xml -n hillshade
 python manage.py packages -o add_tileserver_layer -m arches/tileserver/world.xml -n world
 
