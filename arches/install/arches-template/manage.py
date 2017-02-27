8c26563a8ffd043a22bc0c0970f2db539613eb70
Cyrus <cyrus@fargeo.com>
2017-02-27 14:10:19 -0800

    added command to unregister function and started work on moving arches-app utility to arches-project, re 1662

--- arches/install/arches-template/manage.py
+++ arches/install/arches-template/manage.py
@@ -16,14 +16,14 @@ You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 '''
 
+
 #!/usr/bin/env python
 import os
 import sys
-from django.core.management import execute_from_command_line
 
 if __name__ == "__main__":
-
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ app_name }}.settings")
 
-    execute_from_command_line(sys.argv)
+    from django.core.management import execute_from_command_line
 
+    execute_from_command_line(sys.argv)
