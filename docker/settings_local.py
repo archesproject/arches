c0e455b68ada5c65cfd8a82d8a096b18a3436f16
Vincent <meijer.vincent@gmail.com>
2017-03-02 13:05:44 -0500

    Add site-packages to STATICFILES_DIRS as quick fix for bower_components not being found in site-packages dir, re#1650

--- docker/settings_local.py
+++ docker/settings_local.py
@@ -16,7 +16,9 @@ def get_env_variable(var_name):
 MODE = get_env_variable('DJANGO_MODE') #options are either "PROD" or "DEV" (installing with Dev mode set, get's you extra dependencies)
 DEBUG = ast.literal_eval(get_env_variable('DJANGO_DEBUG'))
 
-STATIC_ROOT = '/static_root'
+STATICFILES_DIRS = STATICFILES_DIRS + (
+    os.path.join(get_env_variable('WEB_ROOT'), 'ENV', 'lib', 'python2.7', 'site-packages', 'arches', 'app', 'media'),
+)
 
 DATABASES = {
     'default': {
