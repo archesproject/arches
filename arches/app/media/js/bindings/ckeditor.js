c42e4e0068459af8a392774f92a4f59df918c0fc
Alexei Peters <apeters@fargeo.com>
2017-04-06 15:20:54 -0700

    remove references to the summernote binding, re #1898

--- arches/app/media/js/bindings/ckeditor.js
+++ arches/app/media/js/bindings/ckeditor.js
@@ -6,13 +6,13 @@ define([
     'ckeditor',
 ], function ($, _, ko) {
     /**
-    * A knockout.js binding for the "summernote" rich text editor widget
-    * - pass options to summernote using the following syntax in the knockout
+    * A knockout.js binding for the "ckeditor" rich text editor widget
+    * - pass options to ckeditor using the following syntax in the knockout
     * data-bind attribute
     * @example
-    * summernote: {height: 250}
+    * ckeditor: {height: 250}
     * @constructor
-    * @name summernote
+    * @name ckeditor
     */
     ko.bindingHandlers.ckeditor = {
         init: function (element, valueAccessor) {
@@ -20,8 +20,6 @@ define([
             var modelValue = valueAccessor();
             var value = ko.utils.unwrapObservable(valueAccessor());
             var $element = $(element);
-            var changedByUI = false;
-            var setByData = false
      
             // Set initial value and create the CKEditor
             $element.html(value);
@@ -31,13 +29,11 @@ define([
             var onChange = function (e) {
                 var self = this;
 
-                if (!setByData && ko.isWriteableObservable(self)) {
-                    changedByUI = true;
+                if (ko.isWriteableObservable(self)) {
                     var newValue = $(e.listenerData).val();
                     if (!((self() === null || self() === "") && (newValue === null || newValue === ""))) {
                         self(newValue);
                     }
-                    changedByUI = false;
                 }
                 return true;
             };
@@ -47,38 +43,21 @@ define([
                 var self = this;
                 var $element = $(element);
                 var newValue = ko.utils.unwrapObservable(valueAccessor());
-                if (!changedByUI && $element.ckeditorGet().getData() != newValue) {
-                    setByData = true;
-                    if (newValue === null){
-                        editor.removeListener( 'change', onChange );
-                    }
+                if ($element.ckeditorGet().getData() != newValue) {
+                    // remove the listener and then add back to prevent `setData` 
+                    // from triggering the onChange event
+                    editor.removeListener('change', onChange );
                     $element.ckeditorGet().setData(newValue);
-                    if (newValue === null){
-                       editor.on('change', onChange, modelValue, element);
-                    }
-                    setByData = false;
+                    editor.on('change', onChange, modelValue, element);
                 }
             }, this)
      
      
-            /* Handle disposal if KO removes an editor 
-             * through template binding */
-            ko.utils.domNodeDisposal.addDisposeCallback(element, 
-                function () {
-                    editor.updateElement();
-                    editor.destroy();
-                });
-        },
- 
-        /* Hook and handle the binding updating so we write 
-         * back to the observable */
-        update: function (element, valueAccessor) {
-            // var self = this;
-            // var $element = $(element);
-            // var newValue = ko.utils.unwrapObservable(valueAccessor());
-            // if ($element.ckeditorGet().getData() != newValue) {
-            //     $element.ckeditorGet().setData(newValue);
-            // }
+            // Handle disposal if KO removes an editor through template binding
+            ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+                editor.updateElement();
+                editor.destroy();
+            });
         }
     };
 
