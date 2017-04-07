3a29c0874d083a6cc4b20429ae147ed74308eb4d
Vincent <meijer.vincent@gmail.com>
2017-04-07 09:00:27 -0400

    Remove remote debugging commands from manage.py, accidentally checked this in, re #1650

--- manage.py
+++ manage.py
@@ -24,8 +24,5 @@ from django.core.management import execute_from_command_line
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "arches.settings")
 
-    import ptvsd
-    ptvsd.enable_attach("my_secret", address=('0.0.0.0', 3000))
-
     execute_from_command_line(sys.argv)
 
