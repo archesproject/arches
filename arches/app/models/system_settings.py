9e63f4d301f229115e98b947aeee5cae1cc186f7
Alexei Peters <apeters@fargeo.com>
2017-04-24 10:40:13 -0700

    start work on making nested settings work properly, e.g.: SOME_SETTING=[{key: value1}, {key: value2}], re #1631

--- arches/app/models/system_settings.py
+++ arches/app/models/system_settings.py
@@ -48,7 +48,7 @@ class SystemSettings(object):
 
     def __init__(self, *args, **kwargs):
         self.cache_settings()
-        #print self
+        print self
 
     def __str__(self):
         ret = []
@@ -75,22 +75,92 @@ class SystemSettings(object):
         # load all the settings from settings.py
         for setting in dir(core_settings):
             cls.settings[setting] = getattr(core_settings, setting)
-            if not setting.startswith('__') and not callable(getattr(core_settings,setting)):
-                setattr(cls, setting, getattr(core_settings, setting))
+            # if not setting.startswith('__') and not callable(getattr(core_settings,setting)):
+            #     setattr(cls, setting, getattr(core_settings, setting))
 
         # get all the possible settings defined by the Arches System Settings Graph
         for node in models.Node.objects.filter(graph_id=cls.graph_id):
-            if node.datatype != 'semantic':
-                cls.settings[node.name] = None
-                setattr(cls, node.name, None)
+            # if node.datatype != 'semantic':
+            #     cls.settings[node.name] = None
+            #     setattr(cls, node.name, None)
+            def setup_node(node, parent_node=None):
+                if node.is_collector:
+                    if node.nodegroup.cardinality == '1':
+                        obj = {}
+                        for decendant_node in cls.get_direct_decendent_nodes(node):
+                            # setup_node(decendant_node)
+                            # if decendant_node.name not in cls.settings:
+                            #     cls.settings[decendant_node.name] = None
+                            #     setattr(cls, decendant_node.name, None)
+                            obj[decendant_node.name] = setup_node(decendant_node, node)
+
+                        cls.settings[node.name] = obj
+                        #setattr(cls, node.name, obj)
+
+                    if node.nodegroup.cardinality == 'n':
+                        cls.settings[node.name] = []
+                        #setattr(cls, node.name, [])
+                    return cls.settings[node.name]
+
+                if parent_node is not None:
+                    cls.settings[node.name] = None
+                    #setattr(cls, node.name, None)
+
+            setup_node(node)
 
         # set any values saved in the instance of the Arches System Settings Graph 
         for tile in models.TileModel.objects.filter(resourceinstance__graph_id=cls.graph_id):
-            for node in tile.nodegroup.node_set.all():
-                if node.datatype != 'semantic':
-                    try:
-                        val = tile.data[str(node.nodeid)]
-                        cls.settings[node.name] = val
-                        setattr(cls, node.name, val)
-                    except:
-                        pass
+            if tile.nodegroup.cardinality == '1':
+                for node in tile.nodegroup.node_set.all():
+                    if node.datatype != 'semantic':
+                        try:
+                            val = tile.data[str(node.nodeid)]
+                            cls.settings[node.name] = val
+                            #setattr(cls, node.name, val)
+                        except:
+                            pass
+
+            if tile.nodegroup.cardinality == 'n':
+                obj = {}
+                collector_nodename = ''
+                for node in tile.nodegroup.node_set.all():
+                    print "%s: %s" % (node.name,node.is_collector)
+                    if node.is_collector:
+                        collector_nodename = node.name
+                    if node.datatype != 'semantic':
+                        obj[node.name] = tile.data[str(node.nodeid)]
+                    # try:
+                    #     #val = tile.data[str(node.nodeid)]
+                    #     obj[node.name] = tile.data[str(node.nodeid)]
+                    #     # cls.settings[node.name] = val
+                    #     # setattr(cls, node.name, val)
+                    # except:
+                    #     pass
+
+                print collector_nodename
+                print obj
+
+                cls.settings[collector_nodename].append(obj)
+                #setattr(cls, collector_nodename, ret)
+
+        for key, setting in cls.settings.iteritems():
+            if not key.startswith('__') and not callable(cls.settings[key]):
+                setattr(cls, key, setting)
+
+        print cls
+
+
+    @classmethod
+    def get_direct_decendent_nodes(cls, node):
+        nodes = []
+        for edge in models.Edge.objects.filter(domainnode=node):
+            nodes.append(edge.rangenode)
+        return nodes
+
+    # @classmethod
+    # def serialize(cls):
+    #     nodes = models.Node.objects.filter(graph_id=cls.graph_id)
+    #     edges = models.Edge.objects.filter(graph_id=cls.graph_id)
+    #     for node in 
+
+
