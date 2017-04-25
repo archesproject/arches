62b35f38fec06144a70e5af6440a774cfdfd8a16
Cyrus Hiatt <chiatt@fargeo.com>
2017-04-25 10:22:27 -0700

    Added IIIF widget and datatype insert statements

--- arches/app/models/migrations/0002.py
+++ arches/app/models/migrations/0002.py
@@ -108,8 +108,17 @@ class Migration(migrations.Migration):
 
     migrations.RunSQL(
             """
+            INSERT INTO widgets(widgetid, name, component, datatype, defaultconfig)
+                VALUES ('10000000-0000-0000-0000-000000000022', 'iiif-widget', 'views/components/widgets/iiif', 'iiif-drawing', '{
+                        "placeholder": "",
+                        "options": [],
+                        "nameLabel": "Name",
+                        "typeLabel": "Type"
+                    }'
+                );
             INSERT INTO widgets(widgetid, name, component, datatype, defaultconfig) VALUES ('10000000-0000-0000-0000-000000000020', 'csv-chart-widget', 'views/components/widgets/csv-chart', 'csv-chart-json', '{"acceptedFiles": "", "maxFilesize": "200"}');
             INSERT INTO d_data_types VALUES ('csv-chart-json', 'fa fa-line-chart', 'datatypes.py', 'CSVChartJsonDataType', null, null, null, FALSE, '10000000-0000-0000-0000-000000000020');
+            INSERT INTO d_data_types VALUES ('iiif-drawing', 'fa fa-file-code-o', 'datatypes.py', 'IIIFDrawingDataType', '{"rdmCollection": null}', 'views/graph/datatypes/concept', 'concept-datatype-config', FALSE, '10000000-0000-0000-0000-000000000022');
             UPDATE d_data_types SET (modulename) = ('datatypes.py') WHERE datatype in ('domain-value', 'domain-value-list');
             """
             ),
