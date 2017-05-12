952ac8190bf76b8f0f39b5060a5a0c929d3c71f6
Alexei Peters <apeters@fargeo.com>
2017-05-12 11:03:19 -0700

    improve “Like” and “Equals” string query logic, re #2035

--- arches/app/datatypes/datatypes.py
+++ arches/app/datatypes/datatypes.py
@@ -66,8 +66,8 @@ class StringDataType(BaseDataType):
     def append_search_filters(self, value, node, query, request):
         try:
             if value['val'] != '':
-                fuzziness = 'AUTO' if '~' in value['op'] else 0
-                match_query = Match(field='tiles.data.%s' % (str(node.pk)), query=value['val'], type='phrase_prefix', fuzziness=fuzziness)
+                match_type = 'phrase_prefix' if '~' in value['op'] else 'phrase'
+                match_query = Match(field='tiles.data.%s' % (str(node.pk)), query=value['val'], type=match_type)
                 if '!' in value['op']:
                     query.must_not(match_query)
                 else:
