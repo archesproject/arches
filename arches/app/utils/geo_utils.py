bffa64f04b851560e20b673ba70ec70af9a76f56
Cyrus Hiatt <chiatt@fargeo.com>
2017-05-23 17:28:11 -0700

    adding geo_utils, re #346

---
+++ arches/app/utils/geo_utils.py
@@ -0,0 +1,17 @@
+from django.contrib.gis.geos import GEOSGeometry, GeometryCollection
+from arches.app.utils.betterJSONSerializer import JSONSerializer, JSONDeserializer
+
+
+class GeoUtils(object):
+
+    def get_bounds_from_geojson(self, geojson):
+        """
+        Takes a geojson object with polygon(s) and returns the coordinates of
+        the extent of the polygons.
+
+        """
+        polygons = []
+        for feature in geojson['features']:
+            if feature['geometry']['type'] == 'Polygon':
+                polygons.append(GEOSGeometry(JSONSerializer().serialize(feature['geometry'])))
+        return GeometryCollection(polygons).extent
