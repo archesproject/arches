26070fef003ab4d0977de80d10c19e07069dfebd
Cyrus Hiatt <chiatt@fargeo.com>
2017-05-25 10:30:21 -0700

    Remove geospatial settings from search, added geocoder settings to map settings, consolidated maps in maps settings to a single map input. re #346

--- arches/app/utils/context_processors.py
+++ arches/app/utils/context_processors.py
@@ -36,7 +36,7 @@ def map_info(request):
             'map_min_zoom': settings.MAP_MIN_ZOOM,
             'map_max_zoom': settings.MAP_MAX_ZOOM,
             'mapbox_api_key': settings.MAPBOX_API_KEY,
-            'hex_bin_size': settings.HEX_BIN_SIZE,
+            'hex_bin_size': settings.HEX_BIN_SIZE if settings.HEX_BIN_SIZE != None else 100,
             'mapbox_sprites': settings.MAPBOX_SPRITES,
             'mapbox_glyphs': settings.MAPBOX_GLYPHS,
             'hex_bin_bounds': GeoUtils().get_bounds_from_geojson(settings.HEX_BIN_BOUNDS),
