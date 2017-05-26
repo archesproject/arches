e0d95b82f67bca433fa36c16002861f4489e597f
Alexei Peters <apeters@fargeo.com>
2017-05-25 17:09:18 -0700

    stub out permission settings form, #re 1115

--- arches/app/media/js/views/graph/permission-manager.js
+++ arches/app/media/js/views/graph/permission-manager.js
@@ -6,8 +6,9 @@ require([
     'views/graph/graph-page-view', 
     'views/graph/permission-manager/users-and-groups-list',
     'views/graph/permission-manager/grouped-node-list',
+    'views/graph/permission-manager/permission-settings-form',
     'permission-manager-data'
-], function($, _, ko, arches, GraphPageView, UsersAndGroupsList, GroupedNodeList, data) {
+], function($, _, ko, arches, GraphPageView, UsersAndGroupsList, GroupedNodeList, PermissionSettingsForm, data) {
     /**
     * set up the page view model with the graph model and related sub views
     */
---
+++ arches/app/media/js/views/graph/permission-manager/permission-settings-form.js
@@ -0,0 +1,65 @@
+define([
+    'backbone',
+    'knockout',
+], function(Backbone,  ko) {
+    var PermissionSettingsForm = Backbone.View.extend({
+        /**
+        * A backbone view representing a card component form
+        * @augments Backbone.View
+        * @constructor
+        * @name PermissionSettingsForm
+        */
+
+        /**
+        * Initializes the view with optional parameters
+        * @memberof PermissionSettingsForm.prototype
+        * @param {boolean} options.selection - the selected item, either a {@link CardModel} or a {@link NodeModel}
+        */
+        initialize: function(options) {
+            // var self = this;
+            // _.extend(this, _.pick(options, 'card'));
+            // this.selection = options.selection || ko.observable(this.card);
+            // this.helpPreviewActive = options.helpPreviewActive || ko.observable(false);
+            // this.card = ko.observable();
+            // this.widget = ko.observable();
+            // this.graph = options.graphModel;
+            // this.widgetLookup = widgets;
+            // this.widgetList = function() {
+            //     var cardWidget = self.widget();
+            //     if (cardWidget) {
+            //         var widgets = _.map(self.widgetLookup, function(widget, id) {
+            //             widget.id = id;
+            //             return widget;
+            //         });
+            //         return _.filter(widgets, function(widget) {
+            //             return widget.datatype === cardWidget.datatype.datatype
+            //         });
+            //     } else {
+            //         return [];
+            //     }
+            // };
+
+            // this.updateSelection = function(selection) {
+            //     if('isContainer' in selection){
+            //         this.card(selection);
+            //     }
+            //     if('node' in selection){
+            //         this.widget(selection);
+            //     }
+            // };
+
+            // this.selection.subscribe(function (selection) {
+            //     this.helpPreviewActive(false);
+            //     this.updateSelection(selection);
+            // }, this);
+
+            // this.updateSelection(this.selection());
+
+            // this.permissionsList = new PermissionsList({
+            //     card: this.card,
+            //     permissions: options.permissions
+            // });
+        }
+    });
+    return PermissionSettingsForm;
+});
