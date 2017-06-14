629b8b47c12405c6f447f2fb294e5ad390f076ef
azerbini <andrea.zerbini@arch.ox.ac.uk>
2017-06-14 15:28:21 +0100

    Revert "Set originalItem = null when initialising the branch-list view model"
    This reverts commit f725eaae992ec22e6773c3f3751640169b9dd0a0.

--- arches/app/media/js/views/forms/sections/branch-list.js
+++ arches/app/media/js/views/forms/sections/branch-list.js
@@ -13,8 +13,7 @@ define(['jquery',
         initialize: function(options) {
             var self = this;
             this.singleEdit = false;
-            this.originalItem = null;
-            
+
             _.extend(this, options);
 
             this.defaults = [];
