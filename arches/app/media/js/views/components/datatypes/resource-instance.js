eadd3826fec65c988584111992362d16a4812314
Rob Gaston <rgaston@fargeo.com>
2017-07-31 11:17:13 -0700

    allowing selection of resource model #2305

--- arches/app/media/js/views/components/datatypes/resource-instance.js
+++ arches/app/media/js/views/components/datatypes/resource-instance.js
@@ -1,11 +1,19 @@
-define(['knockout'], function (ko) {
+define([
+    'knockout',
+    'arches'
+], function (ko, arches) {
     var name = 'resource-instance-datatype-config';
     ko.components.register(name, {
         viewModel: function(params) {
             var self = this;
+            this.resourceModels = [{
+                graphid: null,
+                name: ''
+            }].concat(arches.resources);
+            this.config = params.config;
             this.search = params.search;
-            if (this.search) {
-
+            if (!this.search) {
+                this.isEditable = params.graph ? params.graph.get('is_editable') : true;
             }
         },
         template: { require: 'text!datatype-config-templates/resource-instance' }
