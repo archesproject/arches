72b76f1d81c4d8549b6bae5530d5181857e01179
Alexei Peters <apeters@fargeo.com>
2017-08-01 18:10:37 -0700

    initial work on a new style ontology loader using the rdflib package, re #2306

--- arches/management/commands/load_ontology.py
+++ arches/management/commands/load_ontology.py
@@ -26,6 +26,8 @@ from django.core.management.base import BaseCommand, CommandError
 from django.core.files import File
 from django.db import transaction
 from arches.app.models import models
+from rdflib import *  
+from rdflib.resource import Resource   
 
 class Command(BaseCommand):
     """
@@ -80,142 +82,232 @@ class Command(BaseCommand):
         if data_source is not None and version is not None:
             self.ontology_class_graph = nx.DiGraph()
             self.ontology_property_graph = nx.MultiDiGraph()
+            self.graph = Graph()
             
             with transaction.atomic():
                 ontology = self.add_ontology(id=id, data_source=data_source, version=version, name=name)
-                loaded_extensions = [extension.path.path for extension in models.Ontology.objects.filter(parentontology=ontology)]
+                print ontology
+                # loaded_extensions = [extension.path.path for extension in models.Ontology.objects.filter(parentontology=ontology)]
 
-                if extensions is None:
-                    extensions = loaded_extensions
-                else:
-                    extensions = extensions.split(',') + loaded_extensions
+                # if extensions is None:
+                #     extensions = loaded_extensions
+                # else:
+                #     extensions = extensions.split(',') + loaded_extensions
 
-                for extension in extensions:
-                    if extension:
-                        self.add_ontology(data_source=extension, version=version, name=name, parentontology=ontology)
+                # for extension in extensions:
+                #     if extension:
+                #         print 'haserasdf'
+                #         self.add_ontology(data_source=extension, version=version, name=name, parentontology=ontology)
 
-                for ontology_class, data in self.crawl_graph().iteritems():
-                    models.OntologyClass.objects.update_or_create(source=ontology_class, ontology=ontology, defaults={'target': data})
+                print len(self.graph)
+                print len(set(self.graph.triples((None, None, RDF.Property))))
+                # for subject in self.graph.subjects('rdfs:Class'):
+                #     print subject
+                self.crawl_graph_new()
+                # for ontology_class, data in self.crawl_graph_new().iteritems():
+                #     pass
+                    #models.OntologyClass.objects.update_or_create(source=ontology_class, ontology=ontology, defaults={'target': data})
+                # for ontology_class, data in self.crawl_graph().iteritems():
+                #     models.OntologyClass.objects.update_or_create(source=ontology_class, ontology=ontology, defaults={'target': data})
     
     def add_ontology(self, id=None, data_source=None, version=None, name=None, parentontology=None):
-        filepath = os.path.split(os.path.abspath(data_source))[0]
-        filename = os.path.split(data_source)[1]
-        if name is None:
-            name = os.path.splitext(filename)[0]
-        self.parse_xml(data_source)
-        if models.get_ontology_storage_system().location in filepath:
-            # if the file we're referencing already exists in the location where we 
-            # usually store them then leave it there and just save a reference to it
-            path = '.%s' % os.path.join(filepath.replace(models.get_ontology_storage_system().location, ''), filename)
-        else:
-            path = File(open(data_source))
+        self.graph.parse(data_source)
+        
+        # filepath = os.path.split(os.path.abspath(data_source))[0]
+        # filename = os.path.split(data_source)[1]
+        # if name is None:
+        #     name = os.path.splitext(filename)[0]
+        # if models.get_ontology_storage_system().location in filepath:
+        #     # if the file we're referencing already exists in the location where we 
+        #     # usually store them then leave it there and just save a reference to it
+        #     path = '.%s' % os.path.join(filepath.replace(models.get_ontology_storage_system().location, ''), filename)
+        # else:
+        #     path = File(open(data_source))
 
-        ontology, created = models.Ontology.objects.get_or_create(path=path, parentontology=parentontology, defaults={'version': version, 'name': name, 'path': path, 'pk': id})
-        return ontology
+        # ontology, created = models.Ontology.objects.get_or_create(path=path, parentontology=parentontology, defaults={'version': version, 'name': name, 'path': path, 'pk': id})
+        # return ontology
 
-    def parse_xml(self, filepath):
-        """
-        parses the xml file into a dictionary object keyed off of the ontology class name found in the rdf:about attributes
-        object values are dictionaries with 2 properties, 'down' and 'up' and within each of those another 2 properties, 
-        'ontology_property' and 'ontology_classes'
 
-        "down" assumes a known domain class, while "up" assumes a known range class
 
-        .. code-block:: python
+    # def parse_xml(self, filepath):
+    #     """
+    #     parses the xml file into a dictionary object keyed off of the ontology class name found in the rdf:about attributes
+    #     object values are dictionaries with 2 properties, 'down' and 'up' and within each of those another 2 properties, 
+    #     'ontology_property' and 'ontology_classes'
 
-            "down":[
-                {
-                    "ontology_property": "P1_is_identified_by",
-                    "ontology_classes": [
-                        "E51_Contact_Point",
-                        "E75_Conceptual_Object_Appellation",
-                        "E42_Identifier",
-                        "E45_Address",
-                        "E41_Appellation",
-                        ....
-                    ]
-                }
-            ]
-            "up":[
-                    "ontology_property": "P1i_identifies",
-                    "ontology_classes": [
-                        "E51_Contact_Point",
-                        "E75_Conceptual_Object_Appellation",
-                        "E42_Identifier"
-                        ....
-                    ]
-                }
-            ]
+    #     "down" assumes a known domain class, while "up" assumes a known range class
 
-        Keyword Arguments:
-        filepath -- the path to the file to parse
+    #     .. code-block:: python
 
-        """
+    #         "down":[
+    #             {
+    #                 "ontology_property": "P1_is_identified_by",
+    #                 "ontology_classes": [
+    #                     "E51_Contact_Point",
+    #                     "E75_Conceptual_Object_Appellation",
+    #                     "E42_Identifier",
+    #                     "E45_Address",
+    #                     "E41_Appellation",
+    #                     ....
+    #                 ]
+    #             }
+    #         ]
+    #         "up":[
+    #                 "ontology_property": "P1i_identifies",
+    #                 "ontology_classes": [
+    #                     "E51_Contact_Point",
+    #                     "E75_Conceptual_Object_Appellation",
+    #                     "E42_Identifier"
+    #                     ....
+    #                 ]
+    #             }
+    #         ]
 
-        ns = {
-            'RDF': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
-            'RDFS': 'http://www.w3.org/2000/01/rdf-schema#'
-        }
+    #     Keyword Arguments:
+    #     filepath -- the path to the file to parse
 
-        root = ET.parse(filepath).getroot()
-        # populates a graph with class/subclass relations
-        for concept in root.findall('RDFS:Class', ns):
-            self.ontology_class_graph.add_node(self.get_attr(concept, ns, 'RDF:about'))
-            for subclass in concept.findall('RDFS:subClassOf', ns):
-                self.ontology_class_graph.add_edge(self.get_attr(subclass, ns, 'RDF:resource'), self.get_attr(concept, ns, 'RDF:about'))
+    #     """
 
-        # populates a graph with class/property/class relations
-        for concept in root.findall('RDF:Property', ns):
-            domain = self.get_attr(concept.find('RDFS:domain', ns), ns, 'RDF:resource')
-            range = self.get_attr(concept.find('RDFS:range', ns), ns, 'RDF:resource')
-            if domain and range:
-                self.ontology_property_graph.add_edge(domain, range, name=self.get_attr(concept, ns, 'RDF:about'))
+    #     ns = {
+    #         'RDF': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+    #         'RDFS': 'http://www.w3.org/2000/01/rdf-schema#'
+    #     }
 
-    def crawl_graph(self):
+    #     root = ET.parse(filepath).getroot()
+    #     print ET.parse(filepath)
+    #     # populates a graph with class/subclass relations
+    #     for concept in root.findall('RDFS:Class', ns):
+    #         self.ontology_class_graph.add_node(self.get_attr(concept, ns, 'RDF:about'))
+    #         for subclass in concept.findall('RDFS:subClassOf', ns):
+    #             self.ontology_class_graph.add_edge(self.get_attr(subclass, ns, 'RDF:resource'), self.get_attr(concept, ns, 'RDF:about'))
+
+    #     for concept in root.findall('RDFS:Datatype', ns):
+    #         self.ontology_class_graph.add_node(self.get_attr(concept, ns, 'RDF:about'))
+    #         for subclass in concept.findall('RDFS:subClassOf', ns):
+    #             self.ontology_class_graph.add_edge(self.get_attr(subclass, ns, 'RDF:resource'), self.get_attr(concept, ns, 'RDF:about'))
+
+    #     # populates a graph with class/property/class relations
+    #     for concept in root.findall('RDF:Property', ns):
+    #         domain = self.get_attr(concept.find('RDFS:domain', ns), ns, 'RDF:resource')
+    #         range = self.get_attr(concept.find('RDFS:range', ns), ns, 'RDF:resource')
+    #         if domain and range:
+    #             self.ontology_property_graph.add_edge(domain, range, name=self.get_attr(concept, ns, 'RDF:about'))
+
+    def crawl_graph_new(self):
         ret = {}
         lookup = {'down':{}, 'up':{}}
-        for ontology_class in self.ontology_class_graph.nodes_iter():
-            ret[ontology_class] = {'down':{}, 'up':{}}
-            for direction in ['down', 'up']:
-                if ontology_class not in lookup[direction]:
-                    lookup[direction][ontology_class] = self.get_properties_and_relatedclasses(ontology_class, direction=direction)
+        classes = set()
+        for ontology_property,p,o in self.graph.triples((None, None, RDF.Property)):
+            print '--' * 20
+            print ontology_property
+            for s,p,domain_class in self.graph.triples((ontology_property, RDFS.domain, None)):
+                domain_class = Resource(self.graph, domain_class)
+                for domain_subclass in domain_class.transitive_subjects(RDFS.subClassOf):
+                    print domain_subclass
+                    if self.extract_friendly_name(domain_subclass.identifier) not in ret:
+                        ret[self.extract_friendly_name(domain_subclass.identifier)] = {'down':[], 'up':[]}
+                    for s,p,o in self.graph.triples((ontology_property, RDFS.range, None)):
+                        res_o = Resource(self.graph, o)
+                        ret[self.extract_friendly_name(domain_subclass.identifier)]['down'].append({
+                            'ontology_property':self.extract_friendly_name(ontology_property),
+                            'ontology_classes':[self.extract_friendly_name(subclass.identifier) for subclass in res_o.transitive_subjects(RDFS.subClassOf)]
+                        })
 
-                ret[ontology_class][direction] = lookup[direction][ontology_class]
+            for s,p,domain_class in self.graph.triples((ontology_property, RDFS.range, None)):
+                domain_class = Resource(self.graph, domain_class)
+                for domain_subclass in domain_class.transitive_subjects(RDFS.subClassOf):
+                    print domain_subclass
+                    if self.extract_friendly_name(domain_subclass.identifier) not in ret:
+                        ret[self.extract_friendly_name(domain_subclass.identifier)] = {'down':[], 'up':[]}
+                    for s,p,o in self.graph.triples((ontology_property, RDFS.domain, None)):
+                        res_o = Resource(self.graph, o)
+                        ret[self.extract_friendly_name(domain_subclass.identifier)]['up'].append({
+                            'ontology_property':self.extract_friendly_name(ontology_property),
+                            'ontology_classes':[self.extract_friendly_name(subclass.identifier) for subclass in res_o.transitive_subjects(RDFS.subClassOf)]
+                        })
+                    
+            # for s,p,domain_class in self.graph.triples((ontology_property, RDFS.domain, None)):
+            #     print domain_class
+            #     if domain_class not in ret:
+            #         ret[self.extract_friendly_name(domain_class)] = {'down':[]}
+            #     for s,p,o in self.graph.triples((ontology_property, RDFS.range, None)):
+            #         res_o = Resource(self.graph, o)
+            #         ret[self.extract_friendly_name(domain_class)]['down'].append({
+            #             'ontology_property':self.extract_friendly_name(ontology_property),
+            #             'ontology_classes':[self.extract_friendly_name(subclass.identifier) for subclass in res_o.transitive_subjects(RDFS.subClassOf)]
+            #         })
 
-                for superclass in nx.ancestors(self.ontology_class_graph, ontology_class):
-                    if superclass not in lookup[direction]:
-                        lookup[direction][superclass] = self.get_properties_and_relatedclasses(superclass, direction=direction)
-                    ret[ontology_class][direction] = ret[ontology_class][direction] + lookup[direction][superclass]
-                
-        return ret
+
+
+        # for s,p,o in self.graph.triples((None, RDFS.domain, None)):
+        #     classes.add(o)
+        #     res_o = Resource(self.graph, o)
+        #     if o not in ret:
+        #         print o
+        #         ret[str(o)] = {'down':[]}
+        #     ret[str(o)]['down'].append({
+        #         'ontology_property':str(s),
+        #         'ontology_classes':[str(subclass.identifier) for subclass in res_o.transitive_subjects(RDFS.subClassOf)]
+        #     })
+
+        # for s,p,o in self.graph.triples((None, RDFS.range, None)):
+        #     classes.add(o)
+        # for ontology_class in classes:
+        #     print ontology_class
+
+
+
+
+        print ret
+
+    def extract_friendly_name(self, uri):
+        return str(uri).split('/')[-1]
+
+    # def crawl_graph(self):
+    #     ret = {}
+    #     lookup = {'down':{}, 'up':{}}
+    #     for ontology_class in self.ontology_class_graph.nodes_iter():
+    #         ret[ontology_class] = {'down':{}, 'up':{}}
+    #         for direction in ['down', 'up']:
+    #             if ontology_class not in lookup[direction]:
+    #                 lookup[direction][ontology_class] = self.get_properties_and_relatedclasses(ontology_class, direction=direction)
+
+    #             ret[ontology_class][direction] = lookup[direction][ontology_class]
+
+    #             for superclass in nx.ancestors(self.ontology_class_graph, ontology_class):
+    #                 if superclass not in lookup[direction]:
+    #                     lookup[direction][superclass] = self.get_properties_and_relatedclasses(superclass, direction=direction)
+    #                 ret[ontology_class][direction] = ret[ontology_class][direction] + lookup[direction][superclass]
+
+    #     return ret
         
-        # print len(list(nx.simple_cycles(self.ontology_property_graph)))
-        # print list(nx.all_shortest_paths(self.ontology_class_graph,source='E1_CRM_Entity', target='E12_Production'))
+    #     # print len(list(nx.simple_cycles(self.ontology_property_graph)))
+    #     # print list(nx.all_shortest_paths(self.ontology_class_graph,source='E1_CRM_Entity', target='E12_Production'))
 
-    def get_attr(self, element, namespace, key):
-        key = key.split(':')
-        if element is not None:
-            return element.get('{%s}%s' % (namespace[key[0]], key[1]))
-        else:
-            return None
+    # def get_attr(self, element, namespace, key):
+    #     key = key.split(':')
+    #     if element is not None:
+    #         return element.get('{%s}%s' % (namespace[key[0]], key[1]))
+    #     else:
+    #         return None
 
-    def get_properties_and_relatedclasses(self, ontology_class, direction='down'):
-        ret = []
-        if direction == 'down':
-            for ontology_property in self.ontology_property_graph.out_edges([ontology_class], data=True):
-                subclasses = nx.descendants(self.ontology_class_graph, ontology_property[1])
-                subclasses.add(ontology_property[1])
-                ret.append({
-                    'ontology_property':ontology_property[2]['name'],
-                    'ontology_classes':list(subclasses)
-                })
-        if direction == 'up':
-            for ontology_property in self.ontology_property_graph.in_edges([ontology_class], data=True):
-                subclasses = nx.descendants(self.ontology_class_graph, ontology_property[0])
-                subclasses.add(ontology_property[0])
-                ret.append({
-                    'ontology_property':ontology_property[2]['name'],
-                    'ontology_classes':list(subclasses)
-                })
+    # def get_properties_and_relatedclasses(self, ontology_class, direction='down'):
+    #     ret = []
+    #     if direction == 'down':
+    #         for ontology_property in self.ontology_property_graph.out_edges([ontology_class], data=True):
+    #             subclasses = nx.descendants(self.ontology_class_graph, ontology_property[1])
+    #             subclasses.add(ontology_property[1])
+    #             ret.append({
+    #                 'ontology_property':ontology_property[2]['name'],
+    #                 'ontology_classes':list(subclasses)
+    #             })
+    #     if direction == 'up':
+    #         for ontology_property in self.ontology_property_graph.in_edges([ontology_class], data=True):
+    #             subclasses = nx.descendants(self.ontology_class_graph, ontology_property[0])
+    #             subclasses.add(ontology_property[0])
+    #             ret.append({
+    #                 'ontology_property':ontology_property[2]['name'],
+    #                 'ontology_classes':list(subclasses)
+    #             })
             
-        return ret
+    #     return ret
