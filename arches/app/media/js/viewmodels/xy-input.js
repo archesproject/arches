696d58dfffb25f0cdee6e483a393ec2ceda88428
Cyrus Hiatt <chiatt@fargeo.com>
2017-08-08 16:34:12 -0700

    re #2323, reprojecting coords in input fields when user changes srid

--- arches/app/media/js/viewmodels/xy-input.js
+++ arches/app/media/js/viewmodels/xy-input.js
@@ -14,8 +14,9 @@ define(['knockout', 'proj4', 'arches'], function (ko, proj4, arches) {
         this.defaultProjection = _.findWhere(arches.preferredCoordinateSystems, {default: true}).proj4
         this.defaultProjection ? this.srid = ko.observable(this.defaultProjection) : this.srid('4326');
         this.defaultCoords = (this.srid() === '4326') ? [arches.mapDefaultX, arches.mapDefaultY] : proj4(this.srid(), [arches.mapDefaultX, arches.mapDefaultY]);
-        this.x = ko.observable(this.defaultCoords[0]);
-        this.y = ko.observable(this.defaultCoords[1]);
+        this.x = ko.observable();
+        this.y = ko.observable();
+        this.validCoords = ko.observable(false);
         this.selectedPoint = ko.observable();
         this.availableSrids = _.map(arches.preferredCoordinateSystems, function(v, k) {
             var id = (k === '4326') ? '4326' : v.proj4;
@@ -32,6 +33,20 @@ define(['knockout', 'proj4', 'arches'], function (ko, proj4, arches) {
             return res;
         })
 
+        this.srid.subscribe(function(val){
+            var projectedVals;
+            if (self.srid() === '4326') {
+                projectedVals = proj4(self.defaultProjection).inverse([self.x(), self.y()]);
+            } else if (self.defaultProjection === '4326') {
+                projectedVals = proj4(self.srid(), [self.x(), self.y()])
+            } else {
+                projectedVals = proj4(self.defaultProjection, self.srid(), [self.x(), self.y()])
+            }
+            self.x(projectedVals[0])
+            self.y(projectedVals[1])
+            self.defaultProjection = self.srid();
+        })
+
         this.clearCoordinates = function() {
             self.x(null);
             self.y(null);
@@ -46,11 +61,15 @@ define(['knockout', 'proj4', 'arches'], function (ko, proj4, arches) {
             }
 
         this.updateSelectedPoint = function(){
+            var projected;
             if (mapWidget.draw.getSelected() && mapWidget.draw.getSelected().features) {
                 points = _.filter(mapWidget.draw.getSelected().features, function(item) {return _.some([item.geometry], {type:'Point'})});
                 if (points.length > 0) {
                     self.selectedPoint(points[0])
-                    self.x(points[0].geometry.coordinates[0]), self.y(points[0].geometry.coordinates[1]);
+                    var x = points[0].geometry.coordinates[0]
+                    var y = points[0].geometry.coordinates[1]
+                    projected = self.srid() === '4326' ? [x, y] : proj4(self.srid(),[x, y])
+                    self.x(projected[0]), self.y(projected[1]);
                 } else {
                     if (self.selectedPoint() && !_.contains(['draw_point','draw_polygon','draw_line_string'], mapWidget.draw.getMode())) {
                         self.active(false);
