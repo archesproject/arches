647464c3d8213f156812c314632cb5fc2b2b02e8
Alexei Peters <apeters@fargeo.com>
2017-08-30 22:58:37 -0700

    use fully qualified ontology class and property names, re #2307

--- arches/app/models/migrations/0005_4_0_1.py
+++ arches/app/models/migrations/0005_4_0_1.py
@@ -6,6 +6,7 @@ import os
 from django.db import migrations, models
 from django.core import management
 from arches.app.models.system_settings import settings
+from rdflib import Graph, RDF, RDFS
 
 def forwards_func(apps, schema_editor):
     # We get the model from the versioned app registry;
@@ -14,10 +15,54 @@ def forwards_func(apps, schema_editor):
     management.call_command('load_ontology', source=os.path.join(settings.ONTOLOGY_PATH, settings.ONTOLOGY_BASE),
         version=settings.ONTOLOGY_BASE_VERSION, ontology_name=settings.ONTOLOGY_BASE_NAME, id=settings.ONTOLOGY_BASE_ID, extensions=','.join(extensions), verbosity=0)
 
+    Ontology = apps.get_model("models", "Ontology")
+    Node = apps.get_model("models", "Node")
+    Edge = apps.get_model("models", "Edge")
+
+    for ontology in Ontology.objects.filter(parentontology=None):
+        g = Graph()
+        g.parse(ontology.path.path)
+        for extension in Ontology.objects.filter(parentontology=ontology):
+            g.parse(extension.path.path)
+
+        ontology_classes = set()
+        ontology_properties = set()
+        for ontology_property,p,o in g.triples((None, None, RDF.Property)):
+            ontology_properties.add(ontology_property)
+            for s,p,domain_class in g.triples((ontology_property, RDFS.domain, None)):
+                ontology_classes.add(domain_class)
+            for s,p,range_class in g.triples((ontology_property, RDFS.range, None)):
+                ontology_classes.add(range_class)
+
+        for ontology_class,p,o in g.triples((None, None, RDFS.Class)):
+            ontology_classes.add(ontology_class)
+
+        for ontology_class in ontology_classes:
+            print ontology_class
+            for node in Node.objects.filter(ontologyclass=str(ontology_class).split('/')[-1], graph__in=ontology.graphs.all()):
+                node.ontologyclass = ontology_class
+                node.save()
+
+        for ontology_property in ontology_properties:
+            for edge in Edge.objects.filter(ontologyproperty=str(ontology_property).split('/')[-1], graph__in=ontology.graphs.all()):
+                edge.ontologyproperty = ontology_property
+                edge.save()
+
 def reverse_func(apps, schema_editor):
     extensions = [os.path.join(settings.ONTOLOGY_PATH, x) for x in settings.ONTOLOGY_EXT]
     management.call_command('load_ontology', source=os.path.join(settings.ONTOLOGY_PATH, settings.ONTOLOGY_BASE),
         version=settings.ONTOLOGY_BASE_VERSION, ontology_name=settings.ONTOLOGY_BASE_NAME, id=settings.ONTOLOGY_BASE_ID, extensions=','.join(extensions), verbosity=0)
+
+    Node = apps.get_model("models", "Node")
+    Edge = apps.get_model("models", "Edge")
+
+    for node in Node.objects.all():
+        node.ontologyclass = str(node.ontologyclass).split('/')[-1]
+        node.save()
+
+    for edge in Edge.objects.all():
+        edge.ontologyproperty = str(edge.ontologyproperty).split('/')[-1]
+        edge.save()
 
 class Migration(migrations.Migration):
 
@@ -137,29 +182,4 @@ class Migration(migrations.Migration):
              WHERE ontologyproperty='P167i_was_place_of';
         """),
         migrations.RunPython(forwards_func, reverse_func),
-                migrations.AlterField(
-                    model_name='function',
-                    name='component',
-                    field=models.TextField(blank=True, null=True, unique=True),
-                ),
-                migrations.AlterField(
-                    model_name='geocoder',
-                    name='component',
-                    field=models.TextField(unique=True),
-                ),
-                migrations.AlterField(
-                    model_name='geocoder',
-                    name='name',
-                    field=models.TextField(unique=True),
-                ),
-                migrations.AlterField(
-                    model_name='widget',
-                    name='component',
-                    field=models.TextField(unique=True),
-                ),
-                migrations.AlterField(
-                    model_name='widget',
-                    name='name',
-                    field=models.TextField(unique=True),
-                ),
     ]
