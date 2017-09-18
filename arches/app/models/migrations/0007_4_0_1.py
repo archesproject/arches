f6e6eb178e2f4796c2cc195107405e5cf18786c2
Rob Gaston <rgaston@fargeo.com>
2017-09-18 12:20:14 -0700

    migration to add simplification config re: #2309

---
+++ arches/app/models/migrations/0007_4_0_1.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import os
+from django.db import migrations, models
+from django.core import management
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('models', '0006_4_0_1'),
+    ]
+
+    operations = [
+            migrations.RunSQL("""
+                    update d_data_types
+                    	set defaultconfig = jsonb_set(defaultconfig, '{simplification}', '0.3')
+                    	where datatype = 'geojson-feature-collection';
+                    update nodes
+                    	set config = jsonb_set(config, '{simplification}', '0.3')
+                    	where datatype = 'geojson-feature-collection';
+                """,
+                """
+                    update d_data_types
+                    	set defaultconfig = defaultconfig - 'simplification'
+                    	where datatype = 'geojson-feature-collection';
+                    update nodes
+                    	set config = config - 'simplification'
+                    	where datatype = 'geojson-feature-collection';
+                """),
+    ]
