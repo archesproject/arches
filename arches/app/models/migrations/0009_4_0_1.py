20b75405847e462b80689a8f50c7e219b59ddcce
Cyrus Hiatt <chiatt@fargeo.com>
2017-10-10 17:14:54 -0700

    re #363, Added required nodes forward function

--- arches/app/models/migrations/0009_4_0_1.py
+++ arches/app/models/migrations/0009_4_0_1.py
@@ -2,7 +2,25 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+import json
+import uuid
 
+def forwards_func(apps, schema_editor):
+    
+    FunctionXGraph = apps.get_model("models", "FunctionXGraph")
+    Node = apps.get_model("models", "Node")
+    required_node_configs = FunctionXGraph.objects.filter(function_id = '60000000-0000-0000-0000-000000000002')
+    for config in required_node_configs:
+        d = json.loads(json.dumps(config.config))
+        rn = json.loads(d['required_nodes'])
+        for k, v in rn.iteritems():
+            for nodeid in v:
+                node = Node.objects.get(pk=uuid.UUID(nodeid))
+                node.isrequired = True
+                node.save()
+
+def reverse_func(apps, schema_editor):
+    print 'reversing'
 
 class Migration(migrations.Migration):
 
@@ -11,8 +29,10 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(forwards_func, reverse_func),
         migrations.RunSQL("""
-                DELETE FROM functions WHERE functionid = '60000000-0000-0000-0000-000000000002'
+                DELETE FROM functions_x_graphs where functionid = '60000000-0000-0000-0000-000000000002';
+                DELETE FROM functions WHERE functionid = '60000000-0000-0000-0000-000000000002';
             """,
             """
                 INSERT INTO functions(functionid, modulename, classname, functiontype, name, description, defaultconfig, component)
