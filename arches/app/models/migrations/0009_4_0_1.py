274d67c5c241b808056024cc411d4ec4efb44f4c
Alexei Peters <apeters@fargeo.com>
2017-10-10 17:27:03 -0700

    add reverse function for migration for required nodes, re #2513

--- arches/app/models/migrations/0009_4_0_1.py
+++ arches/app/models/migrations/0009_4_0_1.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.db import migrations, models
 import json
 import uuid
+from arches.app.utils.betterJSONSerializer import JSONSerializer, JSONDeserializer
 
 def forwards_func(apps, schema_editor):
     
@@ -20,7 +21,26 @@ def forwards_func(apps, schema_editor):
                 node.save()
 
 def reverse_func(apps, schema_editor):
-    print 'reversing'
+    Node = apps.get_model("models", "Node")
+    Function = apps.get_model("models", "Function")
+    FunctionXGraph = apps.get_model("models", "FunctionXGraph")
+    required_nodes_function = Function.objects.get(pk='60000000-0000-0000-0000-000000000002')
+
+    graphs = {}
+    required_nodes = Node.objects.filter(isrequired = True)
+    for node in required_nodes:
+        if str(node.graph_id) not in graphs:
+            graphs[str(node.graph_id)] = {}
+        if str(node.nodegroup_id) not in graphs[str(node.graph_id)]:
+            graphs[str(node.graph_id)][str(node.nodegroup_id)] = []
+        graphs[str(node.graph_id)][str(node.nodegroup_id)].append(str(node.pk))
+
+    for graph_id, required_nodes in graphs.iteritems():
+        function_config = {
+            "required_nodes": JSONSerializer().serialize(required_nodes),
+            "triggering_nodegroups": required_nodes.keys()
+        }
+        FunctionXGraph.objects.create(function=required_nodes_function, graph_id=graph_id, config=function_config)
 
 class Migration(migrations.Migration):
 
@@ -38,4 +58,4 @@ class Migration(migrations.Migration):
                 INSERT INTO functions(functionid, modulename, classname, functiontype, name, description, defaultconfig, component)
                     VALUES ('60000000-0000-0000-0000-000000000002', 'required_nodes.py', 'RequiredNodesFunction', 'validation', 'Define Required Nodes', 'Define which values are required for a user to save card', '{"required_nodes":"{}"}', 'views/components/functions/required-nodes');
             """),
-        ]
+    ]
