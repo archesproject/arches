6bc20ad592a61a201534bee7a7e44c189364d8c2
Cyrus Hiatt <chiatt@fargeo.com>
2017-10-16 17:34:24 -0700

    re #2567, moving change password form

--- arches/app/media/js/views/user-profile-manager.js
+++ arches/app/media/js/views/user-profile-manager.js
@@ -8,6 +8,10 @@ define([
     var UserProfileManager = BaseManagerView.extend({
         initialize: function(options){
             var self = this;
+            self.viewModel.showChangePasswordForm = ko.observable(false);
+            self.viewModel.toggleChangePasswordForm = function(val){
+                this.showChangePasswordForm(!this.showChangePasswordForm())
+            }
             BaseManagerView.prototype.initialize.call(this, options)
         }
     });
