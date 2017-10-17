a4f8bc743e046d7b48294168e6b1a73a4e706abb
Rob Gaston <rgaston@fargeo.com>
2017-10-17 16:20:22 -0700

    stubbing out project manager re: #2459

---
+++ arches/app/media/js/views/project-manager.js
@@ -0,0 +1,11 @@
+define([
+    'views/base-manager',
+    'project-manager-data',
+    'arches'
+], function(BaseManagerView, data, arches) {
+    var pageView = new BaseManagerView({
+        viewModel: {}
+    });
+
+    return pageView;
+});
---
+++ arches/app/views/project.py
@@ -0,0 +1,35 @@
+'''
+ARCHES - a program developed to inventory and manage immovable cultural heritage.
+Copyright (C) 2013 J. Paul Getty Trust and World Monuments Fund
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+'''
+from django.shortcuts import render
+from django.utils.translation import ugettext as _
+from django.utils.decorators import method_decorator
+from arches.app.views.base import BaseManagerView
+from arches.app.utils.decorators import group_required
+
+@method_decorator(group_required('Application Administrator'), name='dispatch')
+class ProjectManagerView(BaseManagerView):
+    def get(self, request):
+        context = self.get_context_data(
+            main_script='views/project-manager',
+        )
+
+        context['nav']['title'] = _('Mobile Project Manager')
+        context['nav']['icon'] = 'fa-server'
+        context['nav']['help'] = (_('Mobile Project Manager'),'help/project-manager-help.htm')
+
+        return render(request, 'views/project-manager.htm', context)
