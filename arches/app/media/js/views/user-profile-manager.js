4e9167fb172fa5db6f3bfc2e60447a08229d9885
Cyrus Hiatt <chiatt@fargeo.com>
2017-10-17 18:11:53 -0700

    re #2567 and #2455, Enabling user to update profile info

--- arches/app/media/js/views/user-profile-manager.js
+++ arches/app/media/js/views/user-profile-manager.js
@@ -9,8 +9,13 @@ define([
         initialize: function(options){
             var self = this;
             self.viewModel.showChangePasswordForm = ko.observable(false);
+            self.viewModel.showEditUserForm = ko.observable(false);
             self.viewModel.toggleChangePasswordForm = function(val){
                 this.showChangePasswordForm(!this.showChangePasswordForm())
+            }
+            self.viewModel.toggleEditUserForm = function(val){
+                console.log('toggling user edit form')
+                this.showEditUserForm(!this.showEditUserForm())
             }
             BaseManagerView.prototype.initialize.call(this, options)
         }
--- arches/app/views/users.py
+++ arches/app/views/users.py
@@ -21,12 +21,14 @@ from arches.app.models import models
 from arches.app.models.system_settings import settings
 from arches.app.utils.betterJSONSerializer import JSONSerializer, JSONDeserializer
 from arches.app.views.base import BaseManagerView
+from django.core.exceptions import ValidationError
 from django.shortcuts import render
 from django.utils.translation import ugettext as _
 
 class UserManagerView(BaseManagerView):
 
     def get(self, request):
+        print 'loading request'
         context = self.get_context_data(
             main_script='views/user-profile-manager',
         )
@@ -35,5 +37,41 @@ class UserManagerView(BaseManagerView):
         context['nav']['title'] = _("Profile Manager")
         context['nav']['login'] = True
         context['nav']['help'] = (_('Creating Resources'),'help/resource-editor-landing-help.htm')
+
+        return render(request, 'views/user-profile-manager.htm', context)
+
+
+    def post(self, request):
+
+        context = self.get_context_data(
+            main_script='views/user-profile-manager',
+        )
+        context['nav']['icon'] = "fa fa-user"
+        context['nav']['title'] = _("Profile Manager")
+        context['nav']['login'] = True
+        context['nav']['help'] = (_('Creating Resources'),'help/resource-editor-landing-help.htm')
+
+        user = models.User.objects.get(pk=request.user.id)
+        if user.username != request.POST.get('username'):
+            in_use = models.User.objects.filter(username = request.POST.get('username')).count() > 0
+            if in_use == True:
+                try:
+                    raise ValidationError(
+                        _("The username, '{0}' is already in use".format(request.POST.get('username'))),
+                        code='name in use',
+                    )
+                except ValidationError as e:
+                    context["error"] = e[0]
+                    print context["error"]
+                    return render(request, 'views/user-profile-manager.htm', context)
+
+        user.username = request.POST.get('username')
+        user.first_name = request.POST.get('firstname')
+        user.last_name = request.POST.get('lastname')
+        user.email = request.POST.get('email')
+        user.userprofile.phone = request.POST.get('phone')
+        user.userprofile.save()
+        user.save()
+        request.user = user
 
         return render(request, 'views/user-profile-manager.htm', context)
