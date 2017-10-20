9a3638eaf818a7cc489fccb134d3d83506641803
Alexei Peters <apeters@fargeo.com>
2017-10-20 14:43:18 -0700

    integrate profile form class into view, add error reporting, re #2455

--- arches/app/views/users.py
+++ arches/app/views/users.py
@@ -16,16 +16,11 @@ You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 '''
 
-
-from arches.app.models import models
-from arches.app.models.system_settings import settings
-from arches.app.utils.betterJSONSerializer import JSONSerializer, JSONDeserializer
-from arches.app.views.base import BaseManagerView
-from django.core.exceptions import ValidationError
-from django.core.validators import validate_email
-import django.contrib.auth.password_validation as validation
 from django.shortcuts import render
 from django.utils.translation import ugettext as _
+import django.contrib.auth.password_validation as validation
+from arches.app.views.base import BaseManagerView
+from arches.app.utils.forms import ArchesUserProfileForm
 
 class UserManagerView(BaseManagerView):
 
@@ -54,27 +49,15 @@ class UserManagerView(BaseManagerView):
         context['nav']['help'] = (_('Profile Editing'),'help/profile-manager-help.htm')
         context['validation_help'] = validation.password_validators_help_texts()
 
-        user = models.User.objects.get(pk=request.user.id)
-        try:
-            validate_email(request.POST.get('email'))
-        except ValidationError as e:
-            context['errors'].append(e)
 
-        try:
-            validate_email(request.POST.get('email'))
-        except ValidationError as e:
-            context['errors'].append(e)
-
-        if len(context['errors']) == 0:
-            #user.username = request.POST.get('username')
-            user.first_name = request.POST.get('firstname')
-            user.last_name = request.POST.get('lastname')
-            user.email = request.POST.get('email')
-            if models.UserProfile.objects.filter(user=user).count() == 0:
-                models.UserProfile.objects.create(user=user)
-            user.userprofile.phone = request.POST.get('phone')
-            user.userprofile.save()
-            user.save()
+        user_info = request.POST.copy()
+        user_info['id'] = request.user.id
+        user_info['username'] = request.user.username
+        
+        form = ArchesUserProfileForm(user_info)
+        if form.is_valid():
+            user = form.save()
             request.user = user
+        context['form'] = form
 
         return render(request, 'views/user-profile-manager.htm', context)
