b9a8cc23c8817f7e422c0adbf741494d6276668e
Cyrus Hiatt <chiatt@fargeo.com>
2017-10-23 15:11:11 -0700

    removed cruft

--- arches/app/views/main.py
+++ arches/app/views/main.py
@@ -22,7 +22,6 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import PasswordChangeForm
 import django.contrib.auth.password_validation as validation
 from django.core.exceptions import ValidationError
 from django.core.urlresolvers import reverse
