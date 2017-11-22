ec67510ffc18721eb7e82191972463f4e2a52455
Ryan Anderson <randerson@fargeo.com>
2017-11-21 16:39:21 -0800

    Concept widget default value without migration, re #2706

--- arches/app/media/js/views/components/widgets/concept-select.js
+++ arches/app/media/js/views/components/widgets/concept-select.js
@@ -4,7 +4,34 @@ define([
     'bindings/select2-query'
 ], function(ko, ConceptSelectViewModel) {
     return ko.components.register('concept-select-widget', {
-        viewModel: ConceptSelectViewModel,
+        viewModel: function(params) {
+            params.configKeys = ['defaultValue'];
+            ConceptSelectViewModel.apply(this, [params]);
+
+            var defaultValue = ko.unwrap(this.defaultValue)
+            var self = this;
+
+            if (self.configForm){
+                self.select2Config.value = self.defaultValue
+            };
+
+            if (this.tile && this.tile.tileid() == "" && defaultValue != null && defaultValue != "") {
+                this.value(defaultValue);
+            };
+
+            if (!self.form) {
+                self.value.subscribe(function(val){
+                    if (self.defaultValue() != val) {
+                        self.defaultValue(val)
+                    };
+                });
+                self.defaultValue.subscribe(function(val){
+                    if (self.value() != val) {
+                        self.value(val)
+                    };
+                });
+            };
+        },
         template: {
             require: 'text!widget-templates/concept-select'
         }
