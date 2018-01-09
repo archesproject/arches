1ccd6dc8c6d541501d1e81ba17fb7303b65d8852
Alexei Peters <apeters@fargeo.com>
2018-01-09 12:13:02 -0800

    fix migration dependency, re #2337

--- arches/app/models/migrations/2337_fuzzy_dates.py
+++ arches/app/models/migrations/2337_fuzzy_dates.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('models', '2591_formatted_numbers'),
+        ('models', '2532_boolean_datatype_display_value'),
     ]
 
     operations = [
