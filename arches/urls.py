2df408f97b8bba2b304452515d3aa5d6a16a8a6e
Alexei Peters <apeters@fargeo.com>
2018-03-07 17:09:14 -0800

    don't use debug_toolbar if it isn't installed, re #3106

--- arches/urls.py
+++ arches/urls.py
@@ -140,7 +140,10 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+    try:
+        import debug_toolbar
+        urlpatterns = [
+            url(r'^__debug__/', include(debug_toolbar.urls)),
+        ] + urlpatterns
+    except:
+        pass
