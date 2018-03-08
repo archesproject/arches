59d39623f80bc103484b1e5fe9f30307068d1b17
adam cox <mr.adamcox@gmail.com>
2018-03-08 15:25:55 -0600

    migrate contents of setup.py from arches_hip

--- eamena/eamena/setup.py
+++ eamena/eamena/setup.py
@@ -1,16 +1,90 @@
+import os
+import sys
+from django.conf import settings
+from django.core import management
+from arches.setup import get_version
+from arches.app.search.search_engine_factory import SearchEngineFactory
+from arches.app.utils.data_management.resources.importer import ResourceLoader
+import arches.app.utils.data_management.resources.remover as resource_remover
+from arches.management.commands.package_utils import resource_graphs
+from arches.management.commands.package_utils import authority_files
+from arches.app.models.resource import Resource
 
-
-import arches_hip.setup as setup
+def setup():
+    get_version(path_to_file=os.path.abspath(os.path.dirname(__file__)))
 
 def install(path_to_source_data_dir=None):
-    setup.install()
+    truncate_db()
+    
+    delete_index(index='concept_labels')
+    delete_index(index='term') 
+    Resource().prepare_term_index(create=True)
+
+    load_resource_graphs()
+    load_authority_files(path_to_source_data_dir)
+    load_map_layers()
+
+    resource_remover.truncate_resources()
+    delete_index(index='resource')
+    delete_index(index='entity')
+    delete_index(index='maplayers')
+    delete_index(index='resource_relations') 
+    create_indexes()   
+
+    load_resources()
+
+def export_data():
+    pass
+
+def import_data():
+    pass
+
+def truncate_db():
+    management.call_command('packages', operation='setup_db') 
 
 def load_resource_graphs():
-    setup.resource_graphs.load_graphs(break_on_error=True)
+    resource_graphs.load_graphs(break_on_error=True)
+    pass
 
 def load_authority_files(path_to_files=None):
-    setup.authority_files.load_authority_files(path_to_files, break_on_error=True)
+    authority_files.load_authority_files(path_to_files, break_on_error=True)
+
+def load_map_layers():
+    pass
+
+def delete_index(index=None):
+    se = SearchEngineFactory().create()
+    se.delete_index(index=index)
+
+def create_indexes():
+    Resource().prepare_resource_relations_index(create=True)
+    Resource().prepare_search_index('HERITAGE_RESOURCE_GROUP.E27', create=True)
+    Resource().prepare_search_index('HERITAGE_RESOURCE.E18', create=True)
+    Resource().prepare_search_index('INFORMATION_RESOURCE.E73', create=True)
+    Resource().prepare_search_index('ACTIVITY.E7', create=True)
+    Resource().prepare_search_index('ACTOR.E39', create=True)
+    Resource().prepare_search_index('HISTORICAL_EVENT.E5', create=True)
+
+def install_dependencies():
+    pass
+
+def build_permissions():
+    pass
+
+def load_users():
+    pass
 
 def load_resources(external_file=None):
-    setup.load_resources(external_file)
+    rl = ResourceLoader()
+    if external_file != None:
+        print 'loading:', external_file
+        rl.load(external_file)
+    else:
+        for f in settings.BUSISNESS_DATA_FILES:
+            rl.load(f)
+            
+
+if __name__ == "__main__":
+    install()
+
 
