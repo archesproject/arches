9b191c5831ed84110e7aa734b33a99b982d2c10f
EAMENA <eamena@eamena2.arch.ox.ac.uk>
2018-03-16 15:56:38 +0000

    Move reference data to new dropdowns when extending ontology
    - Move existing authority document concepts to point at new authority
    document entitytype.
    - Update relevant concept labels.

--- arches/management/commands/package_utils/update_schema.py
+++ arches/management/commands/package_utils/update_schema.py
@@ -48,7 +48,9 @@ def load_graphs(break_on_error=True, settings=None):
                         if len(file_errors) > 0:
                             file_errors.insert(0, 'ERRORS IN FILE: %s\n' % (basepath))
                             file_errors.append('\n\n\n\n')
-                            
+                        
+                        logging.warning("\n\n\n------------------\nLoaded new ontology branches\nUpdating edges")
+                        
                         #Update edges
                         for edge in altered_edge_list:
                             # simply update the property referenced by the rule
@@ -58,14 +60,17 @@ def load_graphs(break_on_error=True, settings=None):
                                     entitytypedomain=edge['SOURCE'],
                                     entitytyperange=edge['TARGET']
                                 )
-                            
+                        
                                 newProperty = models.Properties.objects.all().get(propertyid=edge['NEWTYPE'])
                                 rule.propertyid = newProperty
                                 rule.save()
                                 logging.warning("Updated property type for edge: %s", edge)
                             except:
                                 logging.warning("couldn't find rule for edge: %s", edge)
-                                
+                        
+                        
+                        logging.warning("\n\n\n------------------\nUpdated edges\nUpdating reference data")
+                        
                         update_reference_data(altered_node_list)
         else:
             logging.warning('PATH DOESNT EXIST')
@@ -91,41 +96,95 @@ def load_graphs(break_on_error=True, settings=None):
 def update_reference_data(altered_node_list):
     # After the new part of ontology has been inserted. Update the reference data for moved nodes
     for node in altered_node_list:
-        # get root concept for the old node
-        print node['OLDENTITYTYPEID'],node['NEWENTITYTYPEID'], 'HERE'
-        old_entity_type = models.EntityTypes.objects.all().get(entitytypeid=node['OLDENTITYTYPEID'])
-        new_entity_type = models.EntityTypes.objects.all().get(entitytypeid=node['NEWENTITYTYPEID'])
         
-        # set as the root concept for the new node
-        new_entity_type.conceptid = old_entity_type.conceptid
-        new_entity_type.save()
+        if node['OLDENTITYTYPEID'] == '':
+            # ignore empty rows
+            continue;
         
-        # traverse all related concepts, updating legacyOID for concepts which are EntityType or Collection
-        root_concept = Concept()
-        root_concept.get(new_entity_type.conceptid)
-        root_concept.traverse(rewrite_concept, scope=new_entity_type.pk)
+        try:
+            logging.warning("---")
+            logging.warning("UPDATING REFERENCE DATA FOR %s", node['NEWENTITYTYPEID'])
+            # get root concept for the old node
+            old_entity_type = models.EntityTypes.objects.all().get(entitytypeid=node['OLDENTITYTYPEID'])
+            new_entity_type = models.EntityTypes.objects.all().get(entitytypeid=node['NEWENTITYTYPEID'])
+            
+            # set as the root concept for the new node
+            new_entity_type.conceptid = old_entity_type.conceptid
+            new_entity_type.save()
+            
+            # traverse all related concepts, updating legacyOID for concepts which are EntityType or Collection
+            try:
+                root_concept = Concept()
+                root_concept.get(new_entity_type.conceptid)
+                # rewrite_concept(root_concept, new_entity_type.pk)
+                root_concept.traverse(rewrite_concept, scope=new_entity_type.pk)
+                
+            except Exception as e:
+                logging.warning("\n\nUnable to update concepts for migration mapping: %s \n%s", node, e)
+        except Exception as e:
+            logging.warning("\n\nUnable to update reference data for migration mapping: %s \n%s", node, e)
+
+        # Now migrate the authority document concepts also
+        try:
+            logging.warning("Updating auth doc concept for %s", node)
+            # find the old authority document entitytype corresponding to this entitytype
+            old_authority_document_entitytype_rule = models.Rules.objects.get(entitytypedomain=node['OLDENTITYTYPEID'], propertyid='-P71')
+            old_authority_document_entitytypeid = old_authority_document_entitytype_rule.entitytyperange
+            
+            new_authority_document_entitytype_rule = models.Rules.objects.get(entitytypedomain=node['NEWENTITYTYPEID'], propertyid='-P71')
+            new_authority_document_entitytypeid = new_authority_document_entitytype_rule.entitytyperange
+            
+            old_authority_document_entitytype = models.EntityTypes.objects.get(entitytypeid=old_authority_document_entitytypeid)
+            new_authority_document_entitytype = models.EntityTypes.objects.get(entitytypeid=new_authority_document_entitytypeid)
+            
+            # point the new authority document entitytype at the concept for the old one
+            new_authority_document_entitytype.conceptid = old_authority_document_entitytype.conceptid
+            new_authority_document_entitytype.save()
+            
+            # update the value
+            top_concept_value = models.Values.objects.get(conceptid=old_authority_document_entitytype.conceptid, languageid='en-US')
+            top_concept_value.value = node['NEWENTITYTYPEID']
+            top_concept_value.save()
+            
+            # remove the old stub concept created for the new auth doc node?
+        
+        except Exception as e:
+            logging.warning("\n\nUnable to update Entity %s", e)
 
 def rewrite_concept(concept, entitytypeid):
-    if concept.nodetype == 'Collection':
-        logging.warning("Updating collection concept for %s", entitytypeid)
-        #delete the concept which was created when the entitytype was added
-        created_concept_model = models.Concepts.objects.all().get(legacyoid=entitytypeid)
-        created_concept_model.delete()
+    try:
+        if concept.nodetype == 'Collection':
+            #delete the concept which was created when the entitytype was added
+            created_concept_model = models.Concepts.objects.all().get(legacyoid=entitytypeid)
+            created_concept_model.delete()
+            
+            #rename the legacyoid
+            concept_model = models.Concepts.objects.all().get(conceptid=concept.id)
+            concept_model.legacyoid = entitytypeid
+            concept_model.save()
+            
+            # also update value of the root collection concept
+            try:
+                value = models.Values.objects.get(conceptid=concept.id, languageid='en-US')
+                print value.value
+                value.value = entitytypeid
+                value.save()
+                
+            except Exception as e:
+                logging.warning("Couldn't update en-US value for collection concept: %s", concept.legacyoid)
+            
+        elif concept.nodetype == 'EntityType':
+            #delete the concept which was created when the entitytype was added
+            created_concept_model = models.Concepts.objects.all().get(legacyoid=entitytypeid)
+            created_concept_model.delete()
+            
+            #rename the legacyoid
+            concept_model = models.Concepts.objects.all().get(conceptid=concept.id)
+            concept_model.legacyoid = entitytypeid
+            concept_model.save()
         
-        #rename the legacyoid
-        concept_model = models.Concepts.objects.all().get(conceptid=concept.id)
-        concept_model.legacyoid = entitytypeid
-        concept_model.save()
-    elif concept.nodetype == 'EntityType':
-        logging.warning("Updating entitytype concept for %s", entitytypeid)
-        #delete the concept which was created when the entitytype was added
-        created_concept_model = models.Concepts.objects.all().get(legacyoid=entitytypeid)
-        created_concept_model.delete()
-        
-        #rename the legacyoid
-        concept_model = models.Concepts.objects.all().get(conceptid=concept.id)
-        concept_model.legacyoid = entitytypeid
-        concept_model.save()
+    except Exception as e:
+        logging.warning("Unable to update concept to match entitytype %s\n%s", entitytypeid, e)
         
         
 
@@ -209,7 +268,7 @@ def validate_graph(node_list, edge_list):
 
     for node in node_list:
         # validate business table name
-        if node['BUSINESSTABLE'] != u'' and node['BUSINESSTABLE'] not in [u'strings', u'dates', u'domains', u'files', u'geometries', u'numbers']:
+        if node['BUSINESSTABLE'] != u'' and node['BUSINESSTABLE'] not in [u'strings', u'dates', u'domains', u'files', u'geometries', u'numbers', u'uniqueids']:
             append_error('Incorrect business table', node)
 
         # validate node uniqueness
@@ -272,4 +331,3 @@ def link_entitytypes_to_concepts(nodes):
             concepts.insert_concept_relations(str(domainlegacyid), 'hasCollection', node["LABEL"])
         else:
             concepts.insert_concept_relations(str(otherlegacyid), 'hasEntity', node["LABEL"])
-
