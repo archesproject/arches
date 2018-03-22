d65b9f97059dc0f7be69616499eb8bd5fbecae3a
EAMENA <eamena@eamena2.arch.ox.ac.uk>
2018-03-22 13:01:59 +0000

    Move resource types to settings_local

--- eamena/eamena/settings.py
+++ eamena/eamena/settings.py
@@ -1,6 +1,6 @@
 import os
 import inspect
-from arches_hip.settings import *
+from arches.settings import *
 from django.utils.translation import ugettext as _
 
 
@@ -29,98 +29,12 @@ RESOURCE_MODEL = {'default': 'eamena.models.resource.Resource'}
 
 
 
-
-
-
-
-
-
-
-
 EAMENA_RESOURCES = ['HERITAGE_RESOURCE_GROUP.E27'] #Specify which resource types should take on the identifier EAMENA-. All other resource types will take on an identifier beginning with their truncated EntityType, e.g. ACTOR for ACTOR.E39, INFORMATION for INFORMATION_RESOURCE.E73
 ID_LENGTH = 7 #Indicates the length of the Unique Resource IDs after the set tag, e.g. 7 -> EAMENA-0000001. MUST BE GIVEN, AND BE 2 OR OVER.
 
 # DATE_SEARCH_ENTITY_TYPES = ['BEGINNING_OF_EXISTENCE_TYPE.E55', 'END_OF_EXISTENCE_TYPE.E55', 'DISTURBANCE_DATE_TYPE.E55']
 
-def RESOURCE_TYPE_CONFIGS():
-    return {
-        'HERITAGE_RESOURCE_GROUP.E27': {
-            'resourcetypeid': 'HERITAGE_RESOURCE_GROUP.E27',
-            'name': _('Heritage Place'),
-            'icon_class': 'fa fa-university',
-            'default_page': 'summary',
-            'default_description': _('No name available'),
-            'description_node': _('NAME.E41'),
-            'categories': [_('Resource')],
-            'has_layer': True,
-            'on_map': True,
-            'marker_color': '#a44b0f',
-            'stroke_color': '#d9b562',
-            'fill_color': '#eedbad',
-            'primary_name_lookup': {
-                'entity_type': 'EAMENA_ID.E42',
-                'lookup_value': 'Primary'
-            },
-            'sort_order': 1
-        },
-        'HERITAGE_FEATURE.E24': {
-            'resourcetypeid': 'HERITAGE_FEATURE.E24',
-            'name': _('Heritage Feature'),
-            'icon_class': 'fa fa-university',
-            'default_page': 'summary',
-            'default_description': _('No name available'),
-            'description_node': _('NAME.E41'),
-            'categories': [_('Resource')],
-            'has_layer': True,
-            'on_map': True,
-            'marker_color': '#FFC53D',
-            'stroke_color': '#d9b562',
-            'fill_color': '#eedbad',
-            'primary_name_lookup': {
-                'entity_type': 'EAMENA_ID.E42',
-                'lookup_value': 'Primary'
-            },
-            'sort_order': 1
-        },
-        'ACTOR.E39': {
-            'resourcetypeid': 'ACTOR.E39',
-            'name': _('Person/Organization'),
-            'icon_class': 'fa fa-group',
-            'default_page': 'actor-summary',
-            'default_description': _('No description available'),
-            'description_node': _('ACTOR_APPELLATION.E82'),
-            'categories': [_('Resource')],
-            'has_layer': True,
-            'on_map': False,
-            'marker_color': '#a44b0f',
-            'stroke_color': '#a7673d',
-            'fill_color': '#c8b2a3',
-            'primary_name_lookup': {
-                'entity_type': 'EAMENA_ID.E42',
-                'lookup_value': 'Primary'
-            },
-            'sort_order': 5
-        },
-        'INFORMATION_RESOURCE.E73': {
-            'resourcetypeid': 'INFORMATION_RESOURCE.E73',
-            'name': _('Information Resource'),
-            'icon_class': 'fa fa-file-text-o',
-            'default_page': 'information-resource-summary',
-            'default_description': _('No description available'),
-            'description_node': _('TITLE.E41,CATALOGUE_ID.E42,IMAGERY_CREATOR_APPELLATION.E82,TILE_SQUARE_DETAILS.E44,CONTRIBUTOR_APPELLATION.E82,SHARED_DATA_SOURCE_APPELLATION.E82,SHARED_DATA_SOURCE_AFFILIATION.E82,SHARED_DATA_SOURCE_CREATOR_APPELLATION.E82'),
-            'categories': [_('Resource')],
-            'has_layer': True,
-            'on_map': True,
-            'marker_color': '#8D45F8',
-            'stroke_color': '#9367d5',
-            'fill_color': '#c3b5d8',
-            'primary_name_lookup': {
-                'entity_type': 'EAMENA_ID.E42',
-                'lookup_value': 'Primary'
-            },
-            'sort_order': 6
-        }
-    }
+
 
 
 #The list of resource types which should have their geometries indexed into the maplayers index for restriction by geo area.
