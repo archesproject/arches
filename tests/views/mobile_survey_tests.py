7551efdc04f9f3b5dd850fb3db88a77a537c511c
Cyrus Hiatt <chiatt@fargeo.com>
2018-04-02 12:20:00 -0700

    Adds test to confirm that a survey's resource and tile data load into couchdb. re #3147

--- tests/views/mobile_survey_tests.py
+++ tests/views/mobile_survey_tests.py
@@ -22,17 +22,22 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+import os
 import json
 import couchdb
 from tests import test_settings as settings
 from django.contrib.auth.models import User, Group, AnonymousUser
 from django.core.urlresolvers import reverse
 from django.test import Client
+from arches.app.models import models
 from arches.app.models.mobile_survey import MobileSurvey
 from arches.app.models.models import MobileSurveyModel
-from arches.app.views.mobile_survey import MobileSurveyManagerView, MobileSurveyResources
 from arches.app.utils.betterJSONSerializer import JSONSerializer, JSONDeserializer
+from arches.app.utils.data_management.resource_graphs.importer import import_graph as ResourceGraphImporter
+from arches.app.views.mobile_survey import MobileSurveyManagerView, MobileSurveyResources
 from tests.base_test import ArchesTestCase
+from arches.app.utils.data_management.resources.importer import BusinessDataImporter
+
 
 # these tests can be run from the command line via
 # python manage.py test tests/views/mobile_survey_tests.py --pattern="*.py" --settings="tests.test_settings"
@@ -55,14 +60,25 @@ class MobileSurveyTests(ArchesTestCase):
         self.mobile_survey = mobile_survey
         self.client = Client()
 
-    def test_create_mobile_survey(self):
-        """
-        Test that a user can login and is redirected to the home page
+        with open(os.path.join('tests/fixtures/resource_graphs/Mobile Survey Test.json'), 'rU') as f:
+            archesfile = JSONDeserializer().deserialize(f)
+        ResourceGraphImporter(archesfile['graph'])
+        BusinessDataImporter('tests/fixtures/data/mobile_survey_test_data.json').import_business_data()
 
-        """
-
+    def post_mobile_survey(self, post_data):
         self.client.login(username='admin', password='admin')
         url = reverse('mobile_survey_manager')
+        post_data = JSONSerializer().serialize(post_data)
+        content_type = 'application/x-www-form-urlencoded'
+        response = self.client.post(url, post_data, content_type)
+        return json.loads(response.content)
+
+    def test_create_mobile_survey(self):
+        """
+        Test that creation of a mobile survey creates a corresponding couch database.
+
+        """
+
         post_data = {
             "datadownloadconfig": {"count":1000,"download":True,"resources":[],"custom":""},
             "startdate":"2018-01-29",
@@ -76,21 +92,54 @@ class MobileSurveyTests(ArchesTestCase):
             "active":False,
             "users":[],
             "id": str(self.mobile_survey.id),
-            "name":"Catdog",
+            "name":"test survey",
             "description":"a description"
             }
 
-        post_data = JSONSerializer().serialize(post_data)
-        content_type = 'application/x-www-form-urlencoded'
-        response = self.client.post(url, post_data, content_type)
-        response_json = json.loads(response.content)
+        response_json = self.post_mobile_survey(post_data)
         couch = couchdb.Server(settings.COUCHDB_URL)
         self.assertTrue(response_json['success'])
         self.assertTrue('project_' + str(self.mobile_survey.id) in couch)
 
+    def test_load_couchdb(self):
+        """
+        Test that resource instances and tiles load into a mobile survey's couchdb
+
+        """
+
+        post_data = {
+            "datadownloadconfig": {"count":1000,"download":True,"resources":['d84a098c-368b-11e8-bafc-c4b301baab9f'],"custom":""},
+            "startdate":"2018-01-29",
+            "tilecache":"",
+            "enddate":"2018-03-30",
+            "createdby_id":1,
+            "bounds":{"type":"FeatureCollection","features":[]},
+            "cards":['fe035187-368b-11e8-bf56-c4b301baab9f', '4215f135-369c-11e8-9544-c4b301baab9f'],
+            "lasteditedby_id":1,
+            "groups":[],
+            "active":False,
+            "users":[],
+            "id": str(self.mobile_survey.id),
+            "name":"test survey",
+            "description":"a description"
+        }
+
+        response_json = self.post_mobile_survey(post_data)
+        couch = couchdb.Server(settings.COUCHDB_URL)
+
+        if 'project_' + str(self.mobile_survey.id) in couch:
+            db = couch['project_' + str(self.mobile_survey.id)]
+            tile_count = len(db.find({'selector': {'type': 'tile'}}))
+            resource_count = len(db.find({'selector': {'type': 'resource'}}))
+        else:
+            print '{0} is not in couch'.format('project_' + str(self.mobile_survey.id))
+
+        self.assertTrue(tile_count==2)
+        self.assertTrue(resource_count==1)
+
     def test_delete_mobile_survey(self):
         """
-        Test that a user can login and is redirected to the home page
+        Test that deletion of a mobile survey deletes its couchdb
 
         """
 
@@ -107,12 +156,3 @@ class MobileSurveyTests(ArchesTestCase):
         couch = couchdb.Server(settings.COUCHDB_URL)
         self.assertFalse(MobileSurvey.objects.filter(pk=self.mobile_survey.id).exists())
         self.assertTrue('project_' + str(self.mobile_survey.id) not in couch)
-
-
-    def test_load_couchdb(self):
-        """
-        Test that a user can login and is redirected to the home page
-
-        """
-
-        self.assertTrue(1==1)
