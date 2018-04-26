d523ae8cf035d4e4234dedebd1aaa1bd2852a0b4
Andrea Zerbini <andrea.zerbini@arch.ox.ac.uk>
2018-04-26 11:23:10 +0100

    Correct spelling error

--- eamena/eamena/media/js/views/forms/modification.js
+++ eamena/eamena/media/js/views/forms/modification.js
@@ -22,7 +22,7 @@ define(['jquery',
                 this.addBranchList(new BranchList({
                     el: this.$el.find('#modification-section')[0],
                     data: this.data,
-                    dataKey: 'MODIFACATION.E11',
+                    dataKey: 'MODIFICATION.E11',
                     validateBranch: function (nodes) {
                         var ck0 = this.validateHasValues(nodes)
                         var ck1 = vt.isValidDate(nodes,'MODIFICATION_DATE.E49');
