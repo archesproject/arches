21e960c844a3f6a4d289cb9bc18e0423e1f6737d
EAMENA <eamena@eamena2.arch.ox.ac.uk>
2018-04-26 19:00:07 +0100

    Introduce Disclaimer modal on landing page

--- arches/app/media/js/index.js
+++ arches/app/media/js/index.js
@@ -7,23 +7,29 @@ require([
     'plugins/layer_slider/js/layerslider.kreaturamedia.jquery',
     'flexslider'
 ], function($, arches) {
+    $('#Disclaimer').modal('hide');
+    if (arches.editor ===false) {    
+        $('#Disclaimer').modal('show');
+    }
     $(document).ready(function() {
-        $('#photo-flexslider').flexslider({
-            animation: "slide",
-            controlNav: false,
-            animationLoop: false,
-            itemWidth: 80,
-            itemMargin: 0
-        });
 
-        $('#layerslider').layerSlider({
-            skinsPath: arches.urls.media + 'plugins/layer_slider/skins/',
-            skin: 'fullwidth',
-            thumbnailNavigation: false,
-            hoverPrevNext: true,
-            responsive: true,
-            responsiveUnder: 960,
-            sublayerContainer: 960
-        });
+            $('#photo-flexslider').flexslider({
+                animation: "slide",
+                controlNav: false,
+                animationLoop: false,
+                itemWidth: 80,
+                itemMargin: 0
+            });
+    
+            $('#layerslider').layerSlider({
+                skinsPath: arches.urls.media + 'plugins/layer_slider/skins/',
+                skin: 'fullwidth',
+                thumbnailNavigation: false,
+                hoverPrevNext: true,
+                responsive: true,
+                responsiveUnder: 960,
+                sublayerContainer: 960
+            });
+
     });
 });
