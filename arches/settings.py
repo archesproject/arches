f6d9ccefd80de932ffcb6cfd3727c5c6c37f98ea
Cyrus Hiatt <chiatt@fargeo.com>
2018-05-08 15:32:48 -0700

    Formatted timewheel_date_tier setting example to be more readable. re #3306

--- arches/settings.py
+++ arches/settings.py
@@ -387,10 +387,19 @@ CACHES = {
     }
 }
 
-#Example of a custom time wheel configuration:
-# TIMEWHEEL_DATE_TIERS = {"name": "Millennium", "interval": 1000, "root": True, "child": {
-#       "name": "Cen", "interval": 100, "range": (1500, 2000), "child": {
-#           "name": "Decade", "interval": 10, "range": (1750, 2000)
+# Example of a custom time wheel configuration:
+# TIMEWHEEL_DATE_TIERS = {
+#     "name": "Millennium", 
+#     "interval": 1000,
+#     "root": True,
+#     "child": {
+#             "name": "Cen",
+#             "interval": 100,
+#             "range": (1500, 2000),
+#             "child": {
+#                 "name": "Decade",
+#                 "interval": 10,
+#                 "range": (1750, 2000)
 #           }
 #       }
 #   }
