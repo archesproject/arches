b3f08db0946494136766b7156fd2a02e1dc940f6
Ryan Anderson <randerson@fargeo.com>
2018-05-10 12:36:27 -0700

    Add jsonld context widget to graph settings in new and old graph manager templates, re #3314

--- arches/app/views/graph.py
+++ arches/app/views/graph.py
@@ -120,7 +120,7 @@ class GraphSettingsView(GraphBaseView):
         data = JSONDeserializer().deserialize(request.body)
         for key, value in data.get('graph').iteritems():
             if key in ['iconclass', 'name', 'author', 'description', 'isresource',
-                'ontology_id', 'version',  'subtitle', 'isactive', 'color']:
+                'ontology_id', 'version',  'subtitle', 'isactive', 'color', 'jsonldcontext']:
                 setattr(graph, key, value)
 
         node = models.Node.objects.get(graph_id=graphid, istopnode=True)
@@ -170,7 +170,7 @@ class NewGraphSettingsView(GraphBaseView):
         data = JSONDeserializer().deserialize(request.body)
         for key, value in data.get('graph').iteritems():
             if key in ['iconclass', 'name', 'author', 'description', 'isresource',
-                'ontology_id', 'version',  'subtitle', 'isactive', 'color']:
+                'ontology_id', 'version',  'subtitle', 'isactive', 'color', 'jsonldcontext']:
                 setattr(graph, key, value)
 
         node = models.Node.objects.get(graph_id=graphid, istopnode=True)
