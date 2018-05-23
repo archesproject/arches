c6b561203035f9080f1d38e1d158dc9ab08c346c
Alexei Peters <apeters@fargeo.com>
2018-05-22 22:57:34 -0700

    add unit test for api, re #3309

--- arches/app/views/api.py
+++ arches/app/views/api.py
@@ -10,32 +10,27 @@ from arches.app.models.system_settings import settings
 from arches.app.utils.response import JSONResponse
 from arches.app.utils.betterJSONSerializer import JSONSerializer
 
+
 class CouchdbProxy(ProxyView):
     #check user credentials here
     upstream = settings.COUCHDB_URL
+
 
 class APIBase(View):
     def dispatch(self, request, *args, **kwargs):
         get_params = request.GET.copy()
         for key, value in request.META.iteritems():
-            #print "%s - %s" % (key, value)
-            if key.startswith('X_ARCHES_'):
-                pass
-            if key == 'HTTP_CACHE_CONTROL':
-                get_params['HTTP_CACHE_CONTROL'] = value
+            if key.startswith('HTTP_X_ARCHES_'):
+                if key.replace('HTTP_X_ARCHES_','').lower() not in request.GET:
+                    get_params[key.replace('HTTP_X_ARCHES_','').lower()] = value
 
         get_params._mutable = False
         request.GET = get_params
         return super(APIBase, self).dispatch(request, *args, **kwargs)
 
-    # def get_param(self, request, param):
-    #     return request.META['X_ARCHES_%s' % param.upper()] or request.GET.get(param) or request.POST.get(param)
-
 
 class Surveys(APIBase):
     def get(self, request):
-        print request.GET.get('HTTP_CACHE_CONTROL')
-        #request.GET['HTTP_CACHE_CONTROL'] = 'taco'
         group_ids = list(request.user.groups.values_list('id', flat=True))
         projects = MobileSurvey.objects.filter(Q(users__in=[request.user]) | Q(groups__in=group_ids), active=True)
         response = JSONResponse(projects, indent=4)
