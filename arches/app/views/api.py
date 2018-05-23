ab046f76902eaaa50b6268c7067c8dcd6264010d
Alexei Peters <apeters@fargeo.com>
2018-05-22 20:48:39 -0700

    push headers prefixed with X_ARCHES_ to the request.GET dict, re #3309

--- arches/app/views/api.py
+++ arches/app/views/api.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.db.models import Q
+from django.http.request import QueryDict
 from revproxy.views import ProxyView
 
 from arches.app.models import models
@@ -13,8 +14,28 @@ class CouchdbProxy(ProxyView):
     #check user credentials here
     upstream = settings.COUCHDB_URL
 
-class Surveys(View):
+class APIBase(View):
+    def dispatch(self, request, *args, **kwargs):
+        get_params = request.GET.copy()
+        for key, value in request.META.iteritems():
+            #print "%s - %s" % (key, value)
+            if key.startswith('X_ARCHES_'):
+                pass
+            if key == 'HTTP_CACHE_CONTROL':
+                get_params['HTTP_CACHE_CONTROL'] = value
+
+        get_params._mutable = False
+        request.GET = get_params
+        return super(APIBase, self).dispatch(request, *args, **kwargs)
+
+    # def get_param(self, request, param):
+    #     return request.META['X_ARCHES_%s' % param.upper()] or request.GET.get(param) or request.POST.get(param)
+
+
+class Surveys(APIBase):
     def get(self, request):
+        print request.GET.get('HTTP_CACHE_CONTROL')
+        #request.GET['HTTP_CACHE_CONTROL'] = 'taco'
         group_ids = list(request.user.groups.values_list('id', flat=True))
         projects = MobileSurvey.objects.filter(Q(users__in=[request.user]) | Q(groups__in=group_ids), active=True)
         response = JSONResponse(projects, indent=4)
