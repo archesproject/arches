5da5c80150ac757f4c0ca6080ad6feb3b8f76b1c
Rob Gaston <rgaston@fargeo.com>
2018-06-14 15:59:29 -0700

    enhancements to new editor tree

--- arches/app/media/js/views/resource/new-editor.js
+++ arches/app/media/js/views/resource/new-editor.js
@@ -127,6 +127,12 @@ define([
                     }
                 },
                 owner: card
+            }),
+            canAdd: ko.pureComputed({
+                read: function () {
+                    return this.cardinality === 'n' || this.tiles().length === 0
+                },
+                owner: card
             })
         });
         card.isChildSelected = ko.computed(function() {
