0d24e00116c5abfaca932b45c1c93fcd7fbd43b5
Jeffrey Munowitch <jmunowitch@fargeo.com>
2018-06-25 17:23:12 -0700

    Couch: Fix alerts in the UI on connection failures RE: 3131

--- arches/app/media/js/views/mobile-survey-manager.js
+++ arches/app/media/js/views/mobile-survey-manager.js
@@ -67,13 +67,17 @@ define([
             }, function(a){
                 self.loading(true)
                 if (mobilesurvey) {
-                    mobilesurvey.delete(function(){
+                    mobilesurvey.delete(function(data){
+                        if (data.responseJSON.success){
+                            self.mobilesurveys.remove(mobilesurvey);
+                            if (mobilesurvey === self.selectedMobileSurvey()) {
+                                self.selectedMobileSurvey(undefined);
+                            }
+                        } else {
+                            pageView.viewModel.alert(new AlertViewModel('ep-alert-red', data.responseJSON.title, data.responseJSON.message));
+                        }
                         self.loading(false);
-                        self.mobilesurveys.remove(mobilesurvey);
                     });
-                    if (mobilesurvey === self.selectedMobileSurvey()) {
-                        self.selectedMobileSurvey(undefined);
-                    }
                 };
             }));
         }
