020dca0246f1faca2c57205c8140454bbad4fb19
adam cox <mr.adamcox@gmail.com>
2018-07-11 10:48:55 -0500

    allow return of multiple objects, cleanup code

--- arches/management/commands/whatisthis.py
+++ arches/management/commands/whatisthis.py
@@ -1,22 +1,17 @@
 from django.core import management
 from django.core.management.base import BaseCommand, CommandError
-# 
 import django.apps
-# import psycopg2
-import arches
-import json
 import uuid
-import time
 
 class Command(BaseCommand):
 
-    help = 'finds a uuid throughout your database and prints info from postgres'
+    help = 'finds any arches objects whose primary key is the input uuid and'\
+        'returns these objects in the form of a list.'
 
     def add_arguments(self, parser):
         parser.add_argument("uuid",help='input the uuid string to find')
 
     def handle(self, *args, **options):
-
         self.find_uuid(options['uuid'])
 
     def find_uuid(self,in_uuid):
@@ -24,32 +19,36 @@ class Command(BaseCommand):
         ## check for valid uuid input
         try:
             val = uuid.UUID(in_uuid, version=4)
-        except:
-            print "  -- this is not a vali uuid"
+        except ValueError:
+            print("  -- this is not a valid uuid")
             return False
-        
-        # start1 = time.time()
-        
+
         ## search all models and see if the UUID matches an existing object
-        neo = False
+        objs = []
         for m in django.apps.apps.get_models():
-            if m._meta.pk.get_internal_type() == "UUIDField":
-                ob = m.objects.filter(pk=in_uuid)
-                if len(ob) == 1:
-                    neo = ob[0]
-                    break
-        
+            if not m.__module__.startswith('arches'):
+                continue
+            if m._meta.pk.get_internal_type() != "UUIDField":
+                continue
+            ob = m.objects.filter(pk=in_uuid)
+            objs+=ob
+
         ## return False if nothing was found
-        if not neo:
-            print "  -- this uuid doesn't match any objects in your database"
+        if not objs:
+            print("  -- this uuid doesn't match any objects in your database")
             return False
         
-        print "this is a", neo
-        
-        for k,v in vars(neo).iteritems():
-            print k
-            print " ",v
-        
-        # print round(time.time()-start1,2)
-        
-        return neo
+        ## print summary of found objects
+        print(80*"=")
+        print("This UUID is the primary key for {} object{}:".format(
+            len(objs),"s" if len(objs) > 1 else ""))
+        for o in objs:
+            print(80*"-")
+            print(o)
+            keys = vars(o).keys()
+            keys.sort()
+            for k in keys:
+                print(k)
+                print("  {}".format(vars(o)[k]))
+        print(80*"=")
+        return objs
