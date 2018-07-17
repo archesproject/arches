bd907df989d7d75d3ac1b112972dbbf0bf60d15a
Rob Gaston <rgaston@fargeo.com>
2018-07-17 13:57:25 -0700

    adding global for `require`

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
     },
     "globals": {
-        "define": false
+        "define": false,
+        "require": false
     }
 };
