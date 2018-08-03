53d7d0a7afcfc7b7b31e29f946a99b0b9e98d962
Cyrus Hiatt <chiatt@fargeo.com>
2018-08-03 09:30:05 -0700

    Prevents slide knockout bindingHandler from displaying in-app help on page load. re #3712

--- arches/app/media/js/bindings/slide.js
+++ arches/app/media/js/bindings/slide.js
@@ -1,28 +1,23 @@
-// Here's a custom Knockout binding that makes elements shown/hidden via jQuery's fadeIn()/fadeOut() methods
-// Could be stored in a separate utility library
 define([
-  'jquery',
-  'knockout',
-], function ($, ko) {
+    'jquery',
+    'knockout',
+], function($, ko) {
     ko.bindingHandlers.slide = {
-        init: function(element, valueAccessor,allBindingsAccessor, viewModel, bindingContent) {
-            var value = valueAccessor();
-            var bindings = allBindingsAccessor();
-            var value = valueAccessor();
+        init: function() {
+            this.initted = true;
         },
-        update: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContent) {
+        update: function(element, valueAccessor, allBindingsAccessor) {
             var value = valueAccessor();
             var bindings = allBindingsAccessor();
-            var duration = bindings.duration;
             var direction = bindings.direction;
             var easing = bindings.easing;
-            var callback = bindings.callback;
-
             if (value() === true) {
                 $(element).toggle(easing, direction);
-            } else {
+            }
+            else if (this.initted === false && value() === false) {
                 $(element).toggle(easing, direction);
             }
+            this.initted = false;
         }
     };
     return ko.bindingHandlers.slide;
