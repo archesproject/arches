b6b2501b124b119956ed0de98cc27a617ea52fc8
reubenosborne1 <41572010+reubenosborne1@users.noreply.github.com>
2018-08-06 11:47:30 +0100

    Update truncate_db.py

--- arches/db/install/truncate_db.py
+++ arches/db/install/truncate_db.py
@@ -10,18 +10,17 @@ postgres_version = subprocess.check_output(["psql", "--version"])
 pattern = re.compile(r'\s\d+.\d*.\d*')
 matches = pattern.findall(postgres_version)
 psqlversionmatch = matches[0] 
-split = (psqlversionmatch.split('.')) #major[9].minor[2].patch[0]
+split = (psqlversionmatch.split('.'))#major[9].minor[2].patch[0]
 
 #add "0" place holders
-if len(split)<2:           #e.g 9 > 9.0
-     split.append(0)
-if len(split)<3:          #e.g 9.1 > 9.1.0
-     split.append(0)
+if len(split) < 2:#e.g 9 > 9.0
+    split.append(0)
+if len(split) < 3:#e.g 9.1 > 9.1.0
+    split.append(0)
 
 def create_sqlfile(database_settings, path_to_file):
     context = Context(database_settings)
-
-    if (split[0] >= 9) & (split[1] >= 2) & (split[2] >= 0): #9.2.0 or above
+    if (split[0] >= 9) & (split[1] >= 2) & (split[2] >= 0):#9.2.0 or above
         context['PID'] = "pid"
     else:
         context['PID'] = "procpid"
