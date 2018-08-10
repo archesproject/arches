64935833d6759cf7202cd8927b1d71b784fa01fb
Alexei Peters <apeters@fargeo.com>
2018-08-10 12:05:25 -0700

    make sure to pass in the request when saving tiles as part of a resource so that we can use the logged in user, re #3706

--- arches/app/models/resource.py
+++ arches/app/models/resource.py
@@ -91,7 +91,7 @@ class Resource(models.ResourceInstance):
         super(Resource, self).save(*args, **kwargs)
         for tile in self.tiles:
             tile.resourceinstance_id = self.resourceinstanceid
-            saved_tile = tile.save(index=False)
+            saved_tile = tile.save(request=request, index=False)
         if request == '':
             if user == '':
                 user = {}
