68a620ed2239812332fe317b6bf0fa48e7f37489
Jeffrey Munowitch <jmunowitch@fargeo.com>
2018-08-23 18:14:22 -0700

    Fix in-app help for async help loading.

--- arches/app/media/js/views/graph-designer.js
+++ arches/app/media/js/views/graph-designer.js
@@ -460,13 +460,22 @@ define([
             });
 
             var helpContentLookup = {
-                permissions: 'permissions-manager-help',
-                graph: 'graph-designer-help',
-                card: 'card-manager-help'
+                permissions: {
+                    'title': 'Using the Permissions Manager',
+                    'template':'permissions-manager-help',
+                },
+                graph: {
+                    'title':'Using the Graph Designer',
+                    'template':'graph-designer-help',
+                },
+                card: {
+                    'title':'Using the Card Designer',
+                    'template':'card-manager-help',
+                }
             };
 
             viewModel.activeTab.subscribe(function(tab) {
-                viewModel.helpTemplate(helpContentLookup[tab]);
+                viewModel.helpTemplate(helpContentLookup[tab]['template']);
                 viewModel.getHelp();
             });
 
--- arches/app/media/js/views/page-view.js
+++ arches/app/media/js/views/page-view.js
@@ -43,7 +43,6 @@ define([
             this.viewModel.helpOpen = ko.observable(false);
             this.viewModel.provisionalHistoryList = new ProvisionalHistoryList({
                 items: ko.observableArray(),
-                helploaded: this.viewModel.helploaded,
                 helploading: this.viewModel.helploading
             });
 
@@ -74,27 +73,24 @@ define([
                     window.location = url;
                 },
                 getHelp: function() {
-                    if (!self.viewModel.helploaded()) {
-                        self.viewModel.helploading(true);
-                        var el = $('.ep-help-content');
-                        $.ajax({
-                            type: "GET",
-                            url: arches.urls.help_template,
-                            data: {'template': self.viewModel.helpTemplate()}
-                        }).done(function(data) {
-                            el.html(data);
-                            self.viewModel.helploaded(true);
-                            self.viewModel.helploading(false);
-                            $('.ep-help-topic-toggle').click(function() {
-                                var sectionEl = $(this).closest('div');
-                                var contentEl = $(sectionEl).find('.ep-help-topic-content').first();
-                                contentEl.slideToggle();
-                            });
-                            $('.reloadable-img').click(function(){
-                                $(this).attr('src', $(this).attr('src'));
-                            });
+                    self.viewModel.helploading(true);
+                    var el = $('.ep-help-content');
+                    $.ajax({
+                        type: "GET",
+                        url: arches.urls.help_template,
+                        data: {'template': self.viewModel.helpTemplate()}
+                    }).done(function(data) {
+                        el.html(data);
+                        self.viewModel.helploading(false);
+                        $('.ep-help-topic-toggle').click(function() {
+                            var sectionEl = $(this).closest('div');
+                            var contentEl = $(sectionEl).find('.ep-help-topic-content').first();
+                            contentEl.slideToggle();
                         });
-                    }
+                        $('.reloadable-img').click(function(){
+                            $(this).attr('src', $(this).attr('src'));
+                        });
+                    });
                 },
                 getProvisionalHistory: function() {
                     self.viewModel.provisionalHistoryList.updateList();
