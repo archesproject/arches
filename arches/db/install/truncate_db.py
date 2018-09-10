eb8ebc3a6d937a827705c354d55a4d68c1ddd975
Vincent Meijer <meijer.vincent@gmail.com>
2018-09-10 15:02:01 +0200

    #4099 Remove OWNER directive from create database script. Format document & remove unused imports

--- arches/db/install/truncate_db.py
+++ arches/db/install/truncate_db.py
@@ -1,34 +1,31 @@
-﻿import os
-import inspect
-import subprocess
-from django.template import Template
+﻿import subprocess
+
 from django.template import Context
+from django.template import Template
+
 from arches.management.commands import utils
 
+
 def create_sqlfile(database_settings, path_to_file):
-	context = Context(database_settings)
+    context = Context(database_settings)
 
-	postgres_version = subprocess.check_output(["psql", "--version"])
-	if int(postgres_version.split('.')[1]) >= 2:
-		context['PID'] = "pid"
-	else:
-		context['PID'] = "procpid"
+    postgres_version = subprocess.check_output(["psql", "--version"])
+    if int(postgres_version.split('.')[1]) >= 2:
+        context['PID'] = "pid"
+    else:
+        context['PID'] = "procpid"
 
-	t = Template(
-	"SELECT pg_terminate_backend({{ PID }}) from pg_stat_activity where datname='{{ NAME }}';\n"
-	"\n"
+    t = Template(
+        "SELECT pg_terminate_backend({{ PID }}) from pg_stat_activity where datname='{{ NAME }}';\n"
+        "\n"
 
-	"DROP DATABASE IF EXISTS {{ NAME }};\n"
-	"\n"
+        "DROP DATABASE IF EXISTS {{ NAME }};\n"
+        "\n"
 
-	"CREATE DATABASE {{ NAME }}\n"
-	"  WITH ENCODING='UTF8'\n"
-	"       OWNER={{ USER }}\n"
-	"       CONNECTION LIMIT=-1;\n"
-	"\n"
-	)
+        "CREATE DATABASE {{ NAME }}\n"
+        "  WITH ENCODING='UTF8'\n"
+        "       CONNECTION LIMIT=-1;\n"
+        "\n"
+    )
 
-	utils.write_to_file(path_to_file, t.render(context));
-
-
-
+    utils.write_to_file(path_to_file, t.render(context));
