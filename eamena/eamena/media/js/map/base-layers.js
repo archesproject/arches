af0382be3b925f4d30a47696a20251c405e106d2
Andrea <andrea.zerbini@arch.ox.ac.uk>
2018-09-14 12:50:48 +0100

    Remove all basemaps except for Google Maps #46

--- eamena/eamena/media/js/map/base-layers.js
+++ eamena/eamena/media/js/map/base-layers.js
@@ -6,21 +6,19 @@ define([
 //     'gmjs',
 //     'map/ol3gm'
 ], function($, ol, _, arches) {
-    var baseLayers = arches.bingLayers;
-    var editor = arches.editor;
-    _.each(baseLayers, function(layer) {
-        layer.layer = new ol.layer.Tile({
-            visible: false,
-            preload: Infinity,
-            source: new ol.source.BingMaps({
-                key: arches.bingKey,
-                imagerySet: layer.id
-            })
-        });
-    });
+    var baseLayers = [];
+//     _.each(baseLayers, function(layer) {
+//         layer.layer = new ol.layer.Tile({
+//             visible: false,
+//             preload: Infinity,
+//             source: new ol.source.BingMaps({
+//                 key: arches.bingKey,
+//                 imagerySet: layer.id
+//             })
+//         });
+//     });
 
-    //set default map style to Aerial
-    baseLayers[0].layer.setVisible(true);
+
     baseLayers.push({
         id: 'GM',
         name: 'Google Hybrid',
@@ -32,30 +30,8 @@ define([
             })
         })
     });
-    if (editor === true) {
-        baseLayers.push({
-            id: 'DG',
-            name: 'Digital Globe',
-            icon: arches.urls.media + 'img/map/DigitalGlobe.jpg',
-            layer: new ol.layer.Tile({
-                visible: false,
-                source: new ol.source.XYZ({
-                    url: 'http://api.tiles.mapbox.com/v4/digitalglobe.nal0mpda/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGlnaXRhbGdsb2JlIiwiYSI6ImNpdmFzc3ZsNTAwd20yenBiaWxuZzdjaHQifQ.Zirx4s_vruVlBq-UgIyKEw'
-                })
-            })
-        });
-        baseLayers.push({
-            id: 'DARE',
-            name: 'DARE/Pelagios',
-            icon: arches.urls.media + 'img/map/Pelagios.png',
-            layer: new ol.layer.Tile({
-                visible: false,
-                source: new ol.source.XYZ({
-                    url: 'http://pelagios.org/tilesets/imperium/{z}/{x}/{y}.png'
-                })
-            })
-        });
-    }
-        
+
+     //set default map style to Google Hybrid
+    baseLayers[0].layer.setVisible(true);       
     return baseLayers;
 });
