69c3ec1416799db294e1fb36d0e68afcfe7333ef
Vincent Meijer <meijer.vincent@gmail.com>
2018-11-17 17:21:32 +0100

    #2828 Resolve merge conflicts with master and cherry pick.

--- arches/app/models/resource.py
+++ arches/app/models/resource.py
@@ -16,39 +16,42 @@ You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 '''
 
-import datetime
+import uuid
 import importlib
-
-from django.core.exceptions import ObjectDoesNotExist
-from django.db import transaction
-
-from arches.app.datatypes.datatypes import DataTypeFactory
+import datetime
+from django.db.models import Q
 from arches.app.models import models
-from arches.app.models.concept import get_preflabel_from_valueid
 from arches.app.models.models import EditLog
 from arches.app.models.models import TileModel
+from arches.app.models.concept import get_preflabel_from_valueid
 from arches.app.models.system_settings import settings
-from arches.app.search.elasticsearch_dsl_builder import Query, Bool, Terms
 from arches.app.search.search_engine_factory import SearchEngineFactory
-from arches.app.utils.betterJSONSerializer import JSONSerializer
+from arches.app.search.elasticsearch_dsl_builder import Query, Bool, Terms
+from arches.app.utils.betterJSONSerializer import JSONSerializer, JSONDeserializer
+from arches.app.datatypes.datatypes import DataTypeFactory
+from django.db import transaction
 
 
 class Resource(models.ResourceInstance):
+
     class Meta:
         proxy = True
 
     def __init__(self, *args, **kwargs):
         super(Resource, self).__init__(*args, **kwargs)
+        # from models.ResourceInstance
+        # self.resourceinstanceid
+        # self.graph
+        # self.resourceinstancesecurity
+        # end from models.ResourceInstance
         self.tiles = []
 
     def get_descriptor(self, descriptor):
         module = importlib.import_module('arches.app.functions.primary_descriptors')
-        primary_descriptors_function = getattr(module, 'PrimaryDescriptorsFunction')()
-        function_config = models.FunctionXGraph.objects.filter(graph_id=self.graph_id,
-                                                               function__functiontype='primarydescriptors')
-        if len(function_config) == 1:
-            return primary_descriptors_function.get_primary_descriptor_from_nodes(self,
-                                                                                  function_config[0].config[descriptor])
+        PrimaryDescriptorsFunction = getattr(module, 'PrimaryDescriptorsFunction')()
+        functionConfig = models.FunctionXGraph.objects.filter(graph_id=self.graph_id, function__functiontype='primarydescriptors')
+        if len(functionConfig) == 1:
+            return PrimaryDescriptorsFunction.get_primary_descriptor_from_nodes(self, functionConfig[0].config[descriptor])
         else:
             return 'undefined'
 
@@ -137,8 +140,7 @@ class Resource(models.ResourceInstance):
 
         se = SearchEngineFactory().create()
         datatype_factory = DataTypeFactory()
-        node_datatypes = {str(nodeid): datatype for nodeid, datatype in
-                          models.Node.objects.values_list('nodeid', 'datatype')}
+        node_datatypes = {str(nodeid): datatype for nodeid, datatype in models.Node.objects.values_list('nodeid', 'datatype')}
         tiles = []
         documents = []
         term_list = []
@@ -153,18 +155,11 @@ class Resource(models.ResourceInstance):
 
         for resource in resources:
             resource.save_edit(edit_type='create')
-            document, terms = resource.get_documents_to_index(fetchTiles=False,
-                                                              datatype_factory=datatype_factory,
-                                                              node_datatypes=node_datatypes)
+            document, terms = resource.get_documents_to_index(fetchTiles=False, datatype_factory=datatype_factory, node_datatypes=node_datatypes)
             document['root_ontology_class'] = resource.get_root_ontology()
-            documents.append(
-                se.create_bulk_item(index='resource',
-                                    doc_type=document['graph_id'],
-                                    id=document['resourceinstanceid'],
-                                    data=document))
+            documents.append(se.create_bulk_item(index='resource', doc_type=document['graph_id'], id=document['resourceinstanceid'], data=document))
             for term in terms:
-                term_list.append(
-                    se.create_bulk_item(index='strings', doc_type='term', id=term['_id'], data=term['_source']))
+                term_list.append(se.create_bulk_item(index='strings', doc_type='term', id=term['_id'], data=term['_source']))
 
         for tile in tiles:
             tile.save_edit(edit_type='tile create', new_value=tile.data)
@@ -180,10 +175,8 @@ class Resource(models.ResourceInstance):
         if unicode(self.graph_id) != unicode(settings.SYSTEM_SETTINGS_RESOURCE_MODEL_ID):
             se = SearchEngineFactory().create()
             datatype_factory = DataTypeFactory()
-            node_datatypes = {str(nodeid): datatype for nodeid, datatype in
-                              models.Node.objects.values_list('nodeid', 'datatype')}
-            document, terms = self.get_documents_to_index(datatype_factory=datatype_factory,
-                                                          node_datatypes=node_datatypes)
+            node_datatypes = {str(nodeid): datatype for nodeid, datatype in models.Node.objects.values_list('nodeid', 'datatype')}
+            document, terms = self.get_documents_to_index(datatype_factory=datatype_factory, node_datatypes=node_datatypes)
             document['root_ontology_class'] = self.get_root_ontology()
             se.index_data('resource', self.graph_id, JSONSerializer().serializeToPython(document), id=self.pk)
             for term in terms:
@@ -223,10 +216,7 @@ class Resource(models.ResourceInstance):
                     datatype_instance.append_to_document(document, nodevalue, nodeid, tile)
                     node_terms = datatype_instance.get_search_terms(nodevalue, nodeid)
                     for index, term in enumerate(node_terms):
-                        terms.append({'_id': unicode(nodeid) + unicode(tile.tileid) + unicode(index),
-                                      '_source': {'value': term, 'nodeid': nodeid, 'nodegroupid': tile.nodegroup_id,
-                                                  'tileid': tile.tileid, 'resourceinstanceid': tile.resourceinstance_id,
-                                                  'provisional': False}})
+                        terms.append({'_id':unicode(nodeid)+unicode(tile.tileid)+unicode(index), '_source': {'value': term, 'nodeid': nodeid, 'nodegroupid': tile.nodegroup_id, 'tileid': tile.tileid, 'resourceinstanceid':tile.resourceinstance_id, 'provisional': False}})
 
             if tile.provisionaledits is not None:
                 provisionaledits = tile.provisionaledits
@@ -242,12 +232,8 @@ class Resource(models.ResourceInstance):
                                     datatype_instance.append_to_document(document, nodevalue, nodeid, tile, True)
                                     node_terms = datatype_instance.get_search_terms(nodevalue, nodeid)
                                     for index, term in enumerate(node_terms):
-                                        terms.append({'_id': unicode(nodeid) + unicode(tile.tileid) + unicode(index),
-                                                      '_source': {'value': term, 'nodeid': nodeid,
-                                                                  'nodegroupid': tile.nodegroup_id,
-                                                                  'tileid': tile.tileid,
-                                                                  'resourceinstanceid': tile.resourceinstance_id,
-                                                                  'provisional': True}})
+                                        terms.append({'_id':unicode(nodeid)+unicode(tile.tileid)+unicode(index), '_source': {'value': term, 'nodeid': nodeid, 'nodegroupid': tile.nodegroup_id, 'tileid': tile.tileid, 'resourceinstanceid':tile.resourceinstance_id, 'provisional': True}})
+
 
         return document, terms
 
@@ -292,17 +278,11 @@ class Resource(models.ResourceInstance):
 
     def get_related_resources(self, lang='en-US', limit=settings.RELATED_RESOURCES_EXPORT_LIMIT, start=0, page=0):
         """
-        Returns an object that lists the related resources, the relationship types, and a reference to the current
-        resource
+        Returns an object that lists the related resources, the relationship types, and a reference to the current resource
 
         """
-        graphs = models.GraphModel.objects.all() \
-            .exclude(pk=settings.SYSTEM_SETTINGS_RESOURCE_MODEL_ID) \
-            .exclude(isresource=False)
-
-        graph_lookup = {str(graph.graphid): {'name': graph.name, 'iconclass': graph.iconclass, 'fillColor': graph.color}
-                        for graph in graphs}
-
+        graphs = models.GraphModel.objects.all().exclude(pk=settings.SYSTEM_SETTINGS_RESOURCE_MODEL_ID).exclude(isresource=False)
+        graph_lookup = {str(graph.graphid): {'name':graph.name, 'iconclass': graph.iconclass, 'fillColor': graph.color} for graph in graphs}
         ret = {
             'resource_instance': self,
             'resource_relationships': [],
@@ -313,7 +293,7 @@ class Resource(models.ResourceInstance):
 
         if page > 0:
             limit = settings.RELATED_RESOURCES_PER_PAGE
-            start = limit * int(page - 1)
+            start = limit*int(page-1)
 
         def get_relations(resourceinstanceid, start, limit):
             query = Query(se, start=start, limit=limit)
@@ -428,12 +408,3 @@ class Resource(models.ResourceInstance):
                         values.append(value)
 
         return values
-
-    class MultipleNodesFoundException(Exception):
-        def __init__(self, name, nodes):
-            self.nodes = nodes
-            self.message = "Multiple nodes with the name '%s' were found" % name
-
-    class InvalidNodeNameException(Exception):
-        def __init__(self, name):
-            self.message = "Node with the name '%s' does not exist" % name
