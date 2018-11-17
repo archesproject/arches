7295726a449cac9a0f47f7bc7115f1f0f5e8f2af
Vincent Meijer <meijer.vincent@gmail.com>
2018-11-17 18:18:50 +0100

    #2828 Split test json so that resource_test has its own test data

--- tests/models/resource_test.py
+++ tests/models/resource_test.py
@@ -57,7 +57,7 @@ class ResourceTests(ArchesTestCase):
         cls.client.login(username='admin', password='admin')
 
         models.ResourceInstance.objects.all().delete()
-        with open(os.path.join('tests/fixtures/resource_graphs/Search Test Model.json'), 'rU') as f:
+        with open(os.path.join('tests/fixtures/resource_graphs/Resource Test Model.json'), 'rU') as f:
             archesfile = JSONDeserializer().deserialize(f)
         resource_graph_importer(archesfile['graph'])
 
