71e64bc0310f77373b686ac33f6494f741474526
benosteen <bosteen@gmail.com>
2018-12-03 09:43:21 -0800

    Added basic setup for rdf export tests and pep8 conpliant

--- tests/exporter/resource_export_tests.py
+++ tests/exporter/resource_export_tests.py
@@ -28,12 +28,16 @@ from arches.app.utils.skos import SKOSReader
 from arches.app.models.concept import Concept
 from arches.app.models.models import TileModel, ResourceInstance
 from arches.app.utils.betterJSONSerializer import JSONSerializer, JSONDeserializer
-from arches.app.utils.data_management.resources.importer import  BusinessDataImporter
-from arches.app.utils.data_management.resources.exporter import  ResourceExporter as BusinessDataExporter
+from arches.app.utils.data_management.resources.importer import BusinessDataImporter
+from arches.app.utils.data_management.resources.exporter import ResourceExporter as BusinessDataExporter
 from arches.app.utils.data_management.resource_graphs.importer import import_graph as ResourceGraphImporter
+from arches.app.datatypes.datatypes import DataTypeFactory
+from rdflib import Namespace, URIRef, Literal, Graph
+from rdflib.namespace import RDF, RDFS, XSD
 
 # these tests can be run from the command line via
 # python manage.py test tests/exporter/resource_export_tests.py --pattern="*.py" --settings="tests.test_settings"
+
 
 class BusinessDataExportTests(ArchesTestCase):
     @classmethod
@@ -55,6 +59,10 @@ class BusinessDataExportTests(ArchesTestCase):
             archesfile = JSONDeserializer().deserialize(f)
         ResourceGraphImporter(archesfile['graph'])
 
+        # for RDF/JSON-LD export tests
+        self.DT = DataTypeFactory()
+        self.archesproject = Namespace(test_settings.ARCHES_NAMESPACE_FOR_DATA_EXPORT)
+        self.cidoc = Namespace("http://www.cidoc-crm.org/cidoc-crm/")
 
     @classmethod
     def tearDownClass(cls):
@@ -63,12 +71,18 @@ class BusinessDataExportTests(ArchesTestCase):
     def test_csv_export(self):
         BusinessDataImporter('tests/fixtures/data/csv/resource_export_test.csv').import_business_data()
 
-        export = BusinessDataExporter('csv', configs='tests/fixtures/data/csv/resource_export_test.mapping', single_file=True).export()
-        csv_export = filter(lambda export: 'csv' in export['name'], export)[0]['outputfile'].getvalue().split('\r')
+        export = BusinessDataExporter('csv',
+                                      configs='tests/fixtures/data/csv/resource_export_test.mapping',
+                                      single_file=True).export()
+        csv_export = filter(lambda export: 'csv' in export['name'],
+                            export)[0]['outputfile'].getvalue().split('\r')
         csv_output = list(unicodecsv.DictReader(BytesIO(export[0]['outputfile'].getvalue()), encoding='utf-8-sig'))[0]
 
         csvinputfile = 'tests/fixtures/data/csv/resource_export_test.csv'
-        csv_input = list(unicodecsv.DictReader(open(csvinputfile, 'rU'), encoding='utf-8-sig', restkey='ADDITIONAL', restval='MISSING'))[0]
+        csv_input = list(unicodecsv.DictReader(open(csvinputfile, 'rU'),
+                         encoding='utf-8-sig',
+                         restkey='ADDITIONAL',
+                         restval='MISSING'))[0]
 
         self.assertDictEqual(csv_input, csv_output)
 
@@ -76,8 +90,8 @@ class BusinessDataExportTests(ArchesTestCase):
 
         def deep_sort(obj):
             """
-            Recursively sort list or dict nested lists
-            Taken from https://stackoverflow.com/questions/18464095/how-to-achieve-assertdictequal-with-assertsequenceequal-applied-to-values
+            Recursively sort list or dict nested lists. Taken from
+            https://stackoverflow.com/questions/18464095/how-to-achieve-assertdictequal-with-assertsequenceequal-applied-to-values
             """
 
             if isinstance(obj, dict):
@@ -96,17 +110,19 @@ class BusinessDataExportTests(ArchesTestCase):
 
             return _sorted
 
-        BusinessDataImporter('tests/fixtures/data/json/resource_export_business_data_truth.json').import_business_data()
+        BusinessDataImporter(
+            'tests/fixtures/data/json/resource_export_business_data_truth.json').import_business_data()
 
         export = BusinessDataExporter('json').export('ab74af76-fa0e-11e6-9e3e-026d961c88e6')
         json_export = deep_sort(json.loads(export[0]['outputfile'].getvalue()))
 
-        json_truth = deep_sort(json.loads(open('tests/fixtures/data/json/resource_export_business_data_truth.json').read()))
+        json_truth = deep_sort(json.loads(
+            open('tests/fixtures/data/json/resource_export_business_data_truth.json').read()))
 
         self.assertDictEqual(json_export, json_truth)
 
-    # def test_jsonld_export(self):
-        # placeholder for future jsonld export tests
+    # test_rdf_* - * = datatype (rdf fragment) or full graph
+    # test_jsonld_* -> focus on jsonld correct framing and export
 
-    # def test_rdf_export(self):
-        # placeholder for future rdf export tests
+    def test_rdf_string(self):
+        pass
