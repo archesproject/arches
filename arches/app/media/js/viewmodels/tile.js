9f86c1ab8bd4cfd9c421b66c7f3362ebd27dcca2
Cyrus Hiatt <chiatt@fargeo.com>
2018-12-14 14:12:51 -0800

    Prevents icons in the card tree from getting highlighted as provisional if the provisional object is an empty literal (which is the case if all prov edits get deleted). re #4337

--- arches/app/media/js/viewmodels/tile.js
+++ arches/app/media/js/viewmodels/tile.js
@@ -93,7 +93,8 @@ define([
             }),
             expanded: ko.observable(false),
             hasprovisionaledits: ko.pureComputed(function() {
-                return !!ko.unwrap(self.provisionaledits);
+                var edits = ko.unwrap(self.provisionaledits);
+                return !!edits && _.keys(edits).length > 0;
             }, this),
             isfullyprovisional: ko.pureComputed(function() {
                 return !!ko.unwrap(self.provisionaledits()) && _.keys(koMapping.toJS(this.data)).length === 0;
