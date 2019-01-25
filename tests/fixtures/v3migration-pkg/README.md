16549298dc27136a452c511d47677d9bdfd2580e
adam cox <mr.adamcox@gmail.com>
2019-01-25 15:00:37 -0600

    add tests for new v3 migration code

---
+++ tests/fixtures/v3migration-pkg/README.md
@@ -0,0 +1,25 @@
+#### report map settings
+
+zoom: 11
+long: 114.166261289996
+lat: 22.2808334299096
+
+#### colors
+
++. ACTIVITY.E7 - Activity
+    #6DC3FC 109,195,252
+
++. ACTOR.E39 - Actor
+    #a44b0f 164,75,15
+
++. HERITAGE_RESOURCE_GROUP.E27 - Historic District
+    #FFC53D 255,197,61
+
++. HISTORICAL_EVENT.E5 - Historic Event
+    #4EBF41 78,191,65
+
++. HERITAGE_RESOURCE.E18 - Heritage Resource
+    #fa6003 250,96,3
+
++. INFORMATION_RESOURCE.E73 - Information Resource
+    #8D45F8 141,69,248
--- tests/test_settings.py
+++ tests/test_settings.py
@@ -26,6 +26,12 @@ ROOT_DIR = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe(
 ROOT_DIR = os.path.normpath(os.path.join(ROOT_DIR, '..', 'arches'))
 TEST_ROOT = os.path.normpath(os.path.join(ROOT_DIR, '..', 'tests'))
 
+# LOAD_V3_DATA_DURING_TESTS = True will engage the most extensive the of the v3
+# data migration tests, which could add over a minute to the test process. It's
+# recommended that this setting only be set to True in tests/settings_local.py
+# and run in specific cases at the discretion of the developer.
+LOAD_V3_DATA_DURING_TESTS = False
+
 SEARCH_BACKEND = 'tests.base_test.TestSearchEngine'
 
 RESOURCE_GRAPH_LOCATIONS = (os.path.join(TEST_ROOT, 'fixtures', 'resource_graphs'),)
