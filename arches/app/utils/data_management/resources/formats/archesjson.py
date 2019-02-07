9be99eb888c435f68255fa10a9e526ffec519108
adam cox <mr.adamcox@gmail.com>
2019-02-07 10:00:23 -0600

    fix command line export of JSON resources

--- arches/app/utils/data_management/resources/formats/archesjson.py
+++ arches/app/utils/data_management/resources/formats/archesjson.py
@@ -25,34 +25,66 @@ class JsonWriter(Writer):
         super(JsonWriter, self).__init__()
 
     def write_resources(self, resources, resource_export_configs):
-        json_resources = []
-        json_resources_for_export = []
-        iso_date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        json_file_name = os.path.join('{0}_{1}.{2}'.format('EAMENA', iso_date, 'json'))
-        f = StringIO()
 
+        # this is a hacky way of fixing this method, as the real problem lies
+        # lies upstream where this method is called.
 
-        for count, resource in enumerate(resources, 1):
-            if count % 1000 == 0:
-                print "%s Resources exported" % count            
-            errors = []
-            
-            try:
-                a_resource = Resource().get(resource['_id'])
-                
-                a_resource.form_groups = None
-                json_resources.append(a_resource)
-            except Exception as e:
-                if e not in errors:
-                    errors.append(e)
-        if len(errors) > 0:
-            print errors[0], ':', len(errors)
-                   
-                   
-        f.write((JSONSerializer().serialize({'resources':json_resources}, indent = 4, separators=(',',':'))))
-        json_resources_for_export.append({'name': json_file_name, 'outputfile': f})
-        return json_resources_for_export
-        
+        # If the resources variable is a string, assume that it's been passed
+        # in through the command line, and is actually the file path/name of
+        # the desired output json file. In this case, run the original v3 code here.
+        if isinstance(resources, str):
+            dest_dir = resources
+            cursor = connection.cursor()
+            cursor.execute("""select entitytypeid from data.entity_types where isresource = TRUE""")
+            resource_types = cursor.fetchall()
+            json_resources = []
+            with open(dest_dir, 'w') as f:
+                for resource_type in resource_types:
+                    resources = archesmodels.Entities.objects.filter(entitytypeid = resource_type)
+                    print "Writing {0} {1} resources".format(len(resources), resource_type[0])
+                    errors = []
+                    for resource in resources:
+                        try:
+                            a_resource = Resource().get(resource.entityid)
+                            a_resource.form_groups = None
+                            json_resources.append(a_resource)
+                        except Exception as e:
+                            if e not in errors:
+                                errors.append(e)
+                    if len(errors) > 0:
+                        print errors[0], ':', len(errors)
+                f.write((JSONSerializer().serialize({'resources':json_resources}, separators=(',',':'))))
+            return
+        # if resources is not a str, then assume it has been passed in through the
+        # search UI instantiation of this command. In this case, run the EAMENA
+        # export logic
+        else:
+            json_resources = []
+            json_resources_for_export = []
+            iso_date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            json_file_name = os.path.join('{0}_{1}.{2}'.format('EAMENA', iso_date, 'json'))
+            f = StringIO()
+
+            for count, resource in enumerate(resources, 1):
+                if count % 1000 == 0:
+                    print "%s Resources exported" % count            
+                errors = []
+
+                try:
+                    a_resource = Resource().get(resource['_id'])
+
+                    a_resource.form_groups = None
+                    json_resources.append(a_resource)
+                except Exception as e:
+                    if e not in errors:
+                        errors.append(e)
+            if len(errors) > 0:
+                print errors[0], ':', len(errors)
+
+            f.write((JSONSerializer().serialize({'resources':json_resources}, indent = 4, separators=(',',':'))))
+            json_resources_for_export.append({'name': json_file_name, 'outputfile': f})
+            return json_resources_for_export
+
 class JsonReader():
 
     def validate_file(self, archesjson, break_on_error=True):
