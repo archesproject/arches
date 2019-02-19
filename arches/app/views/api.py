5f5b1f1126f9e503aa237e001c892dcd9d5dcc7f
benosteen <bosteen@gmail.com>
2019-02-19 11:15:45 -0800

    Removed a developer print line from the API

--- arches/app/views/api.py
+++ arches/app/views/api.py
@@ -402,7 +402,6 @@ class Resources(APIBase):
                     for resource in reader.resources:
                         with transaction.atomic():
                             resource.save(request=request)
-                        print resource.resourceinstanceid
                         response.append(JSONDeserializer().deserialize(
                             self.get(request, resource.resourceinstanceid).content))
                     return JSONResponse(response, indent=indent, status=201)
