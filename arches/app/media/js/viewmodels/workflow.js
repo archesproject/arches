b1e1766b38ca464f8d9afaa7b2c77ecc26df7991
Alexei Peters <apeters@fargeo.com>
2019-03-14 17:22:17 -0700

    initial work on workflow and step viewmodels, re #4668

---
+++ arches/app/media/js/viewmodels/workflow.js
@@ -0,0 +1,27 @@
+define([
+    'knockout',
+], function(ko) {
+    var Workflow = function(config) {
+        var self = this;
+        this.steps = ko.observableArray(config.steps);
+        this.activeStepIndex = ko.observable(0);
+        this.activeStep = ko.computed(function() {
+            var index = self.activeStepIndex;
+            return self.steps()[index()];
+        });
+
+        this.next = function(){
+            if (self.activeStepIndex() < self.steps().length - 1) {
+                self.activeStepIndex(self.activeStepIndex() + 1);
+                console.log('going to the next step');
+            }
+        };
+        this.back = function(){
+            if (self.activeStepIndex() > 0) {
+                self.activeStepIndex(self.activeStepIndex() - 1);
+                console.log('going back a step');
+            }
+        };
+    };
+    return Workflow;
+});
--- arches/app/media/js/views/components/plugins/workflow.js
+++ arches/app/media/js/views/components/plugins/workflow.js
@@ -1,19 +1,18 @@
 define([
-    'knockout',
-], function(ko) {
+    'knockout', 'viewmodels/workflow', 'viewmodels/workflow-step'
+], function(ko, Workflow, Step) {
     return ko.components.register('workflow-plugin', {
         viewModel: function() {
             this.configJSON = {};
             var steps = [
-                {'color': '#543', 'icon': 'fa-chevron-circle-right'},
-                {'color': '#543', 'icon': 'fa-alicorn'},
-                {'color': '#543', 'icon': 'fa-air-freshener '},
-                {'color': '#543', 'icon': 'fa-android'},
-                {'color': '#543', 'icon': 'fa-anchor'},
-                {'color': '#543', 'icon': 'fa-alarm-clock'},
-                {'color': '#543', 'icon': 'fa-acorn'}
+                new Step({title: 'Step 1'}),
+                new Step({title: 'Step 2'}),
+                new Step({title: 'Step 3'}),
+                new Step({title: 'Step 4'})
             ];
-            this.workflowSteps = ko.observableArray(steps);
+            this.workflow = new Workflow({
+                steps: steps
+            });
         },
         template: { require: 'text!templates/views/components/plugins/workflow.htm' }
     });
