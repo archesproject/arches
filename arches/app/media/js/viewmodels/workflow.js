12f2a11ae090a11dacd79d0f76b142e2f70d2350
Alexei Peters <apeters@fargeo.com>
2019-03-24 21:52:55 -0700

    initial work on an api endpoint to instantiate a card for the new-tile workflow, re #4669

--- arches/app/media/js/viewmodels/workflow.js
+++ arches/app/media/js/viewmodels/workflow.js
@@ -3,21 +3,31 @@ define([
 ], function(ko, Step, NewTileStep) {
     var Workflow = function(config) {
         var self = this;
-        this.steps = ko.observableArray([
-            new Step({title: 'Step 1', description: 'A description here', component: 'new-tile-step'}),
-            new Step({title: 'Step 2', description: 'A very long and verboser description here that explains many different things about the workflow step'}),
-            new Step({title: 'Step 3'}),
-            new Step({title: 'Step 4', description: 'Another description here'})
-        ]);
-        this.activeStepIndex = ko.observable(0);
-        this.activeStep = ko.computed(function() {
-            this.steps().forEach(function(step){
-                step.active(false);
-            });
-            var activeStep = self.steps()[self.activeStepIndex()];
-            activeStep.active(true);
-            return activeStep;
-        }, this);
+
+        fetch('/cards/042fdc54-fa30-11e6-9e3e-026d961c88e6', {
+            method: 'GET'
+        }).then(function(response){
+            if(response.status == 200){
+                return response.json();
+            }
+        }).then(function(response){
+            this.steps = ko.observableArray([
+                new Step({title: 'Step 1', description: 'A description here', component: 'new-tile-step'}),
+                new Step({title: 'Step 2', description: 'A very long and verboser description here that explains many different things about the workflow step'}),
+                new Step({title: 'Step 3'}),
+                new Step({title: 'Step 4', description: 'Another description here'})
+            ]);
+            this.activeStepIndex = ko.observable(0);
+            this.activeStep = ko.computed(function() {
+                this.steps().forEach(function(step){
+                    step.active(false);
+                });
+                var activeStep = self.steps()[self.activeStepIndex()];
+                activeStep.active(true);
+                return activeStep;
+            }, this);
+        })
+
 
         this.next = function(){
             if (self.activeStepIndex() < self.steps().length - 1) {
