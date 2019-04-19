eb15c18ecd742de0e7e4822c4b15a27c07690ae6
Galen Mancino <whatisgalen@gmail.com>
2019-04-19 00:31:09 -0700

    re-inserted cardinality attribute that was accidentally deleted from last commit

--- arches/app/media/js/models/card.js
+++ arches/app/media/js/models/card.js
@@ -37,6 +37,9 @@ define([
             this.helptitle = ko.observable();
             this.helpactive = ko.observable(false);
             this.cardinality = ko.observable();
+            if (attributes.data.nodegroup) {
+                this.cardinality(attributes.data.nodegroup.cardinality);
+            }
             this.visible = ko.observable();
             this.active = ko.observable();
             this.ontologyproperty = ko.observable();
