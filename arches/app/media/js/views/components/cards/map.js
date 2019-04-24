38aed98bd98908d570fd2b7cadccb5f563892ee1
Rob Gaston <rgaston@fargeo.com>
2019-04-23 18:14:25 -0700

    stubs out map card component, re: #4670

---
+++ arches/app/media/js/views/components/cards/map.js
@@ -0,0 +1,28 @@
+define([
+    'arches',
+    'knockout',
+    'viewmodels/card-component'
+], function(arches, ko, CardComponentViewModel) {
+    return ko.components.register('map-card', {
+        viewModel: function(params) {
+            var layers = [];
+            arches.mapLayers.forEach(function(layer) {
+                if (layer.addtomap) {
+                    layers = layers.concat(layer.layer_definitions);
+                }
+            });
+            this.mapStyle = {
+                "version": 8,
+                "sources": arches.mapSources,
+                "sprite": arches.mapboxSprites,
+                "glyphs": arches.mapboxGlyphs,
+                "layers": layers
+            };
+
+            CardComponentViewModel.apply(this, [params]);
+        },
+        template: {
+            require: 'text!templates/views/components/cards/map.htm'
+        }
+    });
+});
