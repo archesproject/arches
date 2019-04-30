3a69158af388041e475656700e9a7fe2c86d186b
Alexei Peters <apeters@fargeo.com>
2019-04-30 16:36:27 -0700

    remove unneccesary view, re #4771

--- arches/urls.py
+++ arches/urls.py
@@ -69,7 +69,6 @@ urlpatterns = [
     url(r'^concepts/get_pref_label', concept.get_pref_label, name="get_pref_label"),
     url(r'^conceptvalue/', concept.concept_value, name="concept_value"),
     url(r'^search$', search.SearchView.as_view(), name="search_home"),
-    url(r'^new_search$', search.NewSearchView.as_view(), name="new_search_home"),
     url(r'^search/terms$', search.search_terms, name="search_terms"),
     url(r'^search/resources$', search.search_results, name="search_results"),
     url(r'^search/time_wheel_config$', search.time_wheel_config, name="time_wheel_config"),
