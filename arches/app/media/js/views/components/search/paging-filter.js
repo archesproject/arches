522790497f0a5ca49a20d915272d10996492ea5b
Alexei Peters <apeters@fargeo.com>
2019-05-09 12:42:58 -0700

    make sure to sync pagination correctly, re #4771

--- arches/app/media/js/views/components/search/paging-filter.js
+++ arches/app/media/js/views/components/search/paging-filter.js
@@ -6,18 +6,13 @@ function(BaseFilter, ko, koMapping) {
     var componentName = 'paging-filter';
     return ko.components.register(componentName, {
         viewModel: BaseFilter.extend({
-
-            events: {
-                'click .related-resources-graph': 'showRelatedResouresGraph',
-                'click .navigate-map': 'zoomToFeature',
-                'mouseover .arches-search-item': 'itemMouseover',
-                'mouseout .arches-search-item': 'itemMouseout'
-            },
-
             initialize: function(options) {
                 options.name = 'Paging Filter';
                 BaseFilter.prototype.initialize.call(this, options);
                 this.page = ko.observable();
+                this.preventLoop = false;
+                this.userRequestedNewPage = false;
+                this.pageInitialized = false;
                 this.paginator = koMapping.fromJS({
                     current_page: 1,
                     end_index: 1,
@@ -29,8 +24,16 @@ function(BaseFilter, ko, koMapping) {
                     previous_page_number: null,
                     start_index: 1
                 });
-                //this.showPaginator = ko.observable(true);
-                this.userRequestedNewPage = ko.observable(false);
+
+                this.query.subscribe(function() {
+                    if (this.preventLoop === false && this.userRequestedNewPage === false && this.pageInitialized === true) {
+                        this.preventLoop = true;
+                        this.page(1);
+                    } else {
+                        this.preventLoop = false;
+                        this.userRequestedNewPage = false;
+                    }
+                }, this, 'beforeChange');
 
                 this.page.subscribe(function(timestamp) {
                     this.updateQuery();
@@ -42,21 +45,18 @@ function(BaseFilter, ko, koMapping) {
 
                 this.filters[componentName](this);
                 this.restoreState();
+                this.pageInitialized = true;
             },
 
             updateQuery: function() {
                 var queryObj = this.query();
                 queryObj[componentName] = this.page();
                 this.query(queryObj);
-
-                // var self = this;
-                // koMapping.fromJS(this.searchResults.paginator, this.paginator);
-                // this.showPaginator(true);
             },
 
             newPage: function(page){
                 if(page){
-                    this.userRequestedNewPage(true);
+                    this.userRequestedNewPage = true;
                     this.page(page);
                 }
             },
@@ -74,7 +74,6 @@ function(BaseFilter, ko, koMapping) {
                 if(!!this.searchResults[componentName] && !!this.searchResults[componentName]['paginator']) {
                     koMapping.fromJS(this.searchResults[componentName]['paginator'], this.paginator);
                 }
-                //this.showPaginator(true);
             }
         }),
         template: { require: 'text!templates/views/components/search/paging-filter.htm' }
