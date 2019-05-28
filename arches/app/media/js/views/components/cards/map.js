f05cbd087a9a393b4ff94922dcda5a0bedd7d05b
Rob Gaston <rgaston@fargeo.com>
2019-05-28 15:50:56 -0700

    fixes card component wrapper styles, basemaps list re: #4760

--- arches/app/media/js/views/components/cards/map.js
+++ arches/app/media/js/views/components/cards/map.js
@@ -1,20 +1,55 @@
 define([
+    'jquery',
     'arches',
     'knockout',
     'viewmodels/card-component'
-], function(arches, ko, CardComponentViewModel) {
+], function($, arches, ko, CardComponentViewModel) {
     return ko.components.register('map-card', {
         viewModel: function(params) {
             var self = this;
             var layers = [];
+            this.basemaps = [];
+            this.activeBasemap = ko.observable();
             arches.mapLayers.forEach(function(layer) {
+                if (!layer.isoverlay) self.basemaps.push(layer);
                 if (layer.addtomap) {
                     layers = layers.concat(layer.layer_definitions);
+                    if (!layer.isoverlay) self.activeBasemap(layer);
                 }
             });
+
             this.mapStyle = {
                 "version": 8,
-                "sources": arches.mapSources,
+                "sources": $.extend(true, {
+                    "resource": {
+                        "type": "geojson",
+                        "data": {
+                            "type": "FeatureCollection",
+                            "features": []
+                        }
+                    },
+                    "search-results-hex": {
+                        "type": "geojson",
+                        "data": {
+                            "type": "FeatureCollection",
+                            "features": []
+                        }
+                    },
+                    "search-results-hashes": {
+                        "type": "geojson",
+                        "data": {
+                            "type": "FeatureCollection",
+                            "features": []
+                        }
+                    },
+                    "search-results-points": {
+                        "type": "geojson",
+                        "data": {
+                            "type": "FeatureCollection",
+                            "features": []
+                        }
+                    }
+                }, arches.mapSources),
                 "sprite": arches.mapboxSprites,
                 "glyphs": arches.mapboxGlyphs,
                 "layers": layers
