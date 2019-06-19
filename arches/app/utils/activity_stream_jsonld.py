896fa822a74a8abb35a24c9bdc20f05244616d77
benosteen <bosteen@gmail.com>
2019-06-19 16:20:29 -0700

    Switching to OrderedCollection/Page pattern

--- arches/app/utils/activity_stream_jsonld.py
+++ arches/app/utils/activity_stream_jsonld.py
@@ -17,35 +17,89 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 '''
 
 from django.utils.feedgenerator import rfc3339_date
-from django.core.urlresolvers import reverse as url_lookup
+from django.core.urlresolvers import reverse
 from arches.app.models.resource import Resource
 from arches.app.models.system_settings import settings
 import json
 
+
 class ActivityStreamCollection(object):
+    # uris MUST contain keys to URIs: first, last, root (Collection URI)
+    def __init__(self, uris, totalItems, summary=None,
+                 base_uri_for_arches="https://example.org",
+                 context="https://www.w3.org/ns/activitystreams",
+                 perm_level="full"):
+        self.representation = {"@context": context,
+                               "type": "OrderedCollection",
+                               "id": uris['root'],
+                               "totalItems": totalItems,
+                               "first": {"id": uris["first"],
+                                         "type": "OrderedCollectionPage"},
+                               "last": {"id": uris["last"],
+                                         "type": "OrderedCollectionPage"},}
+        self.uris = uris
+        self.id = uris["root"]
+        self.perm_level = perm_level
+        self.base_uri_for_arches = base_uri_for_arches
+
+    def to_jsonld(self):
+        return json.dumps(self.representation)
+
+    def to_obj(self):
+        return self.representation.copy()
+
+    def generate_page(self, page_uris, editlog_object_generator):
+        page_uris['root'] = self.id
+        page_uris['base_uri_for_arches'] = self.base_uri_for_arches
+        colpage = ActivityStreamCollectionPage(page_uris, perm_level=self.perm_level)
+        for op in editlog_object_generator:
+            colpage.add_item(op)
+        return colpage
+
+
+class ActivityStreamCollectionPage(object):
 
     type_mapping = {
         'create': "Create",
         'delete': "Delete",
-        'tile delete': "Remove",  # From resint?
-        'tile create': "Add",
+        'tile delete': "Update",  # pass all tile events as Resource updates.
+        'tile create': "Update",
         'tile edit': "Update",
         'delete edit': "Edit Deleted"
     }
 
-    def __init__(self, summary="", 
+    def __init__(self, uris,  # MUST contain keys to URIs: this, root (Collection URI)
+                              # SHOULD contain keys to URIs: next, prev
                        context="https://www.w3.org/ns/activitystreams",
-                       ordered=False,
                        perm_level="full"):   # "full"|"idsonly"
-        self._boilerplate = {"@context": context, "summary": summary, "type": "Collection"}
-        self._ordered = ordered
+        self._boilerplate = {"@context": context,
+                             "type": "OrderedCollectionPage",
+                             "id": uris['this']}
+
+        self._boilerplate["partOf"] = {"id": uris["root"],
+                                       "type": "OrderedCollection"}
+
+        for k,v in [(x,y) for x,y in uris.items() if x in ['next', 'prev']]:
+            self._boilerplate[k] = {"id": v, "type": "OrderedCollectionPage"}
+
         self._items = []
+        self.base_uri_for_arches = uris.get('base_uri_for_arches', '')
         self.perm_level = perm_level
 
     def summary(self, summary=None):
         if summary is not None:
             self._boilerplate["summary": summary]
         return self._boilerplate["summary": summary]
+
+    def startIndex(self, startIndex=None):
+        if startIndex is not None:
+            self._boilerplate["startIndex": startIndex]
+        return self._boilerplate["startIndex": startIndex]
+
+    def total(self, totalItems=None):
+        if totalItems is not None:
+            self._boilerplate["totalItems": totalItems]
+        return self._boilerplate["totalItems": totalItems]
 
     def add_item(self, editlog_object, perm_level="full"):
         # add a JSON-LD obj to a list of the Activities
@@ -54,7 +108,8 @@ class ActivityStreamCollection(object):
     def editlog_to_collection_item(self, editlog_object, perm_level="full"):
         def add_actor(editlog_object, perm_level = perm_level):
             actor = {"type": "Person", 
-                     "url": "{0}/{1}".format(url_lookup("user_profile_manager"), editlog_object.userid)}
+                     "url": "{0}/{1}".format(self.base_uri_for_arches + reverse("user_profile_manager"),
+                                             editlog_object.userid)}
             if perm_level == "full":
                 actor["name"] = "{0}, {1}".format(editlog_object.user_lastname, editlog_object.user_firstname)
                 actor["tag"] = editlog_object.user_username
@@ -62,22 +117,19 @@ class ActivityStreamCollection(object):
 
         def add_resource(editlog_object, perm_level = perm_level):
             r = Resource.objects.get(pk=editlog_object.resourceinstanceid)
-            # FIXME: Get real type, and potentially, add context 
-            obj = {"type": "Object"}
+            rclass = r.get_root_ontology()
+            # FIXME: Get real type if it exists, and potentially, add context
+            obj = {"type": rclass}
             if editlog_object.edittype == 'delete':
                 # Tombstone instead.
                 obj["formerType"] = obj["type"]
                 obj["type"] = "Tombstone"
-            obj["url"] = url_lookup("resources", args=(editlog_object.resourceinstanceid,))
+            obj["url"] = self.base_uri_for_arches + reverse("resources", args=(editlog_object.resourceinstanceid,))
 
             return obj
 
         def add_tile(editlog_object, perm_level = perm_level):
             obj = {"type": "Object"}  # Tile?
-            if editlog_object.edittype.endswith("delete"):
-                # Tombstone instead.
-                obj["formerType"] = obj["type"]
-                obj["type"] = "Tombstone"
             obj['url'] = "{0}node/{1}/tile/{2}".format(settings.ARCHES_NAMESPACE_FOR_DATA_EXPORT,
                                                       editlog_object.nodegroupid,
                                                       editlog_object.tileinstanceid)
@@ -86,23 +138,24 @@ class ActivityStreamCollection(object):
         item = {"type": self.type_mapping.get(editlog_object.edittype, "Activity")}
 
         # what's the correct attr here?
-        item["startTime"] = rfc3339_date(editlog_object.timestamp)
+        item["endTime"] = rfc3339_date(editlog_object.timestamp)
 
-        if editlog_object.edittype in ("create", "delete"):
+        if item['type'] in ("Create", "Delete"):
             # activity affects main resource
             item["actor"] = add_actor(editlog_object)
             item["object"] = add_resource(editlog_object)
 
-        if item['type'] in ("Remove", "Add", "Update"):
+        if item['type'] in ("Update"):
             # activity affects a Tile associated with a resource
+            # If a Tile associated with a resource is altered, bubble that
+            # change up to be about the Resource, and mark it as an 'Update'
             item["actor"] = add_actor(editlog_object)
-            item["object"] = add_tile(editlog_object)
-            item["target"] = add_resource(editlog_object)
+           #item["object"] = add_tile(editlog_object)
+            item["object"] = add_resource(editlog_object)
 
         return item
 
     def to_jsonld(self, pagination=False):
         export = self._boilerplate.copy()
-        export["totalItems"] = len(self._items)
-        export["items"] = self._items
+        export["orderedItems"] = self._items
         return json.dumps(export)
