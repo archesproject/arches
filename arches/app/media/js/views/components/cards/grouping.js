d8cbda45f7de352dd290f993cc19417b170aa680
Alexei Peters <apeters@fargeo.com>
2019-06-25 11:51:30 -0700

    get save of grouping card component working, re #4907

--- arches/app/media/js/views/components/cards/grouping.js
+++ arches/app/media/js/views/components/cards/grouping.js
@@ -13,14 +13,27 @@ define([
         }, this);
         return flatList;
     };
+
+    
     return ko.components.register('grouping-card-component', {
         viewModel: function(params) {
+            var self = this;
             // params.form is the CardTreeViewModel
+            var populateCardTiles = function() {
+                _.each(self.groupedCards().concat(self.card.model.id), function(cardid) {
+                    var card = self.cardLookup[cardid];
+                    if (card.tiles().length === 0) {
+                        card.tiles.push(card.getNewTile());
+                    }
+                    //self.tiles.push(card.tiles()[0]);
+                }, self);
+            };
             params.configKeys = ['groupedCards'];
             CardComponentViewModel.apply(this, [params]);
             //if (params.state !== 'editor-tree') {
                 var cards = !!params.card.parent ? params.card.parent.cards : flattenTree(params.card.topCards, []);
                 this.cardLookup = {};
+                //this.tiles = ko.observableArray();
                 this.siblingCards = ko.observableArray();
                 _.each(cards, function(card) {
                     this.cardLookup[card.model.id] = card;
@@ -31,6 +44,65 @@ define([
                         this.siblingCards.push({'name': card.model.name(), 'id': card.model.id});
                     }
                 }, this);
+
+                if (!!params.preview) {
+                    populateCardTiles();
+                }
+
+                this.groupedTiles = ko.computed(function() {
+                    var tiles = [];
+                    _.each(this.groupedCards().concat(this.card.model.id), function(cardid) {
+                        var card = this.cardLookup[cardid];
+                        if (card.tiles().length > 0) {
+                            tiles.push(card.tiles()[0]);
+                        }
+                    }, this);
+                    return tiles;
+                }, this);
+
+
+                this.dirty = ko.computed(function() {
+                    return _.find(this.groupedTiles(), function(tile) {
+                        return tile.dirty();
+                    }, this);
+                }, this);
+
+
+                this.saveTiles = function(){
+                    console.log('in saveTiles');
+                    var self = this;
+                    var errors = [];
+                    var tile = this.groupedTiles().pop();
+                    tile.save(function(response) {
+                        errors.push(response);
+                            //params.form.alert(new AlertViewModel('ep-alert-red', response.responseJSON.message[0], response.responseJSON.message[1], null, function(){}));
+                    }, function(response){
+                        var resourceInstanceId = response.resourceinstance_id;
+                        _.each(self.groupedTiles(), function(tile) {
+                            tile.resourceinstance_id = resourceInstanceId;
+                            tile.save(function(response) {
+                                errors.push(response);
+                                //params.form.alert(new AlertViewModel('ep-alert-red', response.responseJSON.message[0], response.responseJSON.message[1], null, function(){}));
+                            });
+                        }, self);
+                    });
+                    //errors.forEach
+                };
+
+                this.deleteTiles = function(){
+                    console.log('in deleteTiles');
+                };
+
+                this.resetTiles = function(){
+                    console.log('in resetTiles');
+                };
+
+                this.selectGroupCard = function(blah) {
+                    console.log('selectGroupCard');
+                    console.log(blah)
+                    populateCardTiles();
+                    this.card.selected(true);
+                };
 
                 // this.groupedCards = ko.observableArray();
                 // params.card.model.set('config', this.groupedCards);
