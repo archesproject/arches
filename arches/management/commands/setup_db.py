c196e7d4805c48c819b66804585dbded33088c24
adam cox <mr.adamcox@gmail.com>
2019-06-25 13:26:02 -0500

    update to python 3 print statements re: #4944

--- arches/management/commands/setup_db.py
+++ arches/management/commands/setup_db.py
@@ -142,7 +142,7 @@ CREATE DATABASE {}
         try:
             cursor.execute(create_query)
         except psycopg2.ProgrammingError as e:
-            print e.pgerror
+            print(e.pgerror)
             if "template database" in e.pgerror:
                 msg = """It looks like your PostGIS template database is not correctly referenced in
 settings.py/settings_local.py, or it has not yet been created.
@@ -152,7 +152,7 @@ To create it, use:
     psql -U {0} -c "CREATE DATABASE {1};"
     psql -U {0} -d {1} -c "CREATE EXTENSION postgis;"
 """.format(arches_db['USER'], arches_db['POSTGIS_TEMPLATE'])
-                print msg
+                print(msg)
             exit()
 
     def setup_db(self):
