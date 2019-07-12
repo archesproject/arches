601421b6fec902f0db6ce0bd9f17b6f9db8667e6
Cyrus Hiatt <chiatt@fargeo.com>
2019-07-12 10:11:08 -0700

    Coordinates tabbed report preview with tabbed report config changes. re #4684

--- arches/app/media/js/reports/tabbed.js
+++ arches/app/media/js/reports/tabbed.js
@@ -11,76 +11,55 @@ define([
 ], function(_, ko, koMapping, $, ReportViewModel) {
     return ko.components.register('tabbed-report', {
         viewModel: function(params) {
-            var self = this;
-            params.configKeys = ['nodes'];
+            params.configKeys = ['tabs'];
             ReportViewModel.apply(this, [params]);
-
-            // this.tabs = ko.observableArray([]);
-            // self.config()["tabs"].forEach( function(tab) { self.tabs().push(tab); });
-
-            self.tabs = koMapping.fromJS(self.config()['tabs'])
-
+            var self = this;
+            this.tabs(koMapping.fromJS(this.tabs())());
             this.activeTab = ko.observable(self.tabs()[0]);
 
-            // this.activeStatus = ko.pureComputed( function(tab) {
-            //     return tab["name"] == self.activeTab["name"] ? "active-report-tab": "report-tab";
-            // }, this);
-            this.topcards = self.report.cards().map(function(card){
+            this.topcards = ko.unwrap(self.report.cards).map(function(card){
                 return {name: card.model.name(), nodegroupid: card.nodegroupid};
             });
 
-            this.makeActiveTab = function(tab) { self.activeTab(tab); };
-
-            this.isActiveTab = function(checkTab) {
-                self.tabs().forEach(function(tab) { if(tab["name"] == checkTab["name"]) { return true; } });
-                return false;
-            };
-
             this.cardInActiveTab = function(cardNodegroupId) {
                 if (self.activeTab()) {
-                    self.activeTab()["nodegroup_ids"]().forEach( function(tabNodegroupId) {
-                        if (cardNodegroupId == tabNodegroupId) { return true; }
+                    self.activeTab().nodegroup_ids().forEach( function(tabNodegroupId) {
+                        if (cardNodegroupId === tabNodegroupId) { return true; }
                     });
                 }
                 return false;
             };
 
-            this.tabs.subscribe(function(tabs){
-                console.log(tabs.length)
-            })
-
-            this.jsontabs = ko.pureComputed(function(){
-                console.log(self.tabs()[0].name())
-                return koMapping.toJS(self.tabs);
-            });
-
-            this.jsontabs.subscribe(function(tabs){
-                console.log('updating config')
-                console.log(self.report.configJSON().tabs.length, self.config().tabs.length)
-                self.report.configJSON({tabs: koMapping.toJS(tabs)});
-            });
-
-            this.activeCards = ko.computed( function() {
+            this.activeCards = ko.computed(function() {
                 var cardList = [];
                 ko.unwrap(self.report.cards).forEach(function(card) {
                     if (self.activeTab()) {
                         self.activeTab()["nodegroup_ids"]().forEach( function(tabNodegroupId) {
-                            if(card.nodegroupid == tabNodegroupId) { cardList.push(card); }
+                            if(card.nodegroupid === tabNodegroupId) { cardList.push(card); }
                         });
                     }
                 });
                 return cardList;
-
-                //(expression below) -- filter(...cardInActiveTab(...)) wasn't working for some reason
-                // return self.report.cards().filter(function(card) { return self.cardInActiveTab(card.nodegroupid); });
             });
 
+            this.refreshTabs = function(){
+                var data = self.tabs().slice(0);
+                self.tabs([]);
+                self.tabs(data);
+            };
+
             this.addTab = function(){
-                this.tabs.push({icon:'', name:'', nodegroup_ids:[]});
+                this.tabs.push({
+                    icon: ko.observable(),
+                    name: ko.observable(),
+                    nodegroup_ids: ko.observableArray()
+                });
+                this.refreshTabs();// this.tabs.valueHasMutated() not working here!?;
             };
 
             this.removeTab = function(tab){
                 this.tabs.remove(tab);
+                this.refreshTabs();
             };
 
         },
