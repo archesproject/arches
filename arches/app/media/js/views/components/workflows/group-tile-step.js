5aae2804872131e4bc2592312fcd4102a4e0ae73
Cyrus Hiatt <chiatt@fargeo.com>
2019-07-19 11:14:24 -0700

    Adds 'group-tile-step' which inherits from 'new-tile-step' to handle multiple tiles passed to the onSaveSuccess method. re archesproject/consultations-prj#79

---
+++ arches/app/media/js/views/components/workflows/group-tile-step.js
@@ -0,0 +1,34 @@
+define([
+    'underscore',
+    'jquery',
+    'arches',
+    'knockout',
+    'knockout-mapping',
+    'models/graph',
+    'viewmodels/card',
+    'views/components/workflows/new-tile-step',
+    'viewmodels/provisional-tile',
+    'viewmodels/alert'
+], function(_, $, arches, ko, koMapping, GraphModel, CardViewModel, NewTileStep, ProvisionalTileViewModel, AlertViewModel) {
+    function viewModel(params) {
+        var self = this;
+        NewTileStep.apply(this, [params]);
+        self.onSaveSuccess = function(tiles) {
+            self.tiles = tiles;
+            tiles.forEach(function(tile){
+                params.resourceid(tile.resourceinstance_id);
+                self.resourceId(tile.resourceinstance_id);
+            });
+            if (self.completeOnSave === true) {
+                self.complete(true);
+            }
+        };
+    }
+    ko.components.register('group-tile-step', {
+        viewModel: viewModel,
+        template: {
+            require: 'text!templates/views/components/workflows/new-tile-step.htm'
+        }
+    });
+    return viewModel;
+});
