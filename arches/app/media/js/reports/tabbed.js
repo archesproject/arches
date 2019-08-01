0be262811b8cedf1c867e4a952fc86522b864617
Cyrus Hiatt <chiatt@fargeo.com>
2019-08-01 16:12:53 -0700

    Allows user to select icons from a dropdown. re #4684

--- arches/app/media/js/reports/tabbed.js
+++ arches/app/media/js/reports/tabbed.js
@@ -1,14 +1,12 @@
 define([
+    'jquery',
     'underscore',
     'knockout',
     'knockout-mapping',
-    'jquery',
     'viewmodels/report',
-    'arches',
-    'knockstrap',
-    'bindings/chosen',
-    'views/components/widgets/map'
-], function(_, ko, koMapping, $, ReportViewModel) {
+    'graph-designer-data',
+    'bindings/chosen'
+], function($, _, ko, koMapping, ReportViewModel, data) {
     return ko.components.register('tabbed-report', {
         viewModel: function(params) {
             params.configKeys = ['tabs', 'activeTabIndex'];
@@ -17,6 +15,7 @@ define([
             if (this.activeTabIndex() > self.tabs().length - 1) {
                 this.activeTabIndex(self.tabs().length - 1);
             }
+            this.icons = data.icons;
             this.activeTab = ko.observable(self.tabs()[ko.unwrap(this.activeTabIndex)]);
             this.report.configJSON.subscribe(function(){
                 if (self.tabs.indexOf(self.activeTab()) === -1) {
