d9d3f1dffd5612fb14688ecfa8c8ae720eeabe14
Rob Gaston <rgaston@fargeo.com>
2019-08-20 08:21:37 -0700

    fixes source layer migration

--- arches/app/models/migrations/5076_adds_map_card_sourcelayer_config.py
+++ arches/app/models/migrations/5076_adds_map_card_sourcelayer_config.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
                 where componentid = '3c103484-22d1-4ca9-a9f3-eb3902d567ac';
             update cards
                 set config = jsonb_set(config, '{selectSourceLayer}', '""')
-                where componentid = '3c103484-22d1-4ca9-a9f3-eb3902d567ac' and config;
+                where componentid = '3c103484-22d1-4ca9-a9f3-eb3902d567ac';
             """,
             reverse_sql="""
             update card_components
