cbae18fc784453153f2dd2f67a0c2d53d03222eb
Rob Sanderson <azaroth42@gmail.com>
2019-10-18 15:47:57 -0700

    check if domain is also a concept and use correct URI

--- arches/app/datatypes/concept_types.py
+++ arches/app/datatypes/concept_types.py
@@ -141,6 +141,12 @@ class ConceptDataType(BaseConceptDataType):
         if edge_info['range_tile_data'] is not None:
             c = ConceptValue(str(edge_info['range_tile_data']))
 
+            if edge_info['domain_tile_data'] is not None:
+                dc = ConceptValue(str(edge_info['domain_tile_data']))
+                d_uri = URIRef(archesproject['concepts/%s' % dc.conceptid])
+            else:
+                d_uri = edge_info['d_uri']
+
             # create a default node
             arches_uri = BNode()
             ext_idents = []
@@ -154,7 +160,7 @@ class ConceptDataType(BaseConceptDataType):
             rangenode = get_rangenode(arches_uri, ext_idents)
 
             g.add((rangenode, RDF.type, URIRef(edge.rangenode.ontologyclass)))
-            g.add((edge_info['d_uri'], URIRef(edge.ontologyproperty), rangenode))
+            g.add((d_uri, URIRef(edge.ontologyproperty), rangenode))
 
             assert c.value is not None, "Null or blank concept value"
             g.add((rangenode, URIRef(RDFS.label), Literal(c.value)))
