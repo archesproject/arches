e069ab6a2b52e635a149d54af83eb20610ee623d
Galen Mancino <whatisgalen@gmail.com>
2019-10-24 12:20:21 -0700

    adds canFinish method that checks for tiles created for required steps, re #384

--- arches/app/media/js/viewmodels/workflow.js
+++ arches/app/media/js/viewmodels/workflow.js
@@ -44,8 +44,21 @@ define([
             this.state = res;
         };
 
+        this.canFinish = ko.pureComputed(function(){
+            var required = false, canFinish = true, tileid = null;
+            for(var i = 0; i < self.steps.length; i++) {
+                required = ko.unwrap(self.steps[i].required);
+                tileid = ko.unwrap(self.steps[i].tileid);
+                if(required && (tileid == "" || !tileid)) {
+                    canFinish = false;
+                    break;
+                }
+            }
+            return canFinish;
+        });
+
         this.finishWorkflow = function() {
-            self.activeStep(self.steps[self.steps.length-1]);
+            if(self.canFinish()){ self.activeStep(self.steps[self.steps.length-1]); }
         };
 
         this.quitWorkflow = function(){
