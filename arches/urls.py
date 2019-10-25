3b89c5c99414140d09cb41f4d61d186c130feb7b
Galen Mancino <whatisgalen@gmail.com>
2019-10-25 14:19:44 -0700

    more fixes for linting errors, re #5463

--- arches/urls.py
+++ arches/urls.py
@@ -162,7 +162,11 @@ urlpatterns = [
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', admin.site.urls),
-    url(r'^password_reset/$', auth_views.PasswordResetView.as_view(), name='password_reset', kwargs={"password_reset_form": ArchesPasswordResetForm}),
+    url(r'^password_reset/$',
+        auth_views.PasswordResetView.as_view(),
+        name='password_reset',
+        kwargs={"password_reset_form": ArchesPasswordResetForm}
+        ),
     url(r'^password_reset/done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         auth_views.PasswordResetConfirmView.as_view(),
