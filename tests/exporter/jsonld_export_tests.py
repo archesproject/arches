0ad376c67813508ef55e31fa4bd1ed4d41730643
Rob Sanderson <azaroth42@gmail.com>
2019-10-29 09:26:39 -0700

    403?

--- tests/exporter/jsonld_export_tests.py
+++ tests/exporter/jsonld_export_tests.py
@@ -8,6 +8,7 @@ from tests import test_settings
 from operator import itemgetter
 from django.core import management
 from django.test.client import RequestFactory, Client
+from django.contrib.auth.models import User, Group, AnonymousUser
 from django.urls import reverse
 from tests.base_test import ArchesTestCase
 from arches.app.utils.skos import SKOSReader
@@ -29,13 +30,17 @@ class JsonLDExportTests(ArchesTestCase):
         cls.loadOntology()
         cls.factory = RequestFactory()
         cls.client = Client()
-        skos = SKOSReader()
-        rdf = skos.read_file('tests/fixtures/data/concept_label_test_scheme.xml')
-        ret = skos.save_concepts_from_skos(rdf)
+        cls.client.login(username='admin', password='admin')
 
-        skos = SKOSReader()
-        rdf = skos.read_file('tests/fixtures/data/concept_label_test_collection.xml')
-        ret = skos.save_concepts_from_skos(rdf)
+        #cls.user = User.objects.get(username='anonymous')
+
+        #skos = SKOSReader()
+        #rdf = skos.read_file('tests/fixtures/data/concept_label_test_scheme.xml')
+        #ret = skos.save_concepts_from_skos(rdf)
+
+        #skos = SKOSReader()
+        #rdf = skos.read_file('tests/fixtures/data/concept_label_test_collection.xml')
+        #ret = skos.save_concepts_from_skos(rdf)
 
         # Load up the models once
         # As we need to test resource-instance and resource-instance-list
@@ -45,6 +50,8 @@ class JsonLDExportTests(ArchesTestCase):
 
     def setUp(self):
         # This runs every test
+        #response = self.client.post(reverse('get_token'), {'username': 'admin', 'password': 'admin'})
+        #self.token = response.content
         pass
 
     @classmethod
@@ -53,7 +60,6 @@ class JsonLDExportTests(ArchesTestCase):
 
     def tearDown(self):
         pass
-
 
     def test_missing_resource(self):
         # this is from urls.py
@@ -65,11 +71,14 @@ class JsonLDExportTests(ArchesTestCase):
     def test_basic_export(self):
         # ef52726c-f6b1-11e9-b47f-a4d18cec433a
         BusinessDataImporter('tests/fixtures/jsonld_base/data/test_1_instance.json').import_business_data()
-
-        response = self.client.get(reverse('resources', kwargs={"resourceid": "e6412598-f6b5-11e9-8f09-a4d18cec433a"}))
+        url = reverse('resources', kwargs={"resourceid": "e6412598-f6b5-11e9-8f09-a4d18cec433a"})
+        # response = self.client.get(url, HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        response = self.client.get(url)
+        print(f"test basic gets {response.status_code}")
         self.assertTrue(response.status_code == 200)
         js = response.json()
         self.assertTrue('@id' in js)
         self.assertTrue(js['@id'] == "http://localhost:8000/resources/e6412598-f6b5-11e9-8f09-a4d18cec433a")
         self.assertTrue("http://www.cidoc-crm.org/cidoc-crm/P3_has_note" in js)
         self.assertTrue(js["http://www.cidoc-crm.org/cidoc-crm/P3_has_note"] == "Test Text Here")
+
