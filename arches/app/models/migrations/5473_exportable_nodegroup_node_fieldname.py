900db7b008ecbfc60a662a3b3187b841d56fa37c
Galen Mancino <whatisgalen@gmail.com>
2019-11-01 11:19:39 -0700

    completes migration for #5473 , columns exportable and fieldname for nodegroup and node respectively

--- arches/app/models/migrations/5473_exportable_nodegroup_node_fieldname.py
+++ arches/app/models/migrations/5473_exportable_nodegroup_node_fieldname.py
@@ -4,44 +4,24 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('models', '5407_remove_tileserver'),
+        ('models', '5477_fix_makemigrations'),
     ]
 
     operations = [
         migrations.RunSQL(
-            """
-            update nodes
-            set config =  config - 'cacheTiles'
-            where datatype = 'geojson-feature-collection';
+            sql="""
+            ALTER TABLE nodes
+            ADD fieldname TEXT;
 
-            update nodes
-            set config =  config - 'autoManageCache'
-            where datatype = 'geojson-feature-collection';
-
-            update d_data_types
-            set defaultconfig = defaultconfig - 'cacheTiles'
-            where datatype = 'geojson-feature-collection';
-
-            update d_data_types
-            set defaultconfig = defaultconfig - 'autoManageCache'
-            where datatype = 'geojson-feature-collection';
+            ALTER TABLE node_groups
+            ADD exportable BOOLEAN;
             """,
-            """
-            update d_data_types
-            set defaultconfig = jsonb_set(defaultconfig, '{cacheTiles}', 'false')
-            where datatype = 'geojson-feature-collection';
+            reverse_sql="""
+            ALTER TABLE nodes
+            DROP COLUMN fieldname;
 
-            update nodes
-            set config = jsonb_set(config, '{cacheTiles}', 'false')
-            where datatype = 'geojson-feature-collection';
-
-            update d_data_types
-            set defaultconfig = jsonb_set(defaultconfig, '{autoManageCache}', 'false')
-            where datatype = 'geojson-feature-collection';
-
-            update nodes
-            set config = jsonb_set(config, '{autoManageCache}', 'false')
-            where datatype = 'geojson-feature-collection';
-            """
+            ALTER TABLE node_groups
+            DROP COLUMN exportable;
+            """,
         ),
     ]
