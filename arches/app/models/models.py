91276c584771612d85a3c8876c30ce48fdfafe43
Galen Mancino <whatisgalen@gmail.com>
2019-11-10 21:14:04 -0800

    adds tasks field to UserProfile model, re #5533

--- arches/app/models/models.py
+++ arches/app/models/models.py
@@ -925,6 +925,7 @@ class GraphXMapping(models.Model):
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=16, blank=True)
+    tasks = JSONField(blank=True, null=True, default=dict([]))
 
     def is_reviewer(self):
         return self.user.groups.filter(name='Resource Reviewer').exists()
