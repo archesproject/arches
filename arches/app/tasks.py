0e4ba7c66d110f56f2ce8ec9dcf06437540568d0
Galen Mancino <whatisgalen@gmail.com>
2019-11-13 15:32:13 -0800

    adds helper tasks in core arches tasks.py to create and update user_x_task records, implemented with sync, re #5533

--- arches/app/tasks.py
+++ arches/app/tasks.py
@@ -2,8 +2,40 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from django.core import management
+import datetime
+from arches.app.models import models
+
+@shared_task(bind=True)
+def sync(self, surveyid=None, userid=None):
+    create_user_task_record(self.request.id, self.name, userid)
+    management.call_command('mobile', operation='sync_survey', id=surveyid, user=userid)
+    return self.request.id
+
 
 @shared_task
-def sync(surveyid, userid):
-    management.call_command('mobile', operation='sync_survey', id=surveyid, user=userid)
-    return 'sync complete'
+def update_user_task_record(taskid):
+    task_obj = models.UserXTask.objects.get(taskid=taskid)
+    task_obj.status = "SUCCESS"
+    task_obj.date_done = datetime.datetime.now()
+    task_obj.save()
+
+
+@shared_task
+def log_error(request, exc, traceback):
+    print(exc)
+    task_obj = models.UserXTask.objects.get(taskid=request.id)
+    task_obj.status = "FAILED"
+    task_obj.date_done = datetime.datetime.now()
+    task_obj.save()
+
+
+def create_user_task_record(taskid, taskname, userid):
+    try:
+        new_task_record = models.UserXTask.objects.create(
+            user_id=userid,
+            taskid=taskid,
+            date_start=datetime.datetime.now(),
+            name=taskname
+        )
+    except Exception as e:
+        print("encountered exception in create user")
--- arches/app/views/user_tasks.py
+++ arches/app/views/user_tasks.py
@@ -2,15 +2,18 @@
 from django.views.generic import View
 from arches.app.utils.response import JSONResponse
 from django.http import HttpResponseNotFound
+from arches.app.models import models
 
 class UserTaskView(View):
+    action = ""
 
     def get(self, request):
-        action = ""
 
         if request.user.is_authenticated:
-            tasks = models.UserXTask.objects.filter(user_id=request.user.id)
-            if len(tasks) > 0:
-                return JSONResponse({"tasks": tasks})
+            if request.GET.get("action", None) == "get_all":
+                tasks = models.UserXTask.objects.all()
+            else:
+                tasks = models.UserXTask.objects.filter(user_id=request.user.id)
+            return JSONResponse({"tasks": tasks})
 
         return HttpResponseNotFound()
