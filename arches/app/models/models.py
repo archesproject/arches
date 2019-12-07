44924791cc324ae15715a5607bfb83e8baf4d5c4
stickler-ci <support@stickler-ci.com>
2019-12-07 00:26:46 +0000

    Fixing style errors.

--- arches/app/models/models.py
+++ arches/app/models/models.py
@@ -1042,7 +1042,7 @@ class Notification(models.Model):
     # created.editable = True
     message = models.TextField(blank=True, null=True)
     context = JSONField(blank=True, null=True, default=dict())
-    #TODO: Ideally validate context against a list of keys from NotificationType
+    # TODO: Ideally validate context against a list of keys from NotificationType
     notiftype = models.ForeignKey(NotificationType, on_delete=models.CASCADE, null=True)
 
     class Meta:
