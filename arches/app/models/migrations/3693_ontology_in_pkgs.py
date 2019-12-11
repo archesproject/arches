91f4a31d371a4e50e76ee6c1b379b4ba2e50aade
Cyrus Hiatt <chiatt@fargeo.com>
2019-12-11 11:17:12 -0800

    Consolidates migrations. re #3693

--- arches/app/models/migrations/3693_ontology_in_pkgs.py
+++ arches/app/models/migrations/3693_ontology_in_pkgs.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("models", "5615_search_export_notification_type"),
+        ("models", "5613_notification_type"),
     ]
 
     def forwards_func(apps, schema_editor):
--- arches/app/models/migrations/5613_notification_type.py
+++ arches/app/models/migrations/5613_notification_type.py
@@ -53,4 +53,25 @@ class Migration(migrations.Migration):
             ],
             options={"db_table": "user_x_notifications", "managed": True},
         ),
+        migrations.AddField(
+            model_name="notification",
+            name="context",
+            field=django.contrib.postgres.fields.jsonb.JSONField(blank=True, default={}, null=True),
+        ),
+        migrations.RunSQL(
+            """
+            INSERT INTO notification_types (typeid, name, emailtemplate, emailnotify, webnotify)
+            VALUES (
+                '441e6ed4-188d-11ea-a35b-784f435179ea',
+                'Search Export Download Ready',
+                'email/download_ready_email_notification.htm',
+                true,
+                true
+            );
+            """,
+            """
+            DELETE FROM notification_types
+                WHERE typeid in ('441e6ed4-188d-11ea-a35b-784f435179ea');
+            """,
+        ),
     ]
