45e9b7658d556af6a2d3c309c3a9057075d49955
Cyrus Hiatt <chiatt@fargeo.com>
2019-12-19 13:31:28 -0800

    Repeats attempt to check if celery is running if the first attempt results in a BrokenPipeError. re #5728

--- arches/app/utils/task_management.py
+++ arches/app/utils/task_management.py
@@ -3,11 +3,13 @@ from kombu import Connection
 from django.utils.translation import ugettext as _
 from arches.app.models.system_settings import settings
 from arches.celery import app
+import time
 
 logger = logging.getLogger(__name__)
 
 
 def check_if_celery_available():
+    result = None
     try:
         conn = Connection(settings.CELERY_BROKER_URL)
         conn.ensure_connection(max_retries=2)
@@ -15,7 +17,16 @@ def check_if_celery_available():
         logger.warning(_("Unable to connect to a celery broker"))
         return False
     inspect = app.control.inspect()
-    result = inspect.ping()
+    for i in range(4):
+        try:
+            result = inspect.ping()
+            break
+        except BrokenPipeError as e:
+            time.sleep(0.10)
+            logger.warning(_("Celery worker connection failed. Reattempting"))
+            if i == 3:
+                logger.warning(_("Failed to connect to celery due to a BrokenPipeError"))
+                logger.exception(e)
     if result is None:
         logger.warning(_("A celery broker is running, but a celery worker is not available"))
         result = False  # ping returns True or None, assigning False here so we return only a boolean value
