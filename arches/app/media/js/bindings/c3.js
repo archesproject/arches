39e6c653283b18f5ab1b5e8e6fe7a0a45abe8acc
Cyrus Hiatt <chiatt@fargeo.com>
2020-02-07 17:59:32 -0800

    Adds binding handler for c3 charts. re archesproject/arches-for-science#6

---
+++ arches/app/media/js/bindings/c3.js
@@ -0,0 +1,36 @@
+define([
+    'jquery',
+    'knockout',
+    'c3'
+], function($, ko, c3) {
+    ko.bindingHandlers.c3 = {
+        init: function(element, valueAccessor, allBindings, viewModel) {
+            var config = ko.unwrap(valueAccessor());
+            var options = $.extend(true, config.options, {
+                bindto: element,
+                data: {
+                    x: 'value',
+                    columns: ko.unwrap(config.data),
+                    keys: {
+                        value: ['count'],
+                    }
+                },
+                axis: {
+                    x: {
+                        tick: {
+                        format: function(val) {
+                            return Math.floor(val);
+                            }
+                        }
+                    }
+                }
+            });
+            this.chart = c3.generate(options);
+            config.data.subscribe(function(val){
+                console.log('loading', val)
+                this.chart.load({columns: val});
+            }, this);
+        },
+    }
+    return ko.bindingHandlers.c3;
+});
