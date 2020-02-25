70ce6275186084aac3ac14497e7f6322955ba048
stickler-ci <support@stickler-ci.com>
2020-02-25 19:38:08 +0000

    Fixing style errors.

--- tests/fixtures/testing_prj/testing_prj/celery.py
+++ tests/fixtures/testing_prj/testing_prj/celery.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testing_prj.settings')
-app = Celery('testing_prj')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testing_prj.settings")
+app = Celery("testing_prj")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
--- tests/fixtures/testing_prj/testing_prj/pkg/staging/adds_wkt.py
+++ tests/fixtures/testing_prj/testing_prj/pkg/staging/adds_wkt.py
@@ -1,11 +1,10 @@
-
 import os
 import csv
 import random
 from django.contrib.gis.geos import GEOSGeometry, Point, Polygon
 from pprint import pprint as pp
 
-workspace = '/Users/awuthrich/Downloads'
+workspace = "/Users/awuthrich/Downloads"
 input_file = os.path.join(workspace, "MOCK_DATA.csv")
 output_file = os.path.join(workspace, "MOCK_DATA_COMPLETE.csv")
 
@@ -16,9 +15,9 @@ pp(records[1])
 
 for record in records:
     pnt = Point(random.uniform(-122.5, -122.0), random.uniform(37.0, 37.5), srid=4326)
-    record['location'] = pnt.wkt
+    record["location"] = pnt.wkt
 
-with open(output_file, 'w') as of:
+with open(output_file, "w") as of:
     fieldnames = records[0].keys()
     writer = csv.DictWriter(of, fieldnames=fieldnames)
     writer.writeheader()
