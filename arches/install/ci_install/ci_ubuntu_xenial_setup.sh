f0ae28f3d7d575342c049749e38257d96519c974
Galen Mancino <whatisgalen@gmail.com>
2020-02-27 13:13:02 -0800

    more testing in setup_couch.sh, re #5923

--- arches/install/ci_install/ci_ubuntu_xenial_setup.sh
+++ arches/install/ci_install/ci_ubuntu_xenial_setup.sh
@@ -34,7 +34,6 @@ function install_couchdb {
     sudo add-apt-repository "deb https://apache.bintray.com/couchdb-deb xenial main"
     sudo apt-get update
     sudo apt-get install couchdb
-    sudo chmod o+rwx /var/logs/couchdb/
 }
 
 function install_yarn {
--- arches/install/ci_install/setup_couch.sh
+++ arches/install/ci_install/setup_couch.sh
@@ -7,4 +7,4 @@ echo "adding admin :"
 curl -X PUT $HOST/_config/admins/admin -d '"admin"'
 echo "=_=_=_=_=_printing logs=_=_=_=_=_"
 # curl -X GET $HOST/_config
-cat /var/log/couchdb/couch.log
+sudo cat /var/log/couchdb/couch.log
