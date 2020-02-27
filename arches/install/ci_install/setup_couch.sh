c3d1ee89790a4c3604229d4aad42fbd477ffaf42
Galen Mancino <whatisgalen@gmail.com>
2020-02-27 12:39:59 -0800

    more testing in setup_couch.sh, re #5923

--- arches/install/ci_install/setup_couch.sh
+++ arches/install/ci_install/setup_couch.sh
@@ -6,4 +6,4 @@ curl -X GET $HOST
 echo "adding admin :"
 curl -X PUT $HOST/_config/admins/admin -d '"admin"'
 echo "=_=_=_=_=_printing logs=_=_=_=_=_"
-GET $HOST/_node/_local/_config HTTP/1.1
+ls $HOST/_node/_local/_config
