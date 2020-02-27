ea411f89b69404a11f3d5ce8c620bcf7dd1b02e2
Cyrus Hiatt <chiatt@fargeo.com>
2020-02-27 14:37:39 -0800

    Fixes issue with assigning incorrect resourceinstanceid to a tile. re #5881

--- arches/app/media/js/views/components/cards/file-viewer.js
+++ arches/app/media/js/views/components/cards/file-viewer.js
@@ -212,10 +212,10 @@ define([
                 val.deleteTile(null, self.defaultSelector);
             };
 
-            if (this.card.resourceinstanceid === undefined) {
+            if (this.form && ko.unwrap(this.form.resourceId)) {
+                this.card.resourceinstanceid = ko.unwrap(this.form.resourceId);
+            } else if (this.card.resourceinstanceid === undefined && this.card.tiles().length === 0) {
                 this.card.resourceinstanceid = uuid.generate();
-            } else if (this.form && ko.unwrap(this.form.resourceId()) !== this.card.resourceinstanceid) {
-                this.card.resourceinstanceid = this.form.resourceId();
             }
 
             function sleep(milliseconds) {
