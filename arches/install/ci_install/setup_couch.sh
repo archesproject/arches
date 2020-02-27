7d2dc6e1c2e38c6e5fbaee51b47bb9e316aa0a70
Galen Mancino <whatisgalen@gmail.com>
2020-02-27 12:27:17 -0800

    tests for couch setup, re #5923

--- arches/install/ci_install/setup_couch.sh
+++ arches/install/ci_install/setup_couch.sh
@@ -5,3 +5,6 @@ echo "couch version :"
 curl -X GET $HOST
 echo "adding admin :"
 curl -X PUT $HOST/_config/admins/admin -d '"admin"'
+echo "=_=_=_=_=_printing logs=_=_=_=_=_"
+echo $HOST/var/log/couchdb/couch.log
+ls $HOST/var/log/
