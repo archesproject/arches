b166355065911d53630358429618c2c0521c4d27
Cyrus Hiatt <chiatt@fargeo.com>
2020-03-04 10:40:57 -0800

    Removes the photo gallery card during the migration if it already exists. re #4751

--- arches/app/models/migrations/4751_adds_photo_gallery_card.py
+++ arches/app/models/migrations/4751_adds_photo_gallery_card.py
@@ -10,6 +10,7 @@ class Migration(migrations.Migration):
     operations = [
         migrations.RunSQL(
             sql="""
+            DELETE FROM card_components WHERE componentid = '4e40b397-d6bc-4660-a398-4a72c90dba07';
             INSERT INTO card_components(componentid, name, description, component, componentname, defaultconfig)
                 VALUES ('4e40b397-d6bc-4660-a398-4a72c90dba07', 'Photo Gallery Card', 'Photo gallery card UI', 'views/components/cards/photo-gallery-card', 'photo-gallery-card', '{}');
             """,
