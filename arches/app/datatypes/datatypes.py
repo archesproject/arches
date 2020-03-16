728cce68d0bc6e5a3337ca553c7fb7fb95bea5c0
Rob Gaston <rgaston@fargeo.com>
2020-03-16 13:20:19 -0700

    migrates annotation/iiif datatype/components

--- arches/app/datatypes/datatypes.py
+++ arches/app/datatypes/datatypes.py
@@ -1713,6 +1713,24 @@ class NodeValueDataType(BaseDataType):
         pass
 
 
+class AnnotationDataType(BaseDataType):
+    def validate(self, value, source=None, node=None):
+        errors = []
+        return errors
+
+    def append_to_document(self, document, nodevalue, nodeid, tile):
+        # document["strings"].append({"string": nodevalue["address"], "nodegroup_id": tile.nodegroup_id})
+        return
+
+    def get_search_terms(self, nodevalue, nodeid=None):
+        # return [nodevalue["address"]]
+        return []
+
+    def default_es_mapping(self):
+        # let ES dyanamically map this datatype
+        return
+
+
 def get_value_from_jsonld(json_ld_node):
     try:
         return (json_ld_node[0].get("@value"), json_ld_node[0].get("@language"))
