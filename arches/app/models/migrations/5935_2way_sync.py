e57bf28ab8c694d0949c5b7b94554560ed7eabcd
Alexei Peters <apeters@fargeo.com>
2020-03-17 14:21:33 -0700

    fix migration order, re #5935

--- arches/app/models/migrations/5935_2way_sync.py
+++ arches/app/models/migrations/5935_2way_sync.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("models", "5881_fileviewer"),
+        ("models", "5945_package_load_notiftype"),
     ]
 
     operations = [
