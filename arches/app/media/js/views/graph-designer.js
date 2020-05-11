bc57964015edafdfef028f0c056e3400316a3791
Cyrus Hiatt <chiatt@fargeo.com>
2020-05-11 09:49:46 -0700

    Defers all observable updates in graph designer to improve graph tree selection performance. RE #6162

--- arches/app/media/js/views/graph-designer.js
+++ arches/app/media/js/views/graph-designer.js
@@ -26,6 +26,7 @@ define([
 ], function($, _, ko, koMapping, BaseManagerView, AlertViewModel, JsonErrorAlertViewModel, GraphModel, ReportModel, GraphView, GraphTree, NodeFormView, BranchListView, CardTreeViewModel, PermissionDesigner, data, arches, GraphSettingsViewModel, CardViewModel, viewData, reportLookup) {
     var GraphDesignerView = BaseManagerView.extend({
         initialize: function(options) {
+            ko.options.deferUpdates = true;
             var viewModel = options.viewModel;
             viewModel.graphid = ko.observable(data.graphid);
             viewModel.activeTab = ko.observable('graph');
