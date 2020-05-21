4cd106927095a42004c5afb0197c59afa1d40907
Cyrus Hiatt <chiatt@fargeo.com>
2020-05-21 13:41:05 -0700

    Caches valid ontology classes when node is appended to graph, re #6201

--- arches/app/models/graph.py
+++ arches/app/models/graph.py
@@ -1043,7 +1043,7 @@ class Graph(models.GraphModel):
     def get_valid_ontology_classes(self, nodeid=None, parent_nodeid=None):
         """
         get possible ontology properties (and related classes) a node with the given nodeid can have
-        taking into consideration it's current position in the graph
+        taking into consideration its current position in the graph
 
         Arguments:
         nodeid -- the id of the node in question
@@ -1105,7 +1105,8 @@ class Graph(models.GraphModel):
                 else:
                     # if no parent node then just use the list of ontology classes from above, there will be no properties to return
                     ret = [{"ontology_property": "", "ontology_classes": list(ontology_classes)}]
-
+        key = f'valid_ontology_classes_nodeid_{nodeid}_parent_nodeid_{parent_nodeid}'
+        cache.set(key, ret, 3600 * 24)
         return ret
 
     def get_nodegroups(self, nodegroupid=None):
