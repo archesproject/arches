cb1b01039623f5399d8a141cc7db159f53529d9c
Cyrus Hiatt <chiatt@fargeo.com>
2020-05-25 11:13:06 -0700

    Moves cache timeout params to settings.py, re #6201

--- arches/app/views/api.py
+++ arches/app/views/api.py
@@ -460,7 +460,7 @@ class Graphs(APIBase):
         if graph is None:
             print('not using graph cache')
             graph = Graph.objects.get(graphid=graph_id)
-            cache.set(f"graph_{graph_id}", JSONSerializer().serializeToPython(graph))
+            cache.set(f"graph_{graph_id}", JSONSerializer().serializeToPython(graph), settings.GRAPH_MODEL_CACHE_TIMEOUT)
         permitted_cards = cache.get(f'{user.id}_{graph_id}_permitted_cards')
         cardwidgets = cache.get(f'{user.id}_{graph_id}_cardwidgets')
         if permitted_cards is None or cardwidgets is None:
@@ -474,8 +474,8 @@ class Graphs(APIBase):
             cardwidgets = [
                 widget for widgets in [card.cardxnodexwidget_set.order_by("sortorder").all() for card in permitted_cards] for widget in widgets
             ]
-            cache.set(f'{user.id}_{graph_id}_permitted_cards', permitted_cards, 3600 * 24)
-            cache.set(f'{user.id}_{graph_id}_cardwidgets', cardwidgets, 3600 * 24)
+            cache.set(f'{user.id}_{graph_id}_permitted_cards', permitted_cards, settings.USER_GRAPH_PERMITTED_CARDS_TIMEOUT) 
+            cache.set(f'{user.id}_{graph_id}_cardwidgets', cardwidgets, settings.USER_GRAPH_CARDWIDGETS_TIMEOUT)
 
         return JSONResponse({"datatypes": datatypes, "cards": permitted_cards, "graph": graph, "cardwidgets": cardwidgets})
 
