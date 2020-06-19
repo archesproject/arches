a712cc045c6c57691035e9632ad58fdad910b119
Alexei Peters <apeters@fargeo.com>
2020-06-19 09:53:35 -0700

    fix issue with report and the new resource-instance datatype, re #6182

--- arches/app/media/js/viewmodels/resource-instance-select.js
+++ arches/app/media/js/viewmodels/resource-instance-select.js
@@ -123,7 +123,7 @@ define([
             var relatedResourceModels = ko.computed(function() {
                 var res = [];
                 var graphlist = this.preview ? arches.graphs : arches.resources;
-                if (params.node) {
+                if (params.node && params.state !== 'report') {
                     res = params.node.config.graphs().map(function(item){
                         var graph = graphlist.find(function(graph){
                             return graph.graphid === item.graphid;
