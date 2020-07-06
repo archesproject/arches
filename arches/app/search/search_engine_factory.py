a46ddbd287b600f3af782db2a8accc1e7e876acf
stickler-ci <support@stickler-ci.com>
2020-07-06 22:19:19 +0000

    Fixing style errors.

--- arches/app/search/search_engine_factory.py
+++ arches/app/search/search_engine_factory.py
@@ -37,4 +37,5 @@ class SearchEngineFactory(object):
         _temp = __import__(modulename, globals(), locals(), [classname])  # in py3, level must be >= 0
         return getattr(_temp, classname)(hosts=hosts, prefix=prefix, **connection_options)
 
+
 SearchEngineInstance = SearchEngineFactory().create()
