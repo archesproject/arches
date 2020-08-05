312fc2580c7add505891dd5673f2e5c5ced063b7
stickler-ci <support@stickler-ci.com>
2020-08-05 19:07:47 +0000

    Fixing style errors.

--- arches/app/datatypes/datatypes.py
+++ arches/app/datatypes/datatypes.py
@@ -1744,7 +1744,7 @@ class ResourceInstanceDataType(BaseDataType):
 
     def post_tile_delete(self, tile, nodeid):
         for related in tile.data[nodeid]:
-            se.delete(index=RESOURCE_RELATIONS_INDEX, id=related['resourceXresourceId'])
+            se.delete(index=RESOURCE_RELATIONS_INDEX, id=related["resourceXresourceId"])
 
     def get_display_value(self, tile, node):
         data = self.get_tile_data(tile)
