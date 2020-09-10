90d3c92e15a787c20734f979ed776e19c190d14d
Christopher Byrd <cbyrd@fargeo.com>
2020-09-10 12:54:39 -0700

    nit #5042

--- arches/app/utils/singleton.py
+++ arches/app/utils/singleton.py
@@ -8,7 +8,4 @@ class Singleton(type):
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-            import pdb
-
-            pdb.set_trace()
         return cls._instances[cls]
