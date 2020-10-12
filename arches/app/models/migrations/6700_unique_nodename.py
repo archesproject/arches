504a48581bd7b5f746973428540d4c998c535c61
njkim <nkim@fargeo.com>
2020-10-12 15:59:52 -0700

    Add SQL to rename duplicate node names, re #6700

--- arches/app/models/migrations/6700_unique_nodename.py
+++ arches/app/models/migrations/6700_unique_nodename.py
@@ -10,6 +10,19 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunSQL(
+            """
+                UPDATE nodes AS n1
+                SET name = n1.name || '_' || nodeid
+                WHERE EXISTS (
+                    SELECT n1.nodeid
+                    FROM nodes AS n2
+                    WHERE n1.nodeid <> n2.nodeid
+                    AND n1.name = n2.name
+                    AND n1.nodegroupid = n2.nodegroupid
+                );
+            """,
+        ),
         migrations.AddConstraint(
             model_name='node',
             constraint=models.UniqueConstraint(fields=('name', 'nodegroup'), name='unique_nodename_nodegroup'),
