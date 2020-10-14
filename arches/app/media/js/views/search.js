fe5794833aec7ed5c7f4474437e35bc08a4c2ff9
Christopher Byrd <cbyrd@fargeo.com>
2020-10-14 11:45:44 -0700

    nit #6711

--- arches/app/media/js/views/search.js
+++ arches/app/media/js/views/search.js
@@ -167,6 +167,7 @@ define([
                     }
                 },
                 complete: function(request, status) {
+                    this.viewModel.loading(false);
                     this.updateRequest = undefined;
                     window.history.pushState({}, '', '?' + $.param(queryString).split('+').join('%20'));
                 }
