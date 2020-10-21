c453b38a1d0ded4a5e631c2094e4a1a114c00acb
Alexei Peters <apeters@fargeo.com>
2020-10-21 14:29:21 -0700

    add kibana proxy and endpoint, re #6735

--- arches/app/views/api.py
+++ arches/app/views/api.py
@@ -111,6 +111,19 @@ class CouchdbProxy(ProtectedResourceView, ProxyView):
         return JSONResponse(_("Sync failed"), status=500)
 
 
+class KibanaProxy(ProxyView):
+    upstream = settings.KIBANA_URL
+
+    def dispatch(self, request, path):
+        try:
+            path = f"{settings.KIBANA_CONFIG_BASEPATH}/{path}"
+            return super(KibanaProxy, self).dispatch(request, path)
+        except Exception:
+            logger.exception(_("Failed to dispatch Kibana proxy"))
+
+        return JSONResponse(_("KibanaProxy failed"), status=500)
+
+
 class APIBase(View):
     def dispatch(self, request, *args, **kwargs):
         try:
