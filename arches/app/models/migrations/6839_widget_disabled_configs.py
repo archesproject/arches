f59d5942ca9bce94e16ff36ea504e33b4478eb37
njkim <nkim@fargeo.com>
2020-11-06 13:49:54 -0800

    Fix migration to match 'uneditable' config, #6839

--- arches/app/models/migrations/6839_widget_disabled_configs.py
+++ arches/app/models/migrations/6839_widget_disabled_configs.py
@@ -12,16 +12,16 @@ class Migration(migrations.Migration):
     dependencies = [("models", "6759_fn_unique_component")]
 
     sql="""
-    UPDATE widgets SET defaultconfig = jsonb_set(defaultconfig, '{disabled}', 'false')
+    UPDATE widgets SET defaultconfig = jsonb_set(defaultconfig, '{uneditable}', 'false')
     WHERE widgetid IN ('10000000-0000-0000-0000-000000000001', '10000000-0000-0000-0000-000000000008');
-    UPDATE cards_x_nodes_x_widgets SET config = jsonb_set(config, '{disabled}', 'false') 
+    UPDATE cards_x_nodes_x_widgets SET config = jsonb_set(config, '{uneditable}', 'false') 
     WHERE widgetid IN ('10000000-0000-0000-0000-000000000001', '10000000-0000-0000-0000-000000000008');
     """
 
     reverse_sql="""
-    UPDATE widgets SET defaultconfig = defaultconfig - 'disabled'
+    UPDATE widgets SET defaultconfig = defaultconfig - 'uneditable'
     WHERE widgetid IN ('10000000-0000-0000-0000-000000000001', '10000000-0000-0000-0000-000000000008');
-    UPDATE cards_x_nodes_x_widgets SET config = config - 'disabled'
+    UPDATE cards_x_nodes_x_widgets SET config = config - 'uneditable'
     WHERE widgetid IN ('10000000-0000-0000-0000-000000000001', '10000000-0000-0000-0000-000000000008');
     """,
 
