70823e6c1e20c1b931686808cc934a60318466ce
azaroth42 <azaroth42@gmail.com>
2020-11-06 13:37:11 -0500

    Allow geojson to be imported as a string in JSON-LD

--- arches/app/datatypes/datatypes.py
+++ arches/app/datatypes/datatypes.py
@@ -1068,6 +1068,39 @@ class GeojsonFeatureCollectionDataType(BaseDataType):
         # let ES dyanamically map this datatype
         return
 
+    def is_a_literal_in_rdf(self):
+        return True
+
+    def to_rdf(self, edge_info, edge):
+        # Default to string containing JSON
+        g = Graph()
+        if edge_info['range_tile_data'] is not None:
+            data = edge_info['range_tile_data']
+            if data['type'] == 'FeatureCollection':
+                for f in data['features']:
+                    del f['id']
+                    del f['properties']
+            g.add((edge_info['d_uri'], URIRef(edge.ontologyproperty), Literal(JSONSerializer().serialize(data))))
+        return g
+
+    def from_rdf(self, json_ld_node):
+        # Allow either a JSON literal or a string containing JSON
+        try:
+            val = json.loads(json_ld_node['@value'])
+        except:
+            raise ValueError(f"Bad Data in GeoJSON, should be JSON string: {json_ld_node}")
+        if not 'features' in val or type(val['features']) != list:
+            raise ValueError(f"GeoJSON must have features array")
+        for f in val['features']:
+            f['properties'] = {}
+        return val
+
+    def validate_from_rdf(self, value):
+        if type(value) == str:
+            # first deserialize it from a string
+            value = json.loads(value)
+        return self.validate(value)
+
 
 class FileListDataType(BaseDataType):
     def __init__(self, model=None):
