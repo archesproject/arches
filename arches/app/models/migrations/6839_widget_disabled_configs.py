c049aaaf40d222d964782b6f8bc0cb79789555ce
stickler-ci <support@stickler-ci.com>
2020-11-11 01:40:21 +0000

    Fixing style errors.

--- arches/app/models/migrations/6839_widget_disabled_configs.py
+++ arches/app/models/migrations/6839_widget_disabled_configs.py
@@ -7,20 +7,25 @@ class Migration(migrations.Migration):
 
     dependencies = [("models", "6759_fn_unique_component")]
 
-    sql="""
+    sql = """
     UPDATE widgets SET defaultconfig = jsonb_set(defaultconfig, '{uneditable}', 'false')
     WHERE widgetid IN ('10000000-0000-0000-0000-000000000001', '10000000-0000-0000-0000-000000000008');
     UPDATE cards_x_nodes_x_widgets SET config = jsonb_set(config, '{uneditable}', 'false') 
     WHERE widgetid IN ('10000000-0000-0000-0000-000000000001', '10000000-0000-0000-0000-000000000008');
     """
 
-    reverse_sql="""
+    reverse_sql = (
+        """
     UPDATE widgets SET defaultconfig = defaultconfig - 'uneditable'
     WHERE widgetid IN ('10000000-0000-0000-0000-000000000001', '10000000-0000-0000-0000-000000000008');
     UPDATE cards_x_nodes_x_widgets SET config = config - 'uneditable'
     WHERE widgetid IN ('10000000-0000-0000-0000-000000000001', '10000000-0000-0000-0000-000000000008');
     """,
+    )
 
     operations = [
-        migrations.RunSQL(sql, reverse_sql,),
+        migrations.RunSQL(
+            sql,
+            reverse_sql,
+        ),
     ]
