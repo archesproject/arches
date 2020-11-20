2a2d10b22b5b0521f1ad5dc59d8683b210b26446
njkim <nkim@fargeo.com>
2020-11-19 20:48:26 -0800

    Use str displayValue for number binding, re #6750

--- arches/app/media/js/views/components/widgets/number.js
+++ arches/app/media/js/views/components/widgets/number.js
@@ -33,14 +33,17 @@ define(['knockout', 'underscore', 'viewmodels/widget', 'bindings/formattedNumber
                     val = Number(val).toFixed(self.precision());
                 }
 
-                if (this.state === "report") {
-                    val = val.toString();
-                }
             }
             return val || self.value();
         }, self).extend({throttle: 600});
 
         this.value(this.updateVal());
+
+        this.displayValue = ko.pureComputed(function() {
+            if (self.value() !== null && self.value() !== undefined) {
+                return self.value().toString();
+            }
+        }, self);
 
         if (ko.isObservable(this.precision)) {
             var precisionSubscription = this.precision.subscribe(function(val){
