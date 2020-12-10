2bcfb481a74845cc42b6e052979d501aced42db4
Rob Gaston <rgaston@fargeo.com>
2020-12-10 15:34:17 -0800

    cleanup

--- arches/app/models/migrations/6837_add_annotations_view.py
+++ arches/app/models/migrations/6837_add_annotations_view.py
@@ -6,35 +6,34 @@ class Migration(migrations.Migration):
         ('models', '6839_widget_disabled_configs'),
     ]
 
-
     sql = """
-    DROP VIEW IF EXISTS vw_annotations;
+        DROP VIEW IF EXISTS vw_annotations;
 
-    CREATE OR REPLACE VIEW vw_annotations AS
-    SELECT json_array_elements(t.tiledata::json->n.nodeid::text->'features')->>'id' as feature_id,
-        t.tileid,
-        t.tiledata,
-        t.resourceinstanceid,
-        t.nodegroupid,
-        n.nodeid,
-        json_array_elements(t.tiledata::json->n.nodeid::text->'features') as feature,
-        json_array_elements(t.tiledata::json->n.nodeid::text->'features')->'properties'->>'canvas' as canvas
-    FROM tiles t
-        LEFT JOIN nodes n ON t.nodegroupid = n.nodegroupid
-    WHERE (
-            (
-                SELECT count(*) AS count
-                FROM jsonb_object_keys(t.tiledata) jsonb_object_keys(jsonb_object_keys)
-                WHERE (
-                        jsonb_object_keys.jsonb_object_keys IN (
-                            SELECT n_1.nodeid::text AS nodeid
-                            FROM nodes n_1
-                            WHERE n_1.datatype = 'annotation'::text
+        CREATE OR REPLACE VIEW vw_annotations AS
+        SELECT json_array_elements(t.tiledata::json->n.nodeid::text->'features')->>'id' as feature_id,
+            t.tileid,
+            t.tiledata,
+            t.resourceinstanceid,
+            t.nodegroupid,
+            n.nodeid,
+            json_array_elements(t.tiledata::json->n.nodeid::text->'features') as feature,
+            json_array_elements(t.tiledata::json->n.nodeid::text->'features')->'properties'->>'canvas' as canvas
+        FROM tiles t
+            LEFT JOIN nodes n ON t.nodegroupid = n.nodegroupid
+        WHERE (
+                (
+                    SELECT count(*) AS count
+                    FROM jsonb_object_keys(t.tiledata) jsonb_object_keys(jsonb_object_keys)
+                    WHERE (
+                            jsonb_object_keys.jsonb_object_keys IN (
+                                SELECT n_1.nodeid::text AS nodeid
+                                FROM nodes n_1
+                                WHERE n_1.datatype = 'annotation'::text
+                            )
                         )
-                    )
-            )
-        ) > 0
-    AND n.datatype = 'annotation'::text;
+                )
+            ) > 0
+        AND n.datatype = 'annotation'::text;
     """
 
     reverse_sql = ("DROP VIEW IF EXISTS vw_annotations;",)
