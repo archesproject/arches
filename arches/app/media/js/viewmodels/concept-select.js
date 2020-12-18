4b842fdf571739d13053b3de920bbdde7f7a0728
Christopher Byrd <cbyrd@fargeo.com>
2020-12-17 19:16:51 -0800

    concept select widget now respects value input and selection #6963

--- arches/app/media/js/viewmodels/concept-select.js
+++ arches/app/media/js/viewmodels/concept-select.js
@@ -7,17 +7,18 @@ define([
     var nameLookup = {};
     var ConceptSelectViewModel = function(params) {
         var self = this;
+
         params.configKeys = ['placeholder', 'defaultValue'];
+
         this.multiple = params.multiple || false;
         this.allowClear = true;
+        this.displayName = ko.observable('');
 
         WidgetViewModel.apply(this, [params]);
 
-        var displayName = ko.observable('');
-
         this.valueList = ko.computed(function() {
             var valueList = self.value();
-            displayName();
+            
             if (!self.multiple && valueList) {
                 valueList = [valueList];
             }
@@ -28,7 +29,6 @@ define([
         });
 
         this.valueObjects = ko.computed(function() {
-            displayName();
             return self.valueList().map(function(value) {
                 return {
                     id: value,
@@ -39,45 +39,46 @@ define([
             });
         });
 
-        var updateName = function() {
+        this.updateName = function() {
             var names = [];
+
             self.valueList().forEach(function(val) {
                 if (val) {
                     if (nameLookup[val]) {
                         names.push(nameLookup[val]);
-                        displayName(names.join(', '));
+                        self.displayName(names.join(', '));
                     } else {
                         $.ajax(arches.urls.get_pref_label + '?valueid=' + val, {
                             dataType: "json"
                         }).done(function(data) {
                             nameLookup[val] = data.value;
                             names.push(data.value);
-                            displayName(names.join(', '));
+                            self.displayName(names.join(', '));
                         });
                     }
                 }
             });
         };
-        this.value.subscribe(updateName);
+        this.value.subscribe(self.updateName);
+
         this.displayValue = ko.computed(function() {
             var val = self.value();
-            var name = displayName();
+            var name = self.displayName();
             var displayVal = null;
-
+            
             if (val) {
                 displayVal = name;
             }
 
             return displayVal;
         });
-        updateName();
 
         this.select2Config = {
-            value: this.value,
+            value: self.value,
             clickBubble: true,
-            multiple: this.multiple,
+            multiple: self.multiple,
             closeOnSlect: false,
-            placeholder: this.placeholder,
+            placeholder: self.placeholder,
             allowClear: true,
             ajax: {
                 url: arches.urls.paged_dropdown,
@@ -120,31 +121,71 @@ define([
             },
             isEmpty: ko.computed(function() {
                 return self.value() === '' || !self.value();
-            }, this),
+            }),
             initSelection: function(el, callback) {
-                var valueList = self.valueList();
-                var setSelectionData = function() {
-                    var valueData = self.valueObjects().map(function(item) {
-                        return {
-                            id: item.id,
-                            text: item.name
-                        };
-                    });
-                    valueData = self.multiple ? valueData : valueData[0];
+                /* reversing the values gives correct ordering if multiple === true */ 
+                var valueList = self.valueList().reverse();
+                
+                var setSelectionData = function(data) {
+                    if (!(data instanceof Array)) { data = [data]; }
+                    
+                    var valueData = data.reduce(function(acc, datum) {
+                        var valueDatum;
+
+                        /* coerce different datum types into expected type */ 
+                        if (!datum.value) {
+                            var valueObject = self.valueObjects().find(function(valueObject) {
+                                return valueObject.name === datum;
+                            });
+
+                            if (valueObject) {
+                                valueDatum = {
+                                    id: valueObject.id,
+                                    text: valueObject.name,
+                                };
+                            }
+                        }
+                        else if (datum.value) {
+                            valueDatum = {
+                                id: datum.valueid,
+                                text: datum.value,
+                            };
+                        }
+
+                        if (valueDatum) {
+                            acc.push(valueDatum);
+                        }
+                        
+                        return acc;
+                    }, []);
+                    
+                    if (self.multiple) {
+                        /* add the rest of the valueList */ 
+                        valueList.forEach(function(value) {
+                            if (value !== valueData[0].id) {
+                                valueData.unshift({
+                                    id: value,
+                                    text: nameLookup[value],
+                                });
+                            }
+                        });
+                    }
+
                     if (valueData) {
                         callback(valueData);
                     }
                 };
+
                 valueList.forEach(function(value) {
                     if (value) {
                         if (nameLookup[value]) {
-                            setSelectionData();
+                            setSelectionData(nameLookup[value]);
                         } else {
                             $.ajax(arches.urls.concept_value + '?valueid=' + value, {
                                 dataType: "json"
                             }).done(function(data) {
                                 nameLookup[value] = data.value;
-                                setSelectionData();
+                                setSelectionData(data);
                             });
                         }
                     }
