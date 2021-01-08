3fe29baf44db7cab648f50fae2e8839d42f31f23
njkim <nkim@fargeo.com>
2021-01-08 10:35:52 -0800

    Remove commented blocks, #6979

--- arches/app/media/js/views/components/iiif-viewer.js
+++ arches/app/media/js/views/components/iiif-viewer.js
@@ -26,6 +26,11 @@ define([
         this.filter = ko.observable('');
         this.manifestData = ko.observable();
         this.manifestError = ko.observable();
+        this.manifestName = ko.observable();
+        this.manifestDescription = ko.observable();
+        this.manifestMetaData = ko.observableArray([{"label": "", "value": [""]}]);
+        this.canvasLabel = ko.observable();
+        this.zoomToCanvas = !(params.zoom && params.center);
         this.canvases = ko.pureComputed(function() {
             var manifestData = self.manifestData();
             var sequences = manifestData ? manifestData.sequences : [];
@@ -45,14 +50,6 @@ define([
             });
             return canvases;
         });
-        this.canvasLabel = ko.observable();
-
-        /*this.manifestName = ko.pureComputed(function() {
-            var manifestData = self.manifestData();
-            return getLabel(manifestData || {label: ''});
-        });*/
-
-        this.zoomToCanvas = !(params.zoom && params.center);
 
         var validateUrl = function(value) {
             return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
@@ -251,15 +248,6 @@ define([
             var metadata = object.metadata;
             return metadata || [];
         };
-
-        this.manifestName = ko.observable();
-        this.manifestDescription = ko.observable();
-        this.manifestMetaData = ko.observableArray([{"label": "", "value": [""]}]);
-
-        /*this.canvas.subscribe(function(canvas) {
-            self.origCanvasLabel = getLabel(canvas);
-            self.canvasLabel(getLabel(canvas));
-        });*/
 
         var updateCanvas = !self.canvas();
         this.manifestData.subscribe(function(manifestData) {
