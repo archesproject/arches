31d0c75130904cba6096cba7131232f946dcad19
Alexei Peters <apeters@fargeo.com>
2021-01-08 17:12:21 -0800

    allow users to pass args and kwargs to save method, re #6923

--- arches/app/models/models.py
+++ arches/app/models/models.py
@@ -884,13 +884,11 @@ class TileModel(models.Model):  # Tile
 
     def save(self, *args, **kwargs):
         if self.sortorder is None or (self.provisionaledits is not None and self.data == {}):
-            kwargs.pop("request", None)
-            kwargs.pop("index", None)
             sortorder_max = TileModel.objects.filter(
                 nodegroup_id=self.nodegroup_id, resourceinstance_id=self.resourceinstance_id
             ).aggregate(Max("sortorder"))["sortorder__max"]
             self.sortorder = sortorder_max + 1 if sortorder_max is not None else 0
-        super(TileModel, self).save(**kwargs)  # Call the "real" save() method.
+        super(TileModel, self).save(*args, **kwargs)  # Call the "real" save() method.
 
 
 class Value(models.Model):
