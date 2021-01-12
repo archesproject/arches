6b67b71084b2c0a73db98cc788880b0073b09137
Rob Gaston <rgaston@fargeo.com>
2021-01-12 12:05:28 -0800

    allows for "for...of" usage

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,18 @@ module.exports = {
         'generator-star-spacing': 'off',
         // allow debugger during development
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        "camelcase": [2, {"properties": "always"}]
+        "camelcase": [2, {"properties": "always"}],
+        "no-restricted-syntax": [
+            "error",
+            {
+                selector: "LabeledStatement",
+                message: "Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.",
+            },
+            {
+                selector: "WithStatement",
+                message: "`with` is disallowed in strict mode because it makes code impossible to predict and optimize.",
+            }
+        ]
     },
     "globals": {
         "define": false,
