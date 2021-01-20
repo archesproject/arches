e2d2c358d25930dffa70db4cbdfb09e74b6a2f6d
whatisgalen <whatisgalen@gmail.com>
2021-01-20 00:18:11 +0000

    changes installation of yarn via apt instead of npm, re #7024

--- arches/install/ubuntu_setup.sh
+++ arches/install/ubuntu_setup.sh
@@ -47,7 +47,10 @@ function install_yarn {
   wget --quiet -O - https://deb.nodesource.com/setup_10.x | sudo -E bash -
   sudo apt-get update
   sudo apt-get install -y nodejs
-  sudo npm install -g yarn
+  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+  sudo apt update
+  sudo apt install yarn
 }
 
 function install_elasticsearch {
