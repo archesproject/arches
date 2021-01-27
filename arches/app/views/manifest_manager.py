6dc996b2faa28f35fcc555e7e30a4fa13c9ff52a
Cyrus Hiatt <chiatt@fargeo.com>
2021-01-26 17:47:25 -0800

    Minor changes in error messages, re #7031

--- arches/app/views/manifest_manager.py
+++ arches/app/views/manifest_manager.py
@@ -41,7 +41,7 @@ class ManifestManagerView(View):
             canvas_labels_in_use = [
                 item["label"] for item in canvases_in_manifest if item["images"][0]["resource"]["service"]["@id"] in canvases_in_use
             ]
-            response = "The manifest cannot be deleted because some canvases are in use: {}".format(", ".join(canvas_labels_in_use))
+            response = "This manifest cannot be deleted because the following canvases have resource annotations: {}".format(", ".join(canvas_labels_in_use))
             return JSONResponse({"message": response}, status=500)
         manifest.delete()
         return JSONResponse({"success": True})
@@ -134,7 +134,7 @@ class ManifestManagerView(View):
                     item["label"] for item in canvases if item["images"][0]["resource"]["service"]["@id"] in canvases_in_use
                 ]
                 raise ManifestValidationError(
-                    "The canvases cannot be deleted because some canvases are in use: {}".format(", ".join(canvas_labels_in_use))
+                    "The following canvases cannot be deleted because they have resource annotations: {}".format(", ".join(canvas_labels_in_use))
                 )
             manifest.manifest["sequences"][0]["canvases"] = [
                 canvas for canvas in canvases if canvas["images"][0]["resource"]["service"]["@id"] not in canvas_ids_remove
