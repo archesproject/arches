f854e34adbbdf829c50cd1634761480f5614ab2b
stickler-ci <support@stickler-ci.com>
2021-02-10 20:39:44 +0000

    Fixing style errors.

--- arches/app/search/components/advanced_search.py
+++ arches/app/search/components/advanced_search.py
@@ -53,7 +53,7 @@ class AdvancedSearch(BaseSearchFilter):
             advanced_query.should(grouped_query)
         search_query.must(advanced_query)
         search_results_object["query"].add_query(search_query)
-        
+
     def view_data(self):
         ret = {}
         resource_graphs = (
