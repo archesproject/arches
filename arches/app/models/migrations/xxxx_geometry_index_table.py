cc81093bbe20dc537d67a0b356dcee217ce39334
Rob Gaston <rgaston@fargeo.com>
2021-02-13 14:21:42 -0800

    cleanup migration

--- arches/app/models/migrations/xxxx_geometry_index_table.py
+++ arches/app/models/migrations/xxxx_geometry_index_table.py
@@ -38,39 +38,6 @@ class Migration(migrations.Migration):
             name='tile',
             field=models.ForeignKey(db_column='tileid', on_delete=django.db.models.deletion.DO_NOTHING, to='models.TileModel'),
         ),
-    ]
-
-
-
-    sql = """
-        insert into search_component (
-            "searchcomponentid",
-            "name",
-            "icon",
-            "modulename",
-            "classname",
-            "type",
-            "componentpath",
-            "componentname",
-            "sortorder",
-            "enabled")
-        values (
-            '6a2fe122-de54-4e44-8e93-b6a0cda7955c',
-            'Sort',
-            '',
-            'sort_results.py',
-            'SortResults',
-            '',
-            'views/components/search/sort-results',
-            'sort-results',
-            0,
-            true);
-        """
-    reverse_sql = """
-        delete from search_component where searchcomponentid = '6a2fe122-de54-4e44-8e93-b6a0cda7955c';
-        """
-
-    operations = [
         migrations.RunSQL(
             """
             CREATE OR REPLACE FUNCTION refresh_geojson_geometries() RETURNS BOOLEAN AS $$
