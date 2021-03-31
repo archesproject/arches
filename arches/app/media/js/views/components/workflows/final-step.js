77304e207a06c2ddb4c774c9ccc1923ff705feff
Cyrus Hiatt <chiatt@fargeo.com>
2021-03-31 10:44:17 -0700

    Move fetch of availabe workflows from custom workflow step to final step, re #arches-project/arches-her#578

--- arches/app/media/js/views/components/workflows/final-step.js
+++ arches/app/media/js/views/components/workflows/final-step.js
@@ -5,6 +5,7 @@ define([
     function viewModel(params) {
         this.urls = arches.urls;
         this.loading = ko.observable(true);
+        this.workflows = ko.observableArray();
         try {
             this.resourceid = ko.unwrap(params.workflow.resourceId);
         } catch(e) {
@@ -14,6 +15,13 @@ define([
                 // pass
             }
         }
+        //We may want to allow a user to navigate to any workflow, so we may want this
+        window.fetch(arches.urls.plugin('init-workflow') + '?json=true')
+            .then(response => response.json())
+            .then(data => this.workflows(data['config']['workflows'].map(function(wf){
+                wf.url = '/arches-her' + arches.urls.plugin(wf.slug);
+                return wf;
+            }, this)));   
     }
 
     ko.components.register('final-step', {
