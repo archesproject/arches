d72f4b96195cd299cffbf6d684a6b0615426e8fe
stickler-ci <support@stickler-ci.com>
2021-05-07 00:46:02 +0000

    Fixing style errors.

--- arches/settings.py
+++ arches/settings.py
@@ -616,9 +616,7 @@ GRAPH_MODEL_CACHE_TIMEOUT = None  # seconds * hours * days = ~1mo
 CANTALOUPE_DIR = os.path.join(ROOT_DIR, "cantaloupe")
 CANTALOUPE_HTTP_ENDPOINT = "http://localhost:8182/"
 
-COMPRESS_PRECOMPILERS = (
-    ('text/x-scss', 'django_libsass.SassCompiler'),
-)
+COMPRESS_PRECOMPILERS = (("text/x-scss", "django_libsass.SassCompiler"),)
 
 RENDERERS = [
     {
