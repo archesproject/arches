5bcbd56d7bb2f930f754b46ac0742895ed3e3480
whatisgalen <whatisgalen@gmail.com>
2021-05-11 01:27:49 -0700

    keeps Resource bulk_save kwarg: flat=False for now, re #7405, #7054

--- arches/app/utils/data_management/resources/formats/csvfile.py
+++ arches/app/utils/data_management/resources/formats/csvfile.py
@@ -376,7 +376,7 @@ class CsvReader(Reader):
             if bulk:
                 resources.append(newresourceinstance)
                 if len(resources) >= settings.BULK_IMPORT_BATCH_SIZE:
-                    Resource.bulk_save(resources=resources, flat=True, prevent_indexing=prevent_indexing)
+                    Resource.bulk_save(resources=resources, flat=False, prevent_indexing=prevent_indexing)
                     del resources[:]  # clear out the array
             else:
                 try:
