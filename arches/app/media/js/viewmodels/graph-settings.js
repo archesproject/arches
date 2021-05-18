1312c8508d69256be2ab0fe6905cfc1bdb97e1ba
Christopher Byrd <cbyrd@fargeo.com>
2021-05-18 13:17:14 -0700

    remove graph designer logic #7338

--- arches/app/media/js/viewmodels/graph-settings.js
+++ arches/app/media/js/viewmodels/graph-settings.js
@@ -40,19 +40,6 @@ define([
         var ontologyClass = self.rootnode.ontologyclass;
         var ontologyClassFriendlyName = self.rootnode.ontologyclass_friendlyname;
 
-        self.hasUpdatedCustomSearchResultDetailsTemplatePreference = ko.observable(false);
-
-        self.updateCustomSearchResultDetailsTemplatePreference = function(preference) {
-            self.graph.config['shouldUseCustomSearchResultTemplate'] = preference;
-            self.rootnode.config['shouldUseCustomSearchResultTemplate'] = preference;
-
-            self.graph.root.config = koMapping.toJS(self.rootnode.config);
-            
-            self.hasUpdatedCustomSearchResultDetailsTemplatePreference(!self.hasUpdatedCustomSearchResultDetailsTemplatePreference());
-        };
-
-        console.log("IN GAPH SETTINGS", self, params)
-
         self.jsonData = ko.computed(function() {
             var relatableResourceIds = _.filter(self.resource_data(), function(resource){
                 return resource.isRelatable();
@@ -112,7 +99,6 @@ define([
                 })
                 .always(function(){
                     self.contentLoading(false);
-                    self.hasUpdatedCustomSearchResultDetailsTemplatePreference(false);
                 });
         };
 
