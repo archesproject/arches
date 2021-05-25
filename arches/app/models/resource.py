a18f55cb3ac50e663a44cea5e8e9baf4b1730fb1
whatisgalen <whatisgalen@gmail.com>
2021-05-25 15:19:21 -0400

    only flattens tiles if flat=False, only indexes if prevent_indexing=True (incorporates kwargs in bulk_save method), comments print statements re #7405

--- arches/app/models/resource.py
+++ arches/app/models/resource.py
@@ -170,12 +170,14 @@ class Resource(models.ResourceInstance):
         return tiles
 
     @staticmethod
-    def bulk_save(resources):
+    def bulk_save(resources, flat=False, prevent_indexing=False):
         """
         Saves and indexes a list of resources
 
         Arguments:
         resources -- a list of resource models
+        flat -- boolean value whether incoming resource.tiles list already flat or instead nested
+        prevent_indexing -- prevents any kind of indexing within scope of this method
 
         """
 
@@ -186,37 +188,38 @@ class Resource(models.ResourceInstance):
         term_list = []
 
         for resource in resources:
-            resource.tiles = resource.get_flattened_tiles()
+            if flat is False:
+                resource.tiles = resource.get_flattened_tiles()
             tiles.extend(resource.tiles)
 
         # need to save the models first before getting the documents for index
-        start = time()
+        # start = time()
         Resource.objects.bulk_create(resources)
         TileModel.objects.bulk_create(tiles)
 
-        print(f"Time to bulk create tiles and resources: {datetime.timedelta(seconds=time() - start)}")
+        # print(f"Time to bulk create tiles and resources: {datetime.timedelta(seconds=time() - start)}")
 
-        start = time()
+        # start = time()
         for resource in resources:
             resource.save_edit(edit_type="create")
 
         resources[0].tiles[0].save_edit(note=f"Bulk created: {len(tiles)} for {len(resources)} resources.", edit_type="bulk_create")
 
-        print("Time to save resource edits: %s" % datetime.timedelta(seconds=time() - start))
+        # print("Time to save resource edits: %s" % datetime.timedelta(seconds=time() - start))
 
-        for resource in resources:
-            start = time()
-            document, terms = resource.get_documents_to_index(
-                fetchTiles=False, datatype_factory=datatype_factory, node_datatypes=node_datatypes
-            )
+        if prevent_indexing is False:
+            for resource in resources:
+                document, terms = resource.get_documents_to_index(
+                    fetchTiles=False, datatype_factory=datatype_factory, node_datatypes=node_datatypes
+                )
 
-            documents.append(se.create_bulk_item(index=RESOURCES_INDEX, id=document["resourceinstanceid"], data=document))
+                documents.append(se.create_bulk_item(index=RESOURCES_INDEX, id=document["resourceinstanceid"], data=document))
 
-            for term in terms:
-                term_list.append(se.create_bulk_item(index=TERMS_INDEX, id=term["_id"], data=term["_source"]))
+                for term in terms:
+                    term_list.append(se.create_bulk_item(index=TERMS_INDEX, id=term["_id"], data=term["_source"]))
 
-        se.bulk_index(documents)
-        se.bulk_index(term_list)
+            se.bulk_index(documents)
+            se.bulk_index(term_list)
 
     def index(self):
         """
