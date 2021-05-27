938ea62f4b3e5a08bdc139c9765d4ac915e7a9cb
Christopher Byrd <cbyrd@fargeo.com>
2021-05-26 17:13:58 -0700

    nit #7388

--- arches/app/media/js/viewmodels/foo.js
+++ arches/app/media/js/viewmodels/foo.js
@@ -11,61 +11,12 @@ define([
 ], function(ko, koMapping, _, moment, reportLookup, ReportModel) {
     var FooViewModel = function(params) {
         var self = this;
+        this.reportMetadata = ko.observable(reportLookup[params.report_template_id])
 
-        
-        console.log("foo vm init", self, params, reportLookup, reportLookup[params.report_template_id])
-        
-        this.bar = ko.observable(reportLookup[params.report_template_id])
-        
+        this.loading = ko.observable(true)
         this.loading = ko.observable(false)
 
-        
-        // ko.components.defaultLoader.getConfig('custom_summary_report', function(foo) {
-        //     var foobar = function(ddd){ console.log(ddd, foo); return foo.template };
-
-        //     ko.components.defaultLoader.loadTemplate('custom_summary_report', { element: 'foo'}, foobar)
-        //     foobar()
-        // })
-
-
-        // ko.components.get('custom_summary_report', function(ccc) {
-        //     var foo = $('.foo')[0];
-        //     var bar = ccc.createViewModel()
-        //     console.log("dsfs", ccc, foo, bar, document.querySelector('.foo'))
-
-
-            // ko.renderTemplate('custom_summary_report', ccc.createViewModel(), {}, document.querySelector('.foo'), "replaceNode")
-
-
-            // ko.renderTemplate(
-            //     "custom_summary_report",
-            //     bar,
-            //     {
-            //         afterRender: function(renderedElement) {
-            //             console.log("rendered!");
-            //         }
-            //     },
-            //     foo,
-            //     "replaceNode"
-            // );
-
-            // ko.cleanNode(foo);
-            // ko.applyBindings(ccc.template, foo)
-            // ko.utils.extend(bar)
-
-            // ko.components.defaultLoader.loadTemplate('foo', {}, ccc.template)
-
-
-
-        // })
-
-
-
-        
-
-        // ko.applyBindings(new ReportModel())
-        this.foo = ko.observable(true)
-
+        console.log(self.reportMetadata())
     };
     return FooViewModel;
 });
