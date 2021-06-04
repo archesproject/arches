b1e3ddfa34b7ff79340fa9ed0bb1d3c6f5567c09
whatisgalen <whatisgalen@gmail.com>
2021-06-03 23:37:01 -0700

    reverts default changes for overwrite param on import_business_data in packages.py, re #7054

--- arches/management/commands/packages.py
+++ arches/management/commands/packages.py
@@ -676,7 +676,7 @@ class Command(BaseCommand):
             load_mapbox_styles(basemap_styles, True)
             load_mapbox_styles(overlay_styles, False)
 
-        def load_business_data(package_dir, defer_indexing, overwrite="overwrite"):
+        def load_business_data(package_dir, defer_indexing):
             config_paths = glob.glob(os.path.join(package_dir, "package_config.json"))
             configs = {}
             if len(config_paths) > 0:
@@ -729,19 +729,19 @@ class Command(BaseCommand):
                 # assumes resources in csv do not depend on data being loaded prior from json in same dir
                 chord(
                     [
-                        import_business_data.s(data_source=path, overwrite=overwrite, bulk_load=bulk_load, prevent_indexing=False)
+                        import_business_data.s(data_source=path, overwrite=True, bulk_load=bulk_load, prevent_indexing=False)
                         for path in valid_resource_paths
                     ]
                 )(
                     package_load_complete.signature(kwargs={"valid_resource_paths": valid_resource_paths}).on_error(on_chord_error.s())
-                )  # TODO add a task to index on pkg load complete
+                )
             else:
                 for path in business_data:
                     if path not in erring_csvs:
-                        self.import_business_data(path, overwrite=overwrite, bulk_load=bulk_load, prevent_indexing=defer_indexing)
+                        self.import_business_data(path, overwrite=True, bulk_load=bulk_load, prevent_indexing=defer_indexing)
 
             relations = glob.glob(os.path.join(package_dir, "business_data", "relations", "*.relations"))
-            for relation in relations:  # TODO: create a celery task for this on import_business_data complete
+            for relation in relations:
                 self.import_business_data_relations(relation)
 
             uploaded_files = glob.glob(os.path.join(package_dir, "business_data", "files", "*"))
