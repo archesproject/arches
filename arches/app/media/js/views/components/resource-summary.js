6f1bca54f11e8d509fcd3db6d1e74f43226ddd6a
Christopher Byrd <cbyrd@fargeo.com>
2021-06-15 15:36:25 -0700

    nit #7388

--- arches/app/media/js/views/components/resource-summary.js
+++ arches/app/media/js/views/components/resource-summary.js
@@ -5,8 +5,6 @@ define([
     var viewModel = function(params){
         this.loading = ko.observable(false);
 
-        this.fooCache = ko.observable({});
-
         console.log("resource summary", this, params)
     };
     ko.components.register('resource-summary', {
