60ccb49bc549fbf476757add87d3af02a4cf7098
Adam Cox <mr.adamcox@gmail.com>
2021-07-02 16:19:34 -0500

    remove unneeded jre installation #7562

--- arches/install/ubuntu_setup.sh
+++ arches/install/ubuntu_setup.sh
@@ -51,7 +51,6 @@ function install_yarn {
 }
 
 function install_elasticsearch {
-  sudo apt-get install openjdk-8-jre-headless -y
   sudo apt-get install apt-transport-https
   wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
   sudo sh -c 'echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" > /etc/apt/sources.list.d/elastic-7.x.list'
