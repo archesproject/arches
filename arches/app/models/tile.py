cabdf0cd374d544a0b7cd90edd100a917ba8cf00
Aaron Gundel <aaron.gundel@gmail.com>
2021-07-22 16:33:47 -0600

    check that a node exists for a given tile

--- arches/app/models/tile.py
+++ arches/app/models/tile.py
@@ -488,7 +488,7 @@ class Tile(models.TileModel):
 
     def get_missing_nodes(self):
         missing_nodes = []
-        first_node = next(iter(self.data.items()))
+        first_node = next(iter(self.data.items()), None)
         if first_node is not None:
             first_node_model = models.Node.objects.get(nodeid=first_node[0])
             all_nodes = models.Node.objects.filter(nodegroup_id=first_node_model.nodegroup_id)
