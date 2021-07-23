26a613345b6a5bac80a1e4e890c29ac617059464
stickler-ci <support@stickler-ci.com>
2021-07-23 18:52:48 +0000

    Fixing style errors.

--- arches/app/models/tile.py
+++ arches/app/models/tile.py
@@ -490,7 +490,7 @@ class Tile(models.TileModel):
         missing_nodes = []
         first_node = next(iter(self.data.items()), None)
         if first_node is not None:
-            all_nodes = models.Node.objects.filter(nodegroup_id=self.nodegroup_id).exclude(datatype='semantic').iterator()
+            all_nodes = models.Node.objects.filter(nodegroup_id=self.nodegroup_id).exclude(datatype="semantic").iterator()
             for node in all_nodes:
                 node_id = str(node.nodeid)
                 if self.data.get(node_id, None) is None:
