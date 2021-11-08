e98910322f4912b1f2c8f318a6f6e46bb5a02fd9
Aaron Gundel <aaron.gundel@gmail.com>
2021-11-08 13:12:25 -0700

    test fix

--- arches/settings.py
+++ arches/settings.py
@@ -201,11 +201,11 @@ LANGUAGE_CODE = "en"
 # {langcode}-{regioncode} eg: en, en-gb ....
 # a list of language codes can be found here http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGES = [
-    ("de", _("German")),
-    ("en", _("English")),
-    ("en-gb", _("British English")),
-    ("es", _("Spanish")),
-    ("ar", _("Arabic")),
+    # ("de", _("German")),
+    # ("en", _("English")),
+    # ("en-gb", _("British English")),
+    # ("es", _("Spanish")),
+    # ("ar", _("Arabic")),
 ]
 
 # override this to permenantly display/hide the language switcher
