b22a0fd9745b2157e3d44f0b19118b9b82cc2c50
njkim <nkim@fargeo.com>
2021-11-23 11:53:17 -0800

    Fix formatting

--- arches/app/media/js/views/components/workflows/workflow-component-abstract.js
+++ arches/app/media/js/views/components/workflows/workflow-component-abstract.js
@@ -333,7 +333,7 @@ define([
                     }
                 });
             }
-            self.multiTileUpdated(hasDirtyTiles)
+            self.multiTileUpdated(hasDirtyTiles);
             return hasDirtyTiles;
         });
         
