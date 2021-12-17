ee09eaf3c5336223992a8d05f1d351912dc13a09
Rob Gaston <rgaston@fargeo.com>
2021-12-17 10:18:55 -0800

    adds migration for relational model  re: #7787

--- arches/db/relational_data_model/drop_functions.sql
+++
@@ -1,9 +0,0 @@
-drop function if exists __arches_slugify;
-drop function if exists __arches_get_node_value_sql;
-drop function if exists __arches_create_nodegroup_view;
-drop function if exists __arches_create_resource_model_views;
-drop function if exists __arches_create_branch_views;
-drop function if exists __arches_tile_view_update;
-drop function if exists __arches_get_json_data_for_view;
-drop function if exists __arches_get_parent_id_for_view;
-drop function if exists __arches_instance_view_update;
