ea0b76090f3afd5738a81a81a111c539209f2ca1
Christopher Byrd <cbyrd@fargeo.com>
2021-12-20 16:30:11 -0800

    updates to handle corner case #7616

--- arches/app/utils/label_based_graph.py
+++ arches/app/utils/label_based_graph.py
@@ -232,36 +232,54 @@ class LabelBasedGraph(object):
     def _build_graph(
         cls, input_node, input_tile, parent_tree, node_ids_to_tiles_reference, nodegroup_cardinality_reference, node_cache, datatype_factory
     ):
-        for associated_tile in node_ids_to_tiles_reference.get(str(input_node.pk), []):
+        def is_nodegroup_associated_with_input_tile(nodegroup):
+            if nodegroup.pk == input_tile.nodegroup_id:
+                return True
+            elif nodegroup.parentnodegroup:
+                return is_nodegroup_associated_with_input_tile(nodegroup.parentnodegroup)
+            return False
+
+        associated_tiles = []
+        if node_ids_to_tiles_reference.get(str(input_node.pk)):
+            associated_tiles = node_ids_to_tiles_reference.get(str(input_node.pk))
+        elif is_nodegroup_associated_with_input_tile(input_node.nodegroup):
+            associated_tiles = [input_tile]
+
+        for associated_tile in associated_tiles:
             parent_tile = associated_tile.parenttile
 
             if associated_tile == input_tile or parent_tile == input_tile:
-                label_based_node = LabelBasedNode(
-                    name=input_node.name,
-                    node_id=str(input_node.pk),
-                    tile_id=str(associated_tile.pk),
-                    value=cls._get_display_value(tile=associated_tile, node=input_node, datatype_factory=datatype_factory),
-                    cardinality=nodegroup_cardinality_reference.get(str(associated_tile.nodegroup_id)),
-                )
-
-                if not parent_tree:  # if top node and
-                    if not parent_tile:  # if not top node in separate card
-                        parent_tree = label_based_node
-                else:
-                    parent_tree.child_nodes.append(label_based_node)
-
-                for child_node in input_node.get_direct_child_nodes():
-                    if not node_cache.get(child_node.pk):
-                        node_cache[child_node.pk] = child_node
-
-                    cls._build_graph(
-                        input_node=child_node,
-                        input_tile=associated_tile,
-                        parent_tree=label_based_node,
-                        node_ids_to_tiles_reference=node_ids_to_tiles_reference,
-                        nodegroup_cardinality_reference=nodegroup_cardinality_reference,
-                        node_cache=node_cache,
-                        datatype_factory=datatype_factory,
+                if ( # don't instantiate `LabelBasedNode`s of cardinality `n` unless they are semantic or have value
+                    input_node.nodegroup.cardinality != 'n'
+                    or input_node.nodegroup.cardinality == 'n' 
+                    and ( input_node.datatype == 'semantic' or str(input_node.pk) in associated_tile.data )
+                ):
+                    label_based_node = LabelBasedNode(
+                        name=input_node.name,
+                        node_id=str(input_node.pk),
+                        tile_id=str(associated_tile.pk),
+                        value=cls._get_display_value(tile=associated_tile, node=input_node, datatype_factory=datatype_factory),
+                        cardinality=nodegroup_cardinality_reference.get(str(associated_tile.nodegroup_id)),
                     )
+
+                    if not parent_tree:  # if top node and
+                        if not parent_tile:  # if not top node in separate card
+                            parent_tree = label_based_node
+                    else:
+                        parent_tree.child_nodes.append(label_based_node)
+
+                    for child_node in input_node.get_direct_child_nodes():
+                        if not node_cache.get(child_node.pk):
+                            node_cache[child_node.pk] = child_node
+
+                        cls._build_graph(
+                            input_node=child_node,
+                            input_tile=associated_tile,
+                            parent_tree=label_based_node,
+                            node_ids_to_tiles_reference=node_ids_to_tiles_reference,
+                            nodegroup_cardinality_reference=nodegroup_cardinality_reference,
+                            node_cache=node_cache,
+                            datatype_factory=datatype_factory,
+                        )
 
         return parent_tree
--- arches/app/utils/label_based_graph_v2.py
+++ arches/app/utils/label_based_graph_v2.py
@@ -246,32 +246,37 @@ class LabelBasedGraph(object):
             parent_tile = associated_tile.parenttile
 
             if associated_tile == input_tile or parent_tile == input_tile:
-                label_based_node = LabelBasedNode(
-                    name=input_node.name,
-                    node_id=str(input_node.pk),
-                    tile_id=str(associated_tile.pk),
-                    value=cls._get_display_value(tile=associated_tile, node=input_node, datatype_factory=datatype_factory),
-                    cardinality=nodegroup_cardinality_reference[str(associated_tile.nodegroup_id)],
-                )
-
-                if not parent_tree:  # if top node and
-                    if not parent_tile:  # if not top node in separate card
-                        parent_tree = label_based_node
-                else:
-                    parent_tree.child_nodes.append(label_based_node)
-
-                for child_node in input_node.get_direct_child_nodes():
-                    if not node_cache.get(child_node.pk):
-                        node_cache[child_node.pk] = child_node
-
-                    cls._build_graph(
-                        input_node=child_node,
-                        input_tile=associated_tile,
-                        parent_tree=label_based_node,
-                        node_ids_to_tiles_reference=node_ids_to_tiles_reference,
-                        nodegroup_cardinality_reference=nodegroup_cardinality_reference,
-                        node_cache=node_cache,
-                        datatype_factory=datatype_factory,
+                if ( # don't instantiate `LabelBasedNode`s of cardinality `n` unless they are semantic or have value
+                    input_node.nodegroup.cardinality != 'n'
+                    or input_node.nodegroup.cardinality == 'n' 
+                    and ( input_node.datatype == 'semantic' or str(input_node.pk) in associated_tile.data )
+                ):
+                    label_based_node = LabelBasedNode(
+                        name=input_node.name,
+                        node_id=str(input_node.pk),
+                        tile_id=str(associated_tile.pk),
+                        value=cls._get_display_value(tile=associated_tile, node=input_node, datatype_factory=datatype_factory),
+                        cardinality=nodegroup_cardinality_reference[str(associated_tile.nodegroup_id)],
                     )
+
+                    if not parent_tree:  # if top node and
+                        if not parent_tile:  # if not top node in separate card
+                            parent_tree = label_based_node
+                    else:
+                        parent_tree.child_nodes.append(label_based_node)
+
+                    for child_node in input_node.get_direct_child_nodes():
+                        if not node_cache.get(child_node.pk):
+                            node_cache[child_node.pk] = child_node
+
+                        cls._build_graph(
+                            input_node=child_node,
+                            input_tile=associated_tile,
+                            parent_tree=label_based_node,
+                            node_ids_to_tiles_reference=node_ids_to_tiles_reference,
+                            nodegroup_cardinality_reference=nodegroup_cardinality_reference,
+                            node_cache=node_cache,
+                            datatype_factory=datatype_factory,
+                        )
 
         return parent_tree
