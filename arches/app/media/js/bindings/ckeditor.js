c0ee35361533b21b7e76f82e8743078120890b8a
Aaron Gundel <aaron.gundel@gmail.com>
2022-01-11 15:37:19 -0700

    if setreadonly is not defined - do not proceed

--- arches/app/media/js/bindings/ckeditor.js
+++ arches/app/media/js/bindings/ckeditor.js
@@ -30,7 +30,7 @@ define([
             $element.html(value);
             var editor = $element.ckeditor(options).editor;
 
-            allBindings()?.attr?.disabled?.subscribe(disabled => editor.setReadOnly(disabled));
+            allBindings()?.attr?.disabled?.subscribe(disabled => editor?.setReadOnly(disabled));
 
             // bind to change events and link it to the observable
             var onChange = function (e) {
