be1f0ff66d36c28e38b0a1489a832a940a404ad2
Christopher Byrd <cbyrd@fargeo.com>
2022-02-03 14:38:46 -0800

    nit #7784

--- arches/app/models/migrations/7787_relational_data_model.py
+++ arches/app/models/migrations/7787_relational_data_model.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("models", "7784_add_serialized_graph_to_graph_publications"),
+        ("models", "7128_resource_instance_filter"),
     ]
 
     operations = [
