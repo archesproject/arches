89da78394cb561a15cd3e5bab61986c7fa243536
njkim <nkim@fargeo.com>
2022-02-03 16:28:06 -0800

    Update migrations, #8009

--- arches/app/models/migrations/8009_etlmodule.py
+++ arches/app/models/migrations/8009_etlmodule.py
@@ -61,7 +61,7 @@ remove_csv_importer = """
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('models', '7128_resource_instance_filter'),
+        ('models', '8085_relational_data_model_handle_dates'),
     ]
 
     operations = [
