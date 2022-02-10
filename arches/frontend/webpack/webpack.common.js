ac840254d73a8ed863b2e14d3fbbceefce3690b0
Christopher Byrd <cbyrd@fargeo.com>
2022-02-10 15:13:58 -0800

    nit #8089

--- arches/frontend/webpack/webpack.common.js
+++ arches/frontend/webpack/webpack.common.js
@@ -4,48 +4,48 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    app: Path.resolve(__dirname, '../src/scripts/index.js'),
-  },
-  output: {
-    path: Path.join(__dirname, '../build'),
-    filename: 'js/[name].js',
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-      name: false,
+    entry: {
+        app: Path.resolve(__dirname, '../src/scripts/index.js'),
     },
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new CopyWebpackPlugin({
-      patterns: [{ from: Path.resolve(__dirname, '../public'), to: 'public' }],
-    }),
-    new HtmlWebpackPlugin({
-      template: Path.resolve(__dirname, '../src/index.html'),
-    }),
-  ],
-  resolve: {
-    alias: {
-      '~': Path.resolve(__dirname, '../src'),
+    output: {
+        path: Path.join(__dirname, '../build'),
+        filename: 'js/[name].js',
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.mjs$/,
-        include: /node_modules/,
-        type: 'javascript/auto',
-      },
-      {
-        test: /\.html$/i,
-        loader: 'html-loader',
-      },
-      {
-        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
-        type: 'asset'
-      },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+            name: false,
+        },
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [{ from: Path.resolve(__dirname, '../public'), to: 'public' }],
+        }),
+        new HtmlWebpackPlugin({
+            template: Path.resolve(__dirname, '../src/index.html'),
+        }),
     ],
-  },
+    resolve: {
+        alias: {
+            '~': Path.resolve(__dirname, '../src'),
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.mjs$/,
+                include: /node_modules/,
+                type: 'javascript/auto',
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+                type: 'asset'
+            },
+        ],
+    },
 };
--- arches/frontend/webpack/webpack.config.dev.js
+++ arches/frontend/webpack/webpack.config.dev.js
@@ -1,58 +1,72 @@
 const Path = require('path');
 const Webpack = require('webpack');
 const { merge } = require('webpack-merge');
-const ESLintPlugin = require('eslint-webpack-plugin');
 const StylelintPlugin = require('stylelint-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  target: 'web',
-  mode: 'development',
-  devtool: 'eval-cheap-source-map',
-  output: {
-    chunkFilename: 'js/[name].chunk.js',
-  },
-  devServer: {
-    client: {
-      logging: 'error',
+    mode: 'development',
+    devtool: 'inline-cheap-source-map',
+    output: {
+        chunkFilename: 'js/[name].chunk.js',
     },
-    hot: true,
-  },
-  plugins: [
-    new Webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development'),
-    }),
-    new ESLintPlugin({
-      extensions: 'js',
-      emitWarning: true,
-      files: Path.resolve(__dirname, '../src'),
-    }),
-    new StylelintPlugin({
-      files: Path.join('src', '**/*.s?(a|c)ss'),
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: Path.resolve(__dirname, '../src'),
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.s?css$/i,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-          'postcss-loader',
-          'sass-loader',
-        ],
-      },
+    devServer: {
+        inline: true,
+        hot: true,
+    },
+    plugins: [
+        // new ESLintPlugin({
+        //     extensions: [`js`, `jsx`],
+        //     // include: [
+        //     //     Path.resolve(__dirname, '../src')
+        //     // ],
+        // }),
+        new Webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('development'),
+        }),
+        new StylelintPlugin({
+            files: Path.join('src', '**/*.s?(a|c)ss'),
+        }),
+        new MiniCssExtractPlugin({ filename: 'css/app.css', })
     ],
-  },
+    module: {
+        rules: [
+            {
+                test: /\.html/,
+                include: Path.resolve(__dirname, '../src'),
+                use: {
+                    loader: 'html-loader',
+                }
+            },
+            {
+                test: /\.js$/,
+                include: Path.resolve(__dirname, '../src'),
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.s?css$/i,
+                use: [
+                    {
+                        'loader': MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        'loader': 'css-loader',
+                        'options': {
+                            'sourceMap': true
+                        }
+                    },
+                    {
+                        'loader': 'postcss-loader',
+                    },
+                    {
+                        'loader': 'sass-loader'
+                    }
+                ],
+            },
+        ],
+    },
 });
