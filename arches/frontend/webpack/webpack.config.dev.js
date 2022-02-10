7353fd2285e186f8b5e93d4574b9fa83d8ac6352
Christopher Byrd <cbyrd@fargeo.com>
2022-02-10 13:41:01 -0800

    rough out #8090

---
+++ arches/frontend/webpack/webpack.config.dev.js
@@ -0,0 +1,58 @@
+const Path = require('path');
+const Webpack = require('webpack');
+const { merge } = require('webpack-merge');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const StylelintPlugin = require('stylelint-webpack-plugin');
+
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  target: 'web',
+  mode: 'development',
+  devtool: 'eval-cheap-source-map',
+  output: {
+    chunkFilename: 'js/[name].chunk.js',
+  },
+  devServer: {
+    client: {
+      logging: 'error',
+    },
+    hot: true,
+  },
+  plugins: [
+    new Webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
+    new ESLintPlugin({
+      extensions: 'js',
+      emitWarning: true,
+      files: Path.resolve(__dirname, '../src'),
+    }),
+    new StylelintPlugin({
+      files: Path.join('src', '**/*.s?(a|c)ss'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: Path.resolve(__dirname, '../src'),
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.s?css$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+});
