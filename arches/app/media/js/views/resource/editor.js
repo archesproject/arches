2d96eb0d105eb3f697a07b3d7c86da302970bf38
Aaron Gundel <aaron.gundel@gmail.com>
2022-02-25 15:15:04 -0700

    stylistic changes for stickler.

--- arches/app/media/js/views/resource/editor.js
+++ arches/app/media/js/views/resource/editor.js
@@ -85,7 +85,9 @@ define([
             return group.nodegroupid === card.nodegroup_id;
         });
         return !nodegroup || !nodegroup.parentnodegroup_id;
-    }).sort((firstEl, secondEl) => {return (firstEl.sortorder >= secondEl.sortorder) ? firstEl.sortorder === secondEl.sortorder ? 0 : 1 : -1 }).map(function(card) {
+    }).sort((firstEl, secondEl) => {
+        return firstEl.sortorder >= secondEl.sortorder ? firstEl.sortorder === secondEl.sortorder ? 0 : 1 : -1; 
+    }).map(function(card) {
         return new CardViewModel({
             card: card,
             graphModel: graphModel,
