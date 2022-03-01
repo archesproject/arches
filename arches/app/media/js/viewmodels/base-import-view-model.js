0482de8d1f41f2c89c270e9def46f994ac9dc55d
Cyrus Hiatt <chiatt@fargeo.com>
2022-03-01 10:55:49 -0800

    Add base import etl module view model, re #8009

---
+++ arches/app/media/js/viewmodels/base-import-view-model.js
@@ -0,0 +1,67 @@
+define(['jquery', 'knockout', 'uuid', 'arches', 'js-cookie'], function($, ko, uuid, arches, Cookies) {
+    /**
+    * A base viewmodel for import modules
+    *
+    * @constructor
+    * @name ImportModuleViewModel
+    *
+    * @param  {string} params
+    */
+    const ImportModuleViewModel = function() {
+        const self = this;
+        this.fileAdded = ko.observable(false);
+        this.fileInfo = ko.observable({name:"", size:""});
+        this.formData = new window.FormData();
+        this.uniqueidClass = ko.computed(function() {
+            return "unique_id_" + uuid.generate();
+        });
+
+        this.addFile = function(file){
+            self.loading(true);
+            self.fileInfo({name: file.name, size: file.size});
+            self.formData.append('file', file, file.name);
+            self.submit('read').then(function(data){
+                console.log('success ', data);
+                self.fileAdded(true);
+                self.loading(false);
+            }).catch(function(err) {    
+                // eslint-disable-next-line no-console
+                console.log(err);
+                self.loading(false);
+            });
+        };
+
+        this.submit = function(action) {
+            self.formData.append('action', action);
+            self.formData.append('transaction_id', self.transaction_id);
+            self.formData.append('module', this.moduleId);
+            return fetch(arches.urls.etl_manager, {
+                method: 'POST',
+                body: self.formData,
+                credentials: 'include',
+                headers: {
+                    "X-CSRFToken": Cookies.get('csrftoken')
+                }
+            });
+        };
+
+        this.dropzoneOptions = {
+            url: "arches.urls.root",
+            dictDefaultMessage: '',
+            autoProcessQueue: false,
+            uploadMultiple: false,
+            // acceptedFiles: ["text/csv"],
+            autoQueue: false,
+            clickable: ".fileinput-button." + this.uniqueidClass(),
+            previewsContainer: '#hidden-dz-previews',
+            init: function() {
+                self.dropzone = this;
+                this.on("addedfile", self.addFile); 
+                this.on("error", function(file, error) {
+                    file.error = error;
+                });    
+            }
+        };
+    };
+    return ImportModuleViewModel;
+});
