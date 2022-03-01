d8782038bc7a324a2985b7fee8139f4c79c25155
Christopher Byrd <cbyrd@fargeo.com>
2022-03-01 11:50:42 -0800

    nit #8090

--- arches/app/frontend/webpack/webpack.common.js
+++ arches/app/frontend/webpack/webpack.common.js
@@ -57,6 +57,32 @@ module.exports = {
         libraryTarget: 'amd-require',
         clean: true,
     },
+    optimization: {
+        // concatenateModules: true,
+        // flagIncludedChunks: true,
+        // removeAvailableModules: true,
+        // splitChunks: {  //  CURRENTLY DEFAULT VALUES
+        //     chunks: 'async',
+        //     minSize: 20000,
+        //     minRemainingSize: 0,
+        //     minChunks: 1,
+        //     maxAsyncRequests: 30,
+        //     maxInitialRequests: 30,
+        //     enforceSizeThreshold: 50000,
+        //     cacheGroups: {
+        //       defaultVendors: {
+        //         test: /[\\/]node_modules[\\/]/,
+        //         priority: -10,
+        //         reuseExistingChunk: true,
+        //       },
+        //       default: {
+        //         minChunks: 2,
+        //         priority: -20,
+        //         reuseExistingChunk: true,
+        //       },
+        //     },
+        //   },
+    },
     plugins: [
         new CleanWebpackPlugin(),
         new CopyWebpackPlugin({ patterns: [{from: Path.resolve(__dirname, '../../media/img'), to: 'img'} ] }),
