e44419f54bcccd55790bee025db5b1c441b9d804
Cyrus Hiatt <chiatt@fargeo.com>
2022-03-03 15:42:07 -0800

    Pass request to ETLModule, re #8009

--- arches/app/views/etl_manager.py
+++ arches/app/views/etl_manager.py
@@ -23,7 +23,7 @@ class ETLManagerView(View):
         """
         action = request.POST.get("action")
         module = request.POST.get("module")
-        import_module = ETLModule.objects.get(pk=module).get_class_module()()
+        import_module = ETLModule.objects.get(pk=module).get_class_module()(request)
         import_function = getattr(import_module, action)
         response = import_function(request=request)
         print(response)
