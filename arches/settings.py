ea7411cb9fa0e782dfd762b0c9658640d9213985
Aaron Gundel <aaron.gundel@gmail.com>
2022-03-10 13:24:59 -0700

    modify settings to include build directory in static files.  remove and ignore webpack-stats.json

--- arches/settings.py
+++ arches/settings.py
@@ -280,6 +280,7 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(ROOT_DIR, "app", "media"),
+    os.path.join(ROOT_DIR, "app", "media", "build"),
 )
 
 # List of finder classes that know how to find static files in
