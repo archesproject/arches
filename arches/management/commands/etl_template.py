39a7939be06c89a9bd773ebcda60d0706dc23c57
Cyrus Hiatt <chiatt@fargeo.com>
2022-03-16 17:21:02 -0700

    Add command to create branch csv template, #8009

--- arches/management/commands/etl_template.py
+++ arches/management/commands/etl_template.py
@@ -19,9 +19,8 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 """This module contains commands for building Arches."""
 
 from django.core.management.base import BaseCommand
-from openpyxl import Workbook
+from openpyxl import Workbook, styles
 from django.db import connection
-from arches.app.models.graph import Graph
 from arches.app.models.models import Node
 
 
@@ -41,37 +40,65 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        self.create_template(template=options["template"], dest=options["dest"], graphid=options["graph"])
+        self.create_template(template=options['template'], dest=options["dest"], graphid=options["graph"])
 
-    def create_template(self, template, dest, graphid):
+    def write_metadata(self, workbook, metadata):
+        metadata_sheet = workbook.create_sheet(title='Metadata')
+        metadata_sheet[f"A1"] = "node"
+        metadata_sheet[f"B1"] = "datatype"
+        for i, node in enumerate(metadata):
+            metadata_sheet.cell(column=1, row=i + 2, value=node['alias'])
+            metadata_sheet.cell(column=2, row=i + 2, value=node['datatype'])
+    
+    def style_header(self, length, width, sheet):
+        thin = styles.Side(border_style="thin", color="000000")
+        fill = styles.PatternFill("solid", fgColor="eeeeee")
+        border = styles.Border(top=thin, left=thin, right=thin, bottom=thin)
+        for row in range(1, length + 1):
+            for column in range(1, width + 1):
+                sheet.cell(row=row, column=column).fill = fill
+                sheet.cell(row=row, column=column).border = border
+
+    def create_branchcsv_template(self, dest, graphid):
         wb = Workbook()
         columns = ("root_nodegroupid", "nodegroupid", "parent_nodegroupid", "alias", "name", "depth", "path", "cardinality")
         with connection.cursor() as cursor:
             cursor.execute("""SELECT * FROM __get_nodegroup_tree_by_graph(%s)""", (graphid,))
             rows = cursor.fetchall()
             first_sheet = True
+            metadata = []
+            column_length = 0
             for row in rows:
                 details = dict(zip(columns, row))
-                tab = "    " * details["depth"]
-                nodes = Node.objects.filter(nodegroup_id=details["nodegroupid"]).values("datatype", "alias")
-                data_collecting_nodes = [node["alias"] for node in nodes if node["datatype"] != "semantic"]
+                tab = '    ' * details["depth"]
+                nodes = Node.objects.filter(nodegroup_id=details['nodegroupid']).exclude(datatype='semantic').values('datatype', 'alias')
+                metadata += nodes
                 if details["depth"] == 0:
+                    column_length = 0
                     if first_sheet == True:
                         sheet = wb.active
-                        sheet.title = details["name"]
+                        sheet.title = details["alias"]
                         first_sheet = False
                     else:
-                        sheet = wb.create_sheet(title=details["name"])
+                        sheet = wb.create_sheet(title=details["alias"])
                     row_number = 2
                     sheet[f"A1"] = "resource_id"
-                    sheet[f"B1"] = "node group"
+                    sheet[f"B1"] = "nodegroup"
+                    sheet[f"A{row_number}"] = "--"
                     sheet[f"B2"] = f'{tab}{details["alias"]}  ({details["cardinality"]})'
-                    for i, node_alias in enumerate(data_collecting_nodes):
-                        sheet.cell(column=i + 3, row=row_number, value=f"{node_alias}")
+                    for i, node in enumerate(nodes):
+                        sheet.cell(column=i + 3, row=row_number, value=f"{node['alias']}")
                 else:
                     row_number += 1
-                    sheet[f"B{row_number}"] = f'{tab}{details["alias"]}  ({details["cardinality"]})'
-                    for i, node_alias in enumerate(data_collecting_nodes):
-                        sheet.cell(column=i + 3, row=row_number, value=f"{node_alias}")
-                dest_filename = dest
-                wb.save(filename=dest_filename)
+                    sheet[f"A{row_number}"] = "--"
+                    sheet[f"B{row_number}"]	= f'{tab}{details["alias"]}  ({details["cardinality"]})'
+                    for i, node in enumerate(nodes):
+                        sheet.cell(column=i + 3, row=row_number, value=f"{node['alias']}")
+                column_length = len(nodes) if len(nodes) > column_length else column_length
+                self.style_header(row_number, column_length + 2, sheet)
+            self.write_metadata(wb, metadata)
+            wb.save(filename = dest)
+
+    def create_template(self, template, dest, graphid):
+        if template == 'branchcsv':
+            self.create_branchcsv_template(dest, graphid)
