4dc0b0381c5c218f3e0f36933dc2d80de41635f3
Chantelle <chantelle.smith@HistoricEngland.org.uk>
2022-03-25 12:53:32 +0000

    Move keyup events to viewmodel

--- arches/app/media/js/viewmodels/card-component.js
+++ arches/app/media/js/viewmodels/card-component.js
@@ -63,7 +63,7 @@ define([
             self.dirty = ko.computed(function() {
                 if (!ko.unwrap(self.tiles)) {
                     return true;
-                } 
+                }
                 else {
                     return ko.unwrap(self.tiles).reduce(function(acc, tile) {
                         if (tile.dirty()) {
@@ -93,6 +93,131 @@ define([
 
             if (self.card.preSaveCallback) {
                 self.card.preSaveCallback(self.saveTile);
+            }
+        };
+
+        this.cardComponentEventHandler = function(eventType,additionalParam,data,event){
+            if(event.type=="click" || event.type=="mousedown" ||event.type=="dblclick" ||(event.type=="keyup" && (event.which==13 || event.keyCode==13))){
+                switch (eventType){
+                case "report expanded":
+                    data.reportExpanded(!data.reportExpanded());
+                    break;
+                case "copy tile":
+                    additionalParam.parent.copyTile(additionalParam);
+                    break;
+                case "select":
+                    additionalParam.selected(true);
+                    break;
+                case "reveal":
+                    card = data.card
+                    data.revealForm(card);
+                    break;
+                case "expand card":
+                    data.card.expanded(!data.card.expanded());
+                    break;
+                case "show form":
+                    data.card.showForm(true);
+                    break;
+                case "expanded":
+                    data.expanded(!data.expanded());
+                    break;
+                case "delete provisional edits":
+                    data.provisionalTileViewModel.deleteAllProvisionalEdits();
+                    break;
+                case "select provisional edit":
+                    parent = additionalParam[0]
+                    provisionalEdit = additionalParam[1]
+                    parent.provisionalTileViewModel.selectProvisionalEdit(provisionalEdit)
+                    break;
+                case "select or add card":
+                    if (data.parent.tileid) {
+                        data.canAdd() ? data.selected(true) : data.tiles()[0].selected(true);
+                        break;
+                    }
+                case "create parent and child":
+                    parent = additionalParam
+                    parent.createParentAndChild(parent.tile, data.card)
+                    break;
+                case "delete tile":
+                    data.deleteTile();
+                    break;
+                case "reset parameter":
+                    additionalParam.reset();
+                    break;
+                case "save tile":
+                    data.saveTile();
+                    break;
+                case "close help":
+                    data.card.model.get('helpactive')(false)
+                    break;
+                case "open help":
+                    if(event.type=="keyup"){
+                        event.stopPropagation();
+                    }
+                    data.card.model.get('helpactive')(true)
+                    break;
+                case "stop propagation card select or add":
+                    event.stopPropagation();
+                    data.card.canAdd() ? data.card.selected(true) : data.card.tiles()[0].selected(true);
+                    break;
+                case "stop propagation reset authoritative":
+                    if(event.type=="keyup"){
+                        event.stopPropagation();
+                    }
+                    data.provisionalTileViewModel.resetAuthoritative();
+                    break;
+                case "stop propagation reject provisional edit":
+                    parent = additionalParam[0]
+                    provisionalEdit = additionalParam[1]
+                    if(event.type=="keyup"){
+                        event.stopPropagation();
+                    }
+                    parent.provisionalTileViewModel.rejectProvisionalEdit(provisionalEdit);
+                    break;
+                case "stop propagation select child cards":
+                    if(event.type=="keyup"){
+                        event.stopPropagation();
+                    }
+                    data.card.selectChildCards()
+                    break;
+                }
+
+            }
+        }
+
+        this.cardComponentWidgetEventHandler = function(parent,area,data,event){
+            if(event.type=="click" || (event.type=="keyup" && (event.which==13 || event.keyCode==13))){
+                if(event.type=="keyup"){
+                    event.stopPropagation();
+                }
+                if(area=="form"){
+                    if (!data.selected() && parent.preview) {
+                        data.selected(true);
+                    }
+                }
+                else{
+                    data.selected(true);
+                }
+            }
+            if(event.type=="mouseover"){
+                if(area=="form"){
+                    if (parent.preview){
+                        data.hovered(true);
+                    }
+                }
+                else{
+                    data.hovered(true);
+                }
+            }
+            if(event.type=="mouseout"){
+                if(area=="form"){
+                    if(parent.preview){
+                        data.hovered(null);
+                    }
+                }
+                else{
+                    data.hovered(null);
+                }
             }
         };
 
@@ -210,7 +335,7 @@ define([
                 }
             });
         };
-        
+
         this.createParentAndChild = async (parenttile, childcard) => {
             try{
                 const newSave = await self.card.saveParentTile(parenttile);
