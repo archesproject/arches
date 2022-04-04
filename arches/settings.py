c63600462e85a89ad579e3739c90917f92e786e2
Ferguson, Brett CSNR:EX <brett@qedsystems.ca>
2022-04-04 16:53:46 -0700

    4983 - First cut of custom map popup overlays

--- arches/settings.py
+++ arches/settings.py
@@ -682,6 +682,14 @@ RENDERERS = [
     },
 ]
 
+MAP_POPUP_CONFIG_PROVIDER = "js/utils/map-popup-provider"
+
+# MAP_POPUP_CONFIG = {
+#     "requires": {
+#         "map-popup-provider":
+#     }
+# }
+
 # --- JSON LD sortorder generating functions --- #
 #
 # The functions in the array below will be called in the order given with the json-ld of the node
