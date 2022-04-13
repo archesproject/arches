270b7d4776fbdc3302112617d56a879b2177cab6
stickler-ci <support@stickler-ci.com>
2022-04-13 22:59:42 +0000

    Fixing style errors.

--- arches/app/functions/primary_descriptors.py
+++ arches/app/functions/primary_descriptors.py
@@ -14,7 +14,7 @@ class AbstractPrimaryDescriptorsFunction(BaseFunction):
         Arguments:
         resource -- the resource instance to which the primary decriptor will be assigned
         config -- the descriptor config which indicates how and what will define the descriptor
-        
+
         Keyword Arguments:
         context -- string such as "copy" to indicate conditions under which a resource participates in a function.
         """
