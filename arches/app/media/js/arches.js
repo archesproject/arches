bf261c6731083379998a0d8bc36e94fd9520e28b
Christopher Byrd <cbyrd@fargeo.com>
2022-04-16 10:14:32 -0700

    adds all arches k/v pairs to arches component #8090

--- arches/app/media/js/arches.js
+++ arches/app/media/js/arches.js
@@ -1,17 +1,15 @@
 define([], function () {
+    function removeWhitespace(string){
+        return string.replace(/(\r\n|\n|\r)/gm, "");
+    }
+    
     function buildUrls() {
-        function removeWhitespace(string){
-            return string.replace(/(\r\n|\n|\r)/gm, "");
-        }
-        
         function acceptUserInput(url){
             return function(input) {
                 return url.replace('aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', input);
             };
         }
-    
-        const archesData = document.querySelector('#arches-data');
-    
+
         const staticUrls = JSON.parse(removeWhitespace(archesData.getAttribute('staticUrls') || ''));
         const dynamicUrls = JSON.parse(removeWhitespace(archesData.getAttribute('dynamicUrls') || ''));
         const userInputUrls = JSON.parse(removeWhitespace(archesData.getAttribute('userInputUrls') || ''));
@@ -53,7 +51,46 @@ define([], function () {
         return orderedUrls;
     }
 
+    const archesData = document.querySelector('#arches-data');
+
     return {
+        celeryRunning: archesData.getAttribute('celeryRunning'),
+        conceptCollections: JSON.parse(archesData.getAttribute('conceptCollections')),
+        confirmAllResourceDelete: JSON.parse(archesData.getAttribute('confirmAllResourceDelete')),
+        confirmGraphDelete: JSON.parse(archesData.getAttribute('confirmGraphDelete')),
+        confirmMaplayerDelete: JSON.parse(archesData.getAttribute('confirmMaplayerDelete')),
+        confirmNav: JSON.parse(archesData.getAttribute('confirmNav')),
+        confirmResourceDelete: JSON.parse(archesData.getAttribute('confirmResourceDelete')),
+        confirmSurveyDelete: JSON.parse(archesData.getAttribute('confirmSurveyDelete')),
+        exportHtmlTemplates: JSON.parse(archesData.getAttribute('exportHtmlTemplates')),
+        geocoderDefault: archesData.getAttribute('geocoderDefault'),
+        geocoderPlaceholder: archesData.getAttribute('geocoderPlaceholder'),
+        geoCodingProviders: JSON.parse(archesData.getAttribute('geoCodingProviders')),
+        graphImportFailed: JSON.parse(archesData.getAttribute('graphImportFailed')),
+        graphs: JSON.parse(archesData.getAttribute('graphs')),
+        hexBinBounds: JSON.parse(archesData.getAttribute('hexBinBounds')),
+        hexBinSize: JSON.parse(archesData.getAttribute('hexBinSize')),
+        mapDefaultX: JSON.parse(archesData.getAttribute('mapDefaultX')),
+        mapDefaultY: JSON.parse(archesData.getAttribute('mapDefaultY')),
+        mapDefaultZoom: JSON.parse(archesData.getAttribute('mapDefaultZoom')),
+        mapDefaultMinZoom: JSON.parse(archesData.getAttribute('mapDefaultMinZoom')),
+        mapDefaultMaxZoom: JSON.parse(archesData.getAttribute('mapDefaultMaxZoom')),
+        mapboxApiKey: archesData.getAttribute('mapboxApiKey'),
+        mapboxGlyphs: archesData.getAttribute('mapboxGlyphs'),
+        mapboxSprites: archesData.getAttribute('mapboxSprites'),
+        mapLayers: JSON.parse(archesData.getAttribute('mapLayers')),
+        mapMarkers: JSON.parse(archesData.getAttribute('mapMarkers')),
+        mapSources: JSON.parse(archesData.getAttribute('mapSources')),
+        preferredCoordinateSystems: JSON.parse(archesData.getAttribute('preferredCoordinateSystems')),
+        requestFailed: JSON.parse(archesData.getAttribute('requestFailed')),
+        resourceCopyFailed: JSON.parse(archesData.getAttribute('resourceCopyFailed')),
+        resourceCopySuccess: JSON.parse(archesData.getAttribute('resourceCopySuccess')),
+        resourceHasUnpublishedGraph: JSON.parse(archesData.getAttribute('resourceHasUnpublishedGraph')),
+        resources: JSON.parse(archesData.getAttribute('resources')),
+        translations: JSON.parse(removeWhitespace(archesData.getAttribute('translations'))),
         urls: buildUrls(),
+        userEmail: archesData.getAttribute('userEmail'),
+        useSemanticRelationships: JSON.parse(archesData.getAttribute('useSemanticRelationships')),
+        version: archesData.getAttribute('version'),
     };
 });
