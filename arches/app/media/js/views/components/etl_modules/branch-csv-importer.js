eee00b17a1f86162290a9a5c035393ffdd7dad40
njkim <nkim@fargeo.com>
2022-04-16 22:01:10 -0700

    Update writing ui w/ spinner then reload, #8280

--- arches/app/media/js/views/components/etl_modules/branch-csv-importer.js
+++ arches/app/media/js/views/components/etl_modules/branch-csv-importer.js
@@ -7,12 +7,30 @@ define([
 ], function(_, ko, ImporterViewModel, dropzone) {
     return ko.components.register('branch-csv-importer', {
         viewModel: function(params) {
+            const self = this;
             this.moduleId = params.etlmoduleid;
             ImporterViewModel.apply(this, arguments);
             this.loading = params.config.loading;
             this.response.subscribe(val => {
                 console.log(val);
             });
+
+            this.write = function(){
+                self.loading(true);
+                self.submit('write').then(function(response){
+                    self.loading(false);
+                    if (response.ok) {
+                        return response.json();
+                    }
+                }).then(function(response) {
+                    if (response.result === "success"){
+                        self.response(null);
+                    }
+                }).catch(function(err) {    
+                    // eslint-disable-next-line no-console
+                    console.log(err);
+                });
+            };    
         },
         template: { require: 'text!templates/views/components/etl_modules/branch-csv-importer.htm' }
     });
