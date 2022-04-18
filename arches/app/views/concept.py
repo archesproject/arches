92844a1a61df756b18ab8b62d090d8f60add1e72
Alexei Peters <apeters@fargeo.com>
2022-04-17 21:18:24 -0700

    promote exact match to top of concept dropdown, re #8355

--- arches/app/views/concept.py
+++ arches/app/views/concept.py
@@ -392,6 +392,50 @@ def paged_dropdown(request):
         dict(list(zip(["id", "text", "conceptid", "language", "type"], d["valueto"].values())), depth=d["depth"], collector=d["collector"])
         for d in data
     ]
+
+    # This try/except block trys to find an exact match to the concept the user is searching and if found
+    # it will insert it into the results as the first item so that users don't have to scroll to find it.
+    # See: https://github.com/archesproject/arches/issues/8355
+    try:
+        if page == 1:
+            found = False
+            for i, d in enumerate(data):
+                if i <= 7 and d["text"].lower() == query.lower():
+                    found = True
+                    break
+            if not found:
+                sql = """
+                    SELECT value, valueid
+                    FROM 
+                    (
+                        SELECT *, CASE WHEN LOWER(languageid) = '{languageid}' THEN 10
+                        WHEN LOWER(languageid) like '{short_languageid}%' THEN 5
+                        ELSE 0
+                        END score
+                        FROM values
+                    ) as vals
+                    WHERE LOWER(value)='{query}' AND score > 0
+                    AND valuetype in ('prefLabel')
+                    ORDER BY score desc limit 1
+                """
+                from django.utils.translation import get_language
+                from django.db import connection
+                languageid = get_language().lower()
+                sql = sql.format(
+                    query=query.lower(),
+                    languageid= languageid,
+                    short_languageid=languageid.split("-")[0]
+                )
+                cursor = connection.cursor()
+                cursor.execute(sql)
+                rows = cursor.fetchall()
+
+                if len(rows) == 1:
+                    data.insert(0, {"id": str(rows[0][1]), "text": rows[0][0], "depth": 1, "collector": False})
+                print(data)
+    except:
+        pass
+
     return JSONResponse({"results": data, "more": offset + limit < total_count})
 
 
