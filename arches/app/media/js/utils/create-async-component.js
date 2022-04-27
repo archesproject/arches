d63766def2af208c766f9d180f3a0862419de324
Christopher Byrd <cbyrd@fargeo.com>
2022-04-27 14:32:59 -0700

    polish create-async-component #8090

--- arches/app/media/js/utils/create-async-component.js
+++ arches/app/media/js/utils/create-async-component.js
@@ -1,47 +1,29 @@
 define(['jquery', 'knockout', 'arches'], function($, ko, arches) {
-    function injectComponent(injectionSite, html, viewModel, params) {
-        injectionSite.innerHTML = html;
-                
-        ko.cleanNode(injectionSite);
-        ko.applyBindings( new viewModel(ko.unwrap(params)), injectionSite );
-    }
-
-    return function createAsyncComponent(componentName, viewModel, templatePath) {
-        const htmlElementId = `${componentName}--injection-site`;
-        const params = ko.observable();
-
+    function injectComponent(htmlElementId, params, viewModel, templatePath) {
         $.ajax({
             type: 'GET',
             url: arches.urls.root + templatePath,
             complete: function(e) {
                 const injectionSite = document.querySelector(`#${htmlElementId}`);
-
-                if (injectionSite){
-                    injectComponent(injectionSite, e.responseText, viewModel, params());
-                }
-                else {
-                    let intervalCount = 0;
-
-                    const injectionInterval = setInterval(function() {
-                        const injectionSite = document.querySelector(`#${htmlElementId}`);
-
-                        if (injectionSite) {
-                            injectComponent(injectionSite, e.responseText, viewModel, params());
-                            clearInterval(injectionInterval);
-                        }
-                        else if (intervalCount >= 5) {
-                            clearInterval(injectionInterval);
-                        }
-
-                        intervalCount += 1;
-                    }, 200);
-                }
+                injectionSite.innerHTML = e.responseText;
+                
+                ko.cleanNode(injectionSite);
+                ko.applyBindings( new viewModel(ko.unwrap(params)), injectionSite );
             }
         });
+    }
+
+    return function createAsyncComponent(componentName, viewModel, templatePath) {
+        const htmlElementId = `${componentName}--injection-site`;
 
         return ko.components.register(componentName, {
-            viewModel: function(e){params(e)},
-            template: `<div style='width: 100%; height: 100%;' id=${htmlElementId}></div>`
+            viewModel: function(params){
+                this.injectComponent = function(){ injectComponent(htmlElementId, params, viewModel, templatePath) }
+            },
+            template: `
+                <div style='display:none' data-bind="click: $data.injectComponent()"></div>
+                <div style='width: 100%; height: 100%;' id=${htmlElementId}></div>
+            `
         });
     }
 });
