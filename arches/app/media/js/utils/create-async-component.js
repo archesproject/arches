c99757838a5531010b5b2b4e6ee37dd9a90340f9
Christopher Byrd <cbyrd@fargeo.com>
2022-04-27 13:28:34 -0700

    polish async component loader #8090

--- arches/app/media/js/utils/create-async-component.js
+++ arches/app/media/js/utils/create-async-component.js
@@ -1,4 +1,11 @@
-define(['knockout', 'arches'], function(ko, arches) {
+define(['jquery', 'knockout', 'arches'], function($, ko, arches) {
+    function injectComponent(injectionSite, html, viewModel, params) {
+        injectionSite.innerHTML = html;
+                
+        ko.cleanNode(injectionSite);
+        ko.applyBindings( new viewModel(ko.unwrap(params)), injectionSite );
+    }
+
     return function createAsyncComponent(componentName, viewModel, templatePath) {
         const htmlElementId = `${componentName}--injection-site`;
         const params = ko.observable();
@@ -8,15 +15,33 @@ define(['knockout', 'arches'], function(ko, arches) {
             url: arches.urls.root + templatePath,
             complete: function(e) {
                 const injectionSite = document.querySelector(`#${htmlElementId}`);
-                injectionSite.innerHTML = e.responseText;
 
-                ko.cleanNode(injectionSite);
-                ko.applyBindings( new viewModel(params()), injectionSite );
+                if (injectionSite){
+                    injectComponent(injectionSite, e.responseText, viewModel, params());
+                }
+                else {
+                    let intervalCount = 0;
+
+                    const injectionInterval = setInterval(function() {
+                        const injectionSite = document.querySelector(`#${htmlElementId}`);
+
+                        if (injectionSite) {
+                            injectComponent(injectionSite, e.responseText, viewModel, params());
+                            clearInterval(injectionInterval);
+                        }
+                        else if (intervalCount >= 5) {
+                            clearInterval(injectionInterval);
+                        }
+
+                        intervalCount += 1;
+                    }, 200);
+                }
             }
         });
+
         return ko.components.register(componentName, {
             viewModel: function(e){params(e)},
-            template: `<div id=${htmlElementId}></div>`
+            template: `<div style='width: 100%; height: 100%;' id=${htmlElementId}></div>`
         });
     }
 });
