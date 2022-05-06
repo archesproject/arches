b0ce836c8d2c55fb83923d4b01c04b2b06e7bc00
Christopher Byrd <cbyrd@fargeo.com>
2022-05-06 14:24:36 -0700

    updates webpack prod config #8090

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,36 +1,32 @@
-const Path = require('path');
 const Webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const common = require('./webpack.common.js');
+const { PROJECT_PATH } = require('./webpack-paths.js');
 
-ARCHES_CORE_PATH = Path.resolve(__dirname.split('/webpack')[0], './arches/app');
 
 module.exports = merge(common, {
     mode: 'production',
     devtool: false,
     bail: true,
     output: {
-        filename: 'js/[name].[chunkhash:8].js',
-        chunkFilename: 'js/[name].[chunkhash:8].chunk.js',
+        chunkFilename: 'js/[name].chunk.js',
     },
     plugins: [
         new Webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production'),
         }),
-        new MiniCssExtractPlugin({
-            filename: 'css/app-[contenthash].css',
-        }),
+        new MiniCssExtractPlugin(),
     ],
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: `${ARCHES_CORE_PATH}/media/node_modules/babel-loader`,
+                loader: `${PROJECT_PATH}/media/node_modules/babel-loader`,
                 options: {
                     presets: ['@babel/preset-env'],
-                    cacheDirectory: `${ARCHES_CORE_PATH}/media/node_modules/.cache/babel-loader`,
+                    cacheDirectory: `${PROJECT_PATH}/media/node_modules/.cache/babel-loader`,
                 }
             },
             {
@@ -40,13 +36,13 @@ module.exports = merge(common, {
                         'loader': MiniCssExtractPlugin.loader,
                     },
                     {
-                        'loader': `${ARCHES_CORE_PATH}/media/node_modules/css-loader`,
+                        'loader': `${PROJECT_PATH}/media/node_modules/css-loader`,
                     },
                     {
-                        'loader': `${ARCHES_CORE_PATH}/media/node_modules/postcss-loader`,
+                        'loader': `${PROJECT_PATH}/media/node_modules/postcss-loader`,
                     },
                     {
-                        'loader': `${ARCHES_CORE_PATH}/media/node_modules/sass-loader`,
+                        'loader': `${PROJECT_PATH}/media/node_modules/sass-loader`,
                     }
                 ],
             },
