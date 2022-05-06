e50c83acaaf4544179fb8e6d36d1987ee282ce5a
Christopher Byrd <cbyrd@fargeo.com>
2022-05-06 12:33:29 -0700

    nit #8090

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
       "node": true
     },
     "parserOptions": {
-      "ecmaVersion": "latest",
+      "ecmaVersion": 2022,
       "sourceType": "module",
       "requireConfigFile": false
     },
