d574980c8c8be4a5778cd9118cedd10cdba9d050
stickler-ci <support@stickler-ci.com>
2022-05-17 17:09:21 +0000

    Fixing style errors.

--- arches/app/models/tile.py
+++ arches/app/models/tile.py
@@ -657,7 +657,9 @@ class Tile(models.TileModel):
                     if context:
                         function.save(self, request, context=context)
                     else:
-                        function.save(self, request) # support functions without context TODO: remove if/else block in 7.0 release. All fns to require context
+                        function.save(
+                            self, request
+                        )  # support functions without context TODO: remove if/else block in 7.0 release. All fns to require context
                 except NotImplementedError:
                     pass
         except TypeError:
@@ -686,7 +688,9 @@ class Tile(models.TileModel):
                     if context:
                         function.post_save(self, request, context=context)
                     else:
-                        function.post_save(self, request) # support functions without context TODO: remove if/else block in 7.0 release. All fns to require context
+                        function.post_save(
+                            self, request
+                        )  # support functions without context TODO: remove if/else block in 7.0 release. All fns to require context
                 except NotImplementedError:
                     pass
         except TypeError as e:
