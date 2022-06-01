f7fdb3564f531a566fd3dd2ef0191809130a6161
Gerardo Anglada <ganglada.dlanda@gmail.com>
2022-06-01 15:17:17 +0200

    Modified sttings.py to clone branch in another computer.

--- dataspace/dataspace/settings_local.py
+++ dataspace/dataspace/settings_local.py
@@ -2,9 +2,9 @@ try:
     from .dataspace.settings import *
 except ImportError:
     pass
-# try:
-#     GEOS_LIBRARY_PATH = 'C:\\OSGeo4W\\bin\\geos_c.dll'
-#     GDAL_LIBRARY_PATH = 'C:\\OSGeo4W\\bin\\gdal304.dll'
+try:
+    GEOS_LIBRARY_PATH = 'C:\\OSGeo4W\\bin\\geos_c.dll'
+    GDAL_LIBRARY_PATH = 'C:\\OSGeo4W\\bin\\gdal304.dll'
 
-# except ImportError:
-#     pass
+except ImportError:
+    pass
