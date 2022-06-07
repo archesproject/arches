6f6ab5d38407087d655df648ce2de654c85b38d3
stickler-ci <support@stickler-ci.com>
2022-06-07 21:08:45 +0000

    Fixing style errors.

--- arches/app/etl_modules/base_import_module.py
+++ arches/app/etl_modules/base_import_module.py
@@ -9,17 +9,17 @@ logger = logging.getLogger(__name__)
 
 
 class BaseImportModule(object):
-
     @method_decorator(user_created_transaction_match, name="dispatch")
     def reverse(self, request, **kwargs):
         success = False
         response = {"success": success, "data": ""}
-        loadid = self.loadid if self.loadid else request.POST.get('loadid')
+        loadid = self.loadid if self.loadid else request.POST.get("loadid")
         try:
             with connection.cursor() as cursor:
-                print('updating load event status')
+                print("updating load event status")
                 cursor.execute(
-                    """UPDATE load_event SET status = %s WHERE loadid = %s""", ("reversing", loadid),
+                    """UPDATE load_event SET status = %s WHERE loadid = %s""",
+                    ("reversing", loadid),
                 )
                 response["changes"] = reverse_edit_log_entries(loadid)
                 success = True
@@ -30,6 +30,6 @@ class BaseImportModule(object):
             response["success"] = success
         except Exception as e:
             response["data"] = e
-            logger.error(e);
-        logger.warn(response);
+            logger.error(e)
+        logger.warn(response)
         return response
