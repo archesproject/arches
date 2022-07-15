46e499a7332e68e10645846abe8897532cebb46e
Cyrus Hiatt <chiatt@fargeo.com>
2022-07-15 13:04:38 -0700

    Fix migration dependency order, re #8220

--- arches/app/models/migrations/8220_node_hascustomalias.py
+++ arches/app/models/migrations/8220_node_hascustomalias.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("models", "8140_django_upgrade"),
+        ("models", "8301_descriptors"),
     ]
 
     operations = [
