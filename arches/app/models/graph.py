765cdd9f2a9265d93a6a66b0f787004701f2242c
Ryan Anderson <randerson@fargeo.com>
2022-08-16 16:52:04 -0700

    Fix issue where graphs could not be cloned, re #8724

--- arches/app/models/graph.py
+++ arches/app/models/graph.py
@@ -779,6 +779,7 @@ class Graph(models.GraphModel):
             copy_of_self.subtitle = ""
             copy_of_self.description = ""
             copy_of_self.author = ""
+            copy_of_self.serialized_graph = ""
 
         # returns a list of node ids sorted by nodes that are collector nodes first and then others last
         node_ids = sorted(copy_of_self.nodes, key=lambda node_id: copy_of_self.nodes[node_id].is_collector, reverse=True)
