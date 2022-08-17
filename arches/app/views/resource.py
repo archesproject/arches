d7791b08745da668ec80dd16dfe71ffd8d505ae4
njkim <nkim@fargeo.com>
2022-08-17 12:16:26 -0700

    nit, #8707

--- arches/app/views/resource.py
+++ arches/app/views/resource.py
@@ -150,6 +150,46 @@ class ResourceEditorView(MapBaseManagerView):
                 datatype = datatype_lookup[nodeid]
                 datatype.pre_structure_tile_data(tile, nodeid, languages=languages)
 
+        def add_i18n_to_cardwidget_defaults(cardwidgets):
+            serialized_cardwidgets = JSONSerializer().serializeToPython(cardwidgets)
+
+            for cardwidget in serialized_cardwidgets:
+                if cardwidget["widget_id"] in ["10000000-0000-0000-0000-000000000005", "10000000-0000-0000-0000-000000000001"]:
+                    try:
+                        default_value = cardwidget["config"]["defaultValue"]
+                    except KeyError:
+                        default_value = None
+                    if default_value is None:
+                        existing_languages = []
+                        cardwidget["config"]["defaultValue"] = {}
+                    elif type(default_value) is str:
+                        default_language = languages.get(code=settings.LANGUAGE_CODE)
+                        cardwidget["config"]["defaultValue"] = {
+                            settings.LANGUAGE_CODE: {"value": default_value, "direction": default_language.default_direction}
+                        }
+                        existing_languages = [settings.LANGUAGE_CODE]
+                    else:
+                        existing_languages = list(default_value.keys())
+                    for language in languages:
+                        if language.code not in existing_languages:
+                            cardwidget["config"]["defaultValue"][language.code] = {"value": "", "direction": language.default_direction}
+            return serialized_cardwidgets
+
+        def add_i18n_to_widget_defaults(widgets):
+            for widget in widgets:
+                if widget.datatype == "string":
+                    existing_languages = []
+                    default_value = widget.defaultconfig["defaultValue"]
+                    if default_value != "" and default_value is not None:
+                        existing_languages = list(default_value.keys())
+                        for language in languages:
+                            if language.code not in existing_languages:
+                                widget.defaultconfig["defaultValue"][language.code] = {
+                                    "value": "",
+                                    "direction": language.default_direction,
+                                }
+            return widgets
+
         if resourceid is None:
             resource_instance = None
             graph = models.GraphModel.objects.get(pk=graphid)
@@ -247,51 +287,10 @@ class ResourceEditorView(MapBaseManagerView):
             serialized_cards = JSONSerializer().serializeToPython(cards)
             cardwidgets = [widget for widget in [card.cardxnodexwidget_set.order_by("sortorder").all() for card in cards]]
 
-        def update_cardwidgets_default_for_string(cardwidgets):
-            serialized_cardwidgets = JSONSerializer().serializeToPython(cardwidgets)
-
-            for cardwidget in serialized_cardwidgets:
-                if cardwidget["widget_id"] in ["10000000-0000-0000-0000-000000000005", "10000000-0000-0000-0000-000000000001"]:
-                    try:
-                        default_value = cardwidget["config"]["defaultValue"]
-                    except KeyError:
-                        default_value = None
-                    if default_value is None:
-                        existing_languages = []
-                        cardwidget["config"]["defaultValue"] = {}
-                    elif type(default_value) is str:
-                        default_language = languages.get(code=settings.LANGUAGE_CODE)
-                        cardwidget["config"]["defaultValue"] = {
-                            settings.LANGUAGE_CODE: {"value": default_value, "direction": default_language.default_direction}
-                        }
-                        existing_languages = [settings.LANGUAGE_CODE]
-                    else:
-                        existing_languages = list(default_value.keys())
-                    for language in languages:
-                        if language.code not in existing_languages:
-                            cardwidget["config"]["defaultValue"][language.code] = {"value": "", "direction": language.default_direction}
-            return serialized_cardwidgets
-
-        updated_cardwidgets = update_cardwidgets_default_for_string(cardwidgets)
+        updated_cardwidgets = add_i18n_to_cardwidget_defaults(cardwidgets)
 
         widgets = list(models.Widget.objects.all())
-
-        def update_widgets_list_default_for_string(widgets_list):
-            for widget in widgets_list:
-                if widget.datatype == "string":
-                    existing_languages = []
-                    default_value = widget.defaultconfig["defaultValue"]
-                    if default_value != "" and default_value is not None:
-                        existing_languages = list(default_value.keys())
-                        for language in languages:
-                            if language.code not in existing_languages:
-                                widget.defaultconfig["defaultValue"][language.code] = {
-                                    "value": "",
-                                    "direction": language.default_direction,
-                                }
-            return widgets_list
-
-        widgets_data = update_widgets_list_default_for_string(widgets)
+        updated_widgets = add_i18n_to_widget_defaults(widgets)
 
         card_components = models.CardComponent.objects.all()
         templates = models.ReportTemplate.objects.all()
@@ -316,8 +315,8 @@ class ResourceEditorView(MapBaseManagerView):
                 .exclude(publication=None)
             ),
             relationship_types=get_resource_relationship_types(),
-            widgets=widgets_data,
-            widgets_json=JSONSerializer().serialize(widgets_data),
+            widgets=updated_widgets,
+            widgets_json=JSONSerializer().serialize(updated_widgets),
             card_components=card_components,
             card_components_json=JSONSerializer().serialize(card_components),
             tiles=JSONSerializer().serialize(tiles),
