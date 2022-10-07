1a37acd31f3eb438be937326aa9173d4ac50886b
Phil Weir <phil.weir@flaxandteal.co.uk>
2022-10-07 22:38:36 +0100

    prevent circular recreation of card due to ko subscription trigger

--- arches/app/media/js/viewmodels/resource-instance-select.js
+++ arches/app/media/js/viewmodels/resource-instance-select.js
@@ -239,7 +239,7 @@ define([
     
             self.value.subscribe(updateNameAndOntologyClass);
             // Resolve Resource Instance Names from the incoming values
-            self.value.valueHasMutated();
+            updateNameAndOntologyClass(self.value);
 
             this.relationshipsInFilter = ko.computed(function() {
                 if(!self.value()) {
