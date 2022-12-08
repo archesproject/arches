c0406937d768d44fac451cf7c24c7357a72799f2
stickler-ci <support@stickler-ci.com>
2022-12-08 21:46:42 +0000

    Fixing style errors.

--- arches/app/models/graph.py
+++ arches/app/models/graph.py
@@ -542,7 +542,7 @@ class Graph(models.GraphModel):
                     editable_future_graph = models.GraphModel.objects.get(source_identifier=self.graphid)
                     editable_future_graph.delete()
                 except models.GraphModel.DoesNotExist:
-                    pass # no editable future graph to delete
+                    pass  # no editable future graph to delete
 
                 for nodegroup in self.get_nodegroups():
                     nodegroup.delete()
