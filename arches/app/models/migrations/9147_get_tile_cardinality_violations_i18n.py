42c3fd89e9f436b27600d57a373a3ccf058bacd8
Ferguson, Brett CSNR:EX <brett@qedsystems.ca>
2022-12-14 09:20:46 -0800

    9147 - fix i18n bug in __arches_get_tile_cardinality_violations

---
+++ arches/app/models/migrations/9147_get_tile_cardinality_violations_i18n.py
@@ -0,0 +1,71 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("models", "8974_rr_load_performance_v2"),
+    ]
+
+    sql_string = """
+create or replace function __arches_get_tile_cardinality_violations()
+    returns table(graph_name text, node_names text[], resourceinstanceid uuid, nodegroupid uuid, parenttileid uuid, tilecount bigint) as
+$$
+DECLARE
+BEGIN
+    return query with tile_violations as ( SELECT t.resourceinstanceid,
+                        t.nodegroupid,
+                        t.parenttileid parent_tileid,
+                        count(*)       tilecount
+                 FROM tiles t,
+                      node_groups ng
+                 WHERE t.nodegroupid = ng.nodegroupid
+                   AND ng.cardinality = '1'
+                 group by t.resourceinstanceid, t.nodegroupid,
+                          t.parenttileid
+                 having count(*) > 1)
+        select g.name->>'en',
+               array_agg(n.name), tv.*
+            from tile_violations tv,
+                 nodes n,
+                 graphs g
+                where tv.nodegroupid = n.nodegroupid
+                  and n.graphid = g.graphid
+            group by g.name->>'en', tv.resourceinstanceid, tv.nodegroupid, tv.parent_tileid, tv.tilecount
+    order by nodegroupid, resourceinstanceid;
+END $$
+    language plpgsql;
+
+    """
+
+    reverse_sql_string = """
+create or replace function __arches_get_tile_cardinality_violations()
+    returns table(graph_name text, node_names text[], resourceinstanceid uuid, nodegroupid uuid, parenttileid uuid, tilecount bigint) as
+$$
+DECLARE
+BEGIN
+    return query with tile_violations as ( SELECT t.resourceinstanceid,
+                        t.nodegroupid,
+                        t.parenttileid parent_tileid,
+                        count(*)       tilecount
+                 FROM tiles t,
+                      node_groups ng
+                 WHERE t.nodegroupid = ng.nodegroupid
+                   AND ng.cardinality = '1'
+                 group by t.resourceinstanceid, t.nodegroupid,
+                          t.parenttileid
+                 having count(*) > 1)
+        select g.name, array_agg(n.name), tv.*
+            from tile_violations tv,
+                 nodes n,
+                 graphs g
+                where tv.nodegroupid = n.nodegroupid
+                  and n.graphid = g.graphid
+            group by g.name, tv.resourceinstanceid, tv.nodegroupid, tv.parent_tileid, tv.tilecount
+    order by nodegroupid, resourceinstanceid;
+END $$
+    language plpgsql;
+    """
+
+    operations = [
+        migrations.RunSQL(sql_string, reverse_sql_string),
+    ]
