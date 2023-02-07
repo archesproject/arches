94eb2d4d12ff4df7b5e063c8f9c264b341f6b006
Cyrus Hiatt <chiatt@fargeo.com>
2023-02-07 09:51:38 -0800

    Import only constraints with nodes, re #7181

--- arches/app/models/graph.py
+++ arches/app/models/graph.py
@@ -396,7 +396,8 @@ class Graph(models.GraphModel):
             card.nodegroup = self.get_or_create_nodegroup(nodegroupid=card.nodegroup_id)
             card.config = cardobj.get("config", None)
             constraints = cardobj.get("constraints", "")
-            for constraint in constraints:
+            constraints_with_nodes = [c for c in constraints if len(c['nodes'])]
+            for constraint in constraints_with_nodes:
                 self.add_card_contraint(constraint, card)
 
         card.graph = self
