db57fcf836f31d264bea1923f5051594c1b3dfa8
Cyrus Hiatt <chiatt@fargeo.com>
2023-02-16 16:54:40 -0800

    Set version from beta to final

--- arches/__init__.py
+++ arches/__init__.py
@@ -6,7 +6,7 @@ try:
 except ModuleNotFoundError as e:
     print(e)
 
-VERSION = (6, 2, 2, "beta", 0)  # VERSION[3] options = "alpha", "beta", "rc", or "final"
+VERSION = (6, 2, 2, "final", 0)  # VERSION[3] options = "alpha", "beta", "rc", or "final"
 
 __version__ = get_version(VERSION)
 
