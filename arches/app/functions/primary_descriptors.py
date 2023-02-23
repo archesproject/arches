f6bef316fe10fea1c01e99825a96c19e4b1b0c6c
whatisgalen <whatisgalen@gmail.com>
2023-02-23 00:11:20 -0800

    overrides primary_desc fn for hist res custom display name using multi ngs

--- arches/app/functions/primary_descriptors.py
+++ arches/app/functions/primary_descriptors.py
@@ -10,20 +10,41 @@ class PrimaryDescriptorsFunction(BaseFunction):
     def get_primary_descriptor_from_nodes(self, resource, config):
         map_popup = False
         map_popup_template_default = "<Place Address> (<Address Type>)"
+        display_name_template_default = "<Name> - <Place Address>"
+        display_name_template_preferred = "<Name>\n<Place Address>"
+        # sans_addr_display_name_template_default = "<Name>"
+        hist_res_name = False
+        hist_res_name_ngid = 'a271c354-1037-11ec-b65f-31043b30bbcd'
         if config["string_template"] == map_popup_template_default:
             map_popup = True
+        if config["string_template"] == display_name_template_default:
+            hist_res_name = True
+            config["string_template"] = display_name_template_preferred
         datatype_factory = None
+        # from pprint import pprint
+        # print("COFIG")
+        # pprint(config)
         try:
             if "nodegroup_id" in config and config["nodegroup_id"] != "" and config["nodegroup_id"] is not None:
-                tiles = models.TileModel.objects.filter(nodegroup_id=uuid.UUID(config["nodegroup_id"]), sortorder=0).filter(
+                tiles = list(models.TileModel.objects.filter(nodegroup_id=uuid.UUID(config["nodegroup_id"]), sortorder=0).filter(
                     resourceinstance_id=resource.resourceinstanceid
-                )
+                ))
                 if len(tiles) == 0:
-                    tiles = models.TileModel.objects.filter(nodegroup_id=uuid.UUID(config["nodegroup_id"])).filter(
+                    tiles = list(models.TileModel.objects.filter(nodegroup_id=uuid.UUID(config["nodegroup_id"])).filter(
+                        resourceinstance_id=resource.resourceinstanceid
+                    ))
+                if hist_res_name:
+                    name_tile = models.TileModel.objects.filter(nodegroup_id=uuid.UUID(hist_res_name_ngid), sortorder=0).filter(
                         resourceinstance_id=resource.resourceinstanceid
                     )
+                    if len(name_tile) > 0:
+                        tiles.append(name_tile[0])
                 for tile in tiles:
-                    for node in models.Node.objects.filter(nodegroup_id=uuid.UUID(config["nodegroup_id"])):
+                    if hist_res_name:
+                        ng_ids = [config["nodegroup_id"], hist_res_name_ngid]
+                    else:
+                        ng_ids = [config["nodegroup_id"]]
+                    for node in models.Node.objects.filter(nodegroup_id__in=ng_ids):
                         data = {}
                         if len(list(tile.data.keys())) > 0:
                             data = tile.data
@@ -37,7 +58,8 @@ class PrimaryDescriptorsFunction(BaseFunction):
                             value = datatype.get_display_value(tile, node)
                             if value is None:
                                 value = ""
-                            config["string_template"] = config["string_template"].replace("<%s>" % node.name, str(value))
+                            if str(value) not in config["string_template"]:
+                                config["string_template"] = config["string_template"].replace("<%s>" % node.name, str(value))
                             if map_popup and map_popup_template_default not in config["string_template"]:
                                 config["string_template"] += "<br>" + map_popup_template_default.replace("<%s>" % node.name, str(value))
         except ValueError as e:
