7b723c111f4a2bc3dd3b0e6aa6bf4c5baaeef3a1
Alexei Peters <apeters@fargeo.com>
2023-03-15 12:13:10 -0700

    revert config

--- arches/app/media/js/viewmodels/resource-instance-select.js
+++ arches/app/media/js/viewmodels/resource-instance-select.js
@@ -12,7 +12,7 @@ define([
     var graphCache = {};
 
     require(['views/components/related-instance-creator']);
-
+    
     /**
     * A viewmodel used for generic alert messages
     *
@@ -544,38 +544,38 @@ define([
             }
         };
 
-        this.select2Config = {
-            ajax: {
-                url: "https://api.github.com/search/repositories",
-                dataType: 'json',
-                delay: 250,
-                data: function (params) {
-                    return {
-                        q: params.term, // search term
-                        page: params.page
-                    };
-                },
-                processResults: function (data, params) {
-                    // parse the results into the format expected by Select2
-                    // since we are using custom formatting functions we do not need to
-                    // alter the remote JSON data, except to indicate that infinite
-                    // scrolling can be used
-                    params.page = params.page || 1;
+        // this.select2Config = {
+        //     ajax: {
+        //         url: "https://api.github.com/search/repositories",
+        //         dataType: 'json',
+        //         delay: 250,
+        //         data: function (params) {
+        //             return {
+        //                 q: params.term, // search term
+        //                 page: params.page
+        //             };
+        //         },
+        //         processResults: function (data, params) {
+        //             // parse the results into the format expected by Select2
+        //             // since we are using custom formatting functions we do not need to
+        //             // alter the remote JSON data, except to indicate that infinite
+        //             // scrolling can be used
+        //             params.page = params.page || 1;
 
-                    return {
-                        results: data.items,
-                        pagination: {
-                            more: (params.page * 30) < data.total_count
-                        }
-                    };
-                },
-                cache: true
-            },
-            placeholder: 'Search for a repository',
-            minimumInputLength: 1,
-            templateResult: formatRepo,
-            templateSelection: formatRepoSelection
-        };
+        //             return {
+        //                 results: data.items,
+        //                 pagination: {
+        //                     more: (params.page * 30) < data.total_count
+        //                 }
+        //             };
+        //         },
+        //         cache: true
+        //     },
+        //     placeholder: 'Search for a repository',
+        //     minimumInputLength: 1,
+        //     templateResult: formatRepo,
+        //     templateSelection: formatRepoSelection
+        // };
 
         function formatRepo (repo) {
             if (repo.loading) {
