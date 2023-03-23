a64bea913f4e6cd2723a3d12194f18897e9f52c2
stickler-ci <support@stickler-ci.com>
2023-03-23 16:37:30 +0000

    Fixing style errors.

--- arches/app/utils/forms.py
+++ arches/app/utils/forms.py
@@ -106,7 +106,7 @@ class ArchesUserProfileForm(ArchesUserCreationForm):
 
 
 class ArchesPasswordResetForm(PasswordResetForm):
-    email = forms.CharField(widget=forms.EmailInput(attrs={"class": "form-control input-lg floating-label-input", "required":"required"}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={"class": "form-control input-lg floating-label-input", "required": "required"}))
 
 
 class ArchesSetPasswordForm(SetPasswordForm):
