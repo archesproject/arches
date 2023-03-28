91d0b8bd131bc07e42ec76a47c9337f97104a8b8
njkim <nkim@fargeo.com>
2023-03-27 21:06:26 -0700

    Fix formatting

--- arches/app/models/migrations/9370_concept_relation.py
+++ arches/app/models/migrations/9370_concept_relation.py
@@ -9,8 +9,8 @@ class Migration(migrations.Migration):
     forward_sql = """
         UPDATE nodes SET config = jsonb_set(config, '{graphs}', (
             SELECT jsonb_agg( element || '{"useOntologyRelationship": true}' ) FROM jsonb_array_elements(config -> 'graphs') element)
-        ) 	
-        WHERE datatype IN ('resource-instance', 'resource-instance-list') 
+        )
+        WHERE datatype IN ('resource-instance', 'resource-instance-list')
         AND EXISTS (SELECT * FROM jsonb_array_elements(config -> 'graphs') WHERE value->'ontologyProperty' IS NOT NULL)
         AND EXISTS (SELECT * FROM jsonb_array_elements(config -> 'graphs') WHERE value->'useOntologyRelationship' IS NULL)
         AND jsonb_array_length(config -> 'graphs') > 0;
@@ -19,7 +19,7 @@ class Migration(migrations.Migration):
     """
     Reversing the addition of a property in a nested array means disassembling and reassembling the object. see https://stackoverflow.com/a/58802637
     Because this property would be inert in prior versions of Arches and because of the possible complexity of these configs,
-    there is a chance of rebuilding node configs incorrectly in a reverse migration, the reverse migration will leave the property in place. 
+    there is a chance of rebuilding node configs incorrectly in a reverse migration, the reverse migration will leave the property in place.
     """
 
     reverse_sql = ""
