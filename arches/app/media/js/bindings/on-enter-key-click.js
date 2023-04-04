89bc12044263d2566ddac20e75b47c57034b687d
Johnathan Clementi <johnathan.clementi1@gmail.com>
2023-04-04 14:13:36 -0400

    nit #9337

--- arches/app/media/js/bindings/on-enter-key-click.js
+++ arches/app/media/js/bindings/on-enter-key-click.js
@@ -4,9 +4,9 @@ define([
 ], function($, ko) {
     // Register binding of onEnterkeyClick. e.g. <div data-bind="onEnterkeyClick"> </div>
     ko.bindingHandlers.onEnterkeyClick = {
-        init: function (element, valueAccessor) {
+        init: function(element, valueAccessor) {
             ko.utils.unwrapObservable(valueAccessor()); // Unwrap to get subscription.
-            $(element).keypress(function (event) {
+            $(element).keypress(function(event) {
                 var keyCode = (event.which ? event.which : event.keyCode);
                 if (keyCode === 13) {   // Check if keypress is <enter>.
                     $(element).click();
