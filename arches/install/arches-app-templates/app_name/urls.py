9e627a180c65146ffd207dba7a85969031966a63
Christopher Byrd <cbyrd@fargeo.com>
2023-04-11 15:38:00 -0700

    nit #9304

--- arches/install/arches-app-templates/app_name/urls.py
+++ arches/install/arches-app-templates/app_name/urls.py
@@ -1,4 +1,4 @@
-"""installed_package_example URL Configuration
+"""URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 
 
