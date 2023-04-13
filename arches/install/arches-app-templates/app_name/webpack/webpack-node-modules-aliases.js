ba42f2ae2f0c07261be74483e318e4bd304711a4
Christopher Byrd <cbyrd@fargeo.com>
2023-04-13 15:57:24 -0700

    nit #9304

--- arches/install/arches-app-templates/app_name/webpack/webpack-node-modules-aliases.js
+++ arches/install/arches-app-templates/app_name/webpack/webpack-node-modules-aliases.js
@@ -1,5 +1,5 @@
 module.exports = {
     installedPackageNodeModuleAliases: JSON.stringify({
-        // "example-node-module": "Path.resolve(__dirname, `${APP_ROOT}/media/node_modules/example-node-module`)",
+        // "example-node-module": "Path.resolve(__dirname, `${INSTALLED_PACKAGES_PATH}/${__dirname}/media/node_modules/example-node-module`)",
     }),
 };
