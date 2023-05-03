c09f7b33ce60239c1b42409f2287f74f861381c9
Johnathan Clementi <johnathan.clementi1@gmail.com>
2023-05-02 22:26:21 -0400

    Improve map-widget accessibility #9342

--- arches/app/media/js/views/components/map.js
+++ arches/app/media/js/views/components/map.js
@@ -5,7 +5,8 @@ define([
     'viewmodels/map',
     'templates/views/components/map.htm',
     'bindings/mapbox-gl',
-    'bindings/sortable'
+    'bindings/sortable',
+    'bindings/key-events-click',
 ], function($, _, ko, MapViewModel, mapTemplate) {
     ko.components.register('arches-map', {
         viewModel: MapViewModel,
