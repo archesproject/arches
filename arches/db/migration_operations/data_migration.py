af416682844a7e1267ed556b79fc8e132e600fa8
Christopher Byrd <cbyrd@fargeo.com>
2023-06-16 14:51:21 -0700

    adds datamigration log #9054

--- arches/db/migration_operations/data_migration.py
+++ arches/db/migration_operations/data_migration.py
@@ -1,5 +1,9 @@
-import uuid
+from django.db import DEFAULT_DB_ALIAS, connections
+from django.db.migrations.recorder import MigrationRecorder
 from django.db.migrations.operations.base import Operation
+from django.db.migrations.executor import MigrationExecutor
+
+from arches.app.models import models
 
 
 class UpdatePublicationId(Operation):
@@ -9,6 +13,20 @@ class UpdatePublicationId(Operation):
 
     # If this is False, Django will refuse to reverse past this operation.
     reversible = True
+
+    @staticmethod
+    def get_current_migration():
+        connection = connections[DEFAULT_DB_ALIAS]
+        connection.prepare_database()
+        executor = MigrationExecutor(connection)
+        targets = executor.loader.graph.leaf_nodes()
+        migration_list = executor.migration_plan(targets)
+        
+        current_migration = None
+        if len(migration_list):
+            current_migration = migration_list[0][0]
+
+        return current_migration
 
     def __init__(self, current_publication_id, updated_publication_id):
         self.current_publication_id = current_publication_id
@@ -23,12 +41,42 @@ class UpdatePublicationId(Operation):
     def database_forwards(self, app_label, schema_editor, from_state, to_state):
         # The Operation should use schema_editor to apply any changes it
         # wants to make to the database.
-        schema_editor.execute("UPDATE resource_instances SET graphpublicationid = '%s' WHERE graphpublicationid = '%s'" % (self.updated_publication_id, self.current_publication_id))
+        current_migration = self.get_current_migration()
+        operation_name = self.__class__.__name__
+
+        data_migration = models.DataMigration.objects.create(
+            name=current_migration.name,
+            operation = operation_name,
+            resource_instance_ids=[ 
+                resource_instance_id 
+                for resource_instance_id 
+                in models.ResourceInstance.objects.filter(graph_publication_id=self.current_publication_id).values_list('resourceinstanceid', flat=True)
+            ]
+        )
+        data_migration.save()
+
+        schema_editor.execute(
+            "UPDATE resource_instances SET graphpublicationid = '%s' WHERE graphpublicationid = '%s'" % (self.updated_publication_id, self.current_publication_id)
+        )
 
     def database_backwards(self, app_label, schema_editor, from_state, to_state):
         # If reversible is True, this is called when the operation is reversed.
-        schema_editor.execute("UPDATE resource_instances SET graphpublicationid = '%s' WHERE graphpublicationid = '%s'" % (self.current_publication_id, self.updated_publication_id))
+        most_recent_migration = MigrationRecorder.Migration.objects.latest('id')
+        operation_name = self.__class__.__name__
+
+        data_migration = models.DataMigration.objects.filter(name=most_recent_migration.name, operation=operation_name).last()
+
+        schema_editor.execute(
+            """
+            UPDATE resource_instances 
+            SET graphpublicationid = '%s' 
+            WHERE graphpublicationid = '%s'
+            AND resourceinstanceid = ANY(ARRAY%s::uuid[])
+            """ % (self.current_publication_id, self.updated_publication_id, data_migration.resource_instance_ids)
+        )
+
+        data_migration.delete()
 
     def describe(self):
         # This is used to describe what the operation does in console output.
-        return "Updates resource publication_id from %s to %s" % (self.current_publication_id, self.updated_publication_id)
+        return "Updates resources' publication_id from %s to %s" % (self.current_publication_id, self.updated_publication_id)
