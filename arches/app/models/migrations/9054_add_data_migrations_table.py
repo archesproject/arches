e5be4c185c2721e512d208105598f5677d9848d4
Christopher Byrd <cbyrd@fargeo.com>
2023-06-22 14:47:24 -0700

    nit

--- arches/app/models/migrations/9054_add_data_migrations_table.py
+++ arches/app/models/migrations/9054_add_data_migrations_table.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('models', '9477_fix_for_spatial_view_dbf_function_edtf_displaying_null'),
+        ('models', '9525_add_published_graph_edits'),
     ]
 
     operations = [
