b2f54b951517bcf714ad5d2e34f228d305d19c50
Johnathan Clementi <johnathan.clementi1@gmail.com>
2023-06-22 12:37:43 -0400

    Make escape key handler #9677

--- arches/app/media/js/utils/aria.js
+++ arches/app/media/js/utils/aria.js
@@ -10,6 +10,34 @@ define([], function(){
                 x = "true";
             }
             ele.setAttribute("aria-expanded", x);
+        },
+
+        onEscapeKey: function(toggleElement, listenerScope) {
+            /* 
+            *   toggleElement: element that expands/contracts a panel, menu, etc. 
+            *   listenerScope: when focus is within this element, an escape key press will close the element controled by toggleElement
+            * 
+            *   Implement this function within the toggleElement's click event handler, passing event.currentTarget as toggleElement
+            */
+            let attachListener = function(evt) {
+                    evt = evt || window.event;
+                    var isEscape = false;
+                    
+                    // Check for escape key press
+                    if ('key' in evt) {
+                        isEscape = (evt.key === 'Escape' || evt.key === 'Esc');
+                    } else {
+                        isEscape = (evt.keyCode === 27);
+                    }
+    
+                    // Handle escape key press
+                    if (isEscape) {
+                        $(toggleElement).click();
+                        $(toggleElement).focus();
+                        $(listenerScope).off('keydown', attachListener);
+                    }
+            };
+            $(listenerScope).on('keydown', attachListener);
         }
     };
 
