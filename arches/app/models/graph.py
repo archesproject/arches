5077183206881e246a11f34a047501ee201ca1be
Christopher Byrd <cbyrd@fargeo.com>
2023-06-22 15:37:02 -0700

    nit

--- arches/app/models/graph.py
+++ arches/app/models/graph.py
@@ -539,7 +539,7 @@ class Graph(models.GraphModel):
             for functionxgraph in self._functions:
                 # Right now this only saves a functionxgraph record if the function is present in the database. Otherwise it silently fails.
                 if functionxgraph.function_id in [str(id) for id in models.Function.objects.values_list("functionid", flat=True)]:
-                    previous_functionxgraph = models.FunctionXGraph.objects.get(functionid=functionxgraph.function_id, graphid=self.pk)
+                    previous_functionxgraph = models.FunctionXGraph.objects.get(function_id=functionxgraph.function_id, graph_id=self.pk)
                     previous_functionxgraph.delete()
                     
                     functionxgraph.save()
