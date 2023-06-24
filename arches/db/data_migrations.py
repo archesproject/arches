5067ea00cd0aacf8d5342946233a390661733b9e
Christopher Byrd <cbyrd@fargeo.com>
2023-06-23 17:08:59 -0700

    rough out data migration indexing #9054

--- arches/db/data_migrations.py
+++ arches/db/data_migrations.py
@@ -1,15 +1,35 @@
+import itertools
+
 from django.db import migrations
+
+from arches.app.models import models
+from arches.app.utils.index_database import index_resources_using_multiprocessing
+
 
 class DataMigration(migrations.Migration):
     def __init__(self, name, app_label):
         super().__init__(name, app_label)
 
     def apply(self, project_state, schema_editor, collect_sql=False):    
-        foo = super().apply(project_state, schema_editor, collect_sql=collect_sql)
-        print("!!!!" * 50)
-        return foo
+        project_state = super().apply(project_state, schema_editor, collect_sql=collect_sql)
+
+        data_migrations = models.DataMigration.objects.filter(
+            name=self.name, 
+            app=self.app_label,
+        )
+        resource_instance_ids = list(itertools.chain.from_iterable([data_migration.resource_instance_ids for data_migration in data_migrations]))
+        index_resources_using_multiprocessing(resourceids=resource_instance_ids)
+
+        return project_state
 
     def unapply(self, project_state, schema_editor, collect_sql=False):
-        foo = super().apply(project_state, schema_editor, collect_sql=collect_sql)
-        print("%%%%" * 50)
-        return foo
+        project_state = super().apply(project_state, schema_editor, collect_sql=collect_sql)
+        
+        data_migrations = models.DataMigration.objects.filter(
+            name=self.name, 
+            app=self.app_label,
+        )
+        resource_instance_ids = list(itertools.chain.from_iterable([data_migration.resource_instance_ids for data_migration in data_migrations]))
+        index_resources_using_multiprocessing(resourceids=resource_instance_ids)
+        
+        return project_state
