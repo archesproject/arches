46f67548f2beeb17900b7acc68c902403d7661d2
Christopher Byrd <cbyrd@fargeo.com>
2023-06-29 14:06:29 -0700

    adds migration for tiledata #9648

---
+++ arches/app/models/migrations/9648_add_empty_key_value_pairs_to_tiles.py
@@ -0,0 +1,20 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("models", "9477_fix_for_spatial_view_dbf_function_edtf_displaying_null"),
+    ]
+
+    def forwards_func(apps, schema_editor):
+        TileModel = apps.get_model("models", "TileModel")
+
+        for tile in TileModel.objects.all():
+            tile.save()
+
+    def reverse_func(apps, schema_editor):
+        pass
+
+    operations = [
+        migrations.RunPython(forwards_func, reverse_func),
+    ]
