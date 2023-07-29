7d579a6aae41cce8df1959fcfe3b6f525f48f7c5
Christopher Byrd <cbyrd@fargeo.com>
2023-07-28 17:51:36 -0700

    nit #9775

--- arches/app/utils/update_resource_instance_data_based_on_graph_diff.py
+++ arches/app/utils/update_resource_instance_data_based_on_graph_diff.py
@@ -10,11 +10,11 @@ import arches.app.tasks as tasks
 def update_resource_instance_data_based_on_graph_diff(initial_graph, updated_graph):
     resource_instances = models.ResourceInstance.objects.filter(graph_publication_id=initial_graph['publication_id'])
 
-    initial_node_ids_to_nodegroup_ids = {}
-    for initial_node in initial_graph['nodes']:
-        initial_node_ids_to_nodegroup_ids[initial_node['nodeid']] = initial_node['nodegroup_id']
+    # initial_node_ids_to_nodegroup_ids = {}
+    # for initial_node in initial_graph['nodes']:
+    #     initial_node_ids_to_nodegroup_ids[initial_node['nodeid']] = initial_node['nodegroup_id']
 
-    moved_nodes = {}  # node_id: {'from': nodegroup_id, 'to': nodegroup_id}
+    # moved_nodes = {}  # node_id: {'from': nodegroup_id, 'to': nodegroup_id}
     updated_nodegroup_ids_to_node_ids = {}
     for updated_node in updated_graph['nodes']:
         if not updated_nodegroup_ids_to_node_ids.get(updated_node['nodegroup_id']):
@@ -22,9 +22,9 @@ def update_resource_instance_data_based_on_graph_diff(initial_graph, updated_gra
 
         updated_nodegroup_ids_to_node_ids[updated_node['nodegroup_id']].append(updated_node['nodeid'])
 
-        initial_nodegroup_id = initial_node_ids_to_nodegroup_ids.get(updated_node['nodeid'])
-        if initial_nodegroup_id is not None and initial_nodegroup_id != updated_node['nodegroup_id']:
-            moved_nodes[updated_node['nodeid']] = {'from': initial_nodegroup_id, 'to': updated_node['nodegroup_id']}
+        # initial_nodegroup_id = initial_node_ids_to_nodegroup_ids.get(updated_node['nodeid'])
+        # if initial_nodegroup_id is not None and initial_nodegroup_id != updated_node['nodegroup_id']:
+        #     moved_nodes[updated_node['nodeid']] = {'from': initial_nodegroup_id, 'to': updated_node['nodegroup_id']}
 
     updated_node_ids_to_datatypes = {}
     for updated_node in updated_graph['nodes']:
@@ -41,23 +41,23 @@ def update_resource_instance_data_based_on_graph_diff(initial_graph, updated_gra
     datatype_factory = DataTypeFactory()
 
     # first, explicity move nodes between nodegroups
-    for resource_instance in resource_instances:
-        for key, value in moved_nodes.items():
-            from_tile = models.TileModel.objects.get(nodegroup_id=value['from'], resource_instance=resource_instance)
-            to_tile = models.TileModel.objects.get(nodegroup_id=value['to'], resource_instance=resource_instance)
+    # for resource_instance in resource_instances:
+    #     for key, value in moved_nodes.items():
+    #         from_tile = models.TileModel.objects.get(nodegroup_id=value['from'], resource_instance=resource_instance)
+    #         to_tile = models.TileModel.objects.get(nodegroup_id=value['to'], resource_instance=resource_instance)
 
-            to_tile.tiledata[key] = from_tile.tiledata[key]
-            del from_tile.tiledata[key]
+    #         to_tile.tiledata[key] = from_tile.tiledata[key]
+    #         del from_tile.tiledata[key]
 
-            from_tile.save()
-            to_tile.save()
+    #         from_tile.save()
+    #         to_tile.save()
 
     # then, add/remove nodes and change default values
     for tile in models.TileModel.objects.filter(resourceinstance__in=resource_instances):
         updated_node_ids = updated_nodegroup_ids_to_node_ids[str(tile.nodegroup_id)]
         
         # delete nodes not in updated graph
-        for node_id in tile.data.keys():
+        for node_id in list(tile.data.keys()):
             if node_id not in updated_node_ids:
                 del tile.data[node_id]
 
@@ -66,12 +66,7 @@ def update_resource_instance_data_based_on_graph_diff(initial_graph, updated_gra
         for node_id in updated_node_ids:
             if node_id not in tile.data.keys():
                 value = updated_node_ids_to_default_values.get(node_id)
-
-                # if not value:
-                    # datatype_instance = datatype_factory.get_instance(updated_node_ids_to_datatypes[node_id])
-                    # value = datatype_instance.transform_value_for_tile(None)  #TODO: cbyrd need to create new datatype method for empty value generation
-
-                tile.data['node_id'] = value
+                tile.data[node_id] = value
                     
             elif tile.data[node_id] == initial_node_ids_to_default_values.get(node_id):
                 tile.data[node_id] = updated_node_ids_to_default_values.get(node_id)
