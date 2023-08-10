2fba76b62ca9dce1fad1ada1bd025c9e8aebd7c6
Jacob Walls <jwalls@fargeo.com>
2023-08-10 11:54:24 -0400

    Address review

--- arches/management/commands/validate.py
+++ arches/management/commands/validate.py
@@ -34,67 +34,84 @@ class Command(BaseCommand):
     Takes no action by default (other than printing a summary).
 
     Provide --verbosity=2 to get a richer output (list of affected nodes).
+
+    Example: python manage.py validate --fix-all
     """
-    help = "Validate an Arches database against a set of data integrity checks."
+    help = "Validate an Arches database against a set of data integrity checks, and opt-in to remediation."
 
     def add_arguments(self, parser):
+        choices = [check.value for check in IntegrityCheck]
+
         parser.add_argument("--fix-all", action="store_true", dest="fix_all", default=False,
                             help="Apply all fix actions.")
-        parser.add_argument("--fix", action="extend", nargs="+", type=int, default=[],
+        parser.add_argument("--fix", action="extend", nargs="+", type=int, default=[], choices=choices,
                             help="List the error codes to fix, e.g. --fix 1001 1002 ...")
-        parser.add_argument("--limit", action="store", type=int, default=500,
+        parser.add_argument("--limit", action="store", type=int,
                             help="Maximum number of rows to print; does not affect fix actions")
 
     def handle(self, *args, **options):
         self.options = options
+        if self.options["limit"] and self.options["verbosity"] < 2:
+            # Limit is meaningless w/o the higher verbosity output
+            self.options["verbosity"] = 2
+
         if self.options["verbosity"] > 0:
             self.stdout.write()
             self.stdout.write("Arches integrity report")
-            self.stdout.write(f"Prepared by Arches {__version__} on {datetime.today()}")
+            self.stdout.write(f"Prepared by Arches {__version__} on {datetime.today().strftime('%c')}")
             self.stdout.write()
-            self.stdout.write("\t".join(["", "Error", "Rows", "Description"]))
+            self.stdout.write("\t".join(["", "Error", "Rows", "Fixed", "Description"]))
             self.stdout.write()
-        
-        # add checks here in numerical order
+
+        # Add checks here in numerical order
         self.check_integrity(
-            errno=IntegrityCheck.NODE_HAS_ONTOLOGY_GRAPH_DOES_NOT.value,  # 1005
-            name="Nodes with ontologies found in graphs without ontologies",
+            check=IntegrityCheck.NODE_HAS_ONTOLOGY_GRAPH_DOES_NOT,  # 1005
             queryset=models.Node.objects.only("ontologyclass", "graph").filter(
                 ontologyclass__isnull=False).filter(graph__ontology=None),
             fix_action=None,
         )
         self.check_integrity(
-            errno=IntegrityCheck.NODELESS_NODE_GROUP.value,  # 1012
-            name="Nodeless node groups",
+            check=IntegrityCheck.NODELESS_NODE_GROUP,  # 1012
             queryset=models.NodeGroup.objects.filter(
                 ~Exists(models.Node.objects.filter(nodegroup_id=OuterRef("nodegroupid")))
             ),
             fix_action=DELETE_QUERYSET,
         )
 
-    def check_integrity(self, *, errno, name, queryset, fix_action):
-        count = len(queryset)
-        result = self.style.ERROR("FAIL") if count else self.style.SUCCESS("PASS")
+    def check_integrity(self, check, queryset, fix_action):
+        needs_fix = queryset.exists()
+        fix_status = ""
+        limit = self.options["limit"] or 500
+
+        if needs_fix:
+            if self.options["fix_all"] or check.value in self.options["fix"]:
+                if fix_action:
+                    if fix_action is DELETE_QUERYSET:
+                        with transaction.atomic():
+                            queryset.delete()
+                        fix_status = self.style.SUCCESS("FIXED")
+
+                    # Implement other fix actions here, e.g. pass in a callable
+                    else:
+                        raise NotImplementedError
+                else:
+                    fix_status = self.style.NOTICE("N/A")
+            else:
+                fix_status = self.style.NOTICE("Skipped")
 
         if self.options["verbosity"] > 0:
-            self.stdout.write("\t".join([result, str(errno), str(count), name]))
+            count = len(queryset)
+            result = self.style.ERROR("FAIL") if count else self.style.SUCCESS("PASS")
+            self.stdout.write("\t".join(str(x) for x in (result, check.value, count, fix_status, check)))
+
             if self.options["verbosity"] > 1:
                 self.stdout.write("\t" + "-" * 36)
                 if queryset:
                     for i, n in enumerate(queryset):
-                        if i < self.options["limit"]:
+                        if i < limit:
                             self.stdout.write(f"\t{n.pk}")
                         else:
                             self.stdout.write("\t\t(truncated...)")
                             break
 
-        if not count:
-            return
-
-        if self.options["fix_all"] or errno in self.options["fix"]:
-            if fix_action:
-                self.stdout.write("\tFixing ...")
-            if fix_action is DELETE_QUERYSET:
-                with transaction.atomic():
-                    queryset.delete()
-                self.stdout.write(f"\tDELETED.")
+            self.stdout.write()
