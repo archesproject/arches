c071e2d9599505391d3cf85586b1c2a59c8ba49c
Aaron Gundel <aaron.gundel@gmail.com>
2023-09-07 10:25:30 -0600

    move up minimum ubuntu version to be in line with supported versions

--- arches/install/ubuntu_setup.sh
+++ arches/install/ubuntu_setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# For Ubuntu 18.04+
+# For Ubuntu 20.04+
 
 # Use the yes command if you would like to install postgres/postgis,
 # node/yarn, and elasticsearch.
@@ -44,7 +44,7 @@ function install_yarn {
   sudo apt-get install -y ca-certificates curl gnupg
   sudo mkdir -p /etc/apt/keyrings
   curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-  echo NODE_MAJOR=18 >> ~/.profile
+  echo NODE_MAJOR=16 >> ~/.profile
   source ~/.profile
   sudo apt-get update
   sudo apt-get install nodejs -y
