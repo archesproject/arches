669fc6f57ec87a8f83e0443be63bd2d304f8c245
Ferguson, Brett CSNR:EX <brett@qedsystems.ca>
2023-09-12 12:32:44 -0700

    First cut at adding node name and widget type as CSS classes

--- arches/app/media/js/viewmodels/widget.js
+++ arches/app/media/js/viewmodels/widget.js
@@ -62,6 +62,12 @@ define([
             this.config = ko.observable(this.config);
         }
 
+        this.widgetCssClasses = function() {
+            return [self.node?.alias(),
+                self.widget?.widgetLookup[ko.unwrap(self.widget?.widget_id)].name
+                ].join(" ").trim();
+        }
+
         this.disposables = [];
 
         var subscribeConfigObservable = function(obs, key) {
