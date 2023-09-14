9b170b5d576ecdffb206303aaafce291b8e86af7
Christopher Byrd <cbyrd@fargeo.com>
2023-09-14 12:41:41 -0700

    updates interactions to fail loudly #10016

--- arches/app/media/js/report-templates.js
+++ arches/app/media/js/report-templates.js
@@ -3,11 +3,15 @@ define(['utils/load-component-dependencies'], function(loadComponentDependencies
         return string.replace(/,\s*}*$/, "}");
     }
 
-    const reportTemplateDataHTML = document.querySelector('#reportTemplateData');
-    const reportTemplateData = reportTemplateDataHTML.getAttribute('reportTemplates');
-    const reportTemplates = JSON.parse(removeTrailingCommaFromObject(reportTemplateData));
-
-    loadComponentDependencies(Object.values(reportTemplates).map(value => value['component']));
-
-    return reportTemplates;
+    try {        
+        const reportTemplateDataHTML = document.querySelector('#reportTemplateData');
+        const reportTemplateData = reportTemplateDataHTML.getAttribute('reportTemplates');
+        const reportTemplates = JSON.parse(removeTrailingCommaFromObject(reportTemplateData));
+    
+        loadComponentDependencies(Object.values(reportTemplates).map(value => value['component']));
+    
+        return reportTemplates;
+    } catch (error) {
+        console.error(error);
+    }
 });
