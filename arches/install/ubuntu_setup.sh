38718177ee730538de9a7429333afb29ce1910a8
Aaron Gundel <aaron.gundel@gmail.com>
2023-09-26 13:07:34 -0600

    update sudoes

--- arches/install/ubuntu_setup.sh
+++ arches/install/ubuntu_setup.sh
@@ -46,7 +46,7 @@ function install_yarn {
   sudo mkdir -p /etc/apt/keyrings
   curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
   echo NODE_MAJOR=18 >> ~/.profile
-  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list && apt-get update
+  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list && sudo apt-get update
   source ~/.profile
   sudo apt-get update
   sudo apt-get install nodejs -y
