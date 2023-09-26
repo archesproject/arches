f95a1a52ce7cb10a3e4e4924cc1b44d12ee984ea
Jacob Walls <jwalls@fargeo.com>
2023-09-26 15:16:41 -0400

    nit re #9333

--- arches/app/models/migrations/9333_file_type_image_config.py
+++ arches/app/models/migrations/9333_file_type_image_config.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('models', '9979_update_stage_for_bulk_edit'),
+        ('models', '10041_limit_number_of_bulk_edit'),
     ]
 
     operations = [
