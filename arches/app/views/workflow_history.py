5b8cd8f7493c82a3c70a8dbec8df4406f5906c14
Jacob Walls <jwalls@fargeo.com>
2023-09-27 14:15:37 -0400

    Check for resource reviewer role

--- arches/app/views/workflow_history.py
+++ arches/app/views/workflow_history.py
@@ -1,31 +1,29 @@
-from django.views.generic import View
-from django.core.exceptions import ObjectDoesNotExist
-from arches.app.utils.response import JSONResponse
-from arches.app.models import models
 import json
+
+from django.utils.translation import gettext as _
+from django.views.generic import View
+
+from arches.app.utils.permission_backend import user_is_resource_reviewer
+from arches.app.utils.response import JSONErrorResponse, JSONResponse
+from arches.app.models import models
+
 
 class WorkflowHistoryView(View):
 
     def get(self, request, workflowid):
+        if not user_is_resource_reviewer(request.user):
+            return JSONErrorResponse(_("Request Failed"), _("Permission Denied"), status=403)
         try:
             workflow_history = models.WorkflowHistory.objects.get(workflowid=workflowid)
-        except ObjectDoesNotExist as e:
+        except models.WorkflowHistory.DoesNotExist:
             workflow_history = {}
         return JSONResponse(workflow_history, status=200)
 
     def post(self, request, workflowid):
+        if not user_is_resource_reviewer(request.user):
+            return JSONErrorResponse(_("Request Failed"), _("Permission Denied"), status=403)
+
         data = json.loads(request.body)
-        # try:
-        #     workflow_history = models.WorkflowHistory.objects.get(workflowid=workflowid)
-        #     workflow_history.completed = data['completed']
-        #     workflow_history.save()
-        # except models.WorkflowHistory.DoesNotExist:
-        #     models.WorkflowHistory.objects.udpate_or_create(
-        #         workflowid=data['workflowid'],
-        #         workflowdata=data['workflowdata'],
-        #         user=request.user,
-        #         completed=data['completed']
-        #     )
         models.WorkflowHistory.objects.update_or_create(
             workflowid = data["workflowid"],
             defaults = {
