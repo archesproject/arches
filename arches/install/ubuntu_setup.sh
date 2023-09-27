cba8065827bf7816e21596e01bfc9ac5ac2ffac4
Aaron Gundel <aaron.gundel@gmail.com>
2023-09-26 21:20:02 -0600

    switch order of profile sourcing

--- arches/install/ubuntu_setup.sh
+++ arches/install/ubuntu_setup.sh
@@ -46,8 +46,8 @@ function install_yarn {
   sudo mkdir -p /etc/apt/keyrings
   curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
   echo NODE_MAJOR=18 >> ~/.profile
-  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list && sudo apt-get update
   source ~/.profile
+  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list && sudo apt-get update
   sudo apt-get update
   sudo apt-get install nodejs -y
   curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
