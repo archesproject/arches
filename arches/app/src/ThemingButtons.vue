7f0fe5fa32f804f7a55634d74cc340f095f0e3b0
Christopher Byrd <cbyrd@fargeo.com>
2023-10-06 12:22:10 -0700

    nit

--- arches/app/src/ThemingButtons.vue
+++ arches/app/src/ThemingButtons.vue
@@ -14,10 +14,21 @@ watch(theme, async(theme) => {
         themeElement.setAttribute('href',"/static/node_modules/primevue/resources/themes/mdc-dark-indigo/theme.css")
     }
 })
+
+const htmlElement = document.querySelector('html')
+const direction = ref('left')
+watch(direction, async(direction) => {
+    if (direction === 'left') {
+        htmlElement.setAttribute('dir',"ltr")
+    }
+    else if (direction === 'right') {
+        htmlElement.setAttribute('dir',"rtl")
+    }
+})
 </script>
 
 <template>
-    <div style="background-color: lightgray; padding: 10px; display: flex; justify-content: space-between; width: 100%;">
+    <div style="background-color: lightgray; padding: 10px; display: flex; justify-content: space-between; width: 100%; align-items: center;">
         <div>
             <button @click="theme = 'light'" style="padding: 0px;">
                 <div style="width: 25px; height: 25px; background-color: white; cursor: pointer;"></div>
@@ -30,7 +41,11 @@ watch(theme, async(theme) => {
             </button>
         </div>
         <div>
-            <button>RTL/LTR</button>
+            <button 
+                style="cursor: pointer;"
+                @click="direction = direction === 'left' ? 'right' : 'left'"
+                v-text="direction === 'left' ? 'right-to-left' : 'left-to-right'"
+            ></button>
         </div>
     </div>
 </template>
--- arches/app/src/UserDashboard.vue
+++ arches/app/src/UserDashboard.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Menubar from 'primevue/menubar';
+import InputText from 'primevue/inputtext';
 import { ref, watch } from 'vue'
 import ThemingButtons from '@/ThemingButtons.vue';
 
@@ -130,5 +131,37 @@ const items = ref([
 
 <template>
     <ThemingButtons />
-    <Menubar :model="items" />
+    <Menubar :model="items">
+        <template #start>
+            <InputText placeholder="Search" type="text" />
+        </template>
+
+        <template #item="{ label, item, props, root, hasSubmenu }">
+            <router-link v-if="item.route" v-slot="routerProps" :to="item.route" custom>
+                <a :href="routerProps.href" v-bind="props.action">
+                    <span v-bind="props.icon" />
+                    <span v-bind="props.label">{{ label }}</span>
+                </a>
+            </router-link>
+            <a v-else :href="item.url" :target="item.target" v-bind="props.action">
+                <span v-bind="props.icon" />
+                <span v-bind="props.label">{{ label }}</span>
+                <span :class="[hasSubmenu && (root ? 'pi pi-fw pi-angle-down' : 'pi pi-fw pi-angle-right')]" v-bind="props.submenuicon" />
+            </a>
+        </template>
+
+        <template #end>
+            <img alt="logo" src="https://primefaces.org/cdn/primevue/images/logo.svg" height="40" class="mr-2" />
+        </template>
+    </Menubar>
 </template>
+
+<style scoped>
+    :deep(.p-menubar-end) {
+        margin: 0px;
+    }
+    :deep(.p-menubar-root-list) {
+        width: 100%;
+        justify-content: flex-end;
+    }
+</style>
