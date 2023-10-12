afaeb8a8366e239eabb7cc0b293ffee00b0dfb14
Christopher Byrd <cbyrd@fargeo.com>
2023-10-12 15:13:16 -0700

    merge conflict resolution

--- arches/__init__.py
+++ arches/__init__.py
@@ -10,11 +10,7 @@ try:
 except Exception as e:
     pass
 
-<<<<<<< HEAD
 VERSION = (7, 5, 0, "alpha", 0)  # VERSION[3] options = "alpha", "beta", "rc", or "final"
-=======
-VERSION = (7, 4, 2, "beta", 0)  # VERSION[3] options = "alpha", "beta", "rc", or "final"
->>>>>>> b040c63d5f4b8d0d979190f722135b8d7c90b2e0
 
 __version__ = get_version(VERSION)
 
