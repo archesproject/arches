dd350c4aac066e26cc3e976391316e57762c1432
Christopher Byrd <cbyrd@fargeo.com>
2023-10-16 11:00:13 -0700

    nit

--- arches/app/models/migrations/9053_add_future_graphs.py
+++ arches/app/models/migrations/9053_add_future_graphs.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("models", "10097_add_editlog_index"),
+        ("models", "10120_replace_whole_word_bulk_edit"),
     ]
 
     operations = [
