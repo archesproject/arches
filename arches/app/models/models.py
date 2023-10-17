21e9b7e2207718296b065ae9fee7701ca03343b6
Cyrus Hiatt <chiatt@fargeo.com>
2023-10-17 13:45:50 -0700

    Merge

--- arches/app/models/models.py
+++ arches/app/models/models.py
@@ -1516,8 +1516,12 @@ def send_email_on_save(sender, instance, **kwargs):
                 email_to = instance.recipient.email
             else:
                 email_to = context["email"]
+
+            if type(email_to) is not list:
+                email_to = [email_to]
+
             subject, from_email, to = instance.notif.notiftype.name, settings.DEFAULT_FROM_EMAIL, email_to
-            msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+            msg = EmailMultiAlternatives(subject, text_content, from_email, to)
             msg.attach_alternative(html_content, "text/html")
             msg.send()
             if instance.notif.notiftype.webnotify is not True:
@@ -1525,7 +1529,8 @@ def send_email_on_save(sender, instance, **kwargs):
                 instance.save()
         except Exception as e:
             logger = logging.getLogger(__name__)
-            logger.warning("Email Server not correctly set up. See settings to configure.")
+            logger.warning(e)
+            logger.warning("Error occurred sending email.  See previous stack trace and check email configuration in settings.py.")
 
     return False
 
