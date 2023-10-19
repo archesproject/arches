archesproject/arches

commit 7cccf96d2d755fda5f3fc9172c5586bee5351442

Set completed to True

arches/app/media/js/viewmodels/workflow.js
@@ -404,12 +404,27 @@ define([
         this.finishWorkflow = function() {
             if (self.activeStep().hasUnsavedData()) {
                 self.activeStep().save().then(function() {
-                    window.location.assign(self.quitUrl);
+                    this.finishWorkflow();
                 });
+                return;
             }
-            else {
-                window.location.assign(self.quitUrl);
-            }
+
+            const workflowid = self.id();
+            const workflowHistory = {
+                workflowid,
+                completed: true,
+            };
+
+            fetch(arches.urls.workflow_history + workflowid, {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                    "X-CSRFToken": Cookies.get('csrftoken')
+                },
+                body: JSON.stringify(workflowHistory),
+            });
+
+            window.location.assign(self.quitUrl);
         };
 
         this.quitWorkflow = function(){

commit c4259d6590d686d7e2f6dc4b81d8c084ca4a48f2

Fix saving unsaved data in finish workflow action #10157
Regression in 7cccf96 (unreleased).

arches/app/media/js/viewmodels/workflow.js
@@ -416,14 +416,7 @@ define([
             });
         };
 
-        this.finishWorkflow = function() {
-            if (self.activeStep().hasUnsavedData()) {
-                self.activeStep().save().then(function() {
-                    this.finishWorkflow();
-                });
-                return;
-            }
-
+        this.markWorkflowComplete = function() {
             const workflowid = self.id();
             const workflowHistory = {
                 workflowid,
@@ -440,6 +433,16 @@ define([
             });
 
             window.location.assign(self.quitUrl);
+        }
+
+        this.finishWorkflow = function() {
+            if (self.activeStep().hasUnsavedData()) {
+                self.activeStep().save().then(function() {
+                    self.markWorkflowComplete();
+                });
+                return;
+            }
+            self.markWorkflowComplete();
         };
 
         this.quitWorkflow = function(){