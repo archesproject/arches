36ff6df283ae2bda5a4177cd59c2cee7eaa6f0f6
Cyrus Hiatt <chiatt@fargeo.com>
2023-10-26 08:50:15 -0700

    Fix migrations

--- arches/app/models/migrations/10165_etlmodule_reversible.py
+++ arches/app/models/migrations/10165_etlmodule_reversible.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('models', '10150_add_default_value_file_list'),
+        ('models', '10196_alter_workflowhistory_workflowname'),
     ]
 
     set_reversibility = """
