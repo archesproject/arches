c8006f046cc6329693a5f80289234858907e5074
Phil Weir <phil.weir@flaxandteal.co.uk>
2023-11-19 03:31:05 +0000

    accept sequences of CSVs in business data

--- arches/app/utils/skos.py
+++ arches/app/utils/skos.py
@@ -141,8 +141,9 @@ class SKOSReader(object):
                             concept_id = self.generate_uuid_from_subject(baseuuid, object)
                             raise Exception(f"The Concept Scheme defines a \"narrower\" relation to a concept instead of using \"hasTopConcept\".  First concept with incorrect relation: {object}")
 
-                if len(self.relations) == 0:
-                    raise Exception("The SKOS file doesn't appear to have any Top Concepts defined.")
+                # RMV[PTW]: this seems to regularly be the case for collections.xml
+                # if len(self.relations) == 0:
+                #     raise Exception("The SKOS file doesn't appear to have any Top Concepts defined.")
 
                 concept_scheme.addvalue(
                     {
--- arches/management/commands/packages.py
+++ arches/management/commands/packages.py
@@ -1,4 +1,5 @@
 import json
+import re
 import pyprind
 import csv
 import shutil
@@ -753,10 +754,21 @@ class Command(BaseCommand):
                     for ext in ["*.json", "*.jsonl", "*.csv"]:
                         business_data += glob.glob(os.path.join(package_dir, "business_data", ext))
 
+            def _path_to_mapping(path):
+                prefix, suffix = os.path.splitext(path)
+                prefix = re.sub(r"_[0-9]+$", "", prefix)
+                if suffix == ".csv":
+                    if os.path.isfile(prefix + ".mapping"):
+                        return prefix + ".mapping"
+                    return False
+
+                return None
+
+
             erring_csvs = [
                 path
                 for path in business_data
-                if os.path.splitext(path)[1] == ".csv" and os.path.isfile(os.path.splitext(path)[0] + ".mapping") is False
+                if _path_to_mapping(path) is False
             ]
             message = (
                 f"The following .csv files will not load because they are missing accompanying .mapping files: \n\t {','.join(erring_csvs)}"
@@ -778,7 +790,7 @@ class Command(BaseCommand):
                 valid_resource_paths = [
                     path
                     for path in business_data
-                    if (".csv" in path and os.path.exists(path.replace(".csv", ".mapping"))) or (".json" in path)
+                    if (_path_to_mapping(path) is False) or (".json" in path)
                 ]
 
                 # assumes resources in csv do not depend on data being loaded prior from json in same dir
@@ -819,7 +831,7 @@ class Command(BaseCommand):
                     dest_path = os.path.join(template_dir, os.path.basename(templates[0]))
                     if os.path.exists(dest_path) is False:
                         if os.path.exists(template_dir) is False:
-                            os.mkdir(template_dir)
+                            os.makedirs(template_dir)
                         shutil.copy(templates[0], template_dir)
                     else:
                         logger.info("Not loading {0} from package. Extension already exists".format(templates[0]))
@@ -828,7 +840,7 @@ class Command(BaseCommand):
                     dest_path = os.path.join(component_dir, os.path.basename(components[0]))
                     if os.path.exists(dest_path) is False:
                         if os.path.exists(component_dir) is False:
-                            os.mkdir(component_dir)
+                            os.makedirs(component_dir)
                         shutil.copy(components[0], component_dir)
                     else:
                         logger.info("Not loading {0} from package. Extension already exists".format(components[0]))
@@ -1009,7 +1021,7 @@ class Command(BaseCommand):
         if os.path.exists(css_source):
             css_dest = os.path.join(root, "media", "css")
             if not os.path.exists(css_dest):
-                os.mkdir(css_dest)
+                os.makedirs(css_dest)
             css_files = glob.glob(os.path.join(css_source, "*.css"))
             for css_file in css_files:
                 shutil.copy(css_file, css_dest)
