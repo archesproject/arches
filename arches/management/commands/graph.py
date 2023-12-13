2daf94cd1590c9166c737570f1b80c448a1eeb1c
Jacob Walls <jwalls@fargeo.com>
2023-12-13 12:32:04 -0500

    Avoid trying to update the publication of an unpublished graph #10409 (#10410)

--- arches/management/commands/graph.py
+++ arches/management/commands/graph.py
@@ -96,7 +96,8 @@ class Command(BaseCommand):
             print(graph.name)
             
             if self.update:
-                graph.update_published_graphs()
+                if graph.publication_id:
+                    graph.update_published_graphs()
             else:
                 graph.publish(user)
 
