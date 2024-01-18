9753a6a98bb0e672573d2ad7f7ac98b0b26bdc6c
Christopher Byrd <cbyrd@fargeo.com>
2024-01-17 16:16:26 -0800

    updates webpack to handle css processing for legacy && modern patterns #10533

--- arches/webpack/webpack.common.js
+++ arches/webpack/webpack.common.js
@@ -14,6 +14,7 @@ const { VueLoaderPlugin } = require("vue-loader");
 const { buildImageFilePathLookup } = require('./webpack-utils/build-image-filepath-lookup');
 const { buildJavascriptFilepathLookup } = require('./webpack-utils/build-javascript-filepath-lookup');
 const { buildTemplateFilePathLookup } = require('./webpack-utils/build-template-filepath-lookup');
+const { buildCSSFilepathLookup } = require('./webpack-utils/build-css-filepath-lookup');
 
 module.exports = () => {
     return new Promise((resolve, _reject) => {
@@ -203,6 +204,25 @@ module.exports = () => {
             };
 
             // END create image filepath lookup
+            // BEGIN create CSS filepath lookup
+
+            const coreArchesCSSFilepathConfiguration = buildCSSFilepathLookup(Path.resolve(__dirname, ROOT_DIR, 'app', 'media', 'css'), {});
+            const projectCSSFilepathConfiguration = buildCSSFilepathLookup(Path.resolve(__dirname, APP_ROOT, 'media', 'css'), {});
+
+            const archesApplicationsCSSFilepathConfiguration = ARCHES_APPLICATIONS.reduce((acc, archesApplication) => {   
+                return {
+                    ...acc,
+                    ...buildCSSFilepathLookup(STATIC_URL, Path.resolve(__dirname, ARCHES_APPLICATIONS_PATHS[archesApplication], 'media', 'img'), {})
+                };
+            }, {});
+
+            const CSSFilepathLookup = {
+                ...coreArchesCSSFilepathConfiguration,
+                ...archesApplicationsCSSFilepathConfiguration,
+                ...projectCSSFilepathConfiguration,
+            };
+
+            // END create CSS filepath lookup
             // BEGIN create vue filepath lookup
 
             const archesApplicationsVuePaths = ARCHES_APPLICATIONS.reduce((acc, archesApplication) => { 
@@ -239,6 +259,7 @@ module.exports = () => {
                     ...archesCoreEntryPointConfiguration,
                     ...archesApplicationsEntrypointConfiguration,
                     ...projectEntryPointConfiguration,
+                    ...CSSFilepathLookup
                 },
                 devServer: {
                     port: WEBPACK_DEVELOPMENT_SERVER_PORT,
@@ -248,6 +269,7 @@ module.exports = () => {
                     publicPath: STATIC_URL,
                     libraryTarget: 'amd-require',
                     clean: true,
+                    assetModuleFilename: 'img/[hash][ext][query]',
                 },
                 plugins: [
                     new CleanWebpackPlugin(),
@@ -314,7 +336,17 @@ module.exports = () => {
                         },
                         {
                             test: /\.css$/,
-                            exclude: /node_modules/,
+                            exclude: [
+                                /node_modules/,
+                                Path.resolve(__dirname, APP_ROOT, 'media', 'css'),
+                                Path.resolve(__dirname, ROOT_DIR, 'app', 'media', 'css'),
+                                ...ARCHES_APPLICATIONS.reduce((acc, archesApplication) => { 
+                                    const path = Path.resolve(__dirname, ARCHES_APPLICATIONS_PATHS[archesApplication], 'media', 'css');
+                                    acc.push(path);
+                                    
+                                    return acc;
+                                }, [])
+                            ],
                             use: [
                                 {
                                     'loader': Path.join(APP_ROOT, 'media', 'node_modules', 'style-loader'),
@@ -325,8 +357,13 @@ module.exports = () => {
                             ],
                         },
                         {
-                            test: /\.scss$/i,
-                            exclude: /node_modules/,
+                            test: /\.s?css$/i,
+                            exclude: [
+                                /node_modules/,
+                                Path.resolve(__dirname, APP_ROOT, 'src'),
+                                Path.resolve(__dirname, ROOT_DIR, 'app', 'src'),
+                                ...archesApplicationsVuePaths,
+                            ],
                             use: [
                                 {
                                     'loader': MiniCssExtractPlugin.loader,
@@ -339,6 +376,15 @@ module.exports = () => {
                                 },
                                 {
                                     'loader': Path.join(APP_ROOT, 'media', 'node_modules', 'sass-loader'),
+                                    options: {
+                                        sassOptions: {
+                                            indentWidth: 4,
+                                            includePaths: [
+                                                Path.resolve(__dirname, APP_ROOT, 'media', 'css'),
+                                                Path.resolve(__dirname, ROOT_DIR, 'app', 'media', 'css'),
+                                            ],
+                                        },
+                                    },
                                 }
                             ],
                         },
