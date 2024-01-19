24106269e086e06c0b6dba6c5e08123b4f72de53
Jacob Walls <jwalls@fargeo.com>
2024-01-19 13:07:06 -0500

    Initial work on controlled list manager #10541

--- arches/urls.py
+++ arches/urls.py
@@ -23,6 +23,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path, re_path
 from arches.app.views import concept, main, map, search, graph, api
 from arches.app.views.admin import ReIndexResources, ClearUserPermissionCache
+from arches.app.views.controlled_lists import ControlledListView
 from arches.app.views.etl_manager import ETLManagerView
 from arches.app.views.file import FileView, TempFileView
 from arches.app.views.thumbnail import ThumbnailView
@@ -313,6 +314,7 @@ urlpatterns = [
     re_path(r"^etl-manager$", ETLManagerView.as_view(), name="etl_manager"),
     re_path(r"^clear-user-permission-cache", ClearUserPermissionCache.as_view(), name="clear_user_permission_cache"),
     re_path(r"^transform-edtf-for-tile", api.TransformEdtfForTile.as_view(), name="transform_edtf_for_tile"),
+    path("controlled_lists", ControlledListView.as_view(), name="controlled_lists"),
 ]
 
 if settings.DEBUG:
