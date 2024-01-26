64c7162c4764141f7533c13c0370686bdaa8deae
Jacob Walls <jwalls@fargeo.com>
2024-01-26 18:43:56 -0500

    Test POST views
    TODO: debug last assertion

--- tests/views/controlled_lists_tests.py
+++ tests/views/controlled_lists_tests.py
@@ -1,14 +1,21 @@
 import json
+from collections import defaultdict
 
 from django.contrib.auth.models import User
 from django.urls import reverse
-from django.test import TestCase
 from django.test.client import Client
 
-from arches.app.models.models import ControlledList, ControlledListItem, DValueType, Label, Language
+from arches.app.models.models import (
+    ControlledList,
+    ControlledListItem,
+    DValueType,
+    Label,
+    Language,
+)
+from arches.app.views.controlled_lists import ControlledListsView
+from tests.base_test import ArchesTestCase
 
-
-class ControlledListTests(TestCase):
+class ControlledListTests(ArchesTestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -29,12 +36,28 @@ class ControlledListTests(TestCase):
 
         # Create 5 labels per list. (10)
         ControlledListItem.objects.bulk_create(
-            [ControlledListItem(uri=f"https://archesproject.org/{num}", list=cls.list1, sortorder=num) for num in range(5)]
-            + [ControlledListItem(uri=f"https://getty.edu/{num}", list=cls.list2, sortorder=num) for num in range(5)]
+            [
+                ControlledListItem(
+                    uri=f"https://archesproject.org/{num}",
+                    list=cls.list1,
+                    sortorder=num,
+                )
+                for num in range(5)
+            ]
+            + [
+                ControlledListItem(
+                    uri=f"https://getty.edu/{num}", list=cls.list2, sortorder=num
+                )
+                for num in range(5)
+            ]
         )
 
-        parent = ControlledListItem.objects.get(list=cls.list2, uri=f"https://getty.edu/0")
-        for child in ControlledListItem.objects.filter(list=cls.list2).exclude(pk=parent.pk):
+        parent = ControlledListItem.objects.get(
+            list=cls.list2, uri=f"https://getty.edu/0"
+        )
+        for child in ControlledListItem.objects.filter(list=cls.list2).exclude(
+            pk=parent.pk
+        ):
             child.parent = parent
             child.save()
 
@@ -96,7 +119,9 @@ class ControlledListTests(TestCase):
             # 8: prefetch grandchildren: items
             # there are no grandchildren, so no labels to get
             # 9: languages
-            response = self.client.get(reverse("controlled_lists"), kwargs={"prefetchDepth": 3})
+            response = self.client.get(
+                reverse("controlled_lists"), kwargs={"prefetchDepth": 3}
+            )
 
         self.assertEqual(response.status_code, 200)
         result = json.loads(response.content)
@@ -112,3 +137,61 @@ class ControlledListTests(TestCase):
 
         languages = result["languages"]
         self.assertEqual(languages["de"], "German")
+
+    def test_create_list(self):
+        self.client.force_login(self.admin)
+        self.client.post(reverse("controlled_lists"))
+        self.assertEqual(ControlledList.objects.count(), 3)
+        self.assertEqual(
+            ControlledList.objects.filter(name__startswith="Untitled List: ").count(), 1
+        )
+
+    def test_delete_list(self):
+        self.client.force_login(self.admin)
+        self.client.delete(
+            reverse("controlled_list", kwargs={"id": str(self.list1.pk)}),
+        )
+        self.assertEqual(ControlledList.objects.count(), 1)
+        self.assertEqual(ControlledList.objects.first().pk, self.list2.pk)
+
+    def test_reorder_list_items_valid(self):
+        self.client.force_login(self.admin)
+        serialized_list = ControlledListsView.serialize(self.list1, depth_map=defaultdict(int))
+
+        serialized_list["items"][0]["sortorder"] = 1
+        serialized_list["items"][1]["sortorder"] = 0
+        response = self.client.post(
+            reverse("controlled_list", kwargs={"id": str(self.list1.pk)}),
+            serialized_list,
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            [item.sortorder for item in ControlledListItem.objects.filter(list=self.list1).order_by("uri")],
+            [1, 0, 2, 3, 4]
+        )
+
+    def test_reorder_list_items_invalid_negative(self):
+        self.client.force_login(self.admin)
+        serialized_list = ControlledListsView.serialize(self.list1, depth_map=defaultdict(int))
+
+        serialized_list["items"][0]["sortorder"] = -1
+        response = self.client.post(
+            reverse("controlled_list", kwargs={"id": str(self.list1.pk)}),
+            serialized_list,
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_reorder_list_items_invalid_duplicate(self):
+        self.client.force_login(self.admin)
+        serialized_list = ControlledListsView.serialize(self.list1, depth_map=defaultdict(int))
+
+        # Attempt setting a duplicate sort order.
+        serialized_list["items"][1]["sortorder"] = 0
+        response = self.client.post(
+            reverse("controlled_list", kwargs={"id": str(self.list1.pk)}),
+            serialized_list,
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 400)
