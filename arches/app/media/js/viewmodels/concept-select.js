5a067f575048b210f38ce8a44517af5eaae34f1b
whatisgalen <whatisgalen@gmail.com>
2024-01-25 20:18:19 -0800

    creates alternate select2 to handle multi select for in_list ops re #10529

--- arches/app/media/js/viewmodels/concept-select.js
+++ arches/app/media/js/viewmodels/concept-select.js
@@ -192,6 +192,9 @@ define([
 
             }
         };
+
+        this.select2ConfigMulti = { ...this.select2Config };
+        this.select2ConfigMulti.multiple = true;
     };
 
     return ConceptSelectViewModel;
