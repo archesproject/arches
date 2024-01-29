d4f974cdbdf1d542a853aa50f9751b70ae552d3a
Jacob Walls <jwalls@fargeo.com>
2024-01-29 16:24:57 -0500

    Rough out splitter for list item editor

--- arches/app/src/components/ControlledListManager/ControlledListEditor.vue
+++ arches/app/src/components/ControlledListManager/ControlledListEditor.vue
@@ -1,6 +1,8 @@
 <script setup>
 import arches from "arches";
 import Button from "primevue/button";
+import Splitter from "primevue/splitter";
+import SplitterPanel from "primevue/splitterpanel";
 
 import Characteristics from "./Characteristics.vue";
 import Header from "./Header.vue";
@@ -10,16 +12,47 @@ const { displayedList, editing, setEditing } = defineProps([
     "editing",
     "setEditing",
 ]);
+
+const lightGray = "#f4f4f4";
 </script>
 
 <template>
     <Header :displayedList="displayedList" :isItemEditor="true"></Header>
     <div class="list-editor-container">
         <Characteristics :displayedList="displayedList" />
+
+        <div class="items" style="margin: 1rem">
+            <h4 style="margin-top: 4rem; margin-left: 0">List Item Editor</h4>
+            <Splitter
+                :pt="{
+                    gutter: { style: { background: lightGray } },
+                    gutterHandler: { style: { background: lightGray } },
+                }"
+            >
+                <SplitterPanel :size="30" :minSize="15">
+                    <div>Left</div>
+                </SplitterPanel>
+
+                <SplitterPanel :size="75" :minSize="50" class="mt-0">
+                    <div>Right</div>
+                </SplitterPanel>
+            </Splitter>
+        </div>
         <Button @click="setEditing(false)">{{
             arches.translations.return
         }}</Button>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.p-splitter {
+    width: calc(100vw - 50px);
+    height: calc(100vh - 400px);
+    background: white;
+    font-size: 14px;
+}
+.p-splitter-panel {
+    display: flex;
+    flex-direction: column;
+}
+</style>
