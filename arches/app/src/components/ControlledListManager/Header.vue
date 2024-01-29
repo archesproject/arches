06ed2f46ee09eabe4cc87e574db4a980e0f69637
Jacob Walls <jwalls@fargeo.com>
2024-01-29 16:07:20 -0500

    Factor out <Header>

---
+++ arches/app/src/components/ControlledListManager/Header.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { computed } from "vue";
+
+const slateBlue = "#2d3c4b"; // todo: import from theme somewhere
+
+const { displayedList, isItemEditor } = defineProps([
+    "displayedList",
+    "isItemEditor",
+]);
+
+const heading = computed(() => {
+    const prefix = isItemEditor ? "List Item Editor" : "List Editor";
+    return (
+        prefix + (displayedList.value ? " > " + displayedList.value.name : "")
+    );
+});
+</script>
+
+<template>
+    <div class="header" :style="{ background: slateBlue }">
+        <i class="fa fa-inverse fa-list" aria-hidden="true"></i>
+        <h4>{{ heading }}</h4>
+    </div>
+</template>
+
+<style scoped>
+.header {
+    display: flex;
+    align-items: center;
+}
+i {
+    margin-left: 1rem;
+    margin-top: 0.25rem;
+}
+h4 {
+    margin: 1rem;
+    color: white;
+}
+</style>
