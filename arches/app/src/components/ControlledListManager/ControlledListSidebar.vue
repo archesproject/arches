bc9267192e418280eb3b41e8b29be3a0e2b355ed
Jacob Walls <jwalls@fargeo.com>
2024-01-30 10:31:49 -0500

    Factor out <SearchAddDelete>

--- arches/app/src/components/ControlledListManager/ControlledListSidebar.vue
+++ arches/app/src/components/ControlledListManager/ControlledListSidebar.vue
@@ -3,29 +3,21 @@ import arches from "arches";
 import Cookies from "js-cookie";
 import { ref } from "vue";
 
-import Button from "primevue/button";
-import InputText from "primevue/inputtext";
-import SplitButton from "primevue/splitbutton";
 import { useToast } from "primevue/usetoast";
 
 import ControlledListsAll from "./ControlledListsAll.vue";
+import SearchAddDelete from "./SearchAddDelete.vue";
 import Spinner from "../Spinner.vue";
 
-const buttonGreen = "#10b981";
-const buttonPink = "#ed7979";
 const toast = useToast();
 
 const { displayedList, languageMap } = defineProps([
     "displayedList",
     "languageMap",
 ]);
+const filteredLists = ref([]);
 const selectedLists = ref([]);
-const searchValue = ref("");
 const queryMutator = ref(0);
-
-const clearSearch = () => {
-    searchValue.value = "";
-};
 
 const createList = async () => {
     try {
@@ -90,73 +82,21 @@ const deleteLists = async () => {
         <h4>Controlled Lists</h4>
     </div>
 
-    <div class="controls">
-        <span class="flex p-input-icon-right">
-            <InputText
-                type="text"
-                class="control"
-                :placeholder="arches.translations.find"
-                v-model="searchValue"
-            />
-            <i
-                v-if="searchValue"
-                class="fa fa-times"
-                role="button"
-                tabindex="0"
-                @click="clearSearch"
-                @keydown="clearSearch"
-                :aria-label="arches.translations.clear"
-            ></i>
-            <i
-                v-else
-                class="fa fa-search"
-                :aria-label="arches.translations.search"
-            ></i>
-        </span>
-        <div class="flex" style="flex: 0.8; flex-wrap: wrap">
-            <SplitButton
-                class="button"
-                label="Create New List"
-                raised
-                style="font-size: inherit"
-                :pt="{
-                    button: {
-                        root: {
-                            style: {
-                                background: buttonGreen,
-                                border: `1px solid ${buttonGreen}`,
-                            },
-                        },
-                    },
-                    menubutton: {
-                        root: {
-                            style: {
-                                background: buttonGreen,
-                                border: `1px solid ${buttonGreen}`,
-                            },
-                        },
-                    },
-                }"
-                @click="createList"
-            ></SplitButton>
-            <!-- We might want an are you sure? modal -->
-            <Button
-                class="button delete"
-                :label="
-                    selectedLists.length > 1 ? 'Delete Lists' : 'Delete List'
-                "
-                raised
-                :disabled="!selectedLists.length"
-                @click="deleteLists"
-            ></Button>
-        </div>
-    </div>
+    <SearchAddDelete
+        :addAction="createList"
+        addLabel="Create New List"
+        :deleteAction="deleteLists"
+        deleteLabel="Delete List"
+        deleteLabelPlural="Delete Lists"
+        :filteredItems="filteredLists"
+        :numberToDelete="selectedLists.length"
+    />
 
     <Suspense>
         <ControlledListsAll
             :displayedList="displayedList"
             :selectedLists="selectedLists"
-            :searchValue="searchValue"
+            :filteredLists="filteredLists"
             :languageMap="languageMap"
             :key="queryMutator"
         />
@@ -174,32 +114,5 @@ const deleteLists = async () => {
 }
 h4 {
     margin: 1rem;
-}
-i {
-    margin-top: -0.75rem;
-    margin-right: 2rem;
-}
-.controls {
-    display: flex;
-    flex-direction: column;
-    padding: 1rem;
-    background: #f3fbfd;
-}
-.p-inputtext {
-    flex: 0.95;
-    margin: 0.5rem;
-}
-.button {
-    height: 4rem;
-    margin: 0.5rem;
-    flex: 0.5;
-    justify-content: center;
-    font-weight: 600;
-    color: white;
-    text-wrap: nowrap;
-}
-.button.delete {
-    background: v-bind(buttonPink);
-    border: 1px solid v-bind(buttonPink);
 }
 </style>
