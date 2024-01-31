0269d1c1c11190dee31789883ae6f1d32cf2ec0f
Jacob Walls <jwalls@fargeo.com>
2024-01-31 14:17:44 -0500

    Refactor component tree

--- arches/app/src/components/ControlledListManager/SidepanelDataView.vue
+++ arches/app/src/components/ControlledListManager/SidepanelDataView.vue
@@ -1,65 +1,140 @@
 <script setup>
 import arches from "arches";
-import DataView from "primevue/dataview";
+import Cookies from "js-cookie";
+import { computed, ref } from "vue";
 
-const lightGray = "#f4f4f4";
-const slateBlue = "#2d3c4b";
+import DataView from "primevue/dataview";
+import { useToast } from "primevue/usetoast";
+
+import SearchAddDelete from "./SearchAddDelete.vue";
 
 const {
     displayedItem,
-    filteredItems,
     itemLabel,
     itemsLabel,
     languageMap,
     noItemLabel,
     noSearchResultLabel,
-    selectedItems,
 } = defineProps([
     "displayedItem",
-    "filteredItems",
     "itemLabel",
     "itemsLabel",
     "languageMap",
     "noItemLabel",
     "noSearchResultLabel",
-    "selectedItems",
 ]);
 
-const response = await fetch(arches.urls.controlled_lists);
-const { controlled_lists: controlledLists } = await response
-    .json()
-    .then((data) => {
+const items = ref([]);
+const selectedItems = ref([]);
+const searchValue = ref("");
+
+const toast = useToast();
+const lightGray = "#f4f4f4";
+const slateBlue = "#2d3c4b";
+
+const fetchLists = async () => {
+    const response = await fetch(arches.urls.controlled_lists);
+    await response.json().then((data) => {
         languageMap.value = data.languages;
-        filteredItems.splice(0, filteredItems.length);
-        data.controlled_lists.forEach((list) => {
-            filteredItems.push(list);
-        });
-        return data;
+        items.value = data.controlled_lists;
     });
+};
+await fetchLists();
+
+const createList = async () => {
+    try {
+        const response = await fetch(arches.urls.controlled_list_add, {
+            method: "POST",
+            headers: {
+                "X-CSRFToken": Cookies.get("csrftoken"),
+            },
+        });
+        if (response.ok) {
+            const newItem = await response.json();
+            items.value.push(newItem);
+        } else {
+            throw new Error();
+        }
+    } catch {
+        toast.add({
+            severity: "error",
+            summary: "List creation failed",
+            life: 3000,
+        });
+    }
+};
+
+const deleteLists = async () => {
+    if (!selectedItems.value.length) {
+        return;
+    }
+    const promises = selectedItems.value.map((list) =>
+        fetch(arches.urls.controlled_list(list.id), {
+            method: "DELETE",
+            headers: {
+                "X-CSRFToken": Cookies.get("csrftoken"),
+            },
+        })
+    );
+
+    try {
+        const responses = await Promise.all(promises);
+        if (responses.some((resp) => resp.ok)) {
+            if (selectedItems.value.includes(displayedItem.value)) {
+                displayedItem.value = null;
+            }
+            selectedItems.value = [];
+        }
+        if (responses.some((resp) => !resp.ok)) {
+            throw new Error();
+        }
+    } catch {
+        toast.add({
+            severity: "error",
+            summary: "One or more lists failed to delete.",
+            life: 3000,
+        });
+    }
+    await fetchLists();
+};
 
 const toggleCheckbox = (list) => {
-    const i = selectedItems.indexOf(list);
+    const i = selectedItems.value.indexOf(list);
     if (i === -1) {
-        selectedItems.push(list);
+        selectedItems.value.push(list);
     } else {
-        selectedItems.splice(i);
+        selectedItems.value.splice(i);
     }
 };
 
 const selectAll = () => {
-    selectedItems.splice(0, selectedItems.length);
-    controlledLists.forEach((list) => {
-        selectedItems.push(list);
-    });
+    selectedItems.value = items.value;
 };
 const clearAll = () => {
-    selectedItems.splice(0, selectedItems.length);
+    selectedItems.value = [];
 };
+
+const filteredItems = computed(() => {
+    const loweredTerm = searchValue.value.toLowerCase();
+    return items.value.filter(
+        (item) => !loweredTerm || item.name.includes(loweredTerm)
+    );
+});
 </script>
 
 <template>
+    <SearchAddDelete
+        :v-model="searchValue"
+        :addAction="createList"
+        addLabel="Create New List"
+        :deleteAction="deleteLists"
+        deleteLabel="Delete List"
+        deleteLabelPlural="Delete Lists"
+        :items="items"
+        :numberToDelete="selectedItems.length"
+    />
     <div class="selection-header">
-        <span v-if="controlledLists.length" style="margin-left: 1rem">
+        <span v-if="items.length" style="margin-left: 1rem">
             <button v-if="selectedItems.length" @click="clearAll">
                 {{ arches.translations.clearAll }}
             </button>
@@ -67,13 +142,13 @@ const clearAll = () => {
                 {{ arches.translations.selectAll }}
             </button>
         </span>
-        <span v-if="controlledLists.length" style="margin-right: 1rem">
-            {{ controlledLists.length }}
-            {{ controlledLists.length === 1 ? itemLabel : itemsLabel }}
+        <span v-if="items.length" style="margin-right: 1rem">
+            {{ items.length }}
+            {{ items.length === 1 ? itemLabel : itemsLabel }}
         </span>
     </div>
 
-    <DataView v-if="controlledLists.length" :value="filteredItems">
+    <DataView v-if="items.length" :value="filteredItems">
         <template #list="slotProps">
             <div
                 v-for="(item, index) in slotProps.items"
