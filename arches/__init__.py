7f4207dd7de954e274f43daed9c4132f9c5d2a41
Cyrus Hiatt <chiatt@fargeo.com>
2024-02-07 16:13:45 -0800

    Update version number to 7.4.4

--- arches/__init__.py
+++ arches/__init__.py
@@ -10,6 +10,6 @@ try:
 except Exception as e:
     pass
 
-VERSION = (7, 4, 3, "beta", 0)  # VERSION[3] options = "alpha", "beta", "rc", or "final"
+VERSION = (7, 4, 4, "beta", 0)  # VERSION[3] options = "alpha", "beta", "rc", or "final"
 
 __version__ = get_version(VERSION)
