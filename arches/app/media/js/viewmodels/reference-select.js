6c41a398697520b5df88b5cd395d20d6ef68dd9f
Cyrus Hiatt <chiatt@fargeo.com>
2024-02-21 14:54:28 -0800

    Set undefined prefLabel as Unlabeled Item #10552

--- arches/app/media/js/viewmodels/reference-select.js
+++ arches/app/media/js/viewmodels/reference-select.js
@@ -18,7 +18,7 @@ define([
         WidgetViewModel.apply(this, [params]);
 
         this.getPrefLabel = function(labels){
-            return koMapping.toJS(labels)?.find(label => label.language===arches.activeLanguage && label.valuetype === 'prefLabel')?.value;
+            return koMapping.toJS(labels)?.find(label => label.language===arches.activeLanguage && label.valuetype === 'prefLabel')?.value || arches.translations.unlabeledItem;
         }; 
 
         this.displayValue = ko.computed(function() {
