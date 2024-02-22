b42ed4665f223be8b39c53089c3126362aad2788
Jacob Walls <jwalls@fargeo.com>
2024-02-22 10:32:45 -0500

    fixup! Workaround inability

--- arches/app/src/types/Ref.ts
+++ arches/app/src/types/Ref.ts
@@ -1,9 +1,10 @@
 // Cannot find a way to import this type from vue
 // Copied from https://github.com/vuejs/core/blob/v3.4.19/packages/reactivity/src/ref.ts
 
-declare const RefSymbol: unique symbol
-export declare const RawSymbol: unique symbol
+declare const RefSymbol: unique symbol;
+export declare const RawSymbol: unique symbol;
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export interface Ref<T = any> {
   value: T
   /**
