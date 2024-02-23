35f87cf9963a4b44b301f24540ff7d1ee18c38f1
Jacob Walls <jwalls@fargeo.com>
2024-02-23 15:33:35 -0500

    Show referencing nodes

--- arches/app/src/components/ControlledListManager/ListCharacteristics.vue
+++ arches/app/src/components/ControlledListManager/ListCharacteristics.vue
@@ -2,6 +2,7 @@
 import { useGettext } from "vue3-gettext";
 
 import ItemCharacteristic from "@/components/ControlledListManager/ItemCharacteristic.vue";
+import ReferenceNodeLink from "@/components/ControlledListManager/ReferenceNodeLink.vue";
 
 import type { ControlledList } from "@/types/ControlledListManager";
 
@@ -31,11 +32,29 @@ const { $gettext } = useGettext();
             :label="$gettext('Dynamic')"
             :style="{ width: '4rem' }"
         />
-        <ItemCharacteristic
-            :item="props.displayedList"
-            :editable="false"
-            field="dynamic"
-            :label="$gettext('List used by these nodes')"
-        />
+        <h4 class="nodes-heading">{{ $gettext("List used by these nodes") }}</h4>
+        <div class="nodes">
+            <div
+                v-for="node in props.displayedList.nodes"
+                :key="node.id"
+            >
+                <ReferenceNodeLink :node />
+            </div>
+        </div>
     </div>
 </template>
+
+<style scoped>
+h4 {
+    font-size: 1.25rem;
+}
+.nodes-heading {
+    margin: 1rem 1rem 2rem 1rem;
+}
+.nodes {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
+    margin: 1rem;
+}
+</style>
