archesproject/arches

commit decd1b28e44bec489101c330f94a90e9767e2550

Optimize tile permission check by checking nodegroups once (#10546)
* modifies resource.load_tiles to leverage permission_backend for optimal tile-nodegroup perm check

* leverages resource.load_tiles to filter tiles by nodegroup perm, re #9484

* queries only those cards for which user has nodegroup perm in report api, re #9484

arches/app/views/api.py
@@ -15,6 +15,7 @@ from rdflib import RDF
 from rdflib.namespace import SKOS, DCTERMS
 from revproxy.views import ProxyView
 from slugify import slugify
+from operator import attrgetter
 from urllib import parse
 from collections import OrderedDict
 from django.contrib.auth import authenticate
@@ -1136,24 +1137,25 @@ class ResourceReport(APIBase):
             resp["related_resources"] = related_resources_summary
 
         if "tiles" not in exclude:
-            permitted_tiles = []
-            for tile in TileProxyModel.objects.filter(resourceinstance=resource).select_related("nodegroup").order_by("sortorder"):
-                if request.user.has_perm(perm, tile.nodegroup):
-                    tile.filter_by_perm(request.user, perm)
-                    permitted_tiles.append(tile)
+            resource.load_tiles(user=request.user, perm=perm)
+            permitted_tiles = resource.tiles
 
             resp["tiles"] = permitted_tiles
 
         if "cards" not in exclude:
-            permitted_cards = []
-            for card in CardProxyModel.objects.filter(graph_id=resource.graph_id).select_related("nodegroup").order_by("sortorder"):
-                if request.user.has_perm(perm, card.nodegroup):
-                    card.filter_by_perm(request.user, perm)
-                    permitted_cards.append(card)
+            # collect the nodegroups for which this user has perm
+            readable_nodegroups = get_nodegroups_by_perm(request.user, [perm], any_perm=True)
+            
+            # query only the cards whose nodegroups are readable by user
+            permitted_cards = (
+                CardProxyModel.objects.filter(graph_id=resource.graph_id, nodegroup__in=readable_nodegroups)
+                .prefetch_related("cardxnodexwidget_set")
+                .order_by("sortorder")
+            )
 
             cardwidgets = [
                 widget
-                for widgets in [card.cardxnodexwidget_set.order_by("sortorder").all() for card in permitted_cards]
+                for widgets in [sorted(card.cardxnodexwidget_set.all(), key=attrgetter("sortorder")) for card in permitted_cards]
                 for widget in widgets
             ]
 

arches/app/models/resource.py
@@ -52,6 +52,7 @@ from arches.app.utils.permission_backend import (
     get_restricted_users,
     get_restricted_instances,
     user_can_read_graph,
+    get_nodegroups_by_perm,
 )
 from arches.app.datatypes.datatypes import DataTypeFactory
 
@@ -254,7 +255,8 @@ class Resource(models.ResourceInstance):
 
         self.tiles = list(models.TileModel.objects.filter(resourceinstance=self))
         if user:
-            self.tiles = [tile for tile in self.tiles if tile.nodegroup_id is not None and user.has_perm(perm, tile.nodegroup)]
+            readable_nodegroups = get_nodegroups_by_perm(user, perm, any_perm=True)
+            self.tiles = [tile for tile in self.tiles if tile.nodegroup is not None and tile.nodegroup in readable_nodegroups]
 
     # # flatten out the nested tiles into a single array
     def get_flattened_tiles(self):

commit 4951c847763859f2d742a760e8648c88544098da

Harden ResourceReport.get() against null sortorder
Regression in decd1b28e44bec489101c330f94a90e9767e2550

arches/app/views/api.py
@@ -15,7 +15,6 @@ from rdflib import RDF
 from rdflib.namespace import SKOS, DCTERMS
 from revproxy.views import ProxyView
 from slugify import slugify
-from operator import attrgetter
 from urllib import parse
 from collections import OrderedDict
 from django.contrib.auth import authenticate
@@ -1155,7 +1154,10 @@ class ResourceReport(APIBase):
 
             cardwidgets = [
                 widget
-                for widgets in [sorted(card.cardxnodexwidget_set.all(), key=attrgetter("sortorder")) for card in permitted_cards]
+                for widgets in [
+                    sorted(card.cardxnodexwidget_set.all(), key=lambda x: x.sortorder or 0)
+                    for card in permitted_cards
+                ]
                 for widget in widgets
             ]
 