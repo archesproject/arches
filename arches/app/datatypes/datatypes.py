09402b762adfdd88af8b3533cbf88d42c3f266f0
Cyrus Hiatt <chiatt@fargeo.com>
2024-02-29 06:21:15 -0800

    Fix check for excess prefLabels #10552

--- arches/app/datatypes/datatypes.py
+++ arches/app/datatypes/datatypes.py
@@ -2571,43 +2571,47 @@ class ReferenceDataType(BaseDataType):
     def validate(self, value, row_number=None, source="", node=None, nodeid=None, strict=False, **kwargs):
         errors = []
         title = _("Invalid Reference Datatype Value")
-        if value != None:
-            if type(value) == list and len(value):
-                for reference in value:
-                    if "uri" in reference and len(reference["uri"]):
-                        pass
-                    else:
+        if value is None:
+            return errors
+        
+        if type(value) == list and len(value):
+            for reference in value:
+                if "uri" in reference and len(reference["uri"]):
+                    pass
+                else:
+                    errors.append(
+                        {
+                            "type": "ERROR",
+                            "message": _("Reference objects require a 'uri' property and corresponding value"),
+                            "title": title,
+                        }
+                    )
+                if "labels" in reference:
+                    pref_label_languages = []
+                    for label in reference["labels"]:
+                        if not all([key in label for key in ("id", "value", "language", "valuetype")]):
+                            errors.append(
+                                {
+                                    "type": "ERROR",
+                                    "message": _(
+                                        "Reference labels require properties: id(uuid), value(string), language(e.g. 'en'), and valuetype(e.g. 'prefLabel')"
+                                    ),
+                                    "title": title,
+                                }
+                            )
+                        if label["valuetype"] == "prefLabel":
+                            pref_label_languages.append(label["language"])
+                            
+                    if len(set(pref_label_languages)) < len(pref_label_languages):
                         errors.append(
                             {
                                 "type": "ERROR",
-                                "message": _("Reference objects require a 'uri' property and corresponding value"),
+                                "message": _("A reference can have only one prefLabel per language"),
                                 "title": title,
                             }
                         )
-                    if "labels" in reference:
-                        pref_label_languages = []
-                        for label in reference["labels"]:
-                            if not all([key in label for key in ("id", "value", "language", "valuetype")]):
-                                errors.append(
-                                    {
-                                        "type": "ERROR",
-                                        "message": _(
-                                            "Reference labels require properties: id(uuid), value(string), language(e.g. 'en'), and valuetype(e.g. 'prefLabel')"
-                                        ),
-                                        "title": title,
-                                    }
-                                )
-                            else:
-                                if label["valuetype"] == "prefLabel" and label["language"] in pref_label_languages:
-                                    errors.append(
-                                        {
-                                            "type": "ERROR",
-                                            "message": _("A reference can have only one prefLabel per language"),
-                                            "title": title,
-                                        }
-                                    )
-            else:
-                errors.append({"type": "ERROR", "message": _("Reference value must be a list of reference objects"), "title": title})
+        else:
+            errors.append({"type": "ERROR", "message": _("Reference value must be a list of reference objects"), "title": title})
         return errors
 
     def transform_value_for_tile(self, value, **kwargs):
