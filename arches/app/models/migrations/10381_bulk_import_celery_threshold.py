ac6465e7900a074060b931e48235a22d8f7e482e
njkim <nkim@fargeo.com>
2024-03-15 16:29:42 -0700

    Fix the migration order, #10476

--- arches/app/models/migrations/10381_bulk_import_celery_threshold.py
+++ arches/app/models/migrations/10381_bulk_import_celery_threshold.py
@@ -4,7 +4,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("models", "10012_alter_file_path"),
+        ("models", "10515_i18n_plugins"),
     ]
 
     add_celery_threshold_config = """
