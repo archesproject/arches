e4688a613b76cb5f159dd904cb691dad3c2e6373
Galen <whatisgalen@gmail.com>
2024-03-28 18:11:21 -0700

    restyles to conform to line char limit, sets new editlog snapshot in cache if None in MVT cache key method re #10452

--- arches/app/views/api.py
+++ arches/app/views/api.py
@@ -402,11 +402,19 @@ class MVT(APIBase):
 
     def create_mvt_cache_key(node, zoom, x, y, user, mvt_snapshot=None):
         if not mvt_snapshot:
-            mvt_snapshot = (
-                cache.get(f'editlog_nodegroupid_{str(node.nodegroup_id)}_count')
-                or
-                models.EditLog.objects.filter(nodegroupid=str(node.nodegroup_id)).count()
+            mvt_snapshot = cache.get(
+                f'editlog_nodegroupid_{str(node.nodegroup_id)}_count'
             )
+            if not mvt_snapshot:
+                mvt_snapshot = models.EditLog.objects.filter(
+                    nodegroupid=str(node.nodegroup_id)
+                ).count()
+                cache.set(
+                    f'editlog_nodegroupid_{str(node.nodegroup_id)}_count',
+                    mvt_snapshot, 
+                    settings.CACHE_BY_USER["editlog"]
+                )
+
         return f"mvt_{str(node.nodeid)}_{zoom}_{x}_{y}_{mvt_snapshot}_{user.id}"
 
 @method_decorator(csrf_exempt, name="dispatch")
