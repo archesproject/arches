8d79f95293a77b333d0334ec240743ae8507446a
Galen <whatisgalen@gmail.com>
2024-04-12 16:22:49 -0700

    creates search filter to append related results of a chosen resource model to search results re #10644

---
+++ arches/app/media/js/views/components/search/results-related-resources-filter.js
@@ -0,0 +1,98 @@
+define([
+    'knockout',
+    'arches',
+    'views/components/search/base-filter',
+    'templates/views/components/search/results-related-resources-filter.htm',
+], function(ko, arches, BaseFilter, resultsRelatedResourcesFilterTemplate) {
+    var componentName = 'results-related-resources-filter';
+    const viewModel = BaseFilter.extend({
+        initialize: async function(options) {
+            options.name = 'Results Related Resources Filter';
+
+
+            this.requiredFilters = ['term-filter'];
+            BaseFilter.prototype.initialize.call(this, options);
+            this.resourceModels = ko.observableArray();
+            this.filter = ko.observableArray();
+            const self = this;
+
+            const response = await fetch(arches.urls.api_search_component_data + componentName);
+            if (response.ok) {
+                const data = await response.json();
+                data.resources.forEach(function(res) {
+                    //if (res.isactive === true) { // TODO: Uncomment once active flag is re-added to graph model
+                    self.resourceModels.push(res);
+                    //}
+                });
+            } else {
+                // eslint-disable-next-line no-console
+                console.log('Failed to fetch resource instance list');
+            }
+
+            var filterUpdated = ko.computed(function() {
+                return JSON.stringify(ko.toJS(this.filter()));
+            }, this);
+            filterUpdated.subscribe(function() {
+                this.updateQuery();
+            }, this);
+
+            this.filters[componentName](this);
+
+            if (this.requiredFiltersLoaded() === false) {
+                this.requiredFiltersLoaded.subscribe(function() {
+                    this.restoreState();
+                }, this);
+            } else {
+                this.restoreState();
+            }
+        },
+
+        updateQuery: function() {
+            var queryObj = this.query();
+            if(this.filter().length > 0){
+                queryObj[componentName] = ko.toJSON(this.filter);
+            } else {
+                delete queryObj[componentName];
+            }
+            this.query(queryObj);
+        },
+
+        restoreState: function() {
+            var query = this.query();
+            if (componentName in query) {
+                var resourceTypeQuery = JSON.parse(query[componentName]);
+                if (resourceTypeQuery.length > 0) {
+                    resourceTypeQuery.forEach(function(type){
+                        type.inverted = ko.observable(!!type.inverted);
+                        console.log(type.name);
+                        this.getFilter('term-filter').addTag("Related "+type.name, this.name, type.inverted);
+                    }, this);
+                    this.filter(resourceTypeQuery);
+                }
+            }
+        },
+
+        clear: function() {
+            this.filter.removeAll();
+        },
+
+        selectModelType: function(item){
+            this.filter().forEach(function(item){
+                this.getFilter('term-filter').removeTag(item.name);
+            }, this);
+            if(!!item){
+                var inverted = ko.observable(false);
+                console.log(item.name);
+                this.getFilter('term-filter').addTag("Related "+item.name+"(s)", this.name, inverted);
+                this.filter([{graphid:item.graphid, name: item.name, inverted: inverted}]);
+            }else{
+                this.clear();
+            }
+        }
+    });
+
+    return ko.components.register(componentName, {
+        viewModel: viewModel,
+        template: resultsRelatedResourcesFilterTemplate,
+    });
+});
---
+++ arches/app/search/components/results_related_resources_filter.py
@@ -0,0 +1,90 @@
+from arches.app.utils.betterJSONSerializer import JSONDeserializer
+from arches.app.search.elasticsearch_dsl_builder import Query, Ids
+from arches.app.search.components.base import BaseSearchFilter
+from arches.app.search.mappings import RESOURCES_INDEX
+from arches.app.search.search_engine_factory import SearchEngineFactory
+from arches.app.models.models import Node, ResourceXResource
+from arches.app.utils.permission_backend import get_resource_types_by_perm
+from django.db.models import Q
+import json
+
+
+details = {
+    "searchcomponentid": "166c011c-503b-499d-bc20-bedbe027f30e",
+    "name": "Results Related Resources Filter",
+    "icon": "",
+    "modulename": "results_related_resources_filter.py",
+    "classname": "ResultsRelatedResourcesFilter",
+    "type": "inline-filter",
+    "componentpath": "views/components/search/results-related-resources-filter",
+    "componentname": "results-related-resources-filter",
+    "sortorder": 6,
+    "enabled": True,
+}
+
+
+def get_permitted_graphids(permitted_nodegroups):
+    permitted_graphids = set()
+    for node in Node.objects.filter(nodegroup__in=permitted_nodegroups):
+        permitted_graphids.add(str(node.graph_id))
+    return permitted_graphids
+
+
+class ResultsRelatedResourcesFilter(BaseSearchFilter):
+
+    def post_search_hook(self, search_results_object, results, permitted_nodegroups):
+        select_related_resources_graphids = self.request.GET.get("results-related-resources-filter", None)
+        if select_related_resources_graphids:
+            try:
+                select_related_resources_graphids = json.loads(select_related_resources_graphids)
+                select_related_resources_graphids = [g['graphid'] for g in select_related_resources_graphids]
+            except TypeError:
+                return
+        else:
+            return
+
+        all_ids = [hit["_id"] for hit in results["hits"]["hits"]]
+        if not len(all_ids):
+            return
+        from_query = Q(resourceinstanceidfrom_id__in=all_ids, resourceinstanceto_graphid_id__in=select_related_resources_graphids)
+        to_query = Q(resourceinstanceidto_id__in=all_ids, resourceinstancefrom_graphid_id__in=select_related_resources_graphids)
+        final_query = (from_query | to_query)
+
+        related_resources = []
+        related_resources_query = ResourceXResource.objects.filter(final_query)
+        related_resources.extend([str(rid) for rid in related_resources_query.values_list('resourceinstanceidto_id', flat=True)])
+        related_resources.extend([str(rid) for rid in related_resources_query.values_list('resourceinstanceidfrom_id', flat=True)])
+
+        se = SearchEngineFactory().create()
+        new_search_results_object = {"query": Query(se)}
+        new_dsl = new_search_results_object.pop("query", None)
+        new_dsl.add_query(Ids(ids=related_resources))
+        new_dsl.include("graph_id")
+        new_dsl.include("root_ontology_class")
+        new_dsl.include("permissions.users_without_edit_perm")
+        new_dsl.include("permissions.users_without_read_perm")
+        new_dsl.include("permissions.users_with_no_access")
+        new_dsl.include("permissions.users_without_delete_perm")
+        new_dsl.include("provisional_resource")
+        new_dsl.include("displayname")
+        new_dsl.include("displaydescription")
+        new_dsl.include("map_popup")
+        new_results = new_dsl.search(index=RESOURCES_INDEX, limit=10000, scroll="1m")
+        new_scroll_id = new_results["_scroll_id"]
+        scroll_size = new_results["hits"]["total"]["value"]
+
+        while scroll_size > 0:
+            page = new_dsl.se.es.scroll(scroll_id=new_scroll_id, scroll="3m")
+            scroll_size = len(page["hits"]["hits"])
+            new_results["hits"]["hits"] += page["hits"]["hits"]
+        
+        # print("__printing related hits__")
+        for hit in new_results["hits"]["hits"]:
+            hit["related"] = True
+            # pprint(hit)
+        
+        results["hits"]["hits"] += new_results["hits"]["hits"]
+        results["hits"]["total"]["value"] = len(results["hits"]["hits"])
+
+    def view_data(self):
+        return {"resources": get_resource_types_by_perm(self.request.user, "read_nodegroup")}
