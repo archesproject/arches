cd11e0f300d640d5723851ea7ccf8ff9d2127906
Galen <whatisgalen@gmail.com>
2024-04-18 22:13:50 -0700

    commits migration for 2 new core search filters, re #10804

---
+++ arches/app/models/migrations/10804_core_search_filters.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+from django.db import migrations, models
+import django.db.models.deletion
+import uuid
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("models", "10709_refresh_geos_by_transaction"),
+    ]
+
+    sql = """
+        update search_component where sortorder = 0 set sortorder = 1;
+        insert into search_component (
+            searchcomponentid,
+            name,
+            icon,
+            modulename,
+            classname,
+            type,
+            componentpath,
+            componentname,
+            sortorder,
+            enabled
+        ) values (
+            '69695d63-6f03-4536-8da9-841b07116381',
+            'Core Search',
+            '',
+            'core_search.py.py',
+            'CoreSearchFilter',
+            '',
+            '',
+            'core-search',
+            0,
+            true
+        ),
+        (
+            'ada062d9-092d-400c-bcf7-94a931d1f271',
+            'Localize Result Descriptors',
+            '',
+            'localize_result_descriptors.py',
+            'LocalizeResultDescriptors',
+            '',
+            '',
+            'localize-descriptors',
+            99,
+            true
+        );
+    """
+    reverse_sql = """
+        delete from search_component where searchcomponentid in (
+            '69695d63-6f03-4536-8da9-841b07116381',
+            'ada062d9-092d-400c-bcf7-94a931d1f271'
+        );
+    """
+
+    operations = [
+        migrations.RunSQL(
+            sql,
+            reverse_sql,
+        )
+    ]
