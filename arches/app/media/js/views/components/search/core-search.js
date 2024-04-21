4c4acc9e832b7490043226dfb6c87a4aa5ff92d6
Galen <whatisgalen@gmail.com>
2024-04-20 19:21:40 -0700

    imports deps and params as needed in core-search vm, re #10804

--- arches/app/media/js/views/components/search/core-search.js
+++ arches/app/media/js/views/components/search/core-search.js
@@ -1,11 +1,23 @@
 define([
     'jquery',
+    'underscore',
     'knockout',
+    'arches',
+    'viewmodels/alert',
     'templates/views/components/search/core-search.htm',
-], function($, ko, coreSearchTemplate) {
+], function($, _, ko, arches, AlertViewModel, coreSearchTemplate) {
     const componentName = 'core-search';
     const viewModel = function(params) {
+        const self = this;
         this.query = params.query;
+        this.queryString = params.queryString;
+        this.updateRequest = params.updateRequest;
+        this.searchResults = params.searchResults;
+        this.userIsReviewer = params.userIsReviewer;
+        this.total = params.total;
+        this.userid = params.userid;
+        this.hits = params.hits;
+        this.alert = params.alert;
         let queryObj = this.query();
         queryObj[componentName] = true;
         this.query(queryObj);
