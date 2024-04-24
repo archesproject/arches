11d1b5cd9189ff0983517b5b0a43843f19ba1152
Galen <whatisgalen@gmail.com>
2024-04-23 19:19:02 -0700

    includes geometries on feature props in getPopupData, re #10816

--- arches/app/media/js/viewmodels/map.js
+++ arches/app/media/js/viewmodels/map.js
@@ -344,7 +344,7 @@ define([
                 var data = feature.properties;
                 var id = data.resourceinstanceid;
                 data.showEditButton = self.canEdit;
-                const descriptionProperties = ['displayname', 'graph_name', 'map_popup'];
+                const descriptionProperties = ['displayname', 'graph_name', 'map_popup', 'geometries'];
                 if (id) {
                     if (!self.resourceLookup[id]){
                         data = _.defaults(data, {
@@ -352,6 +352,7 @@ define([
                             'displayname': '',
                             'graph_name': '',
                             'map_popup': '',
+                            'geometries': [],
                             'feature': feature,
                         });
                         if (data.permissions) {
