d681b046eb4df6cfd6c3a7593444ade2cbcb6bfb
Christopher Byrd <cbyrd@fargeo.com>
2024-05-02 14:12:14 -0700

    update merge order #9613

--- arches/app/models/migrations/9053_add_future_graphs.py
+++ arches/app/models/migrations/9053_add_future_graphs.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("models", "9191_string_nonlocalized"),
+        ("models", "10709_refresh_geos_by_transaction"),
     ]
 
     operations = [
