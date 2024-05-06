1e1debe71a939b33f56ef589b0393c6c4314b0f5
Phil Weir <phil.weir@flaxandteal.co.uk>
2024-05-06 13:49:21 +0100

    fix: put user migration at end again

--- arches/app/models/migrations/9604_relational_data_model_handle_user_accounts.py
+++ arches/app/models/migrations/9604_relational_data_model_handle_user_accounts.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("models", "10121_workflowhistory"),
+        ("models", "10709_refresh_geos_by_transaction"),
     ]
 
     operations = [
