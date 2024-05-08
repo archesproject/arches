834b17fd8d9def53060de0c43131df671426b072
Jacob Walls <jwalls@fargeo.com>
2024-05-08 14:12:15 -0400

    Revert "Make language code lookups case-insensitive #10121"
    This reverts commit 503cf81af2bed3c7e582b0f85b21087ba5f9c9dd.

--- arches/app/models/models.py
+++ arches/app/models/models.py
@@ -31,7 +31,6 @@ from django.core.serializers.json import DjangoJSONEncoder
 from django.template.loader import get_template, render_to_string
 from django.core.validators import RegexValidator
 from django.db.models import Q, Max
-from django.db.models.functions import Lower
 from django.db.models.signals import post_delete, pre_save, post_save
 from django.dispatch import receiver
 from django.utils import translation
@@ -591,9 +590,6 @@ class Language(models.Model):
     class Meta:
         managed = True
         db_table = "languages"
-        constraints = [
-            models.UniqueConstraint(Lower("code"), name="case_insensitive_code"),
-        ]
 
 
 class NodeGroup(models.Model):
