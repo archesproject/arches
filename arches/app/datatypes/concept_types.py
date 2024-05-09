d79f1b90530eed812a0e46eafa69b6bff5252311
Galen <whatisgalen@gmail.com>
2024-05-08 18:00:15 -0700

    correct typo of kwarg in baseConceptDatatype, re #10897

--- arches/app/datatypes/concept_types.py
+++ arches/app/datatypes/concept_types.py
@@ -27,8 +27,8 @@ cidoc_nm = Namespace("http://www.cidoc-crm.org/cidoc-crm/")
 logger = logging.getLogger(__name__)
 
 class BaseConceptDataType(BaseDataType):
-    def __init__(self, model=None, datatype_instance=None):
-        super(BaseConceptDataType, self).__init__(model=model, datatype_instance=datatype_instance)
+    def __init__(self, model=None, datatype_factory=None):
+        super(BaseConceptDataType, self).__init__(model=model, datatype_factory=datatype_factory)
         self.value_lookup = {}
         self.collection_lookup = {}
         self.collection_by_node_lookup = {}
