a1a3ad4a43aa173c944603e3d5d259b1ed1dad20
Galen <whatisgalen@gmail.com>
2024-05-08 18:15:39 -0700

    includes datatypefactory kwarg in datatype-specific __init__ overrides, re #10897

--- arches/app/datatypes/datatypes.py
+++ arches/app/datatypes/datatypes.py
@@ -1552,8 +1552,8 @@ class GeojsonFeatureCollectionDataType(BaseDataType):
 
 
 class FileListDataType(BaseDataType):
-    def __init__(self, model=None):
-        super(FileListDataType, self).__init__(model=model)
+    def __init__(self, model=None, datatype_factory=None):
+        super(FileListDataType, self).__init__(model=model, datatype_factory=datatype_factory)
         self.node_lookup = {}
 
     def validate_file_types(self, request=None, nodeid=None):
@@ -1948,8 +1948,8 @@ class FileListDataType(BaseDataType):
 
 
 class BaseDomainDataType(BaseDataType):
-    def __init__(self, model=None):
-        super(BaseDomainDataType, self).__init__(model=model)
+    def __init__(self, model=None, datatype_factory=None):
+        super(BaseDomainDataType, self).__init__(model=model, datatype_factory=datatype_factory)
         self.value_lookup = {}
 
     def get_option_text(self, node, option_id):
