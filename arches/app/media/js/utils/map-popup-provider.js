dd61537de15f64cac1013e901325bad54c6ca41f
Galen <whatisgalen@gmail.com>
2024-05-13 14:33:10 -0700

    commits override of map-popup-provider with changes from #10816

--- arches/app/media/js/utils/map-popup-provider.js
+++ arches/app/media/js/utils/map-popup-provider.js
@@ -43,14 +43,27 @@ define(['arches',
         /**
          * This method enables custom logic for how the feature in the popup should be handled and/or mutated en route to the mapFilter.
          * @param popupFeatureObject - the javascript object of the feature and its associated contexts (e.g. mapCard).
+         * @param featureGeomIndex - int index corresponding to the feature that was clicked, used for lookup against an arry of geoms
          * @required @method mapCard.filterByFeatureGeom()
-         * @required send argument: @param feature - a geojson feature object
-         * @optional send argument: @param resourceid
+         * @required @send argument: @param feature - a geojson feature object 
+         * with an @optional @property: feature.id corresponding to a uuid for that tile_geometrycollection_feature
+         * @optional @send argument: @param resourceid
          */
-        sendFeaturetoMapFilter: function(popupFeatureObject)
+        sendFeatureToMapFilter: function(popupFeatureObject, featureGeomIndex)
         {
-            const feature = popupFeatureObject.geometries()[0].geom.features[0];
+            let feature = popupFeatureObject.geometries()[featureGeomIndex].geom.features[0];
             popupFeatureObject.mapCard.filterByFeatureGeom(feature, popupFeatureObject.resourceinstanceid);
+        },
+
+        /**
+         * Determines whether to show the button for Filter By Feature
+         * @param popupFeatureObject - the javascript object of the feature and its associated contexts (e.g. mapCard).
+         * @param featureGeomIndex - int index corresponding to the feature that was clicked, used for lookup against an arry of geoms
+         * @returns {boolean} - whether to show "Filter by Feature" on map popup
+         * typically dependent on at least 1 feature with a geometry and/or a featureid/resourceid combo
+         */
+        showFilterByFeature: function(popupFeatureObject, featureGeomIndex) {
+            return (ko.unwrap(popupFeatureObject.geometries) || []).length > 0;
         },
 
     };
