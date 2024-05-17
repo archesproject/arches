1343b384cded756b7256076d0c281809f54adc9d
Ryan Anderson <randerson@fargeo.com>
2024-05-17 13:04:32 -0700

    Cleanup editlog query #10866

--- arches/app/models/resource.py
+++ arches/app/models/resource.py
@@ -505,7 +505,7 @@ class Resource(models.ResourceInstance):
                 tiles = list(models.TileModel.objects.filter(resourceinstance=self))
                 resource_is_provisional = True if sum([len(t.data) for t in tiles]) == 0 or [any(t.data.values()) for t in tiles][0] == False else False
                 if resource_is_provisional is True:
-                    creator_id = int(EditLog.objects.filter(resourceinstanceid=self.pk, edittype='create').values('userid').first()['userid'])
+                    creator_id = int(EditLog.objects.filter(resourceinstanceid=self.pk, edittype='create').values_list('userid')[0][0])
                     if creator_id == user.id:
                         permit_deletion = True
             else:
