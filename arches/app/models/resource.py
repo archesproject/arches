b03f3fb4fa6ef79815fdb7c981a4e89717f9202b
Ryan Anderson <randerson@fargeo.com>
2024-05-17 12:58:05 -0700

    Allow provisional editor to delete a resource they created, #10866

--- arches/app/models/resource.py
+++ arches/app/models/resource.py
@@ -503,9 +503,11 @@ class Resource(models.ResourceInstance):
             user_is_reviewer = user_is_resource_reviewer(user)
             if user_is_reviewer is False:
                 tiles = list(models.TileModel.objects.filter(resourceinstance=self))
-                resource_is_provisional = True if sum([len(t.data) for t in tiles]) == 0 else False
+                resource_is_provisional = True if sum([len(t.data) for t in tiles]) == 0 or [any(t.data.values()) for t in tiles][0] == False else False
                 if resource_is_provisional is True:
-                    permit_deletion = True
+                    creator_id = int(EditLog.objects.filter(resourceinstanceid=self.pk, edittype='create').values('userid').first()['userid'])
+                    if creator_id == user.id:
+                        permit_deletion = True
             else:
                 permit_deletion = True
         else:
