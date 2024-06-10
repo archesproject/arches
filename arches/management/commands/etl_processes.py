9c2a4fb3917cd5cd98571e6d280694d8580ffb73
Johnathan Clementi <johnathan.clementi1@gmail.com>
2024-06-10 16:38:04 -0400

    Handle collections not in db before pg func is called #10883

--- arches/management/commands/etl_processes.py
+++ arches/management/commands/etl_processes.py
@@ -1,3 +1,4 @@
+from arches.app.models.models import Concept, Value
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
@@ -19,14 +20,14 @@ class Command(BaseCommand):
         parser.add_argument(
             "-co",
             "--collections",
-            dest="lists_to_migrate",
+            dest="collections_to_migrate",
             nargs="*",
-            default=[],
+            required=True,
             help="One or more collections to migrate to controlled lists"
         )
 
         parser.add_argument(
-            "-preferred_sort_language",
+            "-psl",
             "--preferred_sort_language",
             dest="preferred_sort_language",
             default="en",
@@ -36,29 +37,46 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if options["operation"] == "migrate_collections_to_controlled_lists":
             self.migrate_collections_to_controlled_lists(
-                lists_to_migrate=options["lists_to_migrate"],
+                collections_to_migrate=options["collections_to_migrate"],
                 preferred_sort_language=options["preferred_sort_language"]
             )
     
-    def migrate_collections_to_controlled_lists(self, lists_to_migrate, preferred_sort_language):
+    def migrate_collections_to_controlled_lists(self, collections_to_migrate, preferred_sort_language):
         """
         Uses a postgres function to migrate collections to controlled lists
 
+        Example usage: 
+            python manage.py etl_processes 
+                -o migrate_collections_to_controlled_lists 
+                -co 'Johns list' 'Getty AAT'
+                -psl 'en'
         """
 
-        from django.db import connection
-        with connection.cursor() as cursor:
-            try:
-                cursor.execute(
-                    """
-                    select * from __arches_migrate_collections_to_clm(
-                        ARRAY[%s], %s
-                    );
-                    """,
-                    [lists_to_migrate, preferred_sort_language]
-                )
-                result = cursor.fetchone()
-                if result:
-                    self.stdout.write(result[0])
-            except Exception as e:
-                self.stdout.write(str(e))
+        collections_in_db = list(Value.objects.filter(
+            value__in=collections_to_migrate,
+            valuetype__in=['prefLabel', 'identifier'],
+            concept__nodetype='Collection'
+        ).values_list('value', flat=True))
+
+        failed_collections = [
+            collection for collection in collections_to_migrate if collection not in collections_in_db
+        ]
+
+        if len(failed_collections) > 0:
+            self.stdout.write(
+                'Failed to find the following collections in the database: %s' % ', '.join(failed_collections)
+            )
+
+        if len(collections_in_db) > 0:
+            from django.db import connection
+            cursor = connection.cursor()
+            cursor.execute(
+                """
+                select * from __arches_migrate_collections_to_clm(
+                    ARRAY[%s], %s
+                );
+                """,
+                [collections_in_db, preferred_sort_language]
+            )
+            result = cursor.fetchone()
+            self.stdout.write(result[0])
