90609c55e0658ebc05bb5aa4a1404b1b208b4577
Galen <whatisgalen@gmail.com>
2024-06-22 14:24:00 -0700

    make core-search extend BaseFilter, re #10804

--- arches/app/media/js/views/components/search/core-search.js
+++ arches/app/media/js/views/components/search/core-search.js
@@ -4,90 +4,107 @@ define([
     'knockout',
     'arches',
     'viewmodels/alert',
+    'views/components/search/base-filter',
     'templates/views/components/search/core-search.htm',
-], function($, _, ko, arches, AlertViewModel, coreSearchTemplate) {
+], function($, _, ko, arches, AlertViewModel, BaseFilter, coreSearchTemplate) {
     const componentName = 'core-search';
-    const viewModel = function(params) {
-        const self = this;
-        this.query = params.query;
-        this.queryString = params.queryString;
-        this.updateRequest = params.updateRequest;
-        this.searchResults = params.searchResults;
-        this.userIsReviewer = params.userIsReviewer;
-        this.total = params.total;
-        this.userid = params.userid;
-        this.hits = params.hits;
-        this.alert = params.alert;
-        this.sharedStateObject = params;
-        let localQueryObj = this.query();
-        localQueryObj[componentName] = true;
-        this.query(localQueryObj);
-        this.queryString.subscribe(function() {
-            this.doQuery();
-        }, this);
-        this.selectedPopup = ko.observable('');
-        this.sharedStateObject.selectedPopup = this.selectedPopup;
-        var firstEnabledFilter = _.find(this.sharedStateObject.filtersList, function(filter) {
-            return filter.type === 'filter' && filter.enabled === true;
-        }, this);
-        this.selectedTab = ko.observable(firstEnabledFilter.componentname);
-        this.sharedStateObject.selectedTab = this.selectedTab;
+    const viewModel = BaseFilter.extend({ 
+        initialize: function(sharedStateObject) {
+        
+            let self = this;
 
-        this.filterApplied = ko.pureComputed(function(){
-            var self = this;
-            var filterNames = Object.keys(this.sharedStateObject.filters);
-            return filterNames.some(function(filterName){
-                if (ko.unwrap(self.sharedStateObject.filters[filterName]) && filterName !== 'paging-filter') {
-                    return !!ko.unwrap(self.sharedStateObject.filters[filterName]).query()[filterName];
-                } else {
-                    return false;
+            sharedStateObject.componentName = componentName;
+            BaseFilter.prototype.initialize.call(this, sharedStateObject);
+            console.log(this.requiredFiltersLoaded());
+            console.log(ko.unwrap(this.filters));
+            console.log(ko.unwrap(sharedStateObject.filters));
+
+            this.query = sharedStateObject.query;
+            this.queryString = sharedStateObject.queryString;
+            this.updateRequest = sharedStateObject.updateRequest;
+            // this.searchResults = sharedStateObject.searchResults;
+            this.userIsReviewer = sharedStateObject.userIsReviewer;
+            this.total = sharedStateObject.total;
+            this.userid = sharedStateObject.userid;
+            this.hits = sharedStateObject.hits;
+            this.alert = sharedStateObject.alert;
+            this.sharedStateObject = sharedStateObject;
+            // console.log(sharedStateObject.getRequiredFilters(componentName));
+            this.queryString.subscribe(function() {
+                this.doQuery();
+            }, this);
+            this.selectedPopup = ko.observable('');
+            this.sharedStateObject.selectedPopup = this.selectedPopup;
+            var firstEnabledFilter = _.find(this.sharedStateObject.filtersList, function(filter) {
+                return filter.type === 'filter';
+            }, this);
+            this.selectedTab = ko.observable(firstEnabledFilter.componentname);
+            this.sharedStateObject.selectedTab = this.selectedTab;
+
+            this.filterApplied = ko.pureComputed(function(){
+                var filterNames = Object.keys(sharedStateObject.filters);
+                return filterNames.some(function(filterName){
+                    if (ko.unwrap(sharedStateObject.filters[filterName]) && filterName !== 'paging-filter') {
+                        return !!ko.unwrap(sharedStateObject.filters[filterName]).query()[filterName];
+                    } else {
+                        return false;
+                    }
+                });
+            }, this);
+            this.resultsExpanded = ko.observable(true);
+            this.isResourceRelatable = function(graphId) {
+                var relatable = false;
+                if (this.graph) {
+                    relatable = _.contains(this.graph.relatable_resource_model_ids, graphId);
                 }
-            });
-        }, this);
-        this.resultsExpanded = ko.observable(true);
-        this.isResourceRelatable = function(graphId) {
-            var relatable = false;
-            if (this.graph) {
-                relatable = _.contains(this.graph.relatable_resource_model_ids, graphId);
-            }
-            return relatable;
-        };
-        this.sharedStateObject.isResourceRelatable = this.isResourceRelatable;
-        this.toggleRelationshipCandidacy = function() {
-            var self = this;
-            return function(resourceinstanceid){
-                var candidate = _.contains(self.relationshipCandidates(), resourceinstanceid);
-                if (candidate) {
-                    self.relationshipCandidates.remove(resourceinstanceid);
+                return relatable;
+            };
+            this.sharedStateObject.isResourceRelatable = this.isResourceRelatable;
+            this.toggleRelationshipCandidacy = function() {
+                return function(resourceinstanceid){
+                    var candidate = _.contains(sharedStateObject.relationshipCandidates(), resourceinstanceid);
+                    if (candidate) {
+                        sharedStateObject.relationshipCandidates.remove(resourceinstanceid);
+                    } else {
+                        sharedStateObject.relationshipCandidates.push(resourceinstanceid);
+                    }
+                };
+            };
+            this.sharedStateObject.toggleRelationshipCandidacy = this.toggleRelationshipCandidacy;
+
+            this.clearQuery = function(){
+                Object.values(this.sharedStateObject.filters).forEach(function(value){
+                    if (value()){
+                        if (value().clear){
+                            value().clear();
+                        }
+                    }
+                }, this);
+                this.query({"paging-filter": "1", "core-search": true, tiles: "true"});
+            };
+
+            this.selectPopup = function(componentname) {
+                if(this.selectedPopup() !== '' && componentname === this.selectedPopup()) {
+                    this.selectedPopup('');
                 } else {
-                    self.relationshipCandidates.push(resourceinstanceid);
+                    this.selectedPopup(componentname);
                 }
             };
-        };
-        this.sharedStateObject.toggleRelationshipCandidacy = this.toggleRelationshipCandidacy;
-
-        this.clearQuery = function(){
-            Object.values(this.sharedStateObject.filters).forEach(function(value){
-                if (value()){
-                    if (value().clear){
-                        value().clear();
-                    }
-                }
-            }, this);
-            this.query({"paging-filter": "1", "core-search": true, tiles: "true"});
-        };
-
-        this.selectPopup = function(componentname) {
-            if(this.selectedPopup() !== '' && componentname === this.selectedPopup()) {
-                this.selectedPopup('');
+            if (this.requiredFiltersLoaded() === false) {
+                this.requiredFiltersLoaded.subscribe(function() {
+                    console.log("core-search: OK!");
+                    this.restoreState();
+                }, this);
             } else {
-                this.selectedPopup(componentname);
+                this.restoreState();
             }
-        };
+        },
 
-        this.doQuery = function() {
+        doQuery: function() {
             let queryObj = JSON.parse(this.queryString());
             queryObj[componentName] = true;
+            if (!queryObj["search-results"])
+                queryObj["search-results"] = "";
             if (self.updateRequest) { self.updateRequest.abort(); }
             self.updateRequest = $.ajax({
                 type: "GET",
@@ -95,48 +112,54 @@ define([
                 data: queryObj,
                 context: this,
                 success: function(response) {
-                    _.each(self.searchResults, function(value, key, results) {
+                    _.each(this.sharedStateObject.searchResults, function(value, key, results) {
                         if (key !== 'timestamp') {
-                            delete self.searchResults[key];
+                            delete this.sharedStateObject.searchResults[key];
                         }
                     }, this);
                     _.each(response, function(value, key, response) {
                         if (key !== 'timestamp') {
-                            self.searchResults[key] = value;
+                            this.sharedStateObject.searchResults[key] = value;
                         }
                     }, this);
-                    self.searchResults.timestamp(response.timestamp);
-                    self.userIsReviewer(response.reviewer);
-                    self.userid(response.userid);
-                    self.total(response.total_results);
-                    self.hits(response.results.hits.hits.length);
-                    self.alert(false);
+                    this.sharedStateObject.searchResults.timestamp(response.timestamp);
+                    this.sharedStateObject.userIsReviewer(response.reviewer);
+                    this.sharedStateObject.userid(response.userid);
+                    this.sharedStateObject.total(response.total_results);
+                    this.sharedStateObject.hits(response.results.hits.hits.length);
+                    this.sharedStateObject.alert(false);
                 },
                 error: function(response, status, error) {
                     const alert = new AlertViewModel('ep-alert-red', arches.translations.requestFailed.title, response.responseJSON?.message);
                     if(self.updateRequest.statusText !== 'abort'){
                         self.alert(alert);
                     }
+                    this.sharedStateObject.loading(false);
                 },
                 complete: function(request, status) {
                     self.updateRequest = undefined;
                     window.history.pushState({}, '', '?' + $.param(queryObj).split('+').join('%20'));
+                    this.sharedStateObject.loading(false);
                 }
             });
         },
 
-        updateQuery = function() {
+        updateQuery: function() {
             let queryObj = this.query();
             queryObj[componentName] = true;
             this.query(queryObj);
         },
 
-        restoreState = function(){
+        restoreState: function(){
             if (!this.query()[componentName]) {
                 this.updateQuery();
+            } else {
+                this.sharedStateObject.loading(false);
             }
-        }
-    };
+        },
+
+        clear: function(){ return; }
+    });
 
     return ko.components.register(componentName, {
         viewModel: viewModel,
