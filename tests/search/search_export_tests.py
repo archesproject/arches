dbe24a7b7283a7dfede3a5d7441fd465d8ff74d4
Galen <whatisgalen@gmail.com>
2024-06-25 14:55:38 -0600

    commit test for export as system vals, export as display vals, re #10787

--- tests/search/search_export_tests.py
+++ tests/search/search_export_tests.py
@@ -1,6 +1,8 @@
 import time
 import uuid
 import os
+import csv
+import io
 from base64 import b64encode
 from http import HTTPStatus
 from arches.app.models import models
@@ -10,6 +12,7 @@ from arches.app.utils.betterJSONSerializer import JSONDeserializer
 from arches.app.utils.data_management.resource_graphs.importer import (
     import_graph as ResourceGraphImporter,
 )
+from arches.app.utils.skos import SKOSReader
 from arches.app.utils.i18n import LanguageSynchronizer
 
 from django.contrib.auth.models import User
@@ -27,6 +30,8 @@ class SearchExportTests(ArchesTestCase):
     @classmethod
     def setUpTestData(cls):
         cls.factory = RequestFactory()
+        # cls.client = Client()
+        # cls.client.login(username="admin", password="admin")
 
         LanguageSynchronizer.synchronize_settings_with_db()
         with open(
@@ -36,7 +41,9 @@ class SearchExportTests(ArchesTestCase):
         ResourceGraphImporter(archesfile["graph"])
 
         cls.search_model_graphid = "d291a445-fa5f-11e6-afa8-14109fd34195"
-        # cls.search_model_cultural_period_nodeid = "7a182580-fa60-11e6-96d1-14109fd34195"
+        cls.search_model_cultural_period_nodeid = "7a182580-fa60-11e6-96d1-14109fd34195"
+        cls.search_model_cultural_period_nodename = "Cultural Period Concept"
+        # cls.search_model_cultural_period_nodefieldname
         # cls.search_model_creation_date_nodeid = "1c1d05f5-fa60-11e6-887f-14109fd34195"
         # cls.search_model_destruction_date_nodeid = "e771b8a1-65fe-11e7-9163-14109fd34195"
         cls.search_model_name_nodeid = "2fe14de3-fa61-11e6-897b-14109fd34195"
@@ -47,24 +54,38 @@ class SearchExportTests(ArchesTestCase):
             "unprivileged_user", "unprivileged_user@test.com", "test"
         )
 
-        test_resourceinstanceid = uuid.uuid4()
+        cls.test_resourceinstanceid = uuid.uuid4()
 
         cls.loadOntology()
         cls.ensure_resource_test_model_loaded()
         models.ResourceInstance.objects.get_or_create(
             graph_id=cls.search_model_graphid,
-            resourceinstanceid=test_resourceinstanceid,
+            resourceinstanceid=cls.test_resourceinstanceid,
         )
         tile_data = {}
         tile_data[cls.search_model_name_nodeid] = {
             "en": {"value": "Etiwanda Avenue Street Trees", "direction": "ltr"}
         }
         new_tile = Tile(
-            resourceinstance_id=test_resourceinstanceid,
+            resourceinstance_id=cls.test_resourceinstanceid,
             data=tile_data,
             nodegroup_id=cls.search_model_name_nodeid,
         )
         new_tile.save()
+        skos = SKOSReader()
+        rdf = skos.read_file("tests/fixtures/data/concept_label_test_scheme.xml")
+        ret = skos.save_concepts_from_skos(rdf)
+
+        skos = SKOSReader()
+        rdf = skos.read_file("tests/fixtures/data/concept_label_test_collection.xml")
+        ret = skos.save_concepts_from_skos(rdf)
+        cls.valueid = "dadaeee5-57ef-409d-a6cf-98d332fdada8"
+        cultural_period_tile = Tile(
+            data={cls.search_model_cultural_period_nodeid: cls.valueid},
+            nodegroup_id=cls.search_model_cultural_period_nodeid,
+            resourceinstance_id=cls.test_resourceinstanceid,
+        )
+        cultural_period_tile.save()
         time.sleep(1)  # delay to allow for async indexing
 
         # TODO: create geospatial test data
@@ -115,6 +136,56 @@ class SearchExportTests(ArchesTestCase):
     #     exporter = SearchResultsExporter(search_request=request)
     #     result, _ = exporter.export(format='tilecsv', report_link='true')
     #     self.assertIn('Link', result[0]['outputfile'].getvalue())
+
+    def is_valid_uuid(self, value, version=4):
+        """Check if value is a valid UUID."""
+        try:
+            uuid_obj = uuid.UUID(value, version=version)
+            return str(uuid_obj) == value
+        except ValueError:
+            return False
+
+    def test_export_to_csv_with_system_values(self):
+        """Test exporting search results to CSV with system values included"""
+        request = self.factory.get(
+            "/search?tiles=True&export=True&format=tilecsv&exportsystemvalues=true"
+        )
+        request.user = self.user
+        exporter = SearchResultsExporter(search_request=request)
+        result, _ = exporter.export(format="tilecsv", report_link="false")
+        self.assertIn(".csv", result[0]["name"])
+        csv_content = result[0][
+            "outputfile"
+        ].getvalue()  # Adjust based on actual output handling
+        csv_reader = csv.DictReader(io.StringIO(csv_content))
+        cultural_period_column_name = (
+            self.search_model_cultural_period_nodename
+        )  # or the actual header name in your CSV
+        for row in csv_reader:
+            cultural_period_value = row[cultural_period_column_name]
+            self.assertTrue(
+                self.is_valid_uuid(cultural_period_value),
+                f"Expected UUID, got {cultural_period_value}",
+            )
+
+    def test_export_to_csv_without_system_values(self):
+        """Test exporting search results to CSV without system values"""
+        request = self.factory.get(
+            "/search?tiles=True&export=True&format=tilecsv&exportsystemvalues=false"
+        )
+        request.user = self.user
+        exporter = SearchResultsExporter(search_request=request)
+        result, _ = exporter.export(format="tilecsv", report_link="false")
+        self.assertIn(".csv", result[0]["name"])
+        csv_content = result[0]["outputfile"].getvalue()
+        csv_reader = csv.DictReader(io.StringIO(csv_content))
+        cultural_period_column_name = self.search_model_cultural_period_nodename
+        for row in csv_reader:
+            cultural_period_value = row[cultural_period_column_name]
+            self.assertFalse(
+                self.is_valid_uuid(cultural_period_value),
+                f"Expected non-UUID, got {cultural_period_value}",
+            )
 
     def test_login_via_basic_auth_good(self):
         auth_string = "Basic " + b64encode(b"admin:admin").decode("utf-8")
