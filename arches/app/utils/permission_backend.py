2237de6406232e1049a4b0a8f9ac2308bee54d1a
Christopher Byrd <cbyrd@fargeo.com>
2024-06-26 14:36:44 -0700

    update permissions #11042

--- arches/app/utils/permission_backend.py
+++ arches/app/utils/permission_backend.py
@@ -522,6 +522,18 @@ def user_can_edit_resource(user, resourceid=None):
                 user, resourceid, "change_resourceinstance"
             )
             if result is not None:
+                resource_instance_lifecycle = ResourceInstanceLifecycle.objects.get(
+                    graph_id=result["resource"].graph_id
+                )
+                can_edit_resource_instance_in_current_lifecycle_state = (
+                    resource_instance_lifecycle.states[
+                        result["resource"].lifecycle_state
+                    ]["can_edit"]
+                )
+
+                if not can_edit_resource_instance_in_current_lifecycle_state:
+                    return False
+
                 if result["permitted"] == "unknown":
                     return user.groups.filter(
                         name__in=settings.RESOURCE_EDITOR_GROUPS
