0275293ff1af9543aaaa5d452e2c3a38bf4f4bd9
Andy Jones <andrew.jones@historicengland.org.uk>
2024-06-26 14:51:44 +0100

    updates migration dependency order #10704

--- arches/app/models/migrations/10516_rename_geometrynodeid_spatialview_geometrynode_et_al.py
+++ arches/app/models/migrations/10516_rename_geometrynodeid_spatialview_geometrynode_et_al.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('models', '10710_fix_whatisthis'),
+        ('models', '10798_jsonld_importer'),
     ]
 
     operations = [
