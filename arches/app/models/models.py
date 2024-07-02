9d615a25ddd309475162957af992cdeaab3e4fb8
Jacob Walls <jwalls@fargeo.com>
2024-07-02 08:50:27 -0400

    Prefer django.conf settings for THUMBNAIL_GENERATOR re #11009
    Prevents circular import error when executing settings.py as a module.

--- arches/app/models/models.py
+++ arches/app/models/models.py
@@ -20,6 +20,7 @@ import django.utils.timezone
 
 from arches.app.const import ExtensionType
 from arches.app.utils.module_importer import get_class_from_modulename
+from arches.app.utils.thumbnail_factory import ThumbnailGeneratorInstance
 from arches.app.models.fields.i18n import I18n_TextField, I18n_JSONField
 from arches.app.utils import import_class_from_string
 from django.contrib.gis.db import models
@@ -408,8 +409,6 @@ class File(models.Model):
         super(File, self).save(*args, **kwargs)
 
     def make_thumbnail(self, kwargs_from_save_call, force=False):
-        from arches.app.utils.thumbnail_factory import ThumbnailGeneratorInstance
-
         try:
             if ThumbnailGeneratorInstance and (force or self.thumbnail_data is None):
                 self.thumbnail_data = ThumbnailGeneratorInstance.get_thumbnail_data(
