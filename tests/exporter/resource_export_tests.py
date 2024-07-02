297d20f79e2ae0e48bd183bfaa54a844671e95da
Jacob Walls <jwalls@fargeo.com>
2024-07-02 14:59:11 -0400

    Fix faulty merge

--- tests/exporter/resource_export_tests.py
+++ tests/exporter/resource_export_tests.py
@@ -144,10 +144,5 @@ class BusinessDataExportTests(ArchesTestCase):
         ) as f:
             json_truth = deep_sort(json.load(f))
 
-        # removes generated graph_publication_id
-        for resource_data in json_export["business_data"]["resources"]:
-            if resource_data["resourceinstance"]["graph_publication_id"]:
-                del resource_data["resourceinstance"]["graph_publication_id"]
-
         self.maxDiff = None
         self.assertDictEqual(json_export, json_truth)
