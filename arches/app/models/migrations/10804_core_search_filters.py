09921db020ef398881f999c19706cebd75c31e0b
Galen <whatisgalen@gmail.com>
2024-07-05 15:59:26 -0700

    update core-search migration to ref to latest, re #10804

--- arches/app/models/migrations/10804_core_search_filters.py
+++ arches/app/models/migrations/10804_core_search_filters.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("models", "10798_jsonld_importer"),
+        ("models", "10999_update_principaluser"),
     ]
 
     sql = """
