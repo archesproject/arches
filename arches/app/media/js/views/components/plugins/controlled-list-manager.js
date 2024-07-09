da2b9742651060185b93e83e41a446ccb35ae7ce
Jacob Walls <jwalls@fargeo.com>
2024-07-09 10:44:10 -0400

    Move src/plugins under src/arches

--- arches/app/media/js/views/components/plugins/controlled-list-manager.js
+++ arches/app/media/js/views/components/plugins/controlled-list-manager.js
@@ -1,5 +1,5 @@
 import ko from 'knockout';
-import ControlledListManager from '@/plugins/ControlledListManager.vue';
+import ControlledListManager from '@/arches/plugins/ControlledListManager.vue';
 import createVueApp from 'utils/create-vue-application';
 import ControlledListManagerTemplate from 'templates/views/components/plugins/controlled-list-manager.htm';
 
@@ -8,7 +8,7 @@ ko.components.register('controlled-list-manager', {
     viewModel: function() {
         createVueApp(ControlledListManager).then((vueApp) => {
             vueApp.mount('#controlled-list-manager-mounting-point');
-        })
+        });
     },
     template: ControlledListManagerTemplate,
 });
