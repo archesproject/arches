c23463f1635cdfd634d543cad01cd4ead01de3de
Johnathan Clementi <johnathan.clementi1@gmail.com>
2024-07-22 18:38:09 -0400

    Avoid repeating word "controlled" in new models

--- arches_references/urls.py
+++ arches_references/urls.py
@@ -4,65 +4,61 @@ from django.conf.urls.i18n import i18n_patterns
 from django.urls import include, path
 
 from arches_references.views import (
-    ControlledListItemView,
-    ControlledListView,
-    ControlledListsView,
-    ControlledListItemImageView,
-    ControlledListItemImageMetadataView,
-    ControlledListItemValueView,
+    ListItemView,
+    ListView,
+    ListsView,
+    ListItemImageView,
+    ListItemImageMetadataView,
+    ListItemValueView,
 )
 
 urlpatterns = [
     path("", include("arches.urls")),
-    path(
-        "api/controlled_lists/", ControlledListsView.as_view(), name="controlled_lists"
-    ),
+    path("api/controlled_lists/", ListsView.as_view(), name="controlled_lists"),
     path(
         "api/controlled_list/<uuid:list_id>/",
-        ControlledListView.as_view(),
+        ListView.as_view(),
         name="controlled_list",
     ),
-    path(
-        "api/controlled_list/", ControlledListView.as_view(), name="controlled_list_add"
-    ),
+    path("api/controlled_list/", ListView.as_view(), name="controlled_list_add"),
     path(
         "api/controlled_list_item/<uuid:item_id>/",
-        ControlledListItemView.as_view(),
+        ListItemView.as_view(),
         name="controlled_list_item",
     ),
     path(
         "api/controlled_list_item/",
-        ControlledListItemView.as_view(),
+        ListItemView.as_view(),
         name="controlled_list_item_add",
     ),
     path(
         "api/controlled_list_item_value/<uuid:value_id>/",
-        ControlledListItemValueView.as_view(),
+        ListItemValueView.as_view(),
         name="controlled_list_item_value",
     ),
     path(
         "api/controlled_list_item_value/",
-        ControlledListItemValueView.as_view(),
+        ListItemValueView.as_view(),
         name="controlled_list_item_value_add",
     ),
     path(
         "api/controlled_list_item_image/<uuid:image_id>/",
-        ControlledListItemImageView.as_view(),
+        ListItemImageView.as_view(),
         name="controlled_list_item_image",
     ),
     path(
         "api/controlled_list_item_image/",
-        ControlledListItemImageView.as_view(),
+        ListItemImageView.as_view(),
         name="controlled_list_item_image_add",
     ),
     path(
         "api/controlled_list_item_image_metadata/<uuid:metadata_id>/",
-        ControlledListItemImageMetadataView.as_view(),
+        ListItemImageMetadataView.as_view(),
         name="controlled_list_item_image_metadata",
     ),
     path(
         "api/controlled_list_item_image_metadata/",
-        ControlledListItemImageMetadataView.as_view(),
+        ListItemImageMetadataView.as_view(),
         name="controlled_list_item_image_metadata_add",
     ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
