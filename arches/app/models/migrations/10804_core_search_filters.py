aef291ac2e64e92cbfc9013e4417ef84f63505e5
Galen <whatisgalen@gmail.com>
2024-07-23 14:55:13 -0700

    update to latest migration, re #10804

--- arches/app/models/migrations/10804_core_search_filters.py
+++ arches/app/models/migrations/10804_core_search_filters.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("models", "10999_update_principaluser"),
+        ("models", "10799_geojsongeometry_featureid"),
     ]
 
     sql = """
