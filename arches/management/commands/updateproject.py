41d9e8136174b0bc7c0296727531a5a6674f0352
Christopher Byrd <cbyrd@fargeo.com>
2024-07-23 11:50:12 -0700

    nit #11042

--- arches/management/commands/updateproject.py
+++ arches/management/commands/updateproject.py
@@ -1,9 +1,5 @@
 from django.core.management.base import BaseCommand
 
-from arches.app.search.mappings import (
-    prepare_search_index,
-)
-
 
 class Command(BaseCommand):  # pragma: no cover
     """
@@ -12,7 +8,9 @@ class Command(BaseCommand):  # pragma: no cover
     """
 
     def handle(self, *args, **options):
-        answer = input("This operation currently does nothing.\n" "Continue? ")
+        answer = input(
+            "This operation will upgrade your project to version 8.0\n" "Continue? "
+        )
 
         if answer.lower() in ["y", "yes"]:
             self.update_to_v8()
@@ -20,4 +18,4 @@ class Command(BaseCommand):  # pragma: no cover
             self.stdout.write("Operation aborted.")
 
     def update_to_v8(self):
-        prepare_search_index()
+        pass
