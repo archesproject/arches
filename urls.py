56fca3a7969e9e103115b6a8b929be0093ce4ad7
Jacob Walls <jwalls@fargeo.com>
2024-07-31 09:43:25 -0400

    Remove trailing slashes from URLs

--- urls.py
+++ urls.py
@@ -13,50 +13,50 @@ from arches_references.views import (
 )
 
 urlpatterns = [
-    path("api/controlled_lists/", ListsView.as_view(), name="controlled_lists"),
+    path("api/controlled_lists", ListsView.as_view(), name="controlled_lists"),
     path(
-        "api/controlled_list/<uuid:list_id>/",
+        "api/controlled_list/<uuid:list_id>",
         ListView.as_view(),
         name="controlled_list",
     ),
-    path("api/controlled_list/", ListView.as_view(), name="controlled_list_add"),
+    path("api/controlled_list", ListView.as_view(), name="controlled_list_add"),
     path(
-        "api/controlled_list_item/<uuid:item_id>/",
+        "api/controlled_list_item/<uuid:item_id>",
         ListItemView.as_view(),
         name="controlled_list_item",
     ),
     path(
-        "api/controlled_list_item/",
+        "api/controlled_list_item",
         ListItemView.as_view(),
         name="controlled_list_item_add",
     ),
     path(
-        "api/controlled_list_item_value/<uuid:value_id>/",
+        "api/controlled_list_item_value/<uuid:value_id>",
         ListItemValueView.as_view(),
         name="controlled_list_item_value",
     ),
     path(
-        "api/controlled_list_item_value/",
+        "api/controlled_list_item_value",
         ListItemValueView.as_view(),
         name="controlled_list_item_value_add",
     ),
     path(
-        "api/controlled_list_item_image/<uuid:image_id>/",
+        "api/controlled_list_item_image/<uuid:image_id>",
         ListItemImageView.as_view(),
         name="controlled_list_item_image",
     ),
     path(
-        "api/controlled_list_item_image/",
+        "api/controlled_list_item_image",
         ListItemImageView.as_view(),
         name="controlled_list_item_image_add",
     ),
     path(
-        "api/controlled_list_item_image_metadata/<uuid:metadata_id>/",
+        "api/controlled_list_item_image_metadata/<uuid:metadata_id>",
         ListItemImageMetadataView.as_view(),
         name="controlled_list_item_image_metadata",
     ),
     path(
-        "api/controlled_list_item_image_metadata/",
+        "api/controlled_list_item_image_metadata",
         ListItemImageMetadataView.as_view(),
         name="controlled_list_item_image_metadata_add",
     ),
