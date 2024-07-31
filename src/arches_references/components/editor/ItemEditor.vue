f1022ca835d8927a81d73ab2df1067cf9853fc3f
Christopher Byrd <cbyrd@fargeo.com>
2024-07-31 13:59:45 -0700

    fix pathing

---
+++ src/arches_references/components/editor/ItemEditor.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { inject, provide } from "vue";
+
+import ItemHeader from "@/arches_references/components/editor/ItemHeader.vue";
+import ItemImages from "@/arches_references/components/editor/ItemImages.vue";
+import ItemType from "@/arches_references/components/editor/ItemType.vue";
+import ItemURI from "@/arches_references/components/editor/ItemURI.vue";
+import ValueEditor from "@/arches_references/components/editor/ValueEditor.vue";
+
+import {
+    displayedRowKey,
+    itemKey,
+    ALT_LABEL,
+    NOTE,
+    PREF_LABEL,
+} from "@/arches_references/constants.ts";
+
+import type { DisplayedListItemRefAndSetter } from "@/arches_references/types";
+
+const { displayedRow: item } = inject(
+    displayedRowKey,
+) as DisplayedListItemRefAndSetter;
+provide(itemKey, item);
+</script>
+
+<template>
+    <template v-if="item">
+        <ItemHeader />
+        <ValueEditor :value-type="PREF_LABEL" />
+        <ValueEditor :value-type="ALT_LABEL" />
+        <ItemType />
+        <ValueEditor :value-category="NOTE" />
+        <ItemURI />
+        <ItemImages />
+    </template>
+</template>
