3412f196bccbd271b11e8386880f8b82770d7f08
Jacob Walls <jwalls@fargeo.com>
2024-08-02 14:59:15 -0400

    Reconcile apps.py and github actions with templates

--- apps.py
+++ apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
 
+from arches.settings_utils import generate_frontend_configuration
+
 
 class ArchesReferencesConfig(AppConfig):
     name = "arches_references"
+    verbose_name = "Arches References"
     is_arches_application = True
+
+    def ready(self):
+        generate_frontend_configuration()
