90a3ca1ae2fc18c1f879ec59b942e3daed831e30
Jacob Walls <jwalls@fargeo.com>
2024-08-09 13:33:56 -0400

    Update declarations.d.ts

--- src/arches_references/declarations.d.ts
+++ src/arches_references/declarations.d.ts
@@ -1,10 +1,4 @@
-// import declarations from other projects or Arches core
-import("../../node_modules/arches/arches/app/src/declarations.d.ts");
-
 // declare untyped modules that have been added to your project in `package.json`
 // Module homepage on npmjs.com uses logos "TS" or "DT" to indicate if typed
-declare module "arches";
 
-// declare filetypes used in `./src/` folder
-declare module "*.ts";
-declare module "*.vue";
+import("@/arches/declarations.d.ts");
