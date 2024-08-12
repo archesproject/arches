ab4563301beca71bcd9ef6cbd454d5b6c9a87e46
Jacob Walls <jwalls@fargeo.com>
2024-08-12 10:00:31 -0400

    Support more expressive plugin URLs #11320

--- arches/app/views/plugin.py
+++ arches/app/views/plugin.py
@@ -28,17 +28,14 @@ from arches.app.views.base import MapBaseManagerView
 class PluginView(MapBaseManagerView):
     action = None
 
-    def get(self, request, pluginid=None, slug=None):
+    def get(self, request, pluginid=None, slug=None, path=None):
         if slug is not None:
             plugin = models.Plugin.objects.get(slug=slug)
         else:
             plugin = models.Plugin.objects.get(pk=pluginid)
 
         if not request.user.has_perm("view_plugin", plugin):
-            if slug is not None:
-                return redirect("/auth?next=/plugins/{}".format(slug))
-            if slug is not None:
-                return redirect("/auth?next=/plugins/{}".format(pluginid))
+            return redirect("/auth/?next=" + request.path)
 
         if request.GET.get("json"):
             return JSONResponse(plugin)
