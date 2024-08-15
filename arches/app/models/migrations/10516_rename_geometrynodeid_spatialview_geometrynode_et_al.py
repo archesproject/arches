e3e70c818546e3a54922f9442e1f6550702d1b98
Andy Jones <andrew.jones@historicengland.org.uk>
2024-08-15 12:43:12 +0100

    update migration dependency #10704

--- arches/app/models/migrations/10516_rename_geometrynodeid_spatialview_geometrynode_et_al.py
+++ arches/app/models/migrations/10516_rename_geometrynodeid_spatialview_geometrynode_et_al.py
@@ -8,7 +8,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("models", "10999_update_principaluser"),
+        ("models", "10799_geojsongeometry_featureid"),
     ]
 
     operations = [
