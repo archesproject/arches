c8f621dcf54e18af0a4ed77083595a1388362f42
Jacob Walls <jwalls@fargeo.com>
2024-09-10 17:07:11 -0400

    Fix test hint

--- tests/utils/datatypes/geojson_datatype_tests.py
+++ tests/utils/datatypes/geojson_datatype_tests.py
@@ -15,7 +15,7 @@ from tests.base_test import ArchesTestCase
 from django.test import override_settings
 
 # these tests can be run from the command line via
-# python manage.py test tests.utils.geojson_datatype_tests --settings="tests.test_settings"
+# python manage.py test tests.utils.datatypes.geojson_datatype_tests --settings="tests.test_settings"
 
 
 class GeoJsonDataTypeTest(ArchesTestCase):
