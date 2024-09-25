61cb4ad20d7d8a8582d8e04f77e8a3350acc0f49
Jacob Walls <jwalls@fargeo.com>
2024-09-25 10:35:06 -0400

    Fix path to arches declarations file in project template
    Follow-up to 5770608.

--- arches/install/arches-templates/project_name/src/project_name/declarations.d.ts
+++ arches/install/arches-templates/project_name/src/project_name/declarations.d.ts
@@ -1,4 +1,4 @@
 // declare untyped modules that have been added to your project in `package.json`
 // Module homepage on npmjs.com uses logos "TS" or "DT" to indicate if typed
 
-import("@/arches/declarations.d.ts");
+import("@/arches/arches/declarations.d.ts");
