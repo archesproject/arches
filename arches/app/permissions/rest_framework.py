361aa33a2b0bde02c3ab6da5bf5ce1f13e9fa1a3
Jacob Walls <jwalls@fargeo.com>
2024-11-12 11:25:04 -0500

    Add RDMAdministrator permission class

---
+++ arches/app/permissions/rest_framework.py
@@ -0,0 +1,8 @@
+from rest_framework import permissions
+
+from arches.app.utils.permission_backend import group_required
+
+
+class RDMAdministrator(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return group_required(request.user, ["RDM Administrator"])
