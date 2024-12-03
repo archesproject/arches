bca5cc7a1119cff192c085a03bd7c885c6afd38e
Jacob Walls <jwalls@fargeo.com>
2024-12-03 15:10:02 -0500

    Linearize migrations

--- arches/app/models/migrations/11043_tile_nodegroup_add_related_names.py
+++ arches/app/models/migrations/11043_tile_nodegroup_add_related_names.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("models", "10437_node_alias_not_null"),
+        ("models", "11408_loadstaging_sortorder"),
     ]
 
     operations = [
