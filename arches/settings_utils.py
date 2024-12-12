63ff4c67190179fa277d9747b4f731b9343cbdc8
Christopher Byrd <chrabyrd@gmail.com>
2024-12-12 10:45:56 -0800

    fixes arches-level template overrides #11668

--- arches/settings_utils.py
+++ arches/settings_utils.py
@@ -96,12 +96,17 @@ def build_templates_config(
     """
     directories = []
     try:
+        # allows for manual additions to template overrides
         if additional_directories:
             for additional_directory in additional_directories:
                 directories.append(additional_directory)
 
+        # allows for application-level overrides of generic Django templates
         if app_root:
             directories.append(os.path.join(app_root, "templates"))
+
+        # forces Arches-level overrides of generic Django templates
+        directories.append(os.path.join(os.path.realpath(settings.ROOT_DIR), "app", "templates"))
 
         return [
             {
