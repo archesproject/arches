d98a88f35670bd4b451be8bb37e23308515ce05b
Johnathan Clementi <johnathan.clementi1@gmail.com>
2025-02-24 15:12:06 -0500

    Revert "Optional node aliase filter on controlled list endpoint #57"
    This reverts commit bb0c1f38ef7fa6397e546a33a9c16830d6d57bb8.

--- views.py
+++ views.py
@@ -63,14 +63,9 @@ def _prefetch_terms(request):
 class ListsView(APIBase):
     def get(self, request):
         """Returns either a flat representation (?flat=true) or a tree (default)."""
-        flat = str_to_bool(request.GET.get("flat", "false"))
-        permitted = get_nodegroups_by_perm(request.user, "read_nodegroup")
-        NOT_PROVIDED = object()
-        node_aliases = request.GET.getlist("node_alias", NOT_PROVIDED)
-        lists_query = (
+        lists = (
             List.objects.annotate_node_fields(
                 node_ids="pk",
-                node_alias="alias",
                 node_names="name",
                 nodegroup_ids="nodegroup_id",
                 graph_ids="graph_id",
@@ -79,12 +74,11 @@ class ListsView(APIBase):
             .order_by("name")
             .prefetch_related(*_prefetch_terms(request))
         )
-        if node_aliases is not NOT_PROVIDED:
-            lists_query = lists_query.filter(node_alias__overlap=node_aliases)
 
+        flat = str_to_bool(request.GET.get("flat", "false"))
+        permitted = get_nodegroups_by_perm(request.user, "read_nodegroup")
         serialized = [
-            obj.serialize(flat=flat, permitted_nodegroups=permitted)
-            for obj in lists_query
+            obj.serialize(flat=flat, permitted_nodegroups=permitted) for obj in lists
         ]
 
         return JSONResponse({"controlled_lists": serialized})
