2675193bb2d045cc7b493348f35a39f71045f7e8
Galen <whatisgalen@gmail.com>
2025-03-05 10:38:23 -0800

    create a test for permitted user search_terms, re #11826

--- tests/search/search_tests.py
+++ tests/search/search_tests.py
@@ -175,6 +175,45 @@ class SearchTests(ArchesTestCase):
             print("Failed to parse search result")
         self.assertTrue("terms" in result and len(result["terms"]) == 0)
 
+    def test_search_terms_permitted_user(self):
+        """
+        Test search terms method with a permitted user
+        """
+        admin_user = User.objects.get(username="admin")
+        nodeid = "c9b37b7c-17b3-11eb-a708-acde48001122"
+        tileid = "bebffbea-daf6-414e-80c2-530ec88d2705"
+        resourceinstanceid = "745f5e4a-d645-4c50-bafc-c677ea95f060"
+        resource = Resource(uuid.UUID(resourceinstanceid))
+        resource.graph_id = "c9b37a14-17b3-11eb-a708-acde48001122"
+
+        resource.save(user=admin_user, transaction_id=uuid.uuid4())
+        tile_data = {}
+        tile_data[nodeid] = {
+            "en": {"value": "Alpha Gamma Delta Eta Epsilon", "direction": "ltr"}
+        }
+        new_tile = Tile(
+            tileid=uuid.UUID(tileid),
+            resourceinstance_id=resourceinstanceid,
+            data=tile_data,
+            nodegroup_id=nodeid,
+        )
+        new_tile.save(index=False)
+        new_tile.index()
+        self.sync_es()
+        request = HttpRequest()
+        request.method = "GET"
+        request.GET.__setitem__("lang", "en")
+        request.GET.__setitem__("q", "Alpha Gamma Delta")
+        request.LANGUAGE_CODE = "en"
+        request.user = admin_user
+        response = search_terms(request)
+        result = {}
+        try:
+            result = json.loads(response.content)
+        except json.decoder.JSONDecodeError:
+            print("Failed to parse search result")
+        self.assertTrue("terms" in result and len(result["terms"]) == 1)
+
     def test_adv_search_on_non_null_geom_node(self):
         geojson_nodeid = "be25bdf0-c8bf-11ed-a172-0242ac130009"
         user = User.objects.get(username="admin")
