37a794379d36ea9a625cd971bd95531edfd4cf2c
Cyrus Hiatt <chiatt@fargeo.com>
2025-03-25 14:59:44 -0700

    Allow date_datatype.transform_value_for_tile to process datetime objects, re #11878

---
+++ tests/utils/datatypes/date_datatype_tests.py
@@ -0,0 +1,36 @@
+from arches.app.datatypes.datatypes import DataTypeFactory
+from tests.base_test import ArchesTestCase
+from datetime import datetime
+
+# these tests can be run from the command line via
+# python manage.py test tests.utils.datatypes.date_datatype_tests --settings="tests.test_settings"
+
+
+class DateDataTypeTests(ArchesTestCase):
+
+    sample_dates = [
+        "-2025",
+        "2025",
+        "2025-01",
+        "2025-01-01",
+        "2025-03-25 13:47:11+0100",
+        "2025-03-25T21:53:01.946977+0100",
+    ]
+
+    def test_string_validate(self):
+        datatype = DataTypeFactory().get_instance("date")
+        for date in self.sample_dates:
+            with self.subTest(input=date):
+                errors = datatype.validate(date)
+                self.assertEqual(len(errors), 0)
+
+    def test_tile_transform(self):
+        datatype = DataTypeFactory().get_instance("date")
+        self.sample_dates.append(
+            datetime.strptime("2025-01-01 00:00:00", "%Y-%m-%d %H:%M:%S")
+        )
+        for date in self.sample_dates:
+            with self.subTest(input=date):
+                tile_value = datatype.transform_value_for_tile(date)
+                date_value = datetime.strptime(tile_value, "%Y-%m-%dT%H:%M:%S.%f%z")
+                self.assertEqual(date_value.year, 2025)
