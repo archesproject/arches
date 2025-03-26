e4ad7ce7bebec8b7a3081b61944439b71d9c9db3
vouidaskis <mpampis.vouidaskis@gmail.com>
2025-03-26 13:07:04 +0200

    change to camelCase

--- arches/app/media/js/views/components/search/search-results.js
+++ arches/app/media/js/views/components/search/search-results.js
@@ -319,8 +319,8 @@ define([
                                     result._source.graph_id,
                                 ),
                                 point: point,
-                                report_url:arches.urls.resource_report+result._source.resourceinstanceid,
-                                edit_url:arches.urls.resource_editor+result._source.resourceinstanceid,
+                                reportUrl:arches.urls.resource_report+result._source.resourceinstanceid,
+                                editUrl:arches.urls.resource_editor+result._source.resourceinstanceid,
                                 mapLinkClicked: function () {
                                     self.selectedResourceId(
                                         result._source.resourceinstanceid,
