64fb6b800b0368de2d5819213e75c784d0c8b975
Jacob Walls <jwalls@fargeo.com>
2025-03-27 13:16:08 -0400

    [skip ci] fixup! Fetch

--- tests/views/resource_tests.py
+++ tests/views/resource_tests.py
@@ -20,7 +20,7 @@ from unittest.mock import MagicMock, patch
 from arches.app.views.resource import ResourcePermissionDataView
 from tests.base_test import ArchesTestCase
 from django.urls import reverse
-from arches.app.models.models import Concept, EditLog, ResourceInstance, Value
+from arches.app.models.models import EditLog, ResourceInstance
 from arches.app.models.resource import Resource
 from arches.app.models.tile import Tile
 from tests.utils.search_test_utils import sync_es
