348149453a5abebe9651f05a736369476441e0b0
Jacob Walls <jwalls@fargeo.com>
2025-03-31 10:20:05 -0400

    Avoid unidiomatic type checking in date transform_value_for_tile()

--- arches/app/datatypes/datatypes.py
+++ arches/app/datatypes/datatypes.py
@@ -740,11 +740,10 @@ class DateDataType(BaseDataType):
         if value is not None:
             if isinstance(value, list):
                 value = value[0]
-            elif (
-                type(value) == str and len(value) < 4 and value.startswith("-") is False
-            ):  # a year before 1000 but not BCE
-                value = value.zfill(4)
-            if type(value) == str:
+            if isinstance(value, str):
+                if len(value) < 4 and not value.startswith("-"):
+                    # a year before 1000 but not BCE
+                    value = value.zfill(4)
                 valid_date_format, valid = self.get_valid_date_format(value)
                 if valid:
                     value = datetime.strptime(value, valid_date_format)
