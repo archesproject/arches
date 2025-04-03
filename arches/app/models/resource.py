8780e697628d6040366125309bb6e065ac583a34
Jacob Walls <jwalls@fargeo.com>
2025-04-02 21:04:18 -0400

    Use pipelined GROUP BY re #11885

--- arches/app/models/resource.py
+++ arches/app/models/resource.py
@@ -1023,6 +1023,9 @@ class Resource(models.ResourceInstance):
                     )
                     .values("resourceinstanceidto")
                     .annotate(to_count=Count("resourceinstanceidto"))
+                    # ORDER BY is necessary for "pipelined" GROUP BY, see
+                    # https://use-the-index-luke.com/sql/sorting-grouping/indexed-group-by
+                    .order_by("resourceinstanceidto")
                 )
                 from_counts = (
                     models.ResourceXResource.objects.filter(
@@ -1030,6 +1033,7 @@ class Resource(models.ResourceInstance):
                     )
                     .values("resourceinstanceidfrom")
                     .annotate(from_count=Count("resourceinstanceidfrom"))
+                    .order_by("resourceinstanceidfrom")
                 )
 
                 total_relations_by_resource_id: dict[UUID:int] = defaultdict(int)
