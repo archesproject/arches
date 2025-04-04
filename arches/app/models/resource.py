e7309de5fec2d228afa671113f3fa60ad469c301
Alexei Peters <apeters@fargeo.com>
2025-04-03 17:31:35 -0700

    fix key error, re #11885

--- arches/app/models/resource.py
+++ arches/app/models/resource.py
@@ -1049,9 +1049,9 @@ class Resource(models.ResourceInstance):
                 for resource in related_resources["docs"]:
                     if resource["found"]:
                         if include_rr_count:
-                            resource["_source"]["total_relations"] = (
-                                total_relations_by_resource_id[UUID(resource["_id"])]
-                            )
+                            resource["_source"]["total_relations"] = {
+                                "value": total_relations_by_resource_id[UUID(resource["_id"])]
+                            }
                         for descriptor_type in ("displaydescription", "displayname"):
                             descriptor = get_localized_descriptor(
                                 resource, descriptor_type
