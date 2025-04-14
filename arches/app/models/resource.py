f4d13da9302817b3c2df7e35dd22c94652ee0ba8
Alexei Peters <apeters@fargeo.com>
2025-04-14 16:48:14 -0700

    Fix regression in fix for slow reports, re #11748 (#11959)

--- arches/app/models/resource.py
+++ arches/app/models/resource.py
@@ -1016,35 +1016,42 @@ class Resource(models.ResourceInstance):
                         )
 =======
 
+<<<<<<< HEAD
                 to_counts = (
                     models.ResourceXResource.objects.filter(
                         resourceinstanceidto__in=related_resource_ids
 >>>>>>> c5e3570d3f (Calculate totals without joins)
+=======
+                if include_rr_count:
+                    to_counts = (
+                        models.ResourceXResource.objects.filter(
+                            resourceinstanceidto__in=related_resource_ids
+                        )
+                        .values("resourceinstanceidto")
+                        .annotate(to_count=Count("resourceinstanceidto"))
+                        # ORDER BY NULLS LAST is necessary for "pipelined" GROUP BY, see
+                        # https://use-the-index-luke.com/sql/sorting-grouping/indexed-group-by
+                        .order_by(F("resourceinstanceidto").asc(nulls_last=True))
+>>>>>>> 6d928a9067 (Fix regression in fix for slow reports, re #11748 (#11959))
                     )
-                    .values("resourceinstanceidto")
-                    .annotate(to_count=Count("resourceinstanceidto"))
-                    # ORDER BY NULLS LAST is necessary for "pipelined" GROUP BY, see
-                    # https://use-the-index-luke.com/sql/sorting-grouping/indexed-group-by
-                    .order_by(F("resourceinstanceidto").asc(nulls_last=True))
-                )
-                from_counts = (
-                    models.ResourceXResource.objects.filter(
-                        resourceinstanceidfrom__in=related_resource_ids
+                    from_counts = (
+                        models.ResourceXResource.objects.filter(
+                            resourceinstanceidfrom__in=related_resource_ids
+                        )
+                        .values("resourceinstanceidfrom")
+                        .annotate(from_count=Count("resourceinstanceidfrom"))
+                        .order_by(F("resourceinstanceidfrom").asc(nulls_last=True))
                     )
-                    .values("resourceinstanceidfrom")
-                    .annotate(from_count=Count("resourceinstanceidfrom"))
-                    .order_by(F("resourceinstanceidfrom").asc(nulls_last=True))
-                )
 
-                total_relations_by_resource_id: dict[UUID:int] = defaultdict(int)
-                for related_resource_count in to_counts:
-                    total_relations_by_resource_id[
-                        related_resource_count["resourceinstanceidto"]
-                    ] += related_resource_count["to_count"]
-                for related_resource_count in from_counts:
-                    total_relations_by_resource_id[
-                        related_resource_count["resourceinstanceidfrom"]
-                    ] += related_resource_count["from_count"]
+                    total_relations_by_resource_id: dict[UUID:int] = defaultdict(int)
+                    for related_resource_count in to_counts:
+                        total_relations_by_resource_id[
+                            related_resource_count["resourceinstanceidto"]
+                        ] += related_resource_count["to_count"]
+                    for related_resource_count in from_counts:
+                        total_relations_by_resource_id[
+                            related_resource_count["resourceinstanceidfrom"]
+                        ] += related_resource_count["from_count"]
 
                 for resource in related_resources["docs"]:
                     if resource["found"]:
