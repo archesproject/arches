356eb32c57505452e89e68f4b1f08e658abf5522
Aaron Gundel <aaron.gundel@gmail.com>
2025-04-15 21:28:57 -0600

    adds multipart json parser

---
+++ utils/parsers/multipart_json_parser.py
@@ -0,0 +1,20 @@
+from rest_framework import parsers
+
+
+class MultiPartJSONParser(parsers.MultiPartParser):
+    def parse(self, stream, *args, **kwargs):
+        data = super().parse(stream, *args, **kwargs)
+
+        # Any 'File' found having application/json as type will be moved to data
+        mutable_data = data.data.copy()
+        unmarshaled_blob_names = []
+        json_parser = parsers.JSONParser()
+        for name, blob in data.files.items():
+            if blob.content_type == "application/json" and name not in data.data:
+                mutable_data[name] = json_parser.parse(blob)
+                unmarshaled_blob_names.append(name)
+        for name in unmarshaled_blob_names:
+            del data.files[name]
+        data.data = mutable_data
+
+        return data
