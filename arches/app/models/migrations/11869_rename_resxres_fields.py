72c9cd565c95c9a7251e4d6361c78853dc45cba3
Galen Mancino <whatisgalen@gmail.com>
2025-04-15 17:36:11 -0700

    Update arches/app/models/migrations/11869_rename_resxres_fields.py
    Co-authored-by: Cyrus Hiatt <chiatt@fargeo.com>

--- arches/app/models/migrations/11869_rename_resxres_fields.py
+++ arches/app/models/migrations/11869_rename_resxres_fields.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("models", "11842_userpreferences"),
+        ("models", "11579_ids_search_filter"),
     ]
 
     operations = [
