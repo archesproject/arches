ec994b09e5b575901be9fa0193047bab216f0bdf
Johnathan Clementi <johnatawnclementawn@users.noreply.github.com>
2025-04-17 11:30:08 -0400

    Run update project for latest from core v8.0 (#88)

--- apps.py
+++ apps.py
@@ -1,14 +1,8 @@
 from django.apps import AppConfig
 from django.conf import settings
 
-from arches.settings_utils import generate_frontend_configuration
-
 
 class ArchesControlledListsConfig(AppConfig):
     name = "arches_controlled_lists"
     verbose_name = "Arches Controlled Lists"
     is_arches_application = True
-
-    def ready(self):
-        if settings.APP_NAME.lower() == self.name:
-            generate_frontend_configuration()
