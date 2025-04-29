0940c9010674928922d4a87ac0828257d7446a50
Jacob Walls <jwalls@fargeo.com>
2025-04-29 09:32:39 +0100

    Use a callable for File.path upload destination #12008

--- arches/app/models/models.py
+++ arches/app/models/models.py
@@ -23,8 +23,7 @@ from arches.app.const import ExtensionType
 from arches.app.models.fields.i18n import I18n_TextField, I18n_JSONField
 from arches.app.models.mixins import SaveSupportsBlindOverwriteMixin
 from arches.app.models.query_expressions import UUID4
-from arches.app.models.utils import add_to_update_fields
-from arches.app.utils import import_class_from_string
+from arches.app.models.utils import add_to_update_fields, get_filename_generator_class
 from arches.app.utils.betterJSONSerializer import JSONSerializer
 from arches.app.utils.module_importer import get_class_from_modulename
 from arches.app.utils.thumbnail_factory import ThumbnailGeneratorInstance
@@ -368,9 +367,7 @@ class ResourceRevisionLog(SaveSupportsBlindOverwriteMixin, models.Model):
 
 class File(SaveSupportsBlindOverwriteMixin, models.Model):
     fileid = models.UUIDField(primary_key=True, default=uuid.uuid4, db_default=UUID4())
-    path = models.FileField(
-        upload_to=import_class_from_string(settings.FILENAME_GENERATOR)
-    )
+    path = models.FileField(upload_to=get_filename_generator_class)
     tile = models.ForeignKey(
         "TileModel", db_column="tileid", null=True, on_delete=models.CASCADE
     )
--- arches/app/models/utils.py
+++ arches/app/models/utils.py
@@ -1,3 +1,8 @@
+from django.conf import settings
+
+from arches.app.utils import import_class_from_string
+
+
 def add_to_update_fields(kwargs, field_name):
     """
     Update the `update_field` arg inside `kwargs` (if present) in-place
@@ -16,3 +21,7 @@ def add_to_update_fields(kwargs, field_name):
 
 def field_names(instance_or_class):
     return {f.name for f in instance_or_class._meta.fields}
+
+
+def get_filename_generator_class():
+    return import_class_from_string(settings.FILENAME_GENERATOR)
