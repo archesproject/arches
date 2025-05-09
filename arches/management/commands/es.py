748d1714df066bf910d9b1ff3fbd7be379b4c546
Andy Jones <andrew.jones@historicengland.org.uk>
2025-05-09 11:21:47 +0100

    nit black

--- arches/management/commands/es.py
+++ arches/management/commands/es.py
@@ -125,7 +125,7 @@ class Command(BaseCommand):
             action="store",
             dest="clear_index",
             default=True,
-            type=lambda x: str(x).lower() == 'true',
+            type=lambda x: str(x).lower() == "true",
             help="Set to True (default) to remove all the resources from the index before the reindexing operation. Set to False to overwrite in place the existing resources in the index.",
         )
 
