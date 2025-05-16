d421b782f907a776e7879f9e4020ff3b197e3b7d
Christopher Byrd <chrabyrd@gmail.com>
2025-05-16 08:18:19 -0700

    fixes reference select widget issues

--- media/js/views/components/widgets/reference-select.js
+++ media/js/views/components/widgets/reference-select.js
@@ -3,11 +3,7 @@ import ReferenceSelectViewModel from 'viewmodels/reference-select';
 import referenceSelectTemplate from 'templates/views/components/widgets/reference-select.htm';
 import 'bindings/select2-query';
 
-const viewModel = function(params) {
-    ReferenceSelectViewModel.apply(this, [params]);
-};
-
 export default ko.components.register('reference-select-widget', {
-    viewModel: viewModel,
+    viewModel: ReferenceSelectViewModel,
     template: referenceSelectTemplate,
 });
