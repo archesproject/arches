48166b2ee76fd11a2d9e4158f71bee118327a948
Rob Gaston <rgaston@fargeo.com>
2025-05-24 16:02:55 -0700

    makes index creation in test runner explicit

--- test/runner.py
+++ test/runner.py
@@ -1,19 +1,15 @@
 from arches.app.models.system_settings import settings
-from arches.app.search.base_index import get_index
 from arches.test.runner import ArchesTestRunner
+
+from arches_controlled_lists.search_indexes.reference_index import ReferenceIndex
 
 
 class ArchesControlledListsTestRunner(ArchesTestRunner):
     def setup_databases(self, **kwargs):
         ret = super().setup_databases(**kwargs)
-
-        for index in settings.ELASTICSEARCH_CUSTOM_INDEXES:
-            get_index(index["name"]).prepare_index()
-
+        ReferenceIndex(index_name=settings.REFERENCES_INDEX_NAME).prepare_index()
         return ret
 
     def teardown_databases(self, old_config, **kwargs):
+        ReferenceIndex(index_name=settings.REFERENCES_INDEX_NAME).delete_index()
         super().teardown_databases(old_config, **kwargs)
-
-        for index in settings.ELASTICSEARCH_CUSTOM_INDEXES:
-            get_index(index["name"]).delete_index()
