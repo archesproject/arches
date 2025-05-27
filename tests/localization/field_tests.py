71e43bc6c4862c2a2ee30fca78a5497e5c72233c
Jacob Walls <jwalls@fargeo.com>
2025-05-27 17:45:35 -0400

    Adjust test re #12060

--- tests/localization/field_tests.py
+++ tests/localization/field_tests.py
@@ -283,27 +283,6 @@ class Customi18nJSONFieldTests(ArchesTestCase):
         j = I18n_JSON(test_json)
         self.assertEqual(str(j), expected_output)
 
-    def test_i18n_json_percent_encoding(self):
-        test_json = json.dumps(
-            {
-                "i18n_properties": ["placeholder"],
-                "placeholder": {"en": "choose one", "es": "elija uno"},
-                "width": "100%",
-                "min_length": 19,
-            }
-        )
-        expected_output = json.dumps(
-            {
-                "i18n_properties": ["placeholder"],
-                "placeholder": "choose one",
-                "width": "100%",
-                "min_length": 19,
-            }
-        )
-        translation.activate("en")
-        j = I18n_JSON(test_json)
-        self.assertEqual(str(j), expected_output)
-
     def test_i18n_json_field(self):
         test_json = {
             "i18n_properties": ["placeholder"],
@@ -500,3 +479,14 @@ class AsSqlTests(ArchesTestCase):
         # Apostrophe in "it's" is doubly-escaped so it doesn't close the string
         expected = "jsonb_set('{}'::jsonb, array['en'], '\"it''s a bird\"')"
         self.assertEqual(datatype.i18n_as_sql(domain_value, None, None), expected)
+
+    def test_percent_sign_encoding(self):
+        widget_config = I18n_JSON(
+            {
+                "i18n_properties": ["placeholder"],
+                "placeholder": "Enter text",
+                "width": "100%",
+            }
+        )
+        _, params = widget_config.as_sql(None, None)
+        self.assertEqual(json.loads(params[0])["width"], "100%")
