71b84555e0a1d240b3812d4691c0698277a031d7
Rob Gaston <rgaston@fargeo.com>
2025-05-30 11:37:58 -0700

    fixes concept search module path

--- settings.py
+++ settings.py
@@ -100,7 +100,7 @@ TERM_SEARCH_TYPES = [
         "type": "concept",
         "label": _("Concepts"),
         "key": "concepts",
-        "module": "arches.app.search.search_term.ConceptSearch",
+        "module": "arches.app.search.concept_search.ConceptSearch",
     },
     {
         "type": "reference",
