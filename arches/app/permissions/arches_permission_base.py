6a8d7f75f9e40fc3c3d572b10bc9859d0ec81155
CWDamm-Kint <chris.damm@k-int.co.uk>
2025-06-02 10:01:55 +0100

    explicitly extract dictionary keys for permitted_nodegroups

--- arches/app/permissions/arches_permission_base.py
+++ arches/app/permissions/arches_permission_base.py
@@ -189,11 +189,9 @@ class ArchesPermissionBase(PermissionFramework, metaclass=ABCMeta):
 
         permitted_nodegroups = get_nodegroups_by_perm_for_user_or_group(
             user, perms, any_perm=any_perm
-        )
+        ).keys()
 
-        permitted_nodegroup_ids = list(
-            set(nodegroup.pk for nodegroup in permitted_nodegroups)
-        )
+        permitted_nodegroup_ids = [nodegroup.pk for nodegroup in permitted_nodegroups]
 
         published_permitted_nodegroups = list(
             set(
