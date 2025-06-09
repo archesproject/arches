d897d737f2acfe857f1c0288077202ad9602e3f0
Jacob Walls <jwalls@fargeo.com>
2025-06-09 14:45:00 -0400

    Fix DateField handling of None

--- rest_framework/interchange_fields.py
+++ rest_framework/interchange_fields.py
@@ -15,7 +15,7 @@ class InterchangeValueMixin:
     def to_internal_value(self, data):
         try:
             interchange_value = data["interchange_value"]
-        except:
+        except (TypeError, KeyError):
             interchange_value = data
         return super().to_internal_value(interchange_value)
 
@@ -32,7 +32,20 @@ class CharField(InterchangeValueMixin, fields.CharField): ...
 class UUIDField(InterchangeValueMixin, fields.UUIDField): ...
 
 
-class DateField(InterchangeValueMixin, fields.DateField): ...
+class DateField(InterchangeValueMixin, fields.DateField):
+    def to_internal_value(self, data):
+        """
+        DRF's DateField doesn't handle None despite a few
+        close-but-not-cigar-enough bug reports like:
+        https://github.com/encode/django-rest-framework/issues/4835
+        """
+        try:
+            interchange_value = data["interchange_value"]
+        except (TypeError, KeyError):
+            interchange_value = data
+        if interchange_value is None:
+            return None
+        return super().to_internal_value(interchange_value)
 
 
 class JSONField(InterchangeValueMixin, fields.JSONField): ...
