e16f5bc35c0ebe1071cad18efae5c987a5170d32
Cyrus Hiatt <chiatt@fargeo.com>
2025-06-10 13:45:45 -0700

    Increment beta version to b1

--- arches/__init__.py
+++ arches/__init__.py
@@ -11,6 +11,6 @@ except Exception as e:
     pass
 
 # VERSION[3] options = "alpha", "beta", "rc", or "final"
-VERSION = (8, 0, 0, "alpha", 4)
+VERSION = (8, 0, 0, "beta", 1)
 
 __version__ = get_version(VERSION)
