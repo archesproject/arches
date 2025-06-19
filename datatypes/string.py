34ad7c8be2061ca409d5d0f7810704c58762d146
Jacob Walls <jwalls@fargeo.com>
2025-06-19 13:48:27 -0400

    Remove resolve() method

--- datatypes/string.py
+++ datatypes/string.py
@@ -1,7 +1,4 @@
-from django.utils.translation import gettext as _
-
 from arches.app.datatypes import datatypes
-from arches.app.utils.i18n import rank_label
 
 
 class StringDataType(datatypes.StringDataType):
@@ -12,18 +9,6 @@ class StringDataType(datatypes.StringDataType):
         """
         data = self.get_tile_data(tile)
         tile.data[node_id_str] = (data.get(node_id_str) or {}) | transformed
-
-    def resolve(self, value: dict):
-        """Resolve localized values to a single one."""
-        lang_val_pairs = [(lang, obj["value"]) for lang, obj in value.items()]
-        if not lang_val_pairs:
-            return
-        ranked = sorted(
-            lang_val_pairs,
-            key=lambda pair: rank_label(source_lang=pair[0]),
-            reverse=True,
-        )
-        return ranked[0][1]
 
     def get_interchange_value(self, value, **kwargs):
         if not value or not isinstance(value, dict):
