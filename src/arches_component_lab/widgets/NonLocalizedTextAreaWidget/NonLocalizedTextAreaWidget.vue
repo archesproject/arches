a621522411161d57a3b7b3ef7895d738b58b13a8
Aaron Gundel <aaron.gundel@gmail.com>
2025-06-26 09:41:50 -0600

    Adds new textarea widget

---
+++ src/arches_component_lab/widgets/NonLocalizedTextAreaWidget/NonLocalizedTextAreaWidget.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+import Message from "primevue/message";
+import ProgressSpinner from "primevue/progressspinner";
+
+import NonLocalizedTextAreaWidgetEditor from "@/arches_component_lab/widgets/NonLocalizedTextAreaWidget/components/NonLocalizedTextAreaWidgetEditor.vue";
+import NonLocalizedTextAreaWidgetViewer from "@/arches_component_lab/widgets/NonLocalizedTextAreaWidget/components/NonLocalizedTextAreaWidgetViewer.vue";
+
+import { EDIT, VIEW } from "@/arches_component_lab/widgets/constants.ts";
+import {
+    fetchWidgetData,
+    fetchNodeData,
+} from "@/arches_component_lab/widgets/api.ts";
+
+import type { WidgetMode } from "@/arches_component_lab/widgets/types.ts";
+
+const props = withDefaults(
+    defineProps<{
+        mode: WidgetMode;
+        initialValue: string | null | undefined;
+        nodeAlias: string;
+        graphSlug: string;
+        showLabel?: boolean;
+    }>(),
+    {
+        showLabel: true,
+    },
+);
+
+const isLoading = ref(true);
+const nodeData = ref();
+const widgetData = ref();
+const configurationError = ref();
+
+onMounted(async () => {
+    try {
+        widgetData.value = await fetchWidgetData(
+            props.graphSlug,
+            props.nodeAlias,
+        );
+        nodeData.value = await fetchNodeData(props.graphSlug, props.nodeAlias);
+    } catch (error) {
+        configurationError.value = error;
+    } finally {
+        isLoading.value = false;
+    }
+});
+</script>
+
+<template>
+    <ProgressSpinner
+        v-if="isLoading"
+        style="width: 2em; height: 2em"
+    />
+    <template v-else>
+        <label v-if="props.showLabel">
+            <span>{{ widgetData.label }}</span>
+            <span v-if="nodeData.isrequired && props.mode === EDIT">*</span>
+        </label>
+
+        <div :class="[nodeAlias, graphSlug].join(' ')">
+            <NonLocalizedTextAreaWidgetEditor
+                v-if="mode === EDIT"
+                :initial-value="initialValue"
+                :graph-slug="props.graphSlug"
+                :node-alias="props.nodeAlias"
+            />
+            <NonLocalizedTextAreaWidgetViewer
+                v-else-if="mode === VIEW"
+                :value="props.initialValue"
+            />
+        </div>
+    </template>
+    <Message
+        v-if="configurationError"
+        severity="error"
+        size="small"
+    >
+        {{ configurationError.message }}
+    </Message>
+</template>
---
+++ src/arches_component_lab/widgets/NonLocalizedTextAreaWidget/components/NonLocalizedTextAreaWidgetViewer.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const props = defineProps<{
+    value: string | null | undefined;
+}>();
+</script>
+
+<template>
+    <div>{{ props.value }}</div>
+</template>
