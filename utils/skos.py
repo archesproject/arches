87e7de081051a5b699f331e6c1e3485728456fff
Johnathan Clementi <johnathan.clementi1@gmail.com>
2025-07-01 18:19:09 -0400

    Extract list items and their values #116

--- utils/skos.py
+++ utils/skos.py
@@ -39,25 +39,29 @@ class SKOSReader(SKOSReader):
         # overwrite_options="overwrite",
     ):
         baseuuid = uuid.uuid4()
-        allowed_languages = models.Language.objects.values_list("code", flat=True)
+        allowed_languages = {}
+        for lang in models.Language.objects.all():
+            allowed_languages[lang.code] = lang
         default_lang = settings.LANGUAGE_CODE
+        list_lookup = {}
 
         # if the graph is of the type rdflib.graph.Graph
         if isinstance(graph, Graph):
 
+            ### Lists ###
             # Search for ConceptSchemes first - these will become Lists
             for scheme, v, o in graph.triples((None, RDF.type, SKOS.ConceptScheme)):
                 scheme_id = self.generate_uuidv5_from_subject(baseuuid, scheme)
                 new_list = List(scheme_id)
+                list_lookup[scheme] = new_list
 
                 for predicate, object in graph.predicate_objects(subject=scheme):
                     # Get List name from a ConceptScheme's title element
                     if predicate == DCTERMS.title:
 
                         if not self.language_exists(object, allowed_languages):
-                            allowed_languages = models.Language.objects.values_list(
-                                "code", flat=True
-                            )
+                            for lang in models.Language.objects.all():
+                                allowed_languages[lang.code] = lang
 
                         val = self.unwrapJsonLiteral(object)
                         new_list.name = val["value"]
@@ -71,40 +75,58 @@ class SKOSReader(SKOSReader):
             skos_value_types = value_types.filter(
                 Q(namespace="skos") | Q(namespace="arches")
             )
-            skos_value_types_list = list(
-                skos_value_types.values_list("valuetype", flat=True)
+            skos_note_and_label_types = skos_value_types.filter(
+                Q(category="note") | Q(category="label")
             )
             skos_value_types = {
                 valuetype.valuetype: valuetype for valuetype in skos_value_types
             }
-            dcterms_value_types = value_types.filter(namespace="dcterms")
-            dcterms_identifier_type = dcterms_value_types.get(
-                valuetype=str(DCTERMS.identifier).replace(str(DCTERMS), "")
-            )
+            skos_note_and_label_types = {
+                valuetype.valuetype: valuetype
+                for valuetype in skos_note_and_label_types
+            }
 
+            ### List items & values ###
             # Concepts become ListItems & ListItemValues
             for concept, v, o in graph.triples((None, RDF.type, SKOS.Concept)):
                 list_item_id = self.generate_uuidv5_from_subject(baseuuid, concept)
                 list_item = ListItem(id=list_item_id)
 
                 # rdf:about is fallback URI for a concept, unless it has dcterms:identifier
-                # which overwrites this value below
                 uri = self.unwrapJsonLiteral(str(concept))
 
                 for predicate, object in graph.predicate_objects(subject=concept):
                     if predicate == DCTERMS.identifier:
-                        uri = self.unwrapJsonLiteral(str(object))
+                        uri = self.unwrapJsonLiteral(str(object))["value"]
 
-                    elif str(SKOS) in predicate or str(ARCHES) in predicate:
+                    elif predicate == SKOS.inScheme:
+                        list_item.list = list_lookup.get(object, None)
+
+                    # Capture remaining SKOS properties as list item values
+                    elif any(
+                        type in predicate for type in skos_note_and_label_types.keys()
+                    ):
                         if not self.language_exists(object, allowed_languages):
-                            allowed_languages = models.Language.objects.values_list(
-                                "code", flat=True
-                            )
+                            for lang in models.Language.objects.all():
+                                allowed_languages[lang.code] = lang
 
-                        # Get skos element type from predicate (e.g. prefLabel, broader, etc.)
+                        object_language = (
+                            allowed_languages[object.language]
+                            or allowed_languages[default_lang]
+                        )
                         relation_or_value_type = predicate.replace(SKOS, "").replace(
                             ARCHES, ""
                         )
+                        list_item_value = ListItemValue(
+                            list_item=list_item,
+                            valuetype=skos_value_types.get(relation_or_value_type),
+                            language=object_language,
+                            value=self.unwrapJsonLiteral(str(object))["value"],
+                        )
+                        self.list_item_values.append(list_item_value)
+
+                    elif predicate in [SKOS.broader, SKOS.narrower]:
+                        pass
 
                 list_item.uri = uri
 
