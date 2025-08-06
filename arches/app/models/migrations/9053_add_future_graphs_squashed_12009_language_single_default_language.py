f103b45d459ef9e2ab020e6c4905583281bd4e3d
Jacob Walls <jwalls@fargeo.com>
2025-08-06 11:28:11 -0400

    Remove some reverse functions

--- arches/app/models/migrations/9053_add_future_graphs_squashed_12009_language_single_default_language.py
+++ arches/app/models/migrations/9053_add_future_graphs_squashed_12009_language_single_default_language.py
@@ -341,15 +341,6 @@ class Migration(migrations.Migration):
             name=_("Perpetual"),
         )
 
-    def remove_perpetual_resource_instance_lifecycle(apps, schema_editor):
-        ResourceInstanceLifecycle = apps.get_model(
-            "models", "ResourceInstanceLifecycle"
-        )
-        resource_instance_lifecycle = ResourceInstanceLifecycle.objects.get(
-            id="e9a8a2b0-63b5-47a3-bbc4-9c09c0e759b1"
-        )
-        resource_instance_lifecycle.delete()
-
     def create_perpetual_resource_instance_lifecycle_states(apps, schema_editor):
         ResourceInstanceLifecycleState = apps.get_model(
             "models", "ResourceInstanceLifecycleState"
@@ -367,17 +358,6 @@ class Migration(migrations.Migration):
             can_edit_resource_instances=True,
         )
 
-    def remove_perpetual_resource_instance_lifecycle_states(apps, schema_editor):
-        ResourceInstanceLifecycleState = apps.get_model(
-            "models", "ResourceInstanceLifecycleState"
-        )
-
-        perpetual_state = ResourceInstanceLifecycleState.objects.get(
-            id="4e2a6b8e-2489-4377-9c9f-29cfbd3e76c8"
-        )
-
-        perpetual_state.delete()
-
     def create_default_resource_instance_lifecycle(apps, schema_editor):
         ResourceInstanceLifecycle = apps.get_model(
             "models", "ResourceInstanceLifecycle"
@@ -386,15 +366,6 @@ class Migration(migrations.Migration):
             id=uuid.UUID("7e3cce56-fbfb-4a4b-8e83-59b9f9e7cb75"),
             name=_("Standard"),
         )
-
-    def remove_default_resource_instance_lifecycle(apps, schema_editor):
-        ResourceInstanceLifecycle = apps.get_model(
-            "models", "ResourceInstanceLifecycle"
-        )
-        resource_instance_lifecycle = ResourceInstanceLifecycle.objects.get(
-            id="7e3cce56-fbfb-4a4b-8e83-59b9f9e7cb75"
-        )
-        resource_instance_lifecycle.delete()
 
     def create_default_resource_instance_lifecycle_states(apps, schema_editor):
         ResourceInstanceLifecycleState = apps.get_model(
@@ -440,25 +411,6 @@ class Migration(migrations.Migration):
         active_state.previous_resource_instance_lifecycle_states.add(draft_state)
         retired_state.previous_resource_instance_lifecycle_states.add(active_state)
 
-    def remove_default_resource_instance_lifecycle_states(apps, schema_editor):
-        ResourceInstanceLifecycleState = apps.get_model(
-            "models", "ResourceInstanceLifecycleState"
-        )
-
-        draft_state = ResourceInstanceLifecycleState.objects.get(
-            id="9375c9a7-dad2-4f14-a5c1-d7e329fdde4f"
-        )
-        active_state = ResourceInstanceLifecycleState.objects.get(
-            id="f75bb034-36e3-4ab4-8167-f520cf0b4c58"
-        )
-        retired_state = ResourceInstanceLifecycleState.objects.get(
-            id="d95d9c0e-0e2c-4450-93a3-d788b91abcc8"
-        )
-
-        draft_state.delete()
-        active_state.delete()
-        retired_state.delete()
-
     def add_default_resource_instance_lifecycles_to_graphs(apps, schema_editor):
         GraphModel = apps.get_model("models", "GraphModel")
         ResourceInstanceLifecycle = apps.get_model(
@@ -479,13 +431,6 @@ class Migration(migrations.Migration):
                     default_resource_instance_lifecycle
                 )
                 graph_model.save()
-
-    def remove_default_resource_instance_lifecycles_from_graphs(apps, schema_editor):
-        GraphModel = apps.get_model("models", "GraphModel")
-
-        for graph_model in GraphModel.objects.all():
-            graph_model.resource_instance_lifecycle = None
-            graph_model.save()
 
     def create_node_aliases(apps, schema_editor):
         Node = apps.get_model("models", "Node")
@@ -823,19 +768,19 @@ class Migration(migrations.Migration):
         ),
         migrations.RunPython(
             code=create_default_resource_instance_lifecycle,
-            reverse_code=remove_default_resource_instance_lifecycle,
+            reverse_code=django.db.migrations.operations.special.RunPython.noop,
         ),
         migrations.RunPython(
             code=create_default_resource_instance_lifecycle_states,
-            reverse_code=remove_default_resource_instance_lifecycle_states,
+            reverse_code=django.db.migrations.operations.special.RunPython.noop,
         ),
         migrations.RunPython(
             code=create_perpetual_resource_instance_lifecycle,
-            reverse_code=remove_perpetual_resource_instance_lifecycle,
+            reverse_code=django.db.migrations.operations.special.RunPython.noop,
         ),
         migrations.RunPython(
             code=create_perpetual_resource_instance_lifecycle_states,
-            reverse_code=remove_perpetual_resource_instance_lifecycle_states,
+            reverse_code=django.db.migrations.operations.special.RunPython.noop,
         ),
         migrations.AddField(
             model_name="graphmodel",
@@ -849,7 +794,7 @@ class Migration(migrations.Migration):
         ),
         migrations.RunPython(
             code=add_default_resource_instance_lifecycles_to_graphs,
-            reverse_code=remove_default_resource_instance_lifecycles_from_graphs,
+            reverse_code=django.db.migrations.operations.special.RunPython.noop,
         ),
         migrations.AddConstraint(
             model_name="graphmodel",
