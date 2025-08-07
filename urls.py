633dfb42a449fed16ccd9e073c7a6a9bb00f0145
Cyrus Hiatt <chiatt@fargeo.com>
2025-08-07 11:32:56 -0700

    Namespace routes by app name

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from arches_component_lab.apps import ArchesComponentLabConfig
 from arches_component_lab.views.api.relatable_resources import RelatableResourcesView
 from arches_component_lab.views.api.card_x_node_x_widget import (
     CardXNodeXWidgetView,
@@ -12,34 +13,36 @@ from arches_querysets.rest_framework.generic_views import (
     ArchesTileListCreateView,
 )
 
+app_name = ArchesComponentLabConfig.name
+
 urlpatterns = [
     path(
-        "arches-component-lab/api/relatable-resources/<slug:graph>/<slug:node_alias>",
+        "api/relatable-resources/<slug:graph>/<slug:node_alias>",
         RelatableResourcesView.as_view(),
         name="api-relatable-resources",
     ),
     path(
-        "arches-component-lab/api/card-x-node-x-widget-data/<slug:graph_slug>/<slug:node_alias>",
+        "api/card-x-node-x-widget-data/<slug:graph_slug>/<slug:node_alias>",
         CardXNodeXWidgetView.as_view(),
         name="api-card-x-node-x-widget",
     ),
     path(
-        "arches-component-lab/api/card-x-node-x-widget-list-from-nodegroup/<slug:graph_slug>/<slug:nodegroup_alias>",
+        "api/card-x-node-x-widget-list-from-nodegroup/<slug:graph_slug>/<slug:nodegroup_alias>",
         CardXNodeXWidgetListFromNodegroupView.as_view(),
         name="api-card-x-node-x-widget-list-from-nodegroup",
     ),
     path(
-        "arches-component-lab/api/tile/<slug:graph>/<slug:nodegroup_alias>/blank",
+        "api/tile/<slug:graph>/<slug:nodegroup_alias>/blank",
         ArchesTileBlankView.as_view(),
         name="api-tile-blank",
     ),
     path(
-        "arches-component-lab/api/tile/<slug:graph>/<slug:nodegroup_alias>/<uuid:pk>",
+        "api/tile/<slug:graph>/<slug:nodegroup_alias>/<uuid:pk>",
         ArchesTileDetailView.as_view(),
         name="api-tile",
     ),
     path(
-        "arches-component-lab/api/tile-list-create/<slug:graph>/<slug:nodegroup_alias>/<uuid:pk>",
+        "api/tile-list-create/<slug:graph>/<slug:nodegroup_alias>/<uuid:pk>",
         ArchesTileListCreateView.as_view(),
         name="api-tile-list-create",
     ),
