6d11b53a6f3f9aaaa2c1f097bc301f5cb1fedf2a
Aaron Gundel <aaron.gundel@gmail.com>
2025-08-07 13:26:23 -0600

    fixes issues with file importing

--- arches/app/models/sql_config.py
+++ arches/app/models/sql_config.py
@@ -1,5 +1,5 @@
+from django.db import migrations
 from django_migrate_sql.config import SQLItem
-
 from arches.app.models.utils import format_file_into_sql
 
 
@@ -9,5 +9,12 @@ sql_items = [
         format_file_into_sql("__arches_instance_view_update.sql", "sql/functions"),
         reverse_sql="drop function __arches_instance_view_update;",
         replace=True,
+    ),
+    SQLItem(
+        "__arches_load_staging_get_tile_errors",
+        format_file_into_sql(
+            "__arches_load_staging_get_tile_errors.sql", "sql/functions"
+        ),
+        reverse_sql="drop function __arches_load_staging_get_tile_errors;",
     ),
 ]
