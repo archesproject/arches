443376e8ea6c75004b26be58670c423c175f94f4
Cyrus Hiatt <chiatt@fargeo.com>
2025-08-11 20:09:08 -0700

    Add error message if file does not parse

--- arches/app/etl_modules/base_import_module.py
+++ arches/app/etl_modules/base_import_module.py
@@ -17,7 +17,7 @@ from django.db import connection
 from arches.app.datatypes.datatypes import DataTypeFactory
 from arches.app.etl_modules.decorators import load_data_async
 from arches.app.etl_modules.save import save_to_tiles
-from arches.app.models.models import ETLModule, Node
+from arches.app.models.models import ETLModule, Node, LoadEvent
 from arches.app.models.system_settings import settings
 from arches.app.utils.decorators import user_created_transaction_match
 from arches.app.utils.file_validator import FileValidator
@@ -209,7 +209,17 @@ class BaseImportModule:
     ):
         try:
             with connection.cursor() as cursor:
-                self.stage_files(files, summary, cursor)
+                try:
+                    self.stage_files(files, summary, cursor)
+                except Exception as e:
+                    load_event = LoadEvent.objects.get(loadid=loadid)
+                    load_event.status = "failed"
+                    load_event.successful = False
+                    load_event.complete = True
+                    load_event.load_end_time = datetime.now()
+                    load_event.error_message = _("Unable to parse file. If including extra .xlsx files in a zip file, be sure they are in an 'uploadedfiles' directory")
+                    load_event.save()
+                    raise FileValidationError
                 self.check_tile_cardinality(cursor)
                 result["validation"] = self.validate(loadid)
                 if len(result["validation"]["data"]) == 0:
