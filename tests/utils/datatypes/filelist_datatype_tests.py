3c2168a26cf48901ef4e24162f0d4b2337d81046
njkim <nkim@fargeo.com>
2025-08-13 14:37:56 -0700

    Fix formatting, #95

--- tests/utils/datatypes/filelist_datatype_tests.py
+++ tests/utils/datatypes/filelist_datatype_tests.py
@@ -11,19 +11,22 @@ class FileListDataTypeTests(ArchesTestCase):
     def test_tile_transform(self):
         value1 = "testfile1.png,testfile2.png"
 
-        value2 = [{
-            "name": "testfile3.png",
-            "altText": "Test File 3",
-            "attribution": "archesproject",
-            "description": "A File for Testing",
-            "title": "Test File 3",
-        },{
-            "name": "testfile4.png",
-            "altText": "Test File 4",
-            "attribution": "archesproject",
-            "description": "A File for Testing",
-            "title": "Test File 4",
-        }]
+        value2 = [
+            {
+                "name": "testfile3.png",
+                "altText": "Test File 3",
+                "attribution": "archesproject",
+                "description": "A File for Testing",
+                "title": "Test File 3",
+            },
+            {
+                "name": "testfile4.png",
+                "altText": "Test File 4",
+                "attribution": "archesproject",
+                "description": "A File for Testing",
+                "title": "Test File 4",
+            },
+        ]
 
         datatype = DataTypeFactory().get_instance("file-list")
         language = get_language()
@@ -37,14 +40,22 @@ class FileListDataTypeTests(ArchesTestCase):
             tile_value = datatype.transform_value_for_tile(value2[0])
             self.assertEqual(tile_value[0]["name"], "testfile3.png")
             self.assertEqual(tile_value[0]["altText"][language]["value"], "Test File 3")
-            self.assertEqual(tile_value[0]["attribution"][language]["value"], "archesproject")
-            self.assertEqual(tile_value[0]["description"][language]["value"], "A File for Testing")
+            self.assertEqual(
+                tile_value[0]["attribution"][language]["value"], "archesproject"
+            )
+            self.assertEqual(
+                tile_value[0]["description"][language]["value"], "A File for Testing"
+            )
             self.assertEqual(tile_value[0]["title"][language]["value"], "Test File 3")
 
         with self.subTest("A list of dictionaries input"):
             tile_value = datatype.transform_value_for_tile(value2)
             self.assertEqual(tile_value[1]["name"], "testfile4.png")
             self.assertEqual(tile_value[1]["altText"][language]["value"], "Test File 4")
-            self.assertEqual(tile_value[1]["attribution"][language]["value"], "archesproject")
-            self.assertEqual(tile_value[1]["description"][language]["value"], "A File for Testing")
+            self.assertEqual(
+                tile_value[1]["attribution"][language]["value"], "archesproject"
+            )
+            self.assertEqual(
+                tile_value[1]["description"][language]["value"], "A File for Testing"
+            )
             self.assertEqual(tile_value[1]["title"][language]["value"], "Test File 4")
