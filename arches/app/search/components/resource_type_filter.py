f491656bbf375d08c2ae0f9cc94f09a52a6deb69
CWDamm-Kint <chris.damm@k-int.co.uk>
2025-08-14 10:58:01 +0100

    Revert "revert formatting changes"
    This reverts commit d779cd110e617028e565bb573b63bd11eecc8a12.

--- arches/app/search/components/resource_type_filter.py
+++ arches/app/search/components/resource_type_filter.py
@@ -19,8 +19,10 @@ details = {
 
 def get_permitted_graphids(permitted_nodegroups):
     permitted_graphids = set()
+
     for node in Node.objects.filter(nodegroup__in=permitted_nodegroups):
         permitted_graphids.add(str(node.graph_id))
+
     return permitted_graphids
 
 
