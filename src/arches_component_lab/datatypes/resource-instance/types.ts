613086c0af867625f83b274ab5d103b72736a575
Christopher Byrd <chrabyrd@gmail.com>
2025-08-15 13:20:02 -0700

    nit

--- src/arches_component_lab/datatypes/resource-instance/types.ts
+++ src/arches_component_lab/datatypes/resource-instance/types.ts
@@ -2,19 +2,12 @@ import type { AliasedNodeData } from "@/arches_component_lab/types.ts";
 
 export interface ResourceInstanceValue extends AliasedNodeData {
     display_value: string;
-    node_value: {
-        inverseOntologyProperty: string;
-        ontologyProperty: string;
-        resourceId: string;
-        resourceXresourceId: string;
-    } | null;
+    node_value: ResourceInstanceReference | null;
     details: { display_value: string; resource_id: string }[];
 }
 
 export interface ResourceInstanceReference {
     resource_id: string;
-    display_value: string;
-    interchange_value?: string;
     ontologyProperty?: string;
     resourceXresourceId?: string;
     inverseOntologyProperty?: string;
