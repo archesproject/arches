fe483abd2d3d8be5888f38a2e36c64fd60aece6a
njkim <nkim@fargeo.com>
2025-08-15 23:56:21 -0700

    Update comments, #10426

--- arches/app/datatypes/datatypes.py
+++ arches/app/datatypes/datatypes.py
@@ -1374,8 +1374,8 @@ class FileListDataType(BaseDataType):
 
     def transform_value_for_tile(self, value, **kwargs):
         """
-        Accepts a comma delimited string of file paths as 'value'
-        or a dictionary / a list of dictionaries as following
+        The 'value' argument can be a comma delimited string of file paths,
+        a dictionary, or a list of dictionaries with the following properties:
         {
             "name": "",
             "altText": "",
@@ -1462,7 +1462,7 @@ class FileListDataType(BaseDataType):
             if len(compatible_renderers) == 1:
                 tile_file["renderer"] = compatible_renderers[0]
 
-            # if files includes metadata, add metadata to the tile_file
+            # if files include metadata, add metadata to the tile_file
             localized_metadata_keys = {"altText", "attribution", "description", "title"}
             languages = models.Language.objects.all()
             language = get_language()
