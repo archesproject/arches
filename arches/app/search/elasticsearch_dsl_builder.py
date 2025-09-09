a01290390dceda0de2d77558e028617b5ffdd11e
Galen <whatisgalen@gmail.com>
2025-09-09 15:53:39 -0700

    put minimum_should_match logic in try except for Query.count()

--- arches/app/search/elasticsearch_dsl_builder.py
+++ arches/app/search/elasticsearch_dsl_builder.py
@@ -102,11 +102,14 @@ class Query(Dsl):
             return self.se.search(index=index, scroll=self.scroll, **self.dsl)
 
     def count(self, index="", **kwargs):
-        if (
-            not "minimum_should_match" in self.dsl["query"]["bool"]
-            and len(self.dsl["query"]["bool"]["should"]) > 0
-        ):
-            self.dsl["query"]["bool"]["minimum_should_match"] = 1
+        try:
+            if (
+                not "minimum_should_match" in self.dsl["query"]["bool"]
+                and len(self.dsl["query"]["bool"]["should"]) > 0
+            ):
+                self.dsl["query"]["bool"]["minimum_should_match"] = 1
+        except (KeyError, TypeError):
+            pass
         return self.se.count(index=index, **self.dsl)
 
     def delete(self, index="", **kwargs):
