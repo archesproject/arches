e09a3d3d97c86cb1b443c5dba86e11f84dac801c
njkim <nkim@fargeo.com>
2025-09-12 16:30:06 -0700

    Handle the ontology vs valueid, #12440

--- arches/app/utils/report_utils.py
+++ arches/app/utils/report_utils.py
@@ -1,40 +1,48 @@
+import logging
+
+from django.core.exceptions import ValidationError
 from django.db.models import Prefetch
+
 from arches.app.models.models import Value
 from arches.app.utils.i18n import rank_label, get_language
 
+logger = logging.getLogger(__name__)
 
 def get_resource_relationship_type_label(relationship_types, lang=None):
+    preflabel_lookup = {}
     if lang is None:
         lang = get_language()
-
-    relationship_type_values = (
-        Value.objects.filter(
-            valueid__in=relationship_types,
-        )
-        .select_related("concept")
-        .prefetch_related(
-            Prefetch(
-                "concept__value_set",
-                queryset=Value.objects.order_by("pk"),
-            ),
-        )
-    )
-
-    preflabel_lookup = {
-        str(rel_type.pk): (
-            sorted(
-                rel_type.concept.value_set.all(),
-                key=lambda label: rank_label(
-                    kind=label.valuetype_id,
-                    source_lang=label.language_id,
-                    target_lang=lang,
+    try:
+        relationship_type_values = (
+            Value.objects.filter(
+                valueid__in=relationship_types,
+            )
+            .select_related("concept")
+            .prefetch_related(
+                Prefetch(
+                    "concept__value_set",
+                    queryset=Value.objects.order_by("pk"),
                 ),
-                reverse=True,
-            )[0].value
-            if rel_type.concept.value_set.all()
-            else ""
+            )
         )
-        for rel_type in relationship_type_values
-    }
+        preflabel_lookup = {
+            str(rel_type.pk): (
+                sorted(
+                    rel_type.concept.value_set.all(),
+                    key=lambda label: rank_label(
+                        kind=label.valuetype_id,
+                        source_lang=label.language_id,
+                        target_lang=lang,
+                    ),
+                    reverse=True,
+                )[0].value
+                if rel_type.concept.value_set.all()
+                else ""
+            )
+            for rel_type in relationship_type_values
+        }
+    except ValidationError:
+        # If relationship_type is not a uuids, ie. ontology property
+        pass
 
     return preflabel_lookup
