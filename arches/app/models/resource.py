0fc8667d235d341295d1e6e0970dfa5e6c543e18
njkim <nkim@fargeo.com>
2025-09-12 22:57:38 -0700

    nit, #12440

--- arches/app/models/resource.py
+++ arches/app/models/resource.py
@@ -907,7 +907,9 @@ class Resource(models.ResourceInstance):
             if relation["relationshiptype"]
         }
 
-        preflabel_lookup = get_resource_relationship_type_label(relationship_types, lang)
+        preflabel_lookup = get_resource_relationship_type_label(
+            relationship_types, lang
+        )
 
         for relation in permitted_relation_dicts:
             relation["relationshiptype_label"] = preflabel_lookup.get(
