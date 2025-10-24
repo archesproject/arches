51ac92e43338c45ed54009f08229fb2e24cf3684
Aaron Gundel <124614+aarongundel@users.noreply.github.com>
2025-10-24 10:29:58 -0600

    undo changes that affect explicit permissions

--- arches/app/permissions/arches_permission_base.py
+++ arches/app/permissions/arches_permission_base.py
@@ -364,7 +364,7 @@ class ArchesPermissionBase(PermissionFramework, metaclass=ABCMeta):
                     if result["permitted"] == "unknown":
                         return user.groups.filter(
                             name__in=settings.RESOURCE_EDITOR_GROUPS
-                        ).exists() or self.user_can_edit_model_nodegroups(
+                        ).exists() and self.user_can_edit_model_nodegroups(
                             user, result["resource"]
                         )
                     else:
@@ -401,11 +401,11 @@ class ArchesPermissionBase(PermissionFramework, metaclass=ABCMeta):
                         protected_tiles = {tile.nodegroup_id for tile in tiles} - set(
                             nodegroups
                         )
-                        if len(protected_tiles) < 1:
+                        if len(protected_tiles) > 0:
                             return False
                         return user.groups.filter(
                             name__in=settings.RESOURCE_EDITOR_GROUPS
-                        ).exists() or self.user_can_delete_model_nodegroups(
+                        ).exists() and self.user_can_delete_model_nodegroups(
                             user, result["resource"]
                         )
                     else:
