854ff9008e51c956ba6ef7169ccb5c464c35b0d1
Brett Ferguson <brett@qedsystems.ca>
2026-01-08 15:07:55 -0800

    Use date format from node config (#232)

--- src/arches_component_lab/widgets/DatePickerWidget/components/DatePickerWidgetEditor.vue
+++ src/arches_component_lab/widgets/DatePickerWidget/components/DatePickerWidgetEditor.vue
@@ -65,7 +65,7 @@ function onUpdateModelValue(updatedValue: string) {
 
             formValue = {
                 display_value: formattedDate,
-                node_value: date.toISOString(),
+                node_value: formattedDate,
                 details: [],
             };
         } catch (_error) {
