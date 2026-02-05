429c6365ec4724a30f9898d9129e02ea16ba7538
Johnathan Clementi <johnathan.clementi1@gmail.com>
2026-02-05 13:02:00 -0500

    Fix migration #241

--- migrations/0003_add_langauge_widget.py
+++ migrations/0003_add_langauge_widget.py
@@ -5,6 +5,7 @@ import uuid
 class Migration(migrations.Migration):
 
     dependencies = [
+        ("models", "12557_add_language_datatype"),
         ("arches_component_lab", "0002_populate_widget_mappings"),
     ]
 
@@ -13,11 +14,20 @@ class Migration(migrations.Migration):
         WidgetMapping = apps.get_model("arches_component_lab", "WidgetMapping")
 
         language_widget = ArchesWidget.objects.get(name="language-widget")
-        WidgetMapping.objects.create(
-            id=uuid.uuid4(),
-            widget_id=language_widget.widgetid,
-            component="arches_component_lab/widgets/LanguageSelectWidget/LanguageSelectWidget.vue",
+        existing_mapping = WidgetMapping.objects.filter(
+            widget_id=language_widget.widgetid
         )
+        component = (
+            "arches_component_lab/widgets/LanguageSelectWidget/LanguageSelectWidget.vue"
+        )
+        if existing_mapping.exists():
+            existing_mapping.update(component=component)
+        else:
+            WidgetMapping.objects.create(
+                id=uuid.uuid4(),
+                widget_id=language_widget.widgetid,
+                component=component,
+            )
 
     def reverse(apps, schema_editor):
         ArchesWidget = apps.get_model("models", "Widget")
