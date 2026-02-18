b76a972194079fe468f69af8302ddc2c9d4ad712
Johnathan Clementi <johnathan.clementi1@gmail.com>
2026-02-18 14:56:42 -0500

    Add util for adding mapping records #241

---
+++ utils/widget_synchronizer.py
@@ -0,0 +1,56 @@
+import sys
+
+from django.utils.translation import gettext as _
+
+from arches.app.models.models import Widget
+from arches_component_lab.models import WidgetMapping
+
+
+class WidgetSynchronizer:
+    def synchronize_widgets(self, widget_name: str = None, component_name: str = None):
+        """Ensure that all widgets in the Arches database have a corresponding WidgetMapping."""
+        widgets = Widget.objects.all()
+        mappings = WidgetMapping.objects.all()
+        missing_mappings = set(widgets.values_list("widgetid", flat=True)) - set(
+            mappings.values_list("widget_id", flat=True)
+        )
+
+        if len(missing_mappings) > 1 and widget_name and component_name:
+            raise ValueError(
+                _(
+                    "Multiple widgets are missing mappings."
+                    "Please specify a widget_name and component_name to create "
+                    "a mapping for a specific widget, or accept default (widget name) "
+                    "to create mappings for all missing widgets."
+                )
+            )
+
+        for widget_pk in missing_mappings:
+            widget = Widget.objects.get(widgetid=widget_pk)
+            if not component_name:
+                # Provide a reasonable default mapping based on widget name
+                component_name = (
+                    widget.name.title()
+                    .replace(" ", "")
+                    .replace("-", "")
+                    .replace("_", "")
+                )
+
+            if widget_name:
+                WidgetMapping.objects.create(
+                    widget=widget,
+                    component=f"arches_component_lab/widgets/{component_name}/{component_name}.vue",
+                )
+                return
+
+            else:
+                WidgetMapping.objects.create(
+                    widget=widget,
+                    component=f"arches_component_lab/widgets/{component_name}/{component_name}.vue",
+                )
+            sys.stdout.write(
+                _(
+                    f"Created mapping for widget '{widget.name}' with \
+                component '{component_name}.vue'\n"
+                )
+            )
