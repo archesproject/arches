e3691145ec8ba72a7b6e226d748c97cca2b4989c
Cyrus Hiatt <chiatt@fargeo.com>
2026-02-20 17:30:52 -0800

    Handle for missing tileid

--- arches/app/utils/decorators.py
+++ arches/app/utils/decorators.py
@@ -163,7 +163,11 @@ def check_tile_permissions(func):
         if request.method == "POST":
             resourceid = request.POST.get("resourceinstanceid", None)
             tileid = request.POST.get("tileid", None) or kwargs.get("tileid")
-            if not resourceid and models.TileModel.objects.filter(pk=tileid).exists():
+            if (
+                not resourceid
+                and tileid
+                and models.TileModel.objects.filter(pk=tileid).exists()
+            ):
                 resourceid = models.TileModel.objects.get(pk=tileid).resourceinstance_id
             if not models.ResourceInstance.objects.filter(pk=resourceid).exists():
                 return func(request, *args, **kwargs)
