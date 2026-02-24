e39c97a1242ebdf0d4dd57cd7efec2a1c63157f7
Cyrus Hiatt <chiatt@fargeo.com>
2026-02-23 16:31:46 -0800

    Remove name from decorator

--- arches/app/views/tile.py
+++ arches/app/views/tile.py
@@ -93,9 +93,7 @@ class TileData(View):
             _(title), _(str(message)), {"message": message, "title": title}
         )
 
-    @method_decorator(
-        group_required("Resource Editor", raise_exception=True), name="dispatch"
-    )
+    @method_decorator(group_required("Resource Editor", raise_exception=True))
     def post(self, request):
         original_transaction_id = request.POST.get("transaction_id", None)
         transaction_id = request.POST.get("transaction_id", uuid.uuid1())
@@ -305,9 +303,7 @@ class TileData(View):
 
         return HttpResponseNotFound()
 
-    @method_decorator(
-        group_required("Resource Editor", raise_exception=True), name="dispatch"
-    )
+    @method_decorator(group_required("Resource Editor", raise_exception=True))
     def delete(self, request):
         json = request.body
         if json is not None:
