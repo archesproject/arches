8d4f1b943dab2a694a9881b5f264698b1ac81aeb
Cyrus Hiatt <chiatt@fargeo.com>
2026-02-24 06:20:57 -0800

    nit

--- arches/app/views/api.py
+++ arches/app/views/api.py
@@ -1587,8 +1587,8 @@ class Tile(APIBase):
 
     def post(self, request, tileid):
         resourceid = json.loads(request.POST.get("data"))["resourceinstance_id"]
-        # Important! The resource instance permission decorator on the TileView 
-        # will not be called by the instance of TileView below. 
+        # Important! The resource instance permission decorator on the TileView
+        # will not be called by the instance of TileView below.
         # Resource edit perms must be checked here.
         if not user_can_edit_resource(request.user, resourceid):
             return JSONResponse(
