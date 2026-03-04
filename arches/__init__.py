119e873e571e88c5764bd4083f9fda248fb10c56
Cyrus Hiatt <chiatt@fargeo.com>
2026-03-04 15:26:24 -0800

    Prevent version error in some arches applications

--- arches/__init__.py
+++ arches/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 import importlib.metadata
+from packaging.version import Version
 
 try:
     # This will make sure the app is always imported when
@@ -11,3 +12,16 @@ except Exception as e:
     pass
 
 __version__ = importlib.metadata.version(__package__)
+
+version = Version(__version__)
+major, minor, micro = version.major, version.minor, version.micro
+
+if version.pre:
+    pre_type_mapping = {"a": "alpha", "b": "beta", "rc": "rc"}
+    pre_type = pre_type_mapping.get(version.pre[0], version.pre[0])
+    pre_num = version.pre[1]
+else:
+    pre_type = "final"
+    pre_num = 0
+
+VERSION = (major, minor, micro, pre_type, pre_num)
