55f24ca427f7e98c6f34d2d0024d0273112f1992
brett <brett@qedsystems.ca>
2026-04-14 14:50:26 -0700

    Add the __arches_refresh_tile_resource_relationships function to the SQL configuration

---
+++ arches/app/models/sql/functions/__arches_refresh_tile_resource_relationships.sql
@@ -0,0 +1,52 @@
+CREATE OR REPLACE FUNCTION __arches_refresh_tile_resource_relationships(tile_id uuid)
+    RETURNS boolean AS
+$$
+DECLARE
+    resource_id uuid;
+BEGIN
+    SELECT resourceinstanceid INTO resource_id FROM tiles WHERE tileid = tile_id;
+
+    DELETE FROM resource_x_resource WHERE tileid = tile_id;
+
+    WITH relationships AS (SELECT n.nodeid,
+                                  jsonb_array_elements(t.tiledata -> n.nodeid::text) AS relationship
+                           FROM tiles t
+                                    LEFT JOIN nodes n ON t.nodegroupid = n.nodegroupid
+                           WHERE n.datatype IN ('resource-instance-list', 'resource-instance')
+                             AND t.tileid = tile_id
+                             AND t.tiledata ->> n.nodeid::text IS NOT null)
+    INSERT
+    INTO resource_x_resource (resourcexid,
+                              notes,
+                              relationshiptype,
+                              resourceinstanceidfrom,
+                              resourceinstanceidto,
+                              inverserelationshiptype,
+                              tileid,
+                              nodeid,
+                              created,
+                              modified,
+                              resourceinstancefrom_graphid,
+                              resourceinstanceto_graphid)
+    SELECT CASE relationship ->> 'resourceXresourceId'
+               WHEN '' THEN uuid_generate_v4()
+               ELSE (relationship ->> 'resourceXresourceId')::uuid
+               END,
+           '',
+           relationship ->> 'ontologyProperty',
+           resource_id,
+           (relationship ->> 'resourceId')::uuid,
+           relationship ->> 'inverseOntologyProperty',
+           tile_id,
+           nodeid,
+           now(),
+           now(),
+           resourcefrom.graphid,
+           resourceto.graphid
+    FROM relationships r
+             LEFT JOIN resource_instances resourcefrom ON resourcefrom.resourceinstanceid = resource_id
+             LEFT JOIN resource_instances resourceto
+                       ON resourceto.resourceinstanceid = (r.relationship ->> 'resourceId')::uuid;
+    RETURN true;
+END;
+$$ language plpgsql;
