a6cdfb6c623ccd52c511e8317ec3dcee0d86e309
njkim <nkim@fargeo.com>
2026-04-15 17:51:33 -0700

    Remove Z, M from resulting json from esri, #12704

--- arches/app/utils/geo_utils.py
+++ arches/app/utils/geo_utils.py
@@ -66,19 +66,46 @@ class GeoUtils(object):
         Takes a list of arcgisjson geometries and converts them to a GeoJSON feature collection. Example below:
         '{"x":-0.11515950499995142,"y":51.534958948000053,"spatialReference":{"wkid":4326,"latestWkid":4326}},
          {"x":-0.11337002699997356,"y":51.536050094000075,"spatialReference":{"wkid":4326,"latestWkid":4326}}'
+        Z and M coordinate components are stripped from the resulting GeoJSON.
         """
         payload = json.loads('{"geometries": [' + geom + "]}")
         features = []
         for geometry in payload["geometries"]:
+            geojson_geometry = arcgis2geojson(geometry)
+            if (
+                geometry.get("hasZ", False)
+                or geometry.get("hasM", False)
+                or "z" in geometry
+                or "m" in geometry
+            ):
+                self._strip_z_m(geojson_geometry)
             features.append(
                 {
                     "type": "Feature",
                     "properties": {},
-                    "geometry": arcgis2geojson(geometry),
+                    "geometry": geojson_geometry,
                 }
             )
         feature_collection = {"type": "FeatureCollection", "features": features}
         return feature_collection
+
+    def _strip_z_m(self, geometry):
+        if geometry is None:
+            return
+        geom_type = geometry.get("type")
+        if geom_type == "Point":
+            geometry["coordinates"] = geometry["coordinates"][:2]
+        elif geom_type in ("LineString", "MultiPoint"):
+            geometry["coordinates"] = [c[:2] for c in geometry["coordinates"]]
+        elif geom_type in ("Polygon", "MultiLineString"):
+            geometry["coordinates"] = [
+                [c[:2] for c in ring] for ring in geometry["coordinates"]
+            ]
+        elif geom_type == "MultiPolygon":
+            geometry["coordinates"] = [
+                [[c[:2] for c in ring] for ring in polygon]
+                for polygon in geometry["coordinates"]
+            ]
 
     def convert_geos_geom_collection_to_feature_collection(self, geometry):
         arches_geojson = {}
