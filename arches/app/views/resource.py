c394b3d5a8df8fe8ff66314f0a95087d4a4be7a3
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2026-07-17 23:53:19 +0100

    Add nodegroup and permission checks to recent edits view

--- arches/app/views/resource.py
+++ arches/app/views/resource.py
@@ -51,6 +51,9 @@ from arches.app.utils.permission_backend import (
     user_is_resource_editor,
     user_is_resource_reviewer,
     user_can_delete_resource,
+    user_can_read_resource,
+    user_can_edit_resource,
+    get_nodegroups_by_perm,
 )
 from arches.app.utils.response import JSONResponse, JSONErrorResponse
 from arches.app.utils.string_utils import str_to_bool
@@ -592,21 +595,37 @@ class ResourceEditLogView(BaseManagerView):
     def get(
         self, request, resourceid=None, view_template="views/resource/edit-log.htm"
     ):
+        permitted_nodegroupids = get_nodegroups_by_perm(
+            request.user, "models.read_nodegroup"
+        )
         transaction_id = request.GET.get("transactionid", None)
         if resourceid is None:
             if transaction_id:
                 recent_edits = models.EditLog.objects.filter(
-                    transactionid=transaction_id
+                    transactionid=transaction_id,
+                    nodegroupid__in=permitted_nodegroupids,
                 ).order_by("-timestamp")
             else:
                 recent_edits = (
-                    models.EditLog.objects.all()
-                    .exclude(resourceclassid=settings.SYSTEM_SETTINGS_RESOURCE_MODEL_ID)
+                    models.EditLog.objects.exclude(
+                        resourceclassid=settings.SYSTEM_SETTINGS_RESOURCE_MODEL_ID
+                    )
+                    .filter(nodegroupid__in=permitted_nodegroupids)
                     .order_by("-timestamp")[:100]
                 )
             edited_ids = list({edit.resourceinstanceid for edit in recent_edits})
+            permitted_ids = {
+                res_id
+                for res_id in edited_ids
+                if user_can_read_resource(request.user, res_id)
+            }
+            recent_edits = [
+                edit
+                for edit in recent_edits
+                if edit.resourceinstanceid in permitted_ids
+            ]
             resources = Resource.objects.filter(
-                resourceinstanceid__in=edited_ids
+                resourceinstanceid__in=permitted_ids
             ).select_related("graph")
             edit_type_lookup = {
                 "create": _("Resource Created"),
