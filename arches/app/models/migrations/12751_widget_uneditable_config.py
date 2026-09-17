a051ae297322ff7dd1bb538bb7765631d2c373e9
Travis Semple <travis8814@gmail.com>
2026-09-17 09:28:30 -0700

    fix up migration do it doesn't nuke uneditable on reverse, because it can already be in use previous to this migration

--- arches/app/models/migrations/12751_widget_uneditable_config.py
+++ arches/app/models/migrations/12751_widget_uneditable_config.py
@@ -20,38 +20,13 @@ def add_uneditable_to_widgets(apps, schema_editor):
             card_widget.save()
 
 
-def remove_uneditable_from_widgets(apps, schema_editor):
-    CardXNodeXWidget = apps.get_model("models", "CardXNodeXWidget")
-    Widget = apps.get_model("models", "Widget")
-
-    # these had uneditable before this migration (6839, 9191)
-    widgets_to_remove = Widget.objects.exclude(
-        name__in=[
-            "non-localized-text-widget",
-            "number-widget",
-            "text-widget",
-        ]
-    ).all()
-    for widget in widgets_to_remove:
-        config = dict(widget.defaultconfig or {})
-        config.pop("uneditable", None)
-        widget.defaultconfig = config
-        widget.save()
-
-    for card_widget in CardXNodeXWidget.objects.filter(
-        widget__in=widgets_to_remove
-    ).all():
-        config = dict(card_widget.config or {})
-        config.pop("uneditable", None)
-        card_widget.config = config
-        card_widget.save()
-
-
 class Migration(migrations.Migration):
     dependencies = [
         ("models", "12779_add_multicard_resource_descriptor"),
     ]
 
     operations = [
-        migrations.RunPython(add_uneditable_to_widgets, remove_uneditable_from_widgets),
+        # no reverse: a leftover "uneditable": false is the default value and is
+        # ignored by widgets that don't read it.
+        migrations.RunPython(add_uneditable_to_widgets, migrations.RunPython.noop),
     ]
